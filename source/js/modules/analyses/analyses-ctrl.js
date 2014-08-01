00a662854cb9a77980da2516850301593b9b1b3d
Anna Nachesa <annan@siteworkers.nl>
2014-08-01 02:09:34 +0200

    added sketch for 1b

--- source/js/modules/analyses/analyses-ctrl.js
+++ source/js/modules/analyses/analyses-ctrl.js
@@ -102,7 +102,159 @@ define([
       drawDots: true,
       columnsHGap: 5
     };
-  
+ 
+ /********************************** 1b **************************************/
 
+  $scope.data1b = [{"x":2000,"val_0":47,"val_1":55,"val_2":43,"val_3":93,"val_4":799,"val_5":28,"val_6":680,
+                    "val_7":405,"val_8":107,"val_9":1010,"val_10":1223,"val_11":49, "val_12":595,"val_13":2337},
+                  {"x":2012,"val_0":27,"val_1":30,"val_2":26,"val_3":96,"val_4":464,"val_5":17,"val_6":350,
+                    "val_7":237,"val_8":56,"val_9":543,"val_10":499,"val_11":17, "val_12":184,"val_13":806},
+                  {"x":2017,"val_0":19,"val_1":21,"val_2":18,"val_3":80,"val_4":324,"val_5":12,"val_6":241,
+                    "val_7":164,"val_8":38,"val_9":376,"val_10":327,"val_11":11, "val_12":117,"val_13":659},
+                  {"x":2025,"val_0":13,"val_1":15,"val_2":13,"val_3":74,"val_4":233,"val_5":9,"val_6":173,
+                    "val_7":118,"val_8":27,"val_9":269,"val_10":220,"val_11":8, "val_12":75,"val_13":480},
+                  {"x":2035,"val_0":10,"val_1":11,"val_2":10,"val_3":75,"val_4":176,"val_5":7,"val_6":132,
+                    "val_7":89,"val_8":20,"val_9":202,"val_10":161,"val_11":6, "val_12":54,"val_13":338}];
+
+  $scope.options1b = {
+      stacks: [{axis: "y", series: ["series_0", "series_1", "series_2", "series_3", "series_4", "series_5", "series_6",
+                  "series_7", "series_8", "series_9", "series_10", "series_11", "series_12", "series_13"]}],
+      series: [
+        {
+          y: "val_0",
+          label: "TRU",
+          color: "#4A7EB0",
+          axis: "y",
+          type: "column",
+          thickness: "5px",
+          id: "series_0"
+        },
+        {
+          y: "val_1",
+          label: "MIN",
+          color: "#EC7D31",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_1"
+        },
+        {
+          y: "val_2",
+          label: "UNI",
+          color: "#FFBF00",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_2"
+        },
+        {
+          y: "val_3",
+          label: "MSM",
+          color: "#fc6c00",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_3"
+        },
+        {
+          y: "val_4",
+          label: "MIG",
+          color: "#7e0001",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_4"
+        },
+        {
+          y: "val_5",
+          label: "PRI",
+          color: "#4A7EB0",
+          axis: "y",
+          type: "column",
+          thickness: "5px",
+          id: "series_5"
+        },
+        {
+          y: "val_6",
+          label: "FSW",
+          color: "#EC7D31",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_6"
+        },
+        {
+          y: "val_7",
+          label: "MELD",
+          color: "#FFBF00",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_7"
+        },
+        {
+          y: "val_8",
+          label: "FELD",
+          color: "#fc6c00",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_8"
+        },
+        {
+          y: "val_9",
+          label: "MAD",
+          color: "#7e0001",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_9"
+        },
+        {
+          y: "val_10",
+          label: "FAD",
+          color: "#FFBF00",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_10"
+        },
+        {
+          y: "val_11",
+          label: "MYTH",
+          color: "#fc6c00",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_11"
+        },
+        {
+          y: "val_12",
+          label: "PYTH",
+          color: "#7e0001",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_12"
+        },
+        {
+          y: "val_6",
+          label: "CHLD",
+          color: "#EC7D31",
+          type: "column",
+          axis: "y",
+          thickness: "5px",
+          id: "series_13"
+        }
+      ],
+      axes: {x: {type: "linear", key: "x"}, y: {type: "linear"}},
+      lineMode: "cardinal",
+      tension: 0.7,
+      tooltip: {mode: "scrubber"},
+      drawLegend: true,
+      drawDots: true,
+      columnsHGap: 20
+    };
+ 
   }]);
 });
