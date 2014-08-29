844f5477e3c081bcecec77d08f0c97fafd366af0
Cliff Kerr <cliffk@neurosim.downstate.edu>
2014-08-29 02:48:34 -0400

    done with objectives, about to start on constraints

--- client/source/js/modules/optimization/module.js
+++ client/source/js/modules/optimization/module.js
@@ -1,24 +1,29 @@
 define(['angular', 'ui.router'], function (angular) {
-  'use strict';
+    'use strict';
 
-  return angular.module('app.optimization', ['ui.router'])
-    .config(function ($stateProvider) {
-      $stateProvider
-        .state('optimization', {
-          url: '/optimization',
-          abstract: true,
-          template: '<div ui-view=""></div>'
-        })
-        .state('optimization.objectives', {
-          url: '/objectives',
-          templateUrl: 'js/modules/optimization/objectives.html',
-          controller: 'OptimizationObjectivesController'
-        })
-        .state('optimization.constraints', {
-          url: '/constraints',
-          templateUrl: 'js/modules/optimization/constraints.html',
-          controller: 'OptimizationConstraintsController'
+    return angular.module('app.optimization', ['ui.router'])
+        .config(function ($stateProvider) {
+            $stateProvider
+                .state('optimization', {
+                    url: '/optimization',
+                    abstract: true,
+                    template: '<div ui-view=""></div>'
+                })
+                .state('optimization.objectives', {
+                    url: '/objectives',
+                    templateUrl: 'js/modules/optimization/objectives.html',
+                    controller: 'OptimizationObjectivesController'
+                })
+                .state('optimization.constraints', {
+                    url: '/constraints',
+                    templateUrl: 'js/modules/optimization/constraints.html',
+                    controller: 'OptimizationConstraintsController'
+                })
+                .state('optimization.optimize', {
+                    url: '/optimize',
+                    templateUrl: 'js/modules/optimization/optimize.html',
+                    controller: 'OptimizationOptimizeController'
+                });
         });
-    });
 
 });
