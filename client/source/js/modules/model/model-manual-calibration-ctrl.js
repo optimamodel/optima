440143badd1c8b68a5ef76e9e693dcb9c7578c7b
(new name) <(new email)>
2014-08-29 01:53:10 -0400

    done with manual calibration

--- client/source/js/modules/model/model-manual-calibration-ctrl.js
+++ client/source/js/modules/model/model-manual-calibration-ctrl.js
@@ -3,58 +3,72 @@ define(['./module'], function (module) {
 
   module.controller('ModelManualCalibrationController', function ($scope) {
 
-    $scope.options = {
-      chart: {
-        type: 'lineChart',
-        height: 150,
-        margin: {
-          top: 20,
-          right: 20,
-          bottom: 60,
-          left: 50
-        },
-        xAxis: {
-          axisLabel: 'X Axis'
-        },
-        yAxis: {
-          axisLabel: 'Y Axis',
-          tickFormat: function (d) {
-            return d3.format(',.2f')(d);
+      $scope.linescatteroptions = {
+          chart: {
+              type: 'scatterPlusLineChart',
+              height: 250,
+              margin: {
+                  top: 20,
+                  right: 20,
+                  bottom: 60,
+                  left: 50
+              },
+              useInteractiveGuideline: true,
+              sizeRange: [100,100],
+              xAxis: {
+                  axisLabel: 'Year'
+              },
+              yAxis: {
+                  axisLabel: 'Prevalence',
+                  tickFormat: function (d) {
+                      return d3.format(',.2f')(d);
+                  },
+                  axisLabelDistance: 35
+              }
+          }
+      };
+
+      $scope.linescatterdata = [
+          {
+              values: sinData(10),
+              key: 'Model',
+              color: '#ff7f0e'
           },
-          axisLabelDistance: 35
-        }
-      }
-    };
+          {
+              values: [
+                  {"x":2006, "y":0.3},
+                  {"x":2008, "y":0.7},
+                  {"x":2014, "y":0.85},
+              ],
+              key: 'Data',
+              color: '#A09CF0'
+          }
+      ];
 
-    $scope.data = [
-      {
-        values: sinData(200),
-        key: 'Sine Wave 1',
-        color: '#ff7f0e'
-      },
-      {
-        values: sinData(150),
-        key: 'Sine Wave 2',
-        color: '#2ca02c'
-      },
-      {
-        values: sinData(100),
-        key: 'Sine Wave 3',
-        color: '#A09CF0'
-      }
-    ];
 
-    function sinData(koeff) {
-      var sin = [];
+      function sinData(koeff) {
+          var sin = [];
 
-      //Data is represented as an array of {x,y} pairs.
-      for (var i = 0; i < 500; i++) {
-        sin.push({x: i, y: Math.sin(i / koeff)});
+          //Data is represented as an array of {x,y} pairs.
+          for (var i = 2000; i < 2014; i++) {
+              sin.push({x: i, y: Math.sin(i / koeff)});
+          }
+
+          //Line chart data should be sent as an array of series objects.
+          return sin;
       }
 
-      //Line chart data should be sent as an array of series objects.
-      return sin;
-    }
+      $scope.json = [
+          ['Parameter', 'Value'],
+          [
+              ['Param. 1', 0.23],
+              ['Param. 2', 0.24],
+              ['Param. 3', 0.82],
+              ['Param. 4', 0.83],
+              ['Param. 5', 0.71],
+              ['Param. 6', 0.45]
+          ]
+      ];
 
   });
 
