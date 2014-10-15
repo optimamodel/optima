a8c3fae9de5d9ae0afe6e4f35fa3897f0055e896
(new name) <(new email)>
2014-10-15 20:41:03 +0200

    Prefixes api endpoint with /api

--- server/src/api.py
+++ server/src/api.py
@@ -2,27 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def root():
     return 'API is running!'
 
-@app.route('/data/line')
+@app.route('/api/data/line')
 def line():
     return app.send_static_file('line-chart.json')
 
-@app.route('/data/stacked-area')
+@app.route('/api/data/stacked-area')
 def stackedArea():
     return app.send_static_file('stacked-area-chart.json')
 
-@app.route('/data/multi-bar')
+@app.route('/api/data/multi-bar')
 def multiBar():
     return app.send_static_file('multi-bar-chart.json')
 
-@app.route('/data/pie')
+@app.route('/api/data/pie')
 def pie():
     return app.send_static_file('pie-chart.json')
 
-@app.route('/data/line-scatter-error')
+@app.route('/api/data/line-scatter-error')
 def lineScatterError():
     return app.send_static_file('line-scatter-error-chart.json')
 
