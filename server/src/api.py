7cffb8be4151f9436e98e34de30f23583f6a8f29
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-10-15 19:48:44 +0200

    Returns mocked data for the 5 different charts

--- server/src/api.py
+++ server/src/api.py
@@ -6,9 +6,26 @@ app = Flask(__name__)
 def root():
     return 'API is running!'
 
-@app.route('/data1')
-def data1():
-    return app.send_static_file('data1.json')
+@app.route('/data/line')
+def line():
+    return app.send_static_file('line-chart.json')
+
+@app.route('/data/stacked-area')
+def stackedArea():
+    return app.send_static_file('stacked-area-chart.json')
+
+@app.route('/data/multi-bar')
+def multiBar():
+    return app.send_static_file('multi-bar-chart.json')
+
+@app.route('/data/pie')
+def pie():
+    return app.send_static_file('pie-chart.json')
+
+@app.route('/data/line-scatter-error')
+def lineScatterError():
+    return app.send_static_file('line-scatter-error-chart.json')
+
 
 if __name__ == '__main__':
     app.run()
