00895a9d2c387214175bd3c6813f020cd832b83c
cliffckerr <cliff@thekerrlab.com>
2014-10-28 00:30:05 +1100

    updated spreadsheet example

--- server/src/sim/loaddata.py
+++ server/src/sim/loaddata.py
@@ -50,31 +50,29 @@ def loaddata(filename='./epi-template.xlsx',verbose=True):
     
     data = struct() # Create structure for holding data
     programs = struct() # Create structure for holding program data
-    programnames = [] # Create list for storing program names (probably unnecessary?)
 
     print('opening workbook %s' % filename)
     spreadsheet = open_workbook(filename) # Open spreadsheet
     print('opened workbook %s' % filename)
     print(spreadsheet)
 
-    for i,datanames in enumerate([basicdata, matrices, constants]): # Loop over each type of data, but treat constants differently
+    for sheettype,datanames in enumerate([basicdata, matrices, constants]): # Loop over each type of data, but treat constants differently
         for j in range(len(datanames)): # Loop over each spreadsheet for that data -- just one for constants
-            if verbose: print('  Loading "%s"...' % sheetnames[i][j])
+            if verbose: print('  Loading "%s"...' % sheetnames[sheettype][j])
             name = datanames[j][0] # Pull out the name of this field, e.g. 'epi'
             namelist = datanames[j][1] # Pull out the list subfields, e.g. ['popsize','hivprev'...]
             data[name] = struct() # Create structure for holding data, e.g. data.epi
-            sheetdata = spreadsheet.sheet_by_name(sheetnames[i][j]) # Load this spreadsheet
+            sheetdata = spreadsheet.sheet_by_name(sheetnames[sheettype][j]) # Load this spreadsheet
             parcount = -1 # Initialize the parameter count
-            procount = -1 # Initialize the program count
             for r in range(sheetdata.nrows): # Loop over each row in the spreadsheet
                 paramcategory = sheetdata.cell_value(r,0) # See what's in the first column for this row
                 if len(paramcategory): # It's not blank: e.g. "HIV prevalence"
                     if verbose: print('    Loading "%s"...' % paramcategory)
                     parcount += 1 # Increment the parameter count
-                    if i==0 or i==1: # It's basic data or a matrix
+                    if sheettype==0 or sheettype==1: # It's basic data or a matrix
                         thispar = namelist[parcount] # Get the name of this parameter, e.g. 'popsize'                    
                         data[name][thispar] = [] # Initialize to empty list
-                    if i==2: # It's a constant
+                    if sheettype==2: # It's a constant
                         thispar = namelist[parcount][0] # Get the name of this parameter, e.g. 'trans'
                         data[name][thispar] = struct() # Need yet another structure if it's a constant!
 
@@ -83,7 +81,7 @@ def loaddata(filename='./epi-template.xlsx',verbose=True):
                     if len(subparam): # The subparameter name isn't blank, load something!
                         thesedata = sheetdata.row_values(r,start_colx=2,end_colx=20) # Data starts in 3rd column, finishes in 21st column
                         thesedata = map(lambda val: nan if val=='' else val, thesedata) # Replace blanks with nan
-                        if i==0: # It's basic data, append the data and check for programs
+                        if sheettype==0: # It's basic data, append the data and check for programs
                             data[name][thispar].append(thesedata) # Store data
                             
                             print('dude this code sucks ccocs')
@@ -98,10 +96,10 @@ def loaddata(filename='./epi-template.xlsx',verbose=True):
                                     assumptions = map(lambda val: nan if val=='' else val, assumptions) # Replace blanks with nan
                                     programs[procategory][thispar][subparam].append(assumptions) # Store data in form program name/beahviour/pop
                             
-                        if i==1: # It's a matrix, append the data 
+                        if sheettype==1: # It's a matrix, append the data 
                             data[name][thispar].append(thesedata) # Store data
                                                         
-                        if i==2: # It's a constant, create a new dictionary entry
+                        if sheettype==2: # It's a constant, create a new dictionary entry
                             subpar = namelist[parcount][1].pop(0) # Pop first entry of subparameter list, which is namelist[parcount][1]
                             data[name][thispar][subpar] = thesedata # Store data
     
