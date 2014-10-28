fa01cf8b09380480e94db756f44316bacfdec99b
cliffckerr <cliff@thekerrlab.com>
2014-10-28 15:24:03 +1100

    fixed up data loading

--- server/src/sim/loaddata.py
+++ server/src/sim/loaddata.py
@@ -6,17 +6,17 @@ This function loads the spreadsheet data into Optima.
 Version: 2014oct25
 """
 
-def loaddata(filename='./epi-template.xlsx',verbose=True):
+def loaddata(filename='example.xlsx',verbose=True):
 #if 1: # For troubleshooting
-    filename='./epi-template.xlsx'
-    verbose=True
+#    filename='example.xlsx'
+#    verbose=True
     
     ###########################################################################
     ## Preliminaries
     ###########################################################################
     
     print('Loading data from %s...' % filename)
-    from matplotlib.pylab import nan # For reading in empty values
+    from matplotlib.pylab import nan, array # For reading in empty values
     from xlrd import open_workbook # For opening Excel spreadsheets
     from bunch import Bunch as struct # Replicate Matlab-like structure behavior
     
@@ -25,7 +25,13 @@ def loaddata(filename='./epi-template.xlsx',verbose=True):
     ## Define the spreadsheet and parameter names
     ###########################################################################
     
-    sheetnames = [['Epidemiology', 'Testing and treatment', 'Sexual behavior', 'Drug behavior'], ['Partnerships', 'Transitions'], ['Constants']]
+    sheetnames = [['Populations and programs'], \
+                  ['Epidemiology', 'Testing and treatment', 'Sexual behavior', 'Drug behavior'], \
+                  ['Partnerships', 'Transitions'], \
+                  ['Constants', 'Economics'], \
+                  ['Cost and coverage']]
+    
+    meta = [['meta', ['pops', 'progs']]]
     
     basicdata = [['epi',  ['popsize','hivprev','hivprevlow','hivprevhigh','stiprev','tbprev']], \
                  ['txrx', ['testrate','aidstestrate','numtests','numdiagnoses','numinfections','numdeaths','num1stline','num2ndline','numpmtct','numbreastpmtct']], \
@@ -41,7 +47,11 @@ def loaddata(filename='./epi-template.xlsx',verbose=True):
                             ['recov',    ['gt500','gt350','gt200','aids']],\
                             ['fail',     ['1st','2nd']],\
                             ['death',    ['background','inj','acute','gt500','gt350','gt200','aids','treat','tb']],\
-                            ['eff',      ['condom','circ','dx','sti','meth','pmtct','tx']]]]]
+                            ['eff',      ['condom','circ','dx','sti','meth','pmtct','tx']]]], \
+                ['econ',   [['dalys',    ['acute','gt500','gt350','gt200','aids','tx']], \
+                            ['costs',    ['acute','gt500','gt350','gt200','aids']]]]]
+    
+    costcov = [['costcov',  ['cost', 'cov']]]
     
     
     ###########################################################################
@@ -50,58 +60,89 @@ def loaddata(filename='./epi-template.xlsx',verbose=True):
     
     data = struct() # Create structure for holding data
     programs = struct() # Create structure for holding program data
-
-    print('opening workbook %s' % filename)
     spreadsheet = open_workbook(filename) # Open spreadsheet
-    print('opened workbook %s' % filename)
-    print(spreadsheet)
 
-    for sheettype,datanames in enumerate([basicdata, matrices, constants]): # Loop over each type of data, but treat constants differently
-        for j in range(len(datanames)): # Loop over each spreadsheet for that data -- just one for constants
-            if verbose: print('  Loading "%s"...' % sheetnames[sheettype][j])
-            name = datanames[j][0] # Pull out the name of this field, e.g. 'epi'
-            namelist = datanames[j][1] # Pull out the list subfields, e.g. ['popsize','hivprev'...]
+    for sheettype,datanames in enumerate([meta, basicdata, matrices, constants, costcov]): # Loop over each type of data, but treat constants differently
+        for dataname in range(len(datanames)): # Loop over each spreadsheet for that data -- just one for constants
+            
+            if verbose: print('  Loading "%s"...' % sheetnames[sheettype][dataname])
+            name = datanames[dataname][0] # Pull out the name of this field, e.g. 'epi'
+            namelist = datanames[dataname][1] # Pull out the list subfields, e.g. ['popsize','hivprev'...]
             data[name] = struct() # Create structure for holding data, e.g. data.epi
-            sheetdata = spreadsheet.sheet_by_name(sheetnames[sheettype][j]) # Load this spreadsheet
+            sheetdata = spreadsheet.sheet_by_name(sheetnames[sheettype][dataname]) # Load this spreadsheet
             parcount = -1 # Initialize the parameter count
-            for r in range(sheetdata.nrows): # Loop over each row in the spreadsheet
-                paramcategory = sheetdata.cell_value(r,0) # See what's in the first column for this row
-                if len(paramcategory): # It's not blank: e.g. "HIV prevalence"
+            
+            if sheettype==1: # Initialize row counts
+                lastcol = sheetdata.ncols - 8 # The 8 makes sense...
+                assumptioncol = lastcol + 1 # The "OR" space is in between
+                programcols = assumptioncol + array([3,5,7]) # Sorry for the weird numbering...not sure what the deal is
+            
+            
+            # Loop over each row in the spreadsheet
+            for row in range(sheetdata.nrows): 
+                paramcategory = sheetdata.cell_value(row,0) # See what's in the first column for this row
+                if not(paramcategory==''): # It's not blank: e.g. "HIV prevalence"
                     if verbose: print('    Loading "%s"...' % paramcategory)
                     parcount += 1 # Increment the parameter count
-                    if sheettype==0 or sheettype==1: # It's basic data or a matrix
+                    
+                    if sheettype==0: # Metadata
+                        thispar = namelist[parcount] # Get the name of this parameter, e.g. 'pop'
+                        data[name][thispar] = struct() # Initialize to empty list
+                        data[name][thispar].short = []
+                        data[name][thispar].long = []
+                        if parcount==0:
+                            data[name][thispar].male = []
+                            data[name][thispar].pwid = []
+                    
+                    if sheettype==1 or sheettype==2 or sheettype==4: # It's basic data or a matrix
                         thispar = namelist[parcount] # Get the name of this parameter, e.g. 'popsize'                    
                         data[name][thispar] = [] # Initialize to empty list
-                    if sheettype==2: # It's a constant
+                    
+                    if sheettype==3: # It's a constant
                         thispar = namelist[parcount][0] # Get the name of this parameter, e.g. 'trans'
                         data[name][thispar] = struct() # Need yet another structure if it's a constant!
 
-                else: # The first column is blank: it's time for the data
-                    subparam = sheetdata.cell_value(r,1) # Get the name of a subparameter, e.g. 'FSW', population size for a given population
-                    if len(subparam): # The subparameter name isn't blank, load something!
-                        thesedata = sheetdata.row_values(r,start_colx=2,end_colx=20) # Data starts in 3rd column, finishes in 21st column
-                        thesedata = map(lambda val: nan if val=='' else val, thesedata) # Replace blanks with nan
-                        if sheettype==0: # It's basic data, append the data and check for programs
+                
+                if paramcategory=='': # The first column is blank: it's time for the data
+                    subparam = sheetdata.cell_value(row, 1) # Get the name of a subparameter, e.g. 'FSW', population size for a given population
+                    if not(subparam==''): # The subparameter name isn't blank, load something!
+                        
+                        # It's meta-data, split into pieces
+                        if sheettype==0: 
+                            thesedata = sheetdata.row_values(row, start_colx=2, end_colx=6) # Data starts in 3rd column, finishes in 6th column
+                            data[name][thispar].short.append(thesedata[0])
+                            data[name][thispar].long.append(thesedata[1])
+                            if parcount==0:
+                                data[name][thispar].male.append(thesedata[2])
+                                data[name][thispar].pwid.append(thesedata[3])
+                        
+                        # It's basic data, append the data and check for programs
+                        if sheettype==1: 
+                            thesedata = sheetdata.row_values(row, start_colx=2, end_colx=lastcol) # Data starts in 3rd column, finishes in 21st column
+                            thesedata = map(lambda val: nan if val=='' else val, thesedata) # Replace blanks with nan
+                            assumptiondata = sheetdata.cell_value(row, assumptioncol)
+                            if not(assumptiondata==''): thesedata = [assumptiondata] # Replace the (presumably blank) data if a non-blank assumption has been entered
                             data[name][thispar].append(thesedata) # Store data
                             
-                            print('dude this code sucks ccocs')
-                            procategory = sheetdata.cell_value(r,19) # See what's in the 18th column - any program data? # WARNING KLUDGY AND AWFUL AND DISGUSTING
-                            if len(procategory): # It's not blank: there is a program that affects this parameter
-                                if verbose: print('    Loading "%s"...' % procategory)
-                                programs[thispar] = struct() # Create structure for storing program details (this is hideous, fix later. Could prob be a list)
-                                programs[thispar][subparam] = struct() # Create structure for storing program details (this is hideous, fix later. Could prob be a list)
-                                programs[thispar][subparam][procategory] = 1 # Store program name (this is hideous, fix later. Could prob be a list)
-                                if sheetdata.cell_value(r,23): # Any behavoiural assumptions to store?
-                                    assumptions = sheetdata.row_values(r,start_colx=23,end_colx=24) # Data starts in 3rd column, finishes in 21st column
-                                    assumptions = map(lambda val: nan if val=='' else val, assumptions) # Replace blanks with nan
-                                    programs[procategory][thispar][subparam].append(assumptions) # Store data in form program name/beahviour/pop
-                            
-                        if sheettype==1: # It's a matrix, append the data 
+                            # Load program data -- only exists for basic data
+                            programname = sheetdata.cell_value(row, programcols[0])
+                            if not(programname==''): # Not blank: a program exists!
+                                if not(programs.has_key(programname)): programs[programname] = [] # Create new list if none exists
+                                outcomes = sheetdata.row_values(row, start_colx=programcols[1], end_colx=programcols[2]) # Get outcome data
+                                programs[programname].append([[name,thispar], outcomes]) # Append to program
+                        
+                        # It's a matrix, append the data                                     
+                        if sheettype==2: 
+                            thesedata = sheetdata.row_values(row, start_colx=2, end_colx=sheetdata.ncols) # Data starts in 3rd column, finishes in 21st column
+                            thesedata = map(lambda val: 0 if val=='' else val, thesedata) # Replace blanks with nan
                             data[name][thispar].append(thesedata) # Store data
-                                                        
-                        if sheettype==2: # It's a constant, create a new dictionary entry
+                        
+                        # It's a constant, create a new dictionary entry
+                        if sheettype==3: 
+                            thesedata = sheetdata.row_values(row, start_colx=2, end_colx=5) # Data starts in 3rd column, finishes in 21st column
+                            thesedata = map(lambda val: 0 if val=='' else val, thesedata) # Replace blanks with nan
                             subpar = namelist[parcount][1].pop(0) # Pop first entry of subparameter list, which is namelist[parcount][1]
                             data[name][thispar][subpar] = thesedata # Store data
     
     if verbose: print('  ...done loading data.')
-    return data
+    return data, programs
