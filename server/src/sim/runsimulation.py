88f354cc40f8ffb9bde90eeb10eb875db2a388be
(new name) <(new email)>
2014-10-28 15:34:15 +1100

    added example files

---
+++ server/src/sim/runsimulation.py
@@ -0,0 +1,9 @@
+"""
+RUNSIMULATION
+
+Version: 2014oct28
+"""
+
+def runsimulation(projectdatafile, endyear):
+    # Make an Excel template and then prompt the user to save it
+    return excel_template
