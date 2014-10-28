3d8dab4b1040f66a43f0b1a88403805ba1ce9825
cliffckerr <cliff@thekerrlab.com>
2014-10-28 12:02:45 +0000

    created rest of skeleton functions

---
+++ server/src/sim/viewcounterfactuals.py
@@ -0,0 +1,10 @@
+"""
+MAKETEMPLATE
+
+Version: 2014oct28
+"""
+
+def maketemplate(projectname='Test', numpopgroups=6, numprograms=8, startyear=2000, endyear=2015):
+    # Make an Excel template and then prompt the user to save it
+    excel_template = 'This should be the actual Excel file created rather than some text :)'
+    return excel_template
