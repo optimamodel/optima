dee4826788f42781f83e6830cf35db79cfaf9e37
cliffckerr <cliff@thekerrlab.com>
2014-10-29 16:03:43 +0000

    since 2 make parameters functions, clarified naming

--- server/src/sim/makemodelpars.py
+++ server/src/sim/makemodelpars.py
@@ -9,5 +9,7 @@ Version: 2014oct29
 def makemodelpars(P, options, verbose=2):
     if verbose>=1: print('Making model parameters...')
     
+    
+    
     if verbose>=2: print('  ...done making model parameters.')
     return Q
