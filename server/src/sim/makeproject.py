3f9db2f2d0a9e2065588fe74b5aa9198fa90caa6
cliffckerr <cliff@thekerrlab.com>
2014-10-29 00:21:33 +0000

    working on data read-in and conversion

--- server/src/sim/makeproject.py
+++ server/src/sim/makeproject.py
@@ -4,7 +4,13 @@ MAKETEMPLATE
 Version: 2014oct28
 """
 
-def maketemplate(projectname='Test', numpopgroups=6, numprograms=8, startyear=2000, endyear=2015):
+def makeproject(projectname='example', numpopgroups=6, numprograms=8, startyear=2000, endyear=2015):
+    # Create project -- TODO: check if an existing project exists and don't overwrite it
+    from scipy.io import savemat
+    savemat(projectname+'.mat',{'projectname':projectname})
+    
     # Make an Excel template and then prompt the user to save it
-    excel_template = 'This should be the actual Excel file created rather than some text :)'
-    return excel_template
+    from maketemplate import maketemplate
+    templatename = maketemplate(projectname, numpopgroups, numprograms, startyear, endyear)
+    
+    return templatename
--- server/src/sim/runsimulation.py
+++ server/src/sim/runsimulation.py
@@ -4,28 +4,14 @@ RUNSIMULATION
 Version: 2014oct28
 """
 
-def runsimulation(projectdatafile='Test.mat', endyear=2030):
+def runsimulation(projectdatafile='example.mat', endyear=2030):
     # The project data file name needs to be 
-    from matplotlib.pylab import rand, r_, exp # KLUDGY
-    from bunch import Bunch as struct # Replicate Matlab-like structure behavior
+    from scipy.io import loadmat, savemat
     
-    # Generate data for scatter and line plots
-    nplots = 6
-    plotdata = []
-    for p in range(nplots):
-        plotdata.append(struct())
-        plotdata[p].xmodeldata = r_[2000:endyear+1] # Model output
-        plotdata[p].ymodeldata = exp(-rand(len(plotdata[p].xmodeldata)))
-        plotdata[p].xexpdata = [2000, 2005, 2008] # Experimental data
-        plotdata[p].yexpdata = [0.3, 0.4, 0.6]
-        plotdata[p].xlabel = 'Year'
-        plotdata[p].ylabel = 'Prevalence'
-        
-        # e.g. 
-        from matplotlib.pylab import plot, hold, scatter, subplot
-        subplot(3,2,p)
-        plot(plotdata[p].xmodeldata, plotdata[p].ymodeldata)
-        hold(True)
-        scatter(plotdata[p].xexpdata, plotdata[p].yexpdata);
+    D = loadmat(projectdatafile)
+    
+    
+    
+    
     
     return plotdata
