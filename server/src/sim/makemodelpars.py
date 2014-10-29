47eedd3b72694f1679f92276d1af9dc93eb9587c
(new name) <(new email)>
2014-10-29 21:36:45 +0000

    a few small updates to making model parameters work

--- server/src/sim/makemodelpars.py
+++ server/src/sim/makemodelpars.py
@@ -9,5 +9,9 @@ Version: 2014oct29
 def makemodelpars(P, options, verbose=2):
     if verbose>=1: print('Making model parameters...')
     
+    from bunch import Bunch as struct # Replicate Matlab-like structure behavior
+    M = struct()
+    M.__doc__ = 'Model parameters to be used directly in the model, calculated from data parameters P.'
+
     if verbose>=2: print('  ...done making model parameters.')
-    return Q
+    return M
