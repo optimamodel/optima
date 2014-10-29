0fbbc09ed96316954480c94b85f6c42fb5024cbb
(new name) <(new email)>
2014-10-29 14:30:50 +0000

    refining terminology: template -> spreadsheet

--- server/src/sim/makeproject.py
+++ server/src/sim/makeproject.py
@@ -1,16 +1,25 @@
 """
-MAKETEMPLATE
+MAKEPROJECT
 
-Version: 2014oct28
+Version: 2014oct29
 """
 
-def makeproject(projectname='example', numpopgroups=6, numprograms=8, startyear=2000, endyear=2015):
-    # Create project -- TODO: check if an existing project exists and don't overwrite it
+def makeproject(projectname='example', npops=6, nprogs=8, datastart=2000, dataend=2015):
+    
     from dataio import savedata
-    savedata(projectname+'.prj',{'projectname':projectname})
+    from bunch import Bunch as struct
+    projectfilename = projectname+'.prj'
+    
+    G = struct() # "G" for "general parameters"
+    G.npops = npops
+    G.nprogs = nprogs
+    G.projectname = projectname
+    G.datastart = datastart
+    G.dataend = dataend
+    savedata(projectfilename, G) # Create project -- #TODO: check if an existing project exists and don't overwrite it
     
     # Make an Excel template and then prompt the user to save it
-    from maketemplate import maketemplate
-    spreadsheetname = maketemplate(projectname, numpopgroups, numprograms, startyear, endyear)
+    from makespreadsheet import makespreadsheet
+    spreadsheetname = makespreadsheet(projectname, npops, nprogs, datastart, dataend)
     
     return spreadsheetname
---
+++ server/src/sim/makespreadsheet.py
@@ -0,0 +1,14 @@
+"""
+MAKESPREADSHEET
+
+Version: 2014oct28
+"""
+
+def makespreadsheet(projectname='example', npops=6, nprogs=8, datastart=2000, dataend=2015):
+    
+    # Make an Excel template and then prompt the user to save it
+    print('Need to actually make spreadsheet here!')
+    
+    spreadsheetname = projectname+'.xlsx'
+   
+    return spreadsheetname
