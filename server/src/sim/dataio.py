c0e1afe184013525f832dc1cb97f6e789032059b
cliffckerr <cliff@thekerrlab.com>
2014-10-29 05:32:28 +0000

    got data i/o working

--- server/src/sim/dataio.py
+++ server/src/sim/dataio.py
@@ -9,44 +9,15 @@ Version: 2014oct29
 """
 
 def savedata(filename, data):
-    from bunch import Bunch as struct, unbunchify
+    from bunch import Bunch as unbunchify
     from scipy.io import savemat
-    if type(data)==type(struct()): unbunchify(data)
-    
-    # KLUDGY -- recursively iterate over items, converting any found dictionaries to structures, up to 3 levels
-    if iterable(data):
-        for item1 in data:
-            if type(data[item1])==type(struct()): data[item1] = dict(data[item1])
-            if iterable(data[item1]):
-                for item2 in data[item1]:
-                    if type(data[item1][item2])==type(struct()): data[item1][item2] = dict(data[item1][item2])
-                    if iterable(data[item1][item2]):
-                        for item3 in data[item1][item2]:
-                            if type(data[item1][item2][item3])==type(struct()): data[item1][item2][item3] = dict(data[item1][item2][item3])
-                            if iterable(data[item1][item2][item3]):
-                                raise Exception('Only 3 levels of recursion currently supported!')
-    
+    data = unbunchify(data)
     savemat(filename,data)
     return None
 
 def loaddata(filename):
-    from matplotlib.pylab import iterable
-    from bunch import Bunch as struct
+    from bunch import bunchify
     from scipy.io import loadmat
     data = loadmat(filename)
-    if type(data)==type(dict()): data = struct(data)
-    
-    # KLUDGY -- recursively iterate over items, converting any found dictionaries to structures, up to 3 levels
-    if iterable(data):
-        for item1 in data:
-            if type(data[item1])==type(dict()): data[item1] = struct(data[item1])
-            if iterable(data[item1]):
-                for item2 in data[item1]:
-                    if type(data[item1][item2])==type(dict()): data[item1][item2] = struct(data[item1][item2])
-                    if iterable(data[item1][item2]):
-                        for item3 in data[item1][item2]:
-                            if type(data[item1][item2][item3])==type(dict()): data[item1][item2][item3] = struct(data[item1][item2][item3])
-                            if iterable(data[item1][item2][item3]):
-                                raise Exception('Only 3 levels of recursion currently supported!')
-            
+    data = bunchify(data)
     return data
