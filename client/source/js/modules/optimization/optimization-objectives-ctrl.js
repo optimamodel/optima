7cd07b2883806703d2a5953172507dbbb0f38a83
(new name) <(new email)>
2014-10-30 15:13:33 +0200

    improve(model/optimization): adds submit button and sample post to the back-end

--- client/source/js/modules/optimization/optimization-objectives-ctrl.js
+++ client/source/js/modules/optimization/optimization-objectives-ctrl.js
@@ -1,7 +1,14 @@
 define(['./module'], function (module) {
   'use strict';
 
-  module.controller('OptimizationObjectivesController', function () {
+  module.controller('OptimizationObjectivesController', function ($scope, $http) {
+
+    $scope.defineOptimization = function () {
+      $http.post('/api/analysis/optimisation/define', {})
+        .success(function (response) {
+          console.log('post to /api/analysis/optimisation/define is done!', response);
+        })
+    };
   });
 
 });
