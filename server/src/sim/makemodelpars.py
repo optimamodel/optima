5661321a84722ab867b1efc249ae7a555117fab3
cliffckerr <cliff@thekerrlab.com>
2014-10-30 07:44:30 +0000

    working on balancing acts...not going so well, just going to translate code literally

--- server/src/sim/makemodelpars.py
+++ server/src/sim/makemodelpars.py
@@ -9,6 +9,7 @@ Version: 2014oct29
 def makemodelpars(P, options, verbose=2):
     if verbose>=1: print('Making model parameters...')
     
+    from matplotlib.pylab import zeros
     from bunch import Bunch as struct # Replicate Matlab-like structure behavior
     M = struct()
     M.__doc__ = 'Model parameters to be used directly in the model, calculated from data parameters P.'
@@ -16,9 +17,10 @@ def makemodelpars(P, options, verbose=2):
     tvec = options.tvec # Shorten time vector
     npts = len(tvec) # Number of time points # TODO probably shouldn't be repeated from model.m
     
+    
+    
     def dpar2mpar(datapar):
         """ Take data parameters and turn them into model parameters """
-        from matplotlib.pylab import zeros
         npops = len(datapar.p)
         output = zeros((npops,npts))
         for pop in range(npops): output[pop,:] = datapar.p[pop] # TODO: use time!
@@ -35,21 +37,51 @@ def makemodelpars(P, options, verbose=2):
     M.aidstest = dpar2mpar(P.aidstest) # AIDS testing rates
     
     ## Sexual behavior parameters -- all are parameters so can loop over all
-    for parname in P.sex.keys():
-        D.P[parname] = dpar2mpar(P.sex[parname])
+    M.circum = dpar2mpar(P.circum) # Circumcision
+    M.numacts = struct()
+    M.condom = struct()
+    M.numacts.reg = dpar2mpar(P.numactsreg) # ...
+    M.numacts.cas = dpar2mpar(P.numactscas) # ...
+    M.numacts.com = dpar2mpar(P.numactscom) # ...
+    M.numacts.drug = dpar2mpar(P.numinject) # ..
+    M.condom.reg = dpar2mpar(P.condomreg) # ...
+    M.condom.cas = dpar2mpar(P.condomcas) # ...
+    M.condom.com = dpar2mpar(P.condomcom) # ...
     
     ## Drug behavior parameters
-    M.numinject = dpar2mpar(P.numinject)
     M.ost = dpar2mpar(P.ost)
     M.sharing = dpar2mpar(P.sharing)
     
     ## Matrices can be used directly
-    print('separate out')
     M.pships = P.pships
     M.transit = P.transit
     
     ## Constants...can be used directly -- # TODO should this be copy?
     M.const = P.const
+    
+    
+    ## WARNING need to introduce time!
+    def reconcileacts(numacts, popsize, pships):
+        from matplotlib.pylab import shape
+        npops = shape(numacts)[0]
+        popacts = zeros((npops,npts))
+        for pop in range(npops):
+            popacts[pop] = numacts[pop,:] * popsize[pop,:]
+        
+        return totalacts
+    
+    # Calculate number of acts
+    totalacts = struct()
+    for act in ['reg','cas','com','drug']:
+        totalacts[act] = reconcileacts(M.numacts[act], M.popsize, P.pships[act])
+    
+    
+    
+    # Reconcile number of acts
+    
+    # Apply interventions
+    
+    # Sum matrices
 
     if verbose>=2: print('  ...done making model parameters.')
     return M
