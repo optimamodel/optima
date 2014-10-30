b17bd468ac58936861fd03c5b7266ad4904da540
Anna Nachesa <anachesa@gmail.com>
2014-10-31 00:35:41 +0100

    getting some data back

--- server/src/sim/optimize.py
+++ server/src/sim/optimize.py
@@ -4,30 +4,35 @@ Run optimization. The data structure "objectives" is from the "Optimize outcomes
 Version: 2014oct29
 """
 
-def optimize(projectname='example', maxtime=60, objectives):
+def optimize(projectname='example', maxtime=60, objectives={}):
+    from matplotlib.pylab import rand, r_, exp # KLUDGY
+    from bunch import Bunch as struct # Replicate Matlab-like structure behavior
+
     
     # Generate data for scatter and line plots
-    lineplotdata.append(struct())
-    lineplotdata.xmodeldata = r_[2000:2015] # Model output
-    lineplotdata.ymodeldata = exp(-rand(len(plotdata[p].xmodeldata)))
-    lineplotdata.xlabel = 'Year'
-    lineplotdata.ylabel = 'Prevalence'
-    
     nplots = 2 # Original and optimal
+    lineplotdata = []
+    for p in range(nplots):
+        lineplotdata.append(struct())
+        lineplotdata[p].xmodeldata = r_[2000:2015] # Model output
+        lineplotdata[p].ymodeldata = exp(-rand(len(lineplotdata[p].xmodeldata)))
+        lineplotdata[p].xlabel = 'Year'
+        lineplotdata[p].ylabel = 'Prevalence'
+    
     pieplotdata = []
     for p in range(nplots):
         pieplotdata.append(struct())
         pieplotdata[p].piedata = [0.35, 0.12, 0.34, 0.28] # Experimental data
         pieplotdata[p].title = 'Allocation'
         
-     # e.g. 
-     from matplotlib.pylab import plot, hold, scatter, subplot
-     
-     subplot(2,2,1)
-     plot(plotdata[p].xmodeldata, plotdata[p].ymodeldata)
-     subplot(3,2,p)
-     plot(plotdata[p].xmodeldata, plotdata[p].ymodeldata)
-     hold(True)
-     scatter(plotdata[p].xexpdata, plotdata[p].yexpdata);
+    # e.g. 
+#    from matplotlib.pylab import plot, hold, scatter, subplot
+
+#    subplot(2,2,1)
+#    plot(lineplotdata[p].xmodeldata, lineplotdata[p].ymodeldata)
+#    subplot(3,2,p)
+#    plot(lineplotdata[p].xmodeldata, lineplotdata[p].ymodeldata)
+#    hold(True)
+#    pie(pieplotdata[p].pie);
     
     return lineplotdata, pieplotdata
