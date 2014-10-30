c4b95d2eb2693b0bf99eacbdd982642432f0aac1
(new name) <(new email)>
2014-10-30 09:45:01 +0000

    partnerships are confusing

--- server/src/sim/model.py
+++ server/src/sim/model.py
@@ -8,7 +8,7 @@ Version: 2014sep25
 
 
 
-def model(G, P, options, verbose=2): # extraoutput is to calculate death rates etc.
+def model(G, M, options, verbose=2): # extraoutput is to calculate death rates etc.
 
     if verbose>=1: print('Running model...')
     
@@ -32,9 +32,9 @@ def model(G, P, options, verbose=2): # extraoutput is to calculate death rates e
     
     people = zeros((G.nstates, G.npops, npts)) # Initialize matrix to hold everything
     allpeople = zeros((G.npops, npts))
-    people[0, :, 0] = P.popsize.p * (1-P.hivprev.p) # Set initial population sizes -- # TODO: calculate properly
-    people[1, :, 0] = P.popsize.p * P.hivprev.p # Set initial population sizes
-    effhivprevalence = zeros((G.npops,1)) # HIV effective prevalence (prevalence times infectiousness)
+    people[0, :, 0] = M.popsize[:,0] * (1-M.hivprev[:,0]) # Set initial population sizes
+    people[1, :, 0] = M.popsize[:,0] * M.hivprev[:,0] # Set initial population sizes -- # TODO: equilibrate
+    effhivprev = zeros((G.npops,1)) # HIV effective prevalence (prevalence times infectiousness)
     dU = []; dD = []; dT1 = []; dF = []; dT2 = [] # Initialize differences
     
     
@@ -50,8 +50,8 @@ def model(G, P, options, verbose=2): # extraoutput is to calculate death rates e
     
     ## Calculate other things outside the loop
     cd4trans = h2a(P.const.cd4trans) # Convert a dictionary to an array
-    dxfactor = P.const.eff.dx * cd4trans # Include diagnosis efficacy
-    txfactor = P.const.eff.tx * dxfactor # And treatment efficacy
+    dxfactor = M.const.eff.dx * cd4trans # Include diagnosis efficacy
+    txfactor = M.const.eff.tx * dxfactor # And treatment efficacy
     
     
     
@@ -71,44 +71,28 @@ def model(G, P, options, verbose=2): # extraoutput is to calculate death rates e
             effundx = sum(cd4trans * people[G.undx,pop,t]); # Effective number of infecious undiagnosed people
             effdx   = sum(dxfactor * (people[G.dx,pop,t]+people[G.fail,pop,t])) # ...and diagnosed/failed
             efftx   = sum(txfactor * (people[G.tx1,pop,t]+people[G.tx2,pop,t])) # ...and treated
-            effhivprevalence[pop]=(effundx+effdx+efftx)/allpeople[pop,t]; # Calculate HIV "prevalence", scaled for infectiousness based on CD4 count; assume that treatment failure infectiousness is same as corresponding CD4 count
-            if not(effhivprevalence[pop]>=0): raise Exception('HIV prevalence invalid in population %s! (=%f)' % (pop,effhivprevalence[pop]) )
+            effhivprev[pop]=(effundx+effdx+efftx)/allpeople[pop,t]; # Calculate HIV "prevalence", scaled for infectiousness based on CD4 count; assume that treatment failure infectiousness is same as corresponding CD4 count
+            if not(effhivprev[pop]>=0): raise Exception('HIV prevalence invalid in population %s! (=%f)' % (pop,effhivprev[pop]) )
         
         ###############################################################################
         ## Calculate force-of-infection (forceinf)
         forceinfvec = zeros(G.npops) # Initialize force-of-infection vector for each population group
         
-        # Sexual partnerships
-        for pop in range(G.npops):
-            
-        for psh in range(Q.nsexpartnerships): # Loop over all sexual partnerships
-            
-            # Amazingly, the code runs faster if these are pulled out of the equations for forceinf!
-            popM = Q.sexpartnershippop[psh,1]; # "Male" population (e.g. 5 = CSW)
-            popF = Q.sexpartnershippop[psh,2]; # "Female" population (e.g. 10 = FIDU), but includes MSM and waria
-            numacts = dt*Q.numacts[psh,:,t]; # Number of acts per person per year; 2nd dimension is insertive vs. receptive
-            condomeffect=1-pm.condomprob(psh,t)*pm.condomefficacy; # Condom use
-            circumcisioneffectM=1-pm.circumcisionefficacy*pm.circumcisionprob(popM,t); # Effect of circumcisionumcision
-            circumcisioneffectF=1; # circumcision has no effect on the receptive population
-            otherinterventioneffectM=1;# Turn off for now 1-pm.otherinterventionefficacy*pm.otherinterventionprob(popM,t); # An "other" intervention, e.g. PEP
-            otherinterventioneffectF=1;# Turn off for now 1-pm.otherinterventionefficacy*pm.otherinterventionprob(popF,t); # An "other" intervention, e.g. PEP
-            stieffectM=1+pm.stitransincrease*pm.stiprevalence(popM,t); # STI prevalence effect
-            stieffectF=1+pm.stitransincrease*pm.stiprevalence(popF,t);
-            transmissibilityM=pm.sexpartnershiptrans(psh,1); # Insertive transmissibility
-            transmissibilityF=pm.sexpartnershiptrans(psh,2); # Receptive transmissibility
-            
-            forceinfM = 1 - (1-transmissibilityM*circumcisioneffectM*stieffectM*otherinterventioneffectM) ^ (numacts(1)*condomeffect*effectivehivprevalence(popF)); #/populationsizeM); # The chance of person B infecting person A; pm.cst{psh}(3)=trans; only males are protected by circumcisionumcision
-            forceinfF = 1 - (1-transmissibilityF*circumcisioneffectF*stieffectF*otherinterventioneffectF) ^ (numacts(2)*condomeffect*effectivehivprevalence(popM)); #/populationsizeF); # The chance of person A infecting person B
-            if any([forceinfM forceinfF]~=median([[forceinfM forceinfF];[0 0];[1 1]])), badforceinf('sex'), end # Error checking: make sure both values lie between 0 and 1
-            
-            forceinfvec(popM)=1-(1-forceinfvec(popM))*(1-forceinfM); # Calculate the new "male" forceinf, ensuring that it never gets above 1
-            forceinfvec(popF)=1-(1-forceinfvec(popF))*(1-forceinfF); # Calculate the new "female" forceinf
-            
-            if extraoutput # Store information for calculating how many infections are caused by each population
-                forceinfmatrix(popM,popF)=1-(1-forceinfmatrix(popM,popF))*(1-forceinfF); # Calculate how many people the male population infected
-                forceinfmatrix(popF,popM)=1-(1-forceinfmatrix(popF,popM))*(1-forceinfM); # Calculate how many people the female population infected
-            end
-        end
+        # Sexual partnerships -- # TODO make more efficient
+        for pop1 in range(G.npops):
+            for pop2 in range(G.npops):
+                
+                circeff = 1-M.circumcisionefficacy*M.circumcisionprob[pop1,t] # Effect of circumcision
+                stieff = 1+M.stitransincrease*M.stiprevalence[pop1,t] # STI prevalence effect
+                trans = transmissibilitymatrix[pop1,pop2]
+                
+                for act in ['reg','cas','com']:
+                    if M.pships[act][popM,popF]>0:
+                        numacts = dt*M.totalacts[act][pop1,pop2,t]; # Number of acts per person per year; 2nd dimension is insertive vs. receptive
+                        condomprob = (M.condom[act][pop1,t] + M.condom[act][pop2,t]) / 2 # Reconcile condom probability
+                        condomeff = 1-condomprob*M.condomefficacy; # Condom use
+                        forceinf = 1 - (1-trans*circeff*stieff) ** (numacts*condomeff*effhivprev[pop2]); #/populationsizeM); # The chance of person B infecting person A; pm.cst{psh}(3)=trans; only males are protected by circumcisionumcision
+                        forceinfvec[pop1] = 1 - (1-forceinfvec[pop1]) * (1-forceinf); # Calculate the new "male" forceinf, ensuring that it never gets above 1
         
         
         # Injecting partnerships
