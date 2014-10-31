e78e3221d766b21850d49d2c23282f9187c9581b
Dmitry Evseev <kradmiy@gmail.com>
2014-10-31 02:41:35 +0200

    feat(optimization/optimize): introduces start optimization functionality

--- client/source/js/modules/optimization/optimization-optimize-ctrl.js
+++ client/source/js/modules/optimization/optimization-optimize-ctrl.js
@@ -1,113 +1,113 @@
-define(['./module'], function (module) {
+define(['./module', 'underscore'], function (module, _) {
   'use strict';
 
-  module.controller('OptimizationOptimizeController', function ($scope) {
+  module.controller('OptimizationOptimizeController', function ($scope, $http) {
 
-      $scope.pieoptions = {
-          chart: {
-              type: 'pieChart',
-              height: 350,
-              x: function(d){return d.key;},
-              y: function(d){return d.y;},
-              showLabels: false,
-              transitionDuration: 500,
-              labelThreshold: 0.01,
-              legend: {
-                  margin: {
-                      top: 5,
-                      right: 35,
-                      bottom: 5,
-                      left: 0
-                  }
-              }
+    $scope.pie1 = { title: '', data: [] };
+    $scope.pie2 = { title: '', data: [] };
+
+    $scope.linescatterdata = [{ "values": [
+        {"x": 1, "y": 0.6131168519734338, "series": 0},
+        { "x": 2, "y": 0.6078366513401895, "series": 0 },
+        {"x": 3, "y": 0.6025294358447186, "series": 0}
+      ], "key": "Infections", "color": "#ff7f0e"
+    }];
+
+
+    // @todo now it's just mocked labels
+    var pieLabels = [
+      "Behaviour change and communication",
+      "Female sex workers",
+      "Men who have sex with men",
+      "Injecting drug users",
+      "Sexually transmitted infections",
+      "HIV counselling and testing",
+      "Antiretroviral therapy",
+      "Prevention of mother-to-child transmission"
+    ];
+
+    var setupPie = function (container, data) {
+      container.title = data.title;
+      container.data = _(data.piedata).map(function (value, i) {
+        return {
+          key: pieLabels[i],
+          y: value
+        };
+      });
+    };
+
+    var setupLine = function (container, options, data) {
+      options.xAxis.axisLabel = data.xlabel;
+      options.yAxis.axisLabel = data.ylabel;
+      container.values = _(data.xmodeldata).map(function (value, i) {
+        return {
+          series: 0,
+          x: value,
+          y: data.ymodeldata[i]
+        };
+      });
+    };
+
+    $scope.startRun = false;
+    $scope.startAnalysis = function () {
+      $http.get('/api/analysis/optimisation/start')
+        .success(function (response) {
+          $scope.startRun = true;
+
+          setupPie($scope.pie1, response.dataplot[0]);
+          setupPie($scope.pie2, response.dataplot[1]);
+          setupLine($scope.linescatterdata, $scope.linescatteroptions, response.lineplot[0]);
+        });
+    };
+
+    $scope.pieoptions = {
+      chart: {
+        type: 'pieChart',
+        height: 350,
+        x: function (d) {
+          return d.key;
+        },
+        y: function (d) {
+          return d.y;
+        },
+        showLabels: false,
+        transitionDuration: 500,
+        labelThreshold: 0.01,
+        legend: {
+          margin: {
+            top: 5,
+            right: 35,
+            bottom: 5,
+            left: 0
           }
-      };
-
-      $scope.piedata = [
-          {
-              key: "Behaviour change and communication",
-              y: 4
-          },
-          {
-              key: "Female sex workers",
-              y: 6
-          },
-          {
-              key: "Men who have sex with men",
-              y: 1
-          },
-          {
-              key: "Injecting drug users",
-              y: 20
-          },
-          {
-              key: "Sexually transmitted infections",
-              y: 2
-          },
-          {
-              key: "HIV counselling and testing",
-              y: 6
-          },
-          {
-              key: "Antiretroviral therapy",
-              y: 54
-          },
-          {
-              key: "Prevention of mother-to-child transmission",
-              y: 6
-          }
-      ];
-
-
-      $scope.linescatteroptions = {
-          chart: {
-              type: 'scatterPlusLineChart',
-              height: 250,
-              margin: {
-                  top: 20,
-                  right: 20,
-                  bottom: 60,
-                  left: 50
-              },
-              useInteractiveGuideline: true,
-              sizeRange: [100,100],
-              xAxis: {
-                  axisLabel: 'Iteration'
-              },
-              yAxis: {
-                  axisLabel: 'Infections',
-                  tickFormat: function (d) {
-                      return d3.format(',.2f')(d);
-                  },
-                  axisLabelDistance: 35
-              }
-          }
-      };
-
-      $scope.linescatterdata = [
-          {
-              values: sinData(150),
-              key: 'Infections',
-              color: '#ff7f0e'
-          }
-      ];
-
-
-      function sinData(koeff) {
-          var sin = [];
-
-          //Data is represented as an array of {x,y} pairs.
-          for (var i = 1; i < 100; i++) {
-              sin.push({x: i, y: Math.sin((100-i) / koeff)});
-          }
-
-          //Line chart data should be sent as an array of series objects.
-          return sin;
+        }
       }
+    };
 
-
-
-
+    $scope.linescatteroptions = {
+      chart: {
+        type: 'scatterPlusLineChart',
+        height: 250,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 60,
+          left: 50
+        },
+        useInteractiveGuideline: true,
+        sizeRange: [100, 100],
+        xAxis: {
+          axisLabel: 'Iteration'
+        },
+        yAxis: {
+          axisLabel: 'Infections',
+          tickFormat: function (d) {
+            return d3.format(',.2f')(d);
+          },
+          axisLabelDistance: 35
+        }
+      }
+    };
   });
 
 });
