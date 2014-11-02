cd8e46a13bb3387beb6ab309db207d56829ea73c
(new name) <(new email)>
2014-11-02 21:23:15 +0100

    started emitting "Coverage" page

--- server/src/sim/optima_workbook.py
+++ server/src/sim/optima_workbook.py
@@ -46,6 +46,10 @@ def make_parameter_range(name, params):
   coded_params = [list((abbreviate(item), item)) for item in params]
   return OptimaContent(name, row_names, column_names, coded_params)
 
+def make_ref_years_range(name, ref_range, data_start, data_end):
+  params = ref_range.param_refs()
+  return make_years_range(name, params, data_start, data_end)
+
 
 class OptimaFormats:
   def __init__(self, book):
@@ -78,15 +82,25 @@ class SheetRange:
     self.last_row = self.first_row + self.num_rows -1
     self.last_col = self.first_col + self.num_cols -1
 
-    self.start = xl_rowcol_to_cell(self.first_row, self.first_col, row_abs = True, col_abs = True)
-    self.end = xl_rowcol_to_cell(self.last_row, self.last_col, row_abs = True, col_abs = True)
+    self.start = self.get_cell_address(self.first_row, self.first_col)
+    self.end = self.get_cell_address(self.last_row, self.last_col)
 
   def get_address(self):
     return '%s:%s' % (self.start, self.end)
 
+  def get_cell_address(self, row, col):
+    return xl_rowcol_to_cell(row, col, row_abs = True, col_abs = True)
+
+  """ gives the list of references to the entries in the row names (which are parameters) """
+  def param_refs(self, sheet_name):
+    par_range = range(self.first_row, self.last_row +1)
+    return [ "='%s'!%s" % (sheet_name, self.get_cell_address(row, self.first_col)) for row in par_range ]
+
+
 class TitledRange:
   FIRST_COL = 2
-  def __init__(self, first_row, content):
+  def __init__(self, sheet, first_row, content):
+    self.sheet = sheet
     self.data_range = SheetRange(first_row+2, TitledRange.FIRST_COL, len(content.row_names), len(content.column_names))
     self.content = content
     self.first_row = first_row
@@ -95,19 +109,22 @@ class TitledRange:
     return self.data_range.num_rows + 2
 
   """ emits the range and returns the new current row in the given sheet """
-  def emit(self, sheet, formats):
-    formats.write_block_name(sheet, self.content.name, self.first_row)
+  def emit(self, formats):
+    formats.write_block_name(self.sheet, self.content.name, self.first_row)
     for i, name in enumerate(self.content.column_names):
-      formats.write_rowcol_name(sheet, self.first_row+1, TitledRange.FIRST_COL+i,name)
+      formats.write_rowcol_name(self.sheet, self.first_row+1, TitledRange.FIRST_COL+i,name)
     for i, name in enumerate(self.content.row_names):
-      formats.write_rowcol_name(sheet, self.data_range.first_row+i, self.data_range.first_col-1, name)
+      formats.write_rowcol_name(self.sheet, self.data_range.first_row+i, self.data_range.first_col-1, name)
       if self.content.has_data():
         for j, item in enumerate(self.content.data[i]):
-          formats.write_unlocked(sheet, self.data_range.first_row+i, self.data_range.first_col+j, item)
+          formats.write_unlocked(self.sheet, self.data_range.first_row+i, self.data_range.first_col+j, item)
       else:
-        for j in data_range.num_cols:
-          formats.write_empty_unlocked(sheet, self.data_range.first_row+i, self.data_range.first_col+j)
+        for j in range(self.data_range.num_cols):
+          formats.write_empty_unlocked(self.sheet, self.data_range.first_row+i, self.data_range.first_col+j)
     return self.first_row + self.num_rows() + 1 # for spacing
+
+  def param_refs(self):
+    return self.data_range.param_refs(self.sheet.get_name())
 
 class OptimaWorkbook:
   sheet_names = OrderedDict([('pp','Populations and programs'), \
@@ -136,20 +153,6 @@ class OptimaWorkbook:
 
     self.npops = len(pops)
     self.nprogs = len(progs)
-#    self.coded_pops = OrderedDict([(abbreviate(item), item) for item in self.pops])
-#    self.coded_progs = OrderedDict([(abbreviate(item), item) for item in self.progs])
-
-#  def write_block_name(self, sheet, name, row):
-#    sheet.write(row, 0, name, self.formats['bold'])
-
-#  def write_rowcol_name(self, sheet, row, col, name):
-#    sheet.write(row, col, name, self.formats['bold'])
-
-#  def write_unlocked(self, sheet, row, col, data):
-#    sheet.write(row, col, data, self.formats['unlocked'])
-
-#  def write_empty_unlocked(self, sheet, row, col, data):
-#    sheet.write_blank(row, col, None, self.formats['unlocked'])
 
   def generate_pp(self):
     pp_sheet = self.sheets['pp']
@@ -159,46 +162,21 @@ class OptimaWorkbook:
     current_row = 0
 
     pop_content = make_parameter_range('Populations', self.pops)
-    titled_pop_range = TitledRange(current_row, pop_content)
-    current_row = titled_pop_range.emit(pp_sheet, self.formats)
+    self.pop_range = TitledRange(pp_sheet, current_row, pop_content) # we'll need it for references
+    current_row = self.pop_range.emit(self.formats)
 
     prog_content = make_parameter_range('Programs', self.progs)
-    titled_prog_range = TitledRange(current_row, prog_content)
-    current_row = titled_prog_range.emit(pp_sheet, self.formats)
-
-#    first_col = 2
-#    self.write_block_name(pp_sheet, 'Populations', current_row)
-#    self.write_rowcol_name(pp_sheet, current_row+1, first_col,'Short name')
-#    self.write_rowcol_name(pp_sheet, current_row+1, first_col+1,'Long name')
-
-#    pop_range = SheetRange(current_row+2, first_col, self.npops, 2)
-##    pop_range_name = "'%s'!%s" % (OptimaWorkbook.sheet_names['pp'], pop_range.get_address())
-##    if self.verbose >=1:
-##      print "PopulationsRange = %s", pop_range_name
-##    self.book.define_name('PopulationsRange',pop_range_name)
-
-#    for i, item in enumerate(self.coded_pops):
-#      self.write_rowcol_name(pp_sheet, pop_range.first_row+i, pop_range.first_col-1, i+1)
-#      self.write_unlocked(pp_sheet, pop_range.first_row+i, pop_range.first_col, item)
-#      self.write_unlocked(pp_sheet, pop_range.first_row+i, pop_range.first_col+1, self.coded_pops[item])
-
-#    current_row += 2 + pop_range.num_rows + 1
-
-#    self.write_block_name(pp_sheet, 'Programs', current_row)
-#    self.write_rowcol_name(pp_sheet, current_row+1, first_col,'Short name')
-#    self.write_rowcol_name(pp_sheet, current_row+1, first_col+1,'Long name')
-
-#    prog_range = SheetRange(current_row+2, first_col, self.nprogs, 2)
-##    self.book.define_name('ProgramsRange',"'%s!%s" % (OptimaWorkbook.sheet_names['pp'], prog_range.get_address()))
-#    for i, item in enumerate(self.coded_progs):
-#      self.write_rowcol_name(pp_sheet, prog_range.first_row+i, prog_range.first_col-1, i+1)
-#      self.write_unlocked(pp_sheet, prog_range.first_row+i, prog_range.first_col, item)
-#      self.write_unlocked(pp_sheet, prog_range.first_row+i, prog_range.first_col+1, self.coded_progs[item])
+    self.prog_range = TitledRange(pp_sheet, current_row, prog_content) # ditto
+    current_row = self.prog_range.emit(self.formats)
 
   def generate_cc(self):
     cc_sheet = self.sheets['cc']
     cc_sheet.protect()
     current_row = 0
+
+    coverage_content = make_ref_years_range('Coverage', self.prog_range, self.data_start, self.data_end)
+    coverage_range = TitledRange(cc_sheet, current_row, coverage_content)
+    current_row = coverage_range.emit(self.formats)
 
 
   def create(self, path):
@@ -208,12 +186,10 @@ class OptimaWorkbook:
              (path, self.npops, self.nprogs, self.data_start, self.data_end))
     self.book = xlsxwriter.Workbook(path)
     self.formats = OptimaFormats(self.book)
-#    self.formats = {}
-#    self.formats['unlocked'] = self.book.add_format({'locked':0, 'bg_color':'#B3DEE5','border':1, 'border_color':'white'})
-#    self.formats['bold'] = self.book.add_format({'bold':1})
     self.sheets = {}
     for name in OptimaWorkbook.sheet_names:
       self.sheets[name] = self.book.add_worksheet(OptimaWorkbook.sheet_names[name])
     self.generate_pp()
+    self.generate_cc()
     self.book.close()
 
