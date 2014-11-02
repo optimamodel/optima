e6630070b5bdcd079918eb3ddfd2a30b4cece89e
cliffckerr <cliff@thekerrlab.com>
2014-11-02 17:52:54 +0400

    forgot about death rates

--- server/src/sim/loadspreadsheet.py
+++ server/src/sim/loadspreadsheet.py
@@ -56,8 +56,14 @@ def loadspreadsheet(filename='example.xlsx',verbose=2):
                                                       ['social',    ['acute','gt500','gt350','gt200','aids']]]]
                 ]
     
-    sheetstructure = [metadata, keydata, timedata, matrices, constants] 
-    print('WARNING should change this to have read-in directly based on each class')
+    
+    ## Ugly, but allow the list of groups to be used as name and also as variables
+    sheetstructure = struct()
+    sheetstructure.metadata = metadata
+    sheetstructure.keydata = keydata
+    sheetstructure.timedata = timedata
+    sheetstructure.matrices = matrices
+    sheetstructure.constants = constants
     
 
 
@@ -65,61 +71,96 @@ def loadspreadsheet(filename='example.xlsx',verbose=2):
     ## Load data sheets
     ###########################################################################
     
+    
+    ## Basic setup
     data = struct() # Create structure for holding data
     data.__doc__ = 'Raw data as loaded from the spreadsheet, including both epidemiological and behavioral data, plus economics and velociraptors.'
     programs = struct() # Create structure for holding program data
     programs.__doc__ = 'Parameters that define the HIV programs -- cost-coverage and coverage-outcome curves.'
     spreadsheet = open_workbook(filename) # Open spreadsheet
-
-    for sheetclass in sheetstructure: # Loop over each type of data, but treat constants differently
-        for sheet in sheetclass: # Loop over each spreadsheet for that data -- just one for constants
+    
+    
+    ## Loop over each group of sheets
+    for groupname in sheetstructure.keys(): # Loop over each type of data, but treat constants differently
+        sheetgroup = sheetstructure[groupname]
+        for sheet in sheetgroup: # Loop over each spreadsheet for that data -- just one for constants
             sheetname = sheet[0] # Name of the spreadsheet
             name = sheet[1] # Pull out the name of this field, e.g. 'epi'
             subparlist = sheet[2] # List of subparameters
-            if verbose>=2: print('  Loading "%s"...' % sheetname)
-            
-            
             data[name] = struct() # Create structure for holding data, e.g. data.epi
             sheetdata = spreadsheet.sheet_by_name(sheetname) # Load this spreadsheet
             parcount = -1 # Initialize the parameter count
+            if verbose>=2: print('  Loading "%s"...' % sheetname)
             
-            if sheetname=='Demographics & HIV prevalence': # Initialize row counts by using the first sheet that uses year ranges
-                lastdatacol = sheetdata.ncols - 8 # The 8 makes sense...
+            
+            ## Calculate columns for which data are entered, and store the year ranges
+            if groupname == 'keydata' or groupname == 'timedata': # Need to gather year ranges for epidemic etc. data
+                data.epiyears = [] # Initialize epidemiology data years
+                for col in range(sheetdata.ncols):
+                    thiscell = sheetdata.cell_value(1,col) # 1 is the 2nd row which is where the year data should be
+                    if thiscell=='' and len(data.epiyears)>0: #  We've gotten to the end
+                        lastdatacol = col-1 # Store this column number
+                        break # Quit
+                    else: # Nope, more years, keep going
+                        data.epiyears.append(float(thiscell)) # Add this year
+            
+            if name == 'macro': # Need to gather year ranges for economic data
+                data.econyears = [] # Initialize epidemiology data years
+                for col in range(sheetdata.ncols):
+                    thiscell = sheetdata.cell_value(1,col) # 1 is the 2nd row which is where the year data should be
+                    if thiscell=='' and len(data.econyears)>0: #  We've gotten to the end
+                        lastdatacol = col-1 # Store this column number
+                        break # Quit
+                    else: # Nope, more years, keep going
+                        data.econyears.append(float(thiscell)) # Add this year
+                
                 assumptioncol = lastdatacol + 1 # The "OR" space is in between
-                programcols = assumptioncol + array([3,5,7]) # Sorry for the weird numbering...not sure what the deal is
+                programcols = assumptioncol + array([3,5,6,8,9]) # Sorry for the weird numbering...not sure what the deal is
+            
+            
+            
+            
+            ##################################################################
+            ## Now, actually load the data
+            ##################################################################
             
             
             # Loop over each row in the spreadsheet
             for row in range(sheetdata.nrows): 
                 paramcategory = sheetdata.cell_value(row,0) # See what's in the first column for this row
                 
-                if not(paramcategory==''): # It's not blank: e.g. "HIV prevalence"
+                
+                if paramcategory != '': # It's not blank: e.g. "HIV prevalence"
                     if verbose>=2: print('    Loading "%s"...' % paramcategory)
                     parcount += 1 # Increment the parameter count
                     
-                    if name=='meta': # Metadata
+                    if groupname=='metadata': # Metadata
                         thispar = subparlist[parcount] # Get the name of this parameter, e.g. 'pop'
                         data[name][thispar] = struct() # Initialize to empty list
                         data[name][thispar].short = [] # Store short population/program names, e.g. "FSW"
                         data[name][thispar].long = [] # Store long population/program names, e.g. "Female sex workers"
-                    
-                    elif name=='const' or name=='cost': # It's a constant or a cost: create a structure
-                        thispar = subparlist[parcount][0] # Get the name of this parameter, e.g. 'trans'
-                        data[name][thispar] = struct() # Need yet another structure if it's a constant!
-                        
-                    else: # It's basic data or a matrix: create an empty list
+
+                    elif groupname=='keydata' or groupname=='timedata' or groupname=='matrices': # It's basic data or a matrix: create an empty list
                         thispar = subparlist[parcount] # Get the name of this parameter, e.g. 'popsize'                    
                         data[name][thispar] = [] # Initialize to empty list
+    
+                    elif groupname=='constants': # It's a constant or a cost: create a structure
+                        thispar = subparlist[parcount][0] # Get the name of this parameter, e.g. 'trans'
+                        data[name][thispar] = struct() # Need yet another structure if it's a constant!
+                    
+                    else:
+                        raise Exception('Group name %s not recognized!' % groupname)
+                        
 
                 
-                if paramcategory=='': # The first column is blank: it's time for the data
+                if paramcategory == '': # The first column is blank: it's time for the data
                     subparam = sheetdata.cell_value(row, 1) # Get the name of a subparameter, e.g. 'FSW', population size for a given population
                     
-                    if not(subparam==''): # The subparameter name isn't blank, load something!
+                    if subparam != '': # The subparameter name isn't blank, load something!
                         if verbose >=3: print("      Parameter: subparam" % subparam)
                         
                         # It's meta-data, split into pieces
-                        if name=='meta': 
+                        if groupname=='meta': 
                             thesedata = sheetdata.row_values(row, start_colx=2, end_colx=6) # Data starts in 3rd column, finishes in 6th column
                             data[name][thispar].short.append(thesedata[0])
                             data[name][thispar].long.append(thesedata[1])
@@ -129,12 +170,12 @@ def loadspreadsheet(filename='example.xlsx',verbose=2):
                             thesedata = sheetdata.row_values(row, start_colx=2, end_colx=lastcol) # Data starts in 3rd column, finishes in 21st column
                             thesedata = map(lambda val: nan if val=='' else val, thesedata) # Replace blanks with nan
                             assumptiondata = sheetdata.cell_value(row, assumptioncol)
-                            if not(assumptiondata==''): thesedata = [assumptiondata] # Replace the (presumably blank) data if a non-blank assumption has been entered
+                            if assumptiondata != '': thesedata = [assumptiondata] # Replace the (presumably blank) data if a non-blank assumption has been entered
                             data[name][thispar].append(thesedata) # Store data
                             
                             # Load program data -- only exists for basic data
                             programname = str(sheetdata.cell_value(row, programcols[0])) # Convert to plain string since otherwise can't be used as a dict key
-                            if not(programname==''): # Not blank: a program exists!
+                            if programname != '': # Not blank: a program exists!
                                 if not(programs.has_key(programname)): programs[programname] = [] # Create new list if none exists
                                 outcomes = sheetdata.row_values(row, start_colx=programcols[1], end_colx=programcols[2]) # Get outcome data
                                 programs[programname].append([[name,thispar], outcomes]) # Append to program
