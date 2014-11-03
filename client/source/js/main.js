35320e06fe3dc5889b7504e43216927897f09ff1
(new name) <(new email)>
2014-11-03 23:34:40 +0200

    feat(active project): displays information about active project at the home page and in the footer

--- client/source/js/main.js
+++ client/source/js/main.js
@@ -3,14 +3,22 @@
  * NOTE: the ng-app attribute should not be on the index.html when using ng.bootstrap
  */
 define([
-  'require',
   'angular',
   './app'
-], function (require, angular) {
+], function (angular) {
   'use strict';
 
   // You can place operations that need to initialize prior to app start here
   // using the `run` function on the top-level module
-
-  angular.bootstrap(document, ['app']);
+  angular.injector(['ng', 'app.resources.project', 'app.active-project'])
+    .invoke(['Project', 'activeProject', function (Project, activeProject) {
+      Project
+        .getCurrent(function (project) {
+          activeProject.name = project.project;
+        })
+        .$promise
+        .finally(function () {
+          angular.bootstrap(document, ['app']);
+        });
+    }]);
 });
