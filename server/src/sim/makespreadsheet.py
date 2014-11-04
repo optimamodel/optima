3c4cbd28b424d940d0e4b8cb9e09f49e74d9cb66
Anna Nachesa <anachesa@gmail.com>
2014-11-04 02:58:24 +0100

    wired up template generation

--- server/src/sim/makespreadsheet.py
+++ server/src/sim/makespreadsheet.py
@@ -3,26 +3,22 @@ MAKESPREADSHEET
 
 Version: 2014oct28
 """
-
+import os
+from loadspreadsheet import loadspreadsheet
 from optima_workbook import OptimaWorkbook
 
-def generate_spreadsheet(path, npops=6, nprogs=8, datastart=2000, dataend=2015, verbose=2):
+def makespreadsheet(path, pops, progs, datastart=2000, dataend=2015, \
+  econ_datastart=2015, econ_dataend=2030, verbose=2):
+
   if verbose >=1: 
     print("""Generating spreadsheet with parameters:
-             path = %s, npops = %s, nprogs = %s, datastart = %s, dataend = %s""" % \
-             (path, npops, nprogs, datastart, dataend))
+             path = %s, pops = %s, progs = %s, datastart = %s, dataend = %s, 
+             econ_datastart = %s, econ_dataend = %s""" % \
+             (path, pops, progs, datastart, dataend, econ_datastart, econ_dataend))
     (folder, name) = os.path.split(path)
-    book = OptimaWorkbook(name, npops, nprogs, datastart, dataend)
+    book = OptimaWorkbook(name, pops, progs, datastart, dataend)
     book.create(path)
-
-
-def makespreadsheet(spreadsheetname, npops=6, nprogs=8, datastart=2000, dataend=2015, verbose=2):
-    if verbose>=1: print('Making spreadsheet...')
     
-    # Make an Excel template and then prompt the user to save it
-    print('      ***Need to actually make spreadsheet here!***')
-    
-    spreadsheetname = projectname+'.xlsx'
-    loadspreadsheet(spreadsheetname)
-    if verbose>=2: print('  ...done making spreadsheet.')
-    return spreadsheetname
+    loadspreadsheet(path)
+    if verbose>=2: print('  ...done making spreadsheet %s.' % path)
+    return path
