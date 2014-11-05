643a52ea7ec188873f4ec6464d8b32bc08c9d41e
cliffckerr <cliff@thekerrlab.com>
2014-11-05 15:33:54 +0400

    almost done with epiresults

--- server/src/sim/viewresults.py
+++ server/src/sim/viewresults.py
@@ -1,9 +1,40 @@
-def viewresults(D, whichgraphs={'prevpop':1, 'prevtot':1, 'incipop':1, 'prevtot':1, 'dalypop':1, 'dalytot':1, 'deathpop':1, 'deathtot':1}, startyear=2000, endyear=2015, verbose=2):
+def viewresults(D, whichgraphs={'prev':1, 'inci':1, 'daly':1, 'death':1, 'pops':1, 'tot':1}, startyear=2000, endyear=2015, verbose=2):
     """
     Generate all outputs required for the model, including prevalence, incidence,
     deaths, etc.
     Version: 2014nov05
     """
+    
+    from matplotlib.pylab import figure, plot, hold, scatter, xlabel, ylabel, xlim, ylim, legend, title
+    
+    for graph in ['prev', 'inci', 'daly', 'death']:
+        if whichgraphs[graph]:
+            
+            
+            if whichgraphs['tot']:
+                figure()
+                plot(D.O.tvec, D.O[graph].tot)
+                
+                xlabel(D.O[graph].xlabel)
+                ylabel(D.O[graph].ylabel)
+                xlim(xmin=startyear, xmax=endyear)
+                ylim(ymin=0)
+                
+            if whichgraphs['pops']:
+                for p in range(D.G.npops):
+                    figure()
+                    hold(True)
+                    plot(D.O.tvec, D.O[graph].pops[p,:], c=D.O.colorm)
+                    scatter(D.O.xdata, D.O[graph].ydata[p,:], c=D.O.colord);
+                    
+                    title(D.O.poplabels[p])
+                    legend(('Model','Data'))
+                    xlabel(D.O[graph].xlabel)
+                    ylabel(D.O[graph].ylabel)
+                    xlim(xmin=startyear, xmax=endyear)
+                    ylim(ymin=0)
+            
+        
     
     # Generate data for scatter and line plots
 #    nplots = 6
