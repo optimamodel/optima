6634a8b8361835101ba02422b654710adbbdbca4
(new name) <(new email)>
2014-11-05 17:54:19 +0400

    created gui ccocs

---
+++ server/src/sim/makeccocs.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  5 17:12:17 2014
+
+@author: robynstuart
+"""
+
+def makeccocs
+    ###############################################################################
+    ## Setup
+    ###############################################################################
+
+    D
+    
+    
+    ## Get inputs from spreadsheet
+    minbehaviour # Assumptions of behaviour at zero coverage
+    maxbehaviour # Assumptions of behaviour at maximal coverage
+
+    ## Get user inputs from gui (?)
+    saturation # Maximum coverage assumption
+    growthrate # Growth rate of cost-coverage curve
+    
+    
+    
+    
+    
+    
+    
