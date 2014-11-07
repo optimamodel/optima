451eccbc3808cb25df59a702b2182115cadd8d89
Khurram Ali <khurram.ali@riksof.com>
2014-11-07 19:06:01 +0500

    added project model

--- server/src/optima/models.py
+++ server/src/optima/models.py
@@ -1,5 +1,4 @@
 from api import db
-from sqlalchemy import Column, Integer, String
 
 class UserDb(db.Model):
     __tablename__ = 'users'
@@ -7,6 +6,25 @@ class UserDb(db.Model):
     name = db.Column(db.String(60))
     email = db.Column(db.String(200))
     openid = db.Column(db.String(200))
+    projects = db.relationship('ProjectDb', backref='user',
+                                lazy='dynamic')
+
+    def __init__(self, name, email, openid):
+        self.name = name
+        self.email = email
+        self.openid = openid
+
+class ProjectDb(db.Model)
+    __tablename__ = 'projects'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    datastart = db.Column(db.DateTime)
+    dataend = db.Column(db.DateTime)
+    econ_datastart = db.Column(db.DateTime)
+    econ_dataend = db.Column(db.DateTime)
+    programs = db.Column(db.String(60))
+    populations = db.Column(db.String(60))
 
     def __init__(self, name, email, openid):
         self.name = name
