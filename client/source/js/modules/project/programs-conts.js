97cf7e49a5ed33a4d58f62684d5dd4ec69039346
Anna Nachesa <anachesa@gmail.com>
2014-11-07 01:44:23 +0100

    added more programs

--- client/source/js/modules/project/programs-conts.js
+++ client/source/js/modules/project/programs-conts.js
@@ -25,7 +25,237 @@ define(['./module'], function (module) {
             { name: 'Uptake of other programs and services', active: true },
             { name: 'Linkages to biomedical services', active: false }
           ]
-        }
+        },
+        {
+          name: '01.03 Voluntary counselling and testing (VCT)',
+          acronym: '01.03',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.04 Risk-reduction for vulnerable and accessible populations',
+          acronym: '01.04',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.05 Prevention – youth in school',
+          acronym: '01.05',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.06 Prevention – youth out-of-school',
+          acronym: '01.06',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.07 Prevention of HIV transmission aimed at people living with HIV (PLHIV)',
+          acronym: '01.07',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.08 Prevention programmes for sex workers and their clients',
+          acronym: '01.08',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.09 Programmes for men who have sex with men (MSM)',
+          acronym: '01.09',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.10 Harm-reduction programmes for people who inject drugs (PWID)',
+          acronym: '01.10',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.11 Prevention programmes in the workplace',
+          acronym: '01.11',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.12 Condom social marketing',
+          acronym: '01.12',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.13 Public and commercial sector male condom provision',
+          acronym: '01.13',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.14 Public and commercial sector female condom provision',
+          acronym: '01.14',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.15 Microbicides',
+          acronym: '01.15',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.16 Prevention, diagnosis and treatment of sexually transmitted infections (STI)',
+          acronym: '01.16',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.17 Prevention of mother-to-child transmission (PMTCT)',
+          acronym: '01.17',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.18 Male circumcision',
+          acronym: '01.18',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.19 Blood safety',
+          acronym: '01.19',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.20 Safe medical injections',
+          acronym: '01.20',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.21 Universal precautions',
+          acronym: '01.21',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },
+        {
+          name: '01.22 Post-exposure prophylaxis (PEP)',
+          acronym: '01.22',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },        
+        {
+          name: 'Pre-Exposure prophylaxis (PrEP)',
+          acronym: 'PrEP',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },        
+        {
+          name: '01.98 Prevention activities not disaggregated by intervention',
+          acronym: '01.98',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        },        
+        {
+          name: '01.99 Prevention activities n.e.c.',
+          acronym: '01.99',
+          indicators: [
+            { name: 'Relative increase in condom use and testing in general populations', active: false },
+            { name: 'VMMC uptake and ART adherence', active: true },
+            { name: 'Uptake of other programs and services', active: true },
+            { name: 'Linkages to biomedical services', active: false }
+          ]
+        }        
       ]
     },
     {
