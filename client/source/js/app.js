da92dba522a88da44b469de5a9d6a4b17f561a53
(new name) <(new email)>
2014-11-11 23:04:40 +0200

    feat(auth): introduces authentication functionality: - if user is not logged in he is allowed to public states only (see app.js isStatePublic()) - http interceptor is set up to all api calls to logout user if api returns 401 (it is to prevent the case when user got logged out by the back-end, but did not reload page in browser) - formInputValidate directive added to allow nicer forms validation in UI - savedLoginForm directive takes care of the case when login form is being prefilled by a browser - exit button in a footer is a logout now - footer is different for logged in and anonymous users

--- client/source/js/app.js
+++ client/source/js/app.js
@@ -6,19 +6,22 @@ define([
   'ui.router',
   './config',
   './modules/about/index',
+  './modules/auth/index',
   './modules/analyses/index',
   './modules/common/active-project-service',
+  './modules/common/form-input-validate-directive',
   './modules/common/save-graph-as-directive',
   './modules/d3-charts/index',
+  './modules/forecasts/index',
   './modules/graphs/index',
   './modules/help/index',
   './modules/home/index',
+  './modules/import-export/index',
   './modules/model/index',
-  './modules/forecasts/index',
   './modules/optimization/index',
   './modules/playground/index',
   './modules/project/index',
-  './modules/import-export/index',
+  './modules/user-manager/index',
   './modules/results/index',
   './modules/ui/index'
 ], function (angular) {
@@ -27,27 +30,81 @@ define([
   return angular.module('app', [
     'angularFileUpload',
     'app.about',
+    'app.auth',
     'app.active-project',
     'app.analyses',
+    'app.common.form-input-validate',
     'app.constants',
     'app.d3-charts',
+    'app.forecasts',
     'app.graphs',
     'app.help',
     'app.home',
     'app.import-export',
     'app.model',
-    'app.forecasts',
     'app.optimization',
     'app.playground',
     'app.project',
     'app.results',
     'app.save-graph-as',
     'app.ui',
+    'app.user-manager',
     'nvd3',
     'ui.bootstrap',
     'ui.router'
-  ]).config(function ($urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
-  });
+  ])
+
+    .config(function ($httpProvider) {
+      var logoutUserOn401 = ['$q', function ($q) {
+        var success = function (response) {
+          return response;
+        };
+
+        var error = function (response) {
+          if (response.status === 401 && response.config.url !== '/api/users/current') {
+            // Redirect them back to login page
+            location.href = '/#/login';
+
+            return $q.reject(response);
+          } else {
+            if (response.data.message) {
+              alert(response.data.message);
+            }
+
+            return $q.reject(response);
+          }
+        };
+
+        return function (promise) {
+          return promise.then(success, error);
+        };
+      }];
+
+      $httpProvider.responseInterceptors.push(logoutUserOn401);
+    })
+
+    .config(function ($urlRouterProvider) {
+      $urlRouterProvider.otherwise('/');
+    })
+
+    .run(function ($rootScope, $state, UserManager) {
+      if (window.user) {
+        UserManager.set(window.user);
+        delete window.user;
+      }
+
+      var isStatePublic = function (stateName) {
+        var publicStates = ['about', 'contact', 'help', 'login', 'register'];
+
+        return publicStates.indexOf(stateName) !== -1;
+      };
+
+      $rootScope.$on('$stateChangeStart', function (event, to) {
+        if (!UserManager.isLoggedIn && !isStatePublic(to.name)) {
+          event.preventDefault();
+          $state.go('login');
+        }
+      });
+    });
 
 });
