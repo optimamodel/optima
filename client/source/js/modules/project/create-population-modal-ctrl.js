2e471293f8d82dff55101f71811a7b63b721d43d
Dmitry Evseev <kradmiy@gmail.com>
2014-11-13 20:57:08 +0200

    style(app): cleans up code formatting

--- client/source/js/modules/project/create-population-modal-ctrl.js
+++ client/source/js/modules/project/create-population-modal-ctrl.js
@@ -9,7 +9,7 @@ define(['./module'], function (module) {
 
     $scope.submit = function (form) {
       if (form.$invalid) {
-        alert('Your valiant attempts to fill in the form correctly have failed; please try again');
+        alert('Your valiant attempts to fill in the form correctly have failed. Please try again');
       }
 
       $modalInstance.close($scope.population);
