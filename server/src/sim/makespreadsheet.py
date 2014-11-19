07b0e9f88cb111db378a6a81ea65d6790f097b31
(new name) <(new email)>
2014-11-19 10:23:52 -0500

    added population metadata

--- server/src/sim/makespreadsheet.py
+++ server/src/sim/makespreadsheet.py
@@ -1,25 +1,20 @@
 """
 MAKESPREADSHEET
 
-Version: 2014oct28
+Version: 2014nov19
 """
-import os
-from loadspreadsheet import loadspreadsheet
 from optima_workbook import OptimaWorkbook
 from dataio import templatepath
+from printv import printv
 
-def makespreadsheet(name, pops, progs, datastart=2000, dataend=2015, \
-  econ_datastart=2015, econ_dataend=2030, verbose=2):
+def makespreadsheet(name, pops, progs, datastart=2000, dataend=2015, econ_datastart=2015, econ_dataend=2030, verbose=2):
 
-  if verbose >=1: 
-    print("""Generating spreadsheet with parameters:
+    printv("""Generating spreadsheet with parameters:
              name = %s, pops = %s, progs = %s, datastart = %s, dataend = %s, 
-             econ_datastart = %s, econ_dataend = %s""" % \
-             (name, pops, progs, datastart, dataend, econ_datastart, econ_dataend))
+             econ_datastart = %s, econ_dataend = %s""" % (name, pops, progs, datastart, dataend, econ_datastart, econ_dataend), 1, verbose)
     path = templatepath(name)
     book = OptimaWorkbook(name, pops, progs, datastart, dataend, econ_datastart, econ_dataend)
     book.create(path)
     
-    loadspreadsheet(path)
-    if verbose>=2: print('  ...done making spreadsheet %s.' % path)
+    printv('  ...done making spreadsheet %s.' % path, 2, verbose)
     return path
