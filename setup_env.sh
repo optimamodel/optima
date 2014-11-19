c1c1b7237b468ef00f60b2327ce6188d219dba31
Khurram Ali <khurram.ali@riksof.com>
2014-11-19 15:47:17 +0500

    Helper to setup environments

---
+++ setup_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=AKIAIUZH7VCXKFMUX4YA
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
+export AWS_WORKER_QUEUE_PREFIX=optima
+
+# To be set only during development
+export AWS_WORKER_PORT=5001
