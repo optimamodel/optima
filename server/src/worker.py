278de4af74a2ad0c4cf10120f42e571b82775932
Khurram Ali <khurram.ali@riksof.com>
2014-11-19 19:02:19 +0500

    Fixes

--- server/src/worker.py
+++ server/src/worker.py
@@ -30,7 +30,11 @@ conn = boto.sqs.connect_to_region( region )
 @application.route('/execute', methods=['POST'])
 def optima_compute():
     application.logger.info("Inside the request")
-    application.logger.info(request.json)
+    application.logger.info(request)
+    try:
+        application.logger.info(request.json)
+    except Exception as ex:
+        application.logger.error('Error processing message: %s' % request.json)
     
     response = None
     if request.json is None:
