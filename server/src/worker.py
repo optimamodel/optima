5b1477a733174f6732645995780657b5e99f57f9
Khurram Ali <khurram.ali@riksof.com>
2014-11-19 20:00:08 +0500

    Change url

--- server/src/worker.py
+++ server/src/worker.py
@@ -27,7 +27,7 @@ if region is None:
 
 conn = boto.sqs.connect_to_region( region )
 
-@application.route('/execute', methods=['POST'])
+@application.route('/', methods=['POST'])
 def optima_compute():
     application.logger.info("Inside the request")
     
