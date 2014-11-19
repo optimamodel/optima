8a7f37c407ab8692a857153b1fe9366ce12df676
Khurram Ali <khurram.ali@riksof.com>
2014-11-19 15:55:25 +0500

    Update configurations

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-export AWS_ACCESS_KEY_ID=AKIAIUZH7VCXKFMUX4YA
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-east-1
-export AWS_WORKER_QUEUE_PREFIX=optima
+# EB tools
+EB=
+AWS_REPO_SETUP=
+
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=us-west-2
+export AWS_WORKER_QUEUE_PREFIX=awseb
 
 # To be set only during development
-export AWS_WORKER_PORT=5001
+export AWS_WORKER_PORT=
