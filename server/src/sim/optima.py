d996b1c93153a2a019b4a1b85981bd6fc379fe25
cliffckerr <cliff@thekerrlab.com>
2014-11-22 20:33:05 -0500

    it works with the new spreadsheet format

--- server/src/sim/optima.py
+++ server/src/sim/optima.py
@@ -10,7 +10,7 @@ This function does everything. The basic procedure is as follows:
 3. setupmodel -- reconcile partnerships and calculate parameters to go into the model
 4. model -- actually run the model
 
-Version: 2014nov19 by cliffk
+Version: 2014nov22 by cliffk
 """
 print('WELCOME TO OPTIMA')
 import shutil
