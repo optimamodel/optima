d66bd0328e40872d3ffe24104a7ac300f81a3d32
cliffckerr <cliff@thekerrlab.com>
2014-11-23 12:04:17 -0500

    actually removed spreadsheet overwite; redoing logic of options etc.

--- server/src/sim/runsimulation.py
+++ server/src/sim/runsimulation.py
@@ -1,39 +1,55 @@
+def uncertainties(Sarray, quantiles):
+    """
+    Take an array of simulation results and calculate the corresponding uncertainty
+    intervals defined by the list quantiles.
+    
+    Version: 2014nov23 by cliffk
+    """
+    
+    S = Sarray
+    
+    return S
+
+
+
+
+
 def runsimulation(D, startyear=2000, endyear=2030, verbose=2):
     """
     RUNSIMULATION
-    View data and model estimates
+    Calculate model estimates
     
-    Version: 2014nov05 by cliffk
+    Version: 2014nov23 by cliffk
     """
-
     
-    
-    # Create options structure
-    from bunch import Bunch as struct
-    from matplotlib.pylab import arange
     from printv import printv
     printv('Running simulation...', 1, verbose)
     
-    options = struct()
-    options.startyear = startyear
-    options.endyear = endyear
-    options.dt = 0.1 # Timestep
-    options.nsims = 4 # Number of simulations to store for purposes of uncertainty
-    options.tvec = arange(options.startyear, options.endyear, options.dt) # Time vector
+    # update options structure
+    from setoptions import setoptions
+    D.opt = setoptions(D, startyear, endyear)
     
     # Convert data parameters to model parameters
     from makemodelpars import makemodelpars
-    D.M = makemodelpars(D.P, options, verbose=verbose)
+    D.M = makemodelpars(D.P, D.opt, verbose=verbose)
+    
+    # Create fitted parameters
+    from makefittedpars import makefittedpars
+    D.F = makefittedpars()
     
     # Run model
     from model import model
-    D.S = []
-    for s in range(options.nsims):
-        D.S = model(D.G, D.M, D.F, options, verbose=verbose)
+    Sarray = []
+    for s in range(D.opt.nsims): # TODO -- parallelize
+        tmpS = model(D.G, D.M, D.F[s], D.opt, verbose=verbose)
+        Sarray.append(tmpS)
+    
+    # Calculate statistics
+    D.S = uncertainties(Sarray, D.opt.quantiles)
+    
     
     # Save output
     from dataio import savedata
     savedata(D.projectfilename, D, verbose=verbose)
     printv('...done running simulation for project %s.' % D.projectfilename, 2, verbose)
     return D
-    # should frontend do anything with the model? 
