7a9e6ea97ad761816b010ce35d404617b5eeb562
cliffckerr <cliff@thekerrlab.com>
2014-11-24 11:44:50 -0500

    finished autofit api and added to optima.py

--- server/src/sim/optima.py
+++ server/src/sim/optima.py
@@ -8,8 +8,15 @@ This function does everything. The basic procedure is as follows:
 3. runsimulation -- run the simulation
 4. epiresults -- calculate the results
 5. viewresults -- display the results
+6. autofit -- automatically calibrate
+7. viewresults -- display the updated results
+...
+8. scenarios -- run some scenarios
+9. viewscenarios -- view the results of the scenarios
+10. optimize -- run an optimization
+11. viewoptimization -- view an optimization
 
-Version: 2014nov23 by cliffk
+Version: 2014nov24 by cliffk
 """
 
 
@@ -45,6 +52,13 @@ D = gatherplotdata(D, verbose=verbose)
 
 print('\n\n\n5. Viewing results...')
 from viewresults import viewresults
-viewresults(D.O, whichgraphs={'prev':[1,1], 'inci':[0,1], 'daly':[0,1], 'death':[0,1], 'dx':[0,1], 'tx1':[0,1], 'tx2':[0,1]}, onefig=True, verbose=verbose, show_wait=show_wait)
+viewresults(D.O, whichgraphs={'prev':[1,1], 'inci':[0,1], 'daly':[0,1], 'death':[0,1], 'dx':[0,1], 'tx1':[0,1], 'tx2':[0,1]}, startyear=2000, endyear=2015, onefig=True, verbose=verbose, show_wait=show_wait)
+
+print('\n\n\n6. Automatic calibration...')
+from autofit import autofit
+D = autofit(D, timelimit=5, startyear=2000, endyear=2015, verbose=verbose)
+
+print('\n\n\n7. Viewing results again...')
+viewresults(D.O)
 
 print('\n\n\nDONE.')
