465aaa4289e6a414b8d0912b5337c951b0796fb4
Dmitry Evseev <kradmiy@gmail.com>
2014-11-24 14:34:11 +0200

    feat(project/delete): allows to delete a project

--- client/source/js/modules/project/project-open-ctrl.js
+++ client/source/js/modules/project/project-open-ctrl.js
@@ -15,7 +15,22 @@ define(['./module', 'underscore'], function (module, _) {
           }
           activeProject.setValue(name);
         });
-    }
+    };
+
+    $scope.delete = function (name) {
+      $http.delete('/api/project/delete/' + name)
+        .success(function (response) {
+          if (response && response.status === 'NOK') {
+            alert(response.reason);
+            return;
+          }
+
+          $scope.projects.splice($scope.projects.indexOf(name), 1);
+        })
+        .error(function () {
+          alert('Could not remove the project')
+        });
+    };
   });
 
 });
