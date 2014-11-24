e3914df58d7f84abc1b898f77ea7922bd7306377
Dmitry Evseev <kradmiy@gmail.com>
2014-11-24 12:53:45 +0200

    improve(charts/line-scatter): customizes directive to use external options object, uses line-scatter and model/view page

--- client/source/js/modules/d3-charts/line-scatter-directive.js
+++ client/source/js/modules/d3-charts/line-scatter-directive.js
@@ -4,17 +4,21 @@ define(['./module'], function (module) {
   module.directive('lineScatterChart', function (d3Charts) {
     return {
       scope: {
-        data: '='
+        data: '=',
+        options: '='
       },
       link: function (scope, element) {
-        var dimensions = { width: 1000, height: 300 };
-        var margins = { top: 10, right: 10, bottom: 35, left: 60 };
-        var chartSize = {
-          width: dimensions.width - margins.left - margins.right,
-          height: dimensions.height - margins.top - margins.bottom
+        var dimensions = {
+          height: scope.options.height,
+          width: scope.options.width
         };
 
-        var svg = d3Charts.createSvg(element[0], dimensions, margins);
+        var chartSize = {
+          width: scope.options.width - scope.options.margin.left - scope.options.margin.right,
+          height: scope.options.height - scope.options.margin.top - scope.options.margin.bottom
+        };
+
+        var svg = d3Charts.createSvg(element[0], dimensions, scope.options.margin);
 
         // Define svg groups
         var chartGroup = svg.append("g").attr("class", "chart_group");
@@ -23,18 +27,20 @@ define(['./module'], function (module) {
         var lineChartInstance = new d3Charts.LineChart(chartGroup, '', chartSize, 100);
         var scatterChartInstance = new d3Charts.ScatterChart(chartGroup, '', chartSize, 100);
 
-        scope.data.$promise.then(function(d){
-          var scatterErrorData = d['scatter-error'];
-          var lineData = d.line;
+        var d = scope.data;
+        var lineData = d.line;
 
-          var calculatedLineScales = lineChartInstance.scales(lineData);
-          scatterChartInstance.scales(lineData);
+        var calculatedLineScales = lineChartInstance.scales(lineData);
+        scatterChartInstance.scales(lineData);
 
-          d3Charts.drawAxes(calculatedLineScales, { x: 'Label X', y: 'Label Y' }, axesGroup, chartSize, margins);
-          lineChartInstance.draw(lineData);
-          scatterChartInstance.draw(scatterErrorData);
-        });
-
+        d3Charts.drawAxes(
+          calculatedLineScales,
+          scope.options,
+          axesGroup,
+          chartSize
+        );
+        lineChartInstance.draw(lineData);
+        scatterChartInstance.draw(d['scatter-error']);
       }
     }
   });
