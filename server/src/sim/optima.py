3c5927d0869e45f65ee45426e25d25c39aab9d5f
(new name) <(new email)>
2014-11-25 06:01:19 -0500

    finished specifying api for scenarios

--- server/src/sim/optima.py
+++ server/src/sim/optima.py
@@ -47,16 +47,16 @@ print('\n\n\n3. Running simulation...')
 from runsimulation import runsimulation
 D = runsimulation(D, startyear=2000, endyear=2015, verbose=verbose)
 
-#print('\n\n\n4. Viewing results...')
-#from viewresults import viewepiresults
-#viewepiresults(D.plot.E, whichgraphs={'prev':[1,1], 'inci':[0,1], 'daly':[0,1], 'death':[0,1], 'dx':[0,1], 'tx1':[0,1], 'tx2':[0,1]}, startyear=2000, endyear=2015, onefig=True, verbose=verbose, show_wait=show_wait)
-#
-#print('\n\n\n5. Automatic calibration...')
-#from autofit import autofit
-#D = autofit(D, timelimit=timelimit, startyear=2000, endyear=2015, verbose=verbose)
-#
-#print('\n\n\n6. Viewing results again...')
-#viewepiresults(D.plot.E)
+print('\n\n\n4. Viewing results...')
+from viewresults import viewepiresults
+viewepiresults(D.plot.E, whichgraphs={'prev':[1,1], 'inci':[0,1], 'daly':[0,1], 'death':[0,1], 'dx':[0,1], 'tx1':[0,1], 'tx2':[0,1]}, startyear=2000, endyear=2015, onefig=True, verbose=verbose, show_wait=show_wait)
+
+print('\n\n\n5. Automatic calibration...')
+from autofit import autofit
+D = autofit(D, timelimit=timelimit, startyear=2000, endyear=2015, verbose=verbose)
+
+print('\n\n\n6. Viewing results again...')
+viewepiresults(D.plot.E)
 
 print('\n\n\n7. Running scenarios...')
 from scenarios import runscenarios
@@ -66,13 +66,13 @@ print('\n\n\n8. Viewing scenarios...')
 from viewresults import viewmodels
 viewmodels(D.plot.scens)
 
-#print('\n\n\n9. Running optimization...')
-#from optimize import optimize
-#D = optimize(D, objectives=None, constraints=None, timelimit=timelimit, verbose=2)
-#
-#print('\n\n\n10. Viewing optimization...')
-#from viewresults import viewallocpies
-#viewallocpies(D.plot.OA)
-#viewmodels(D.plot.OM)
+print('\n\n\n9. Running optimization...')
+from optimize import optimize
+D = optimize(D, objectives=None, constraints=None, timelimit=timelimit, verbose=2)
+
+print('\n\n\n10. Viewing optimization...')
+from viewresults import viewallocpies
+viewallocpies(D.plot.OA)
+viewmodels(D.plot.OM)
 
 print('\n\n\nDONE.')
