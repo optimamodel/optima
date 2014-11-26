62cffaeac6ad09be40750a1cc94b233c3ff1cf94
(new name) <(new email)>
2014-11-26 13:08:20 -0200

    test(project): add test for retrieving a project

--- server/src/tests/project_test.py
+++ server/src/tests/project_test.py
@@ -3,6 +3,7 @@
 from optima_test_base import OptimaTestCase
 import unittest
 import json
+from optima.dbmodels import ProjectDb
 
 class ProjectTestCase(OptimaTestCase):
     """
@@ -10,18 +11,20 @@ class ProjectTestCase(OptimaTestCase):
 
     """
 
-
     def setUp(self):
         super(ProjectTestCase, self).setUp()
         response = self.create_user()
         response = self.login()
 
-    def tearDown(self):
-        self.logout()
-
     def test_create_project(self):
-        response = self.client.post('/api/project/create/test', data = '{}', follow_redirects=True)
-        assert(response.status_code==200)
+        response = self.client.post('/api/project/create/test', data = '{}')
+        self.assertEqual(response.status_code, 200)
+
+    def test_retrieve_project_info_fails(self):
+        headers = [('project', 'test')]
+        response = self.client.get('/api/project/info', headers=headers)
+        self.assertEqual(response.status_code, 500)
+        self.assertEqual(json.loads(response.data), { "status": "NOK" })
 
 if __name__ == '__main__':
     unittest.main()
