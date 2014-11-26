be516066246f294eebd937eb5015ed229f142bdf
(new name) <(new email)>
2014-11-26 23:34:51 +0200

    feat(cost coverage): draws graphs for plotdata and plotdata_cc

--- client/source/js/modules/model/model-view-calibration-ctrl.js
+++ client/source/js/modules/model/model-view-calibration-ctrl.js
@@ -28,31 +28,106 @@ define(['./module', 'underscore'], function (module, _) {
     $scope.behaviorWithMin = 0.7;
     $scope.behaviorWithMax = 0.9;
 
-    var linescatteroptions = {
-      height: 400,
-      width: 800,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 100
-      },
-      xAxis: {
-        axisLabel: 'Year',
-        tickFormat: function (d) {
-          return d3.format('s')(d);
+    var getLineScatterOptions = function (options, xLabel, yLabel) {
+      var defaults = {
+        height: 400,
+        width: 800,
+        margin: {
+          top: 20,
+          right: 20,
+          bottom: 60,
+          left: 100
+        },
+        xAxis: {
+          axisLabel: xLabel || 'X',
+          tickFormat: function (d) {
+            return d3.format('s')(d);
+          }
+        },
+        yAxis: {
+          axisLabel: yLabel || 'Y',
+          tickFormat: function (d) {
+            return d3.format(',.2f')(d);
+          }
         }
-      },
-      yAxis: {
-        axisLabel: 'Prevalence (%)',
-        tickFormat: function (d) {
-          return d3.format(',.2f')(d);
-        }
-      }
+      };
+
+      return _(angular.copy(defaults)).extend(options);
+    };
+
+    $scope.graphs = {
+      plotdata: [],
+      plotdata_cc: [],
+      plotdata_co: []
     };
 
     /* Methods
      ========= */
+
+    var prepareGraph = function (data, type) {
+      var graph;
+
+      if (type === 'plotdata_cc') {
+        graph = {
+          options: getLineScatterOptions({}, data.xlabel, data.ylabel),
+          data: {
+            // there is a single line for that type
+            lines: [[]],
+            scatter: []
+          }
+        };
+
+        _(data.xlinedata).each(function (x, index) {
+          var y = data.ylinedata;
+          graph.data.lines[0].push([x, y[index]]);
+        });
+
+        _(data.xscatterdata).each(function (x, index) {
+          var y = data.yscatterdata;
+
+          if (y[index]) {
+            graph.data.scatter.push([x, y[index]]);
+          }
+        });
+
+        $scope.graphs[type] = graph;
+
+      } else if (type === 'plotdata') {
+
+        _(data).each(function (graphData) {
+          var graph = {
+            options: getLineScatterOptions({}, graphData.xlabel, graphData.ylabel),
+            data: {
+              lines: [],
+              scatter: []
+            }
+          };
+
+          var numOfLines = graphData.ylinedata.length;
+
+          _(graphData.xlinedata).each(function (x, index) {
+            var y = graphData.ylinedata;
+            for (var i = 0; i < numOfLines; i++) {
+              if (!graph.data.lines[i]) {
+                graph.data.lines[i] = [];
+              }
+
+              graph.data.lines[i].push([x, y[i][index]]);
+            }
+          });
+
+          _(graphData.xscatterdata).each(function (x, index) {
+            var y = graphData.yscatterdata;
+
+            if (y[index]) {
+              graph.data.scatter.push([x, y[index]]);
+            }
+          });
+
+          $scope.graphs[type].push(graph);
+        });
+      }
+    };
 
     $scope.generateCurves = function () {
       $http.post('/api/model/costcoverage', {
@@ -73,33 +148,8 @@ define(['./module', 'underscore'], function (module, _) {
         if (response.status === 'OK') {
           $scope.apiData = response;
 
-          $scope.graph = {
-            options: linescatteroptions,
-            data: {
-              lines: [],
-              scatter: []
-            }
-          };
-
-          var numOfLines = response.plotdata[0].ylinedata.length;
-
-          _(response.plotdata[0].xlinedata).each(function (x, index) {
-            var y = response.plotdata[0].ylinedata;
-            for (var i = 0; i < numOfLines; i++) {
-              if (!$scope.graph.data.lines[i]) {
-                $scope.graph.data.lines[i] = [];
-              }
-
-              $scope.graph.data.lines[i].push([x, y[i][index]]);
-            }
-          });
-
-          _(response.plotdata[0].xscatterdata).each(function (x, index) {
-            var y = response.plotdata[0].yscatterdata;
-
-            if (y[index]) {
-              $scope.graph.data.scatter.push([x, y[index]]);
-            }
+          _(['plotdata', 'plotdata_cc', 'plotdata_co']).each(function (prop) {
+            prepareGraph(response[prop], prop);
           });
         } else {
           alert(response.exception);
