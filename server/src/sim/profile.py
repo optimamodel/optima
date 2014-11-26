db889ac253b8ed8f89fb370eb0204c7e2d8524d0
cliffckerr <cliff@thekerrlab.com>
2014-11-26 11:06:45 -0500

    added profiler for model.py

---
+++ server/src/sim/profile.py
@@ -0,0 +1,43 @@
+"""
+Profile model.py -- see https://zapier.com/engineering/profiling-python-boss/
+
+Version: 2014nov26 by cliffk
+"""
+
+print('Profiling...')
+
+from line_profiler import LineProfiler
+
+def do_profile(follow=[]):
+  def inner(func):
+      def profiled_func(*args, **kwargs):
+          try:
+              profiler = LineProfiler()
+              profiler.add_function(func)
+              for f in follow:
+                  profiler.add_function(f)
+              profiler.enable_by_count()
+              return func(*args, **kwargs)
+          finally:
+              profiler.print_stats()
+      return profiled_func
+  return inner
+
+from dataio import loaddata
+D = loaddata('/tmp/projects/example.prj')
+
+from makemodelpars import makemodelpars
+D.M = makemodelpars(D.P, D.opt)
+
+from model import model
+
+
+@do_profile(follow=[model])
+def runmodel():
+    S = model(D.G, D.M, D.F[0], D.opt, verbose=0)
+    return S
+
+result = runmodel()
+
+
+print('Done.')
