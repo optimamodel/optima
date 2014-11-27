ee563f98f40ad8cc6df23336b7a8c5cfe1292da7
(new name) <(new email)>
2014-11-27 15:24:18 -0500

    doesnt crash but no difference between scenarios

--- server/src/sim/scenarios.py
+++ server/src/sim/scenarios.py
@@ -1,17 +1,17 @@
 ## Imports
 from bunch import Bunch as struct
 from copy import deepcopy
+from numpy import linspace, ndim
 
 def runscenarios(D, scenariolist=None, verbose=2):
     """
-    Allocation optimization code:
-        D is the project data structure
-        objectives is a dictionary defining the objectives of the optimization
-        constraints is a dictionary defining the constraints on the optimization
-        timelimit is the maximum time in seconds to run optimization for
-        verbose determines how much information to print.
+    Run all the scenarios. The hard work is actually done by makescenarios, which
+    takes the list of scenarios and makes the required changes to the model parameters
+    M. Note that if a value is -1, it uses the current value. The code is a little
+    ugly since some of it is duplicated between getparvalues() (not used here, but
+    used for the GUI) and makescenarios().
         
-    Version: 2014nov24 by cliffk
+    Version: 2014nov27 by cliffk
     """
     
     from model import model
@@ -36,7 +36,7 @@ def runscenarios(D, scenariolist=None, verbose=2):
         D.scens[scen].scenario = deepcopy(scenariolist[scen]) # Copy scenario data
         D.scens[scen].label = scenariolist[scen].name # Copy name
         D.scens[scen].M = scenariopars[scen].M
-        D.scens[scen].S = model(D.G, D.scens[scen].M, D.F[scen], D.opt, verbose=verbose) # TODO don't change F
+        D.scens[scen].S = model(D.G, D.scens[scen].M, D.F[0], D.opt, verbose=verbose)
         printv('Scenario: %i/%i' % (scen+1, nscenarios), 2, verbose)
     
     # Calculate results
@@ -56,29 +56,29 @@ def runscenarios(D, scenariolist=None, verbose=2):
 
 def makescenarios(D, scenariolist, verbose=2):
     """ Convert a list of scenario parameters into a list of changes to model parameters """
-#    from numpy import find, linspace
- # TODO -- actually implement :)
     
     nscenarios = len(scenariolist)
     scenariopars = [struct() for s in range(nscenarios)]
-    
-
-    
     for scen in range(nscenarios):
         scenariopars[scen].name = scenariolist[scen].name
         scenariopars[scen].M = deepcopy(D.M) # Copy the whole thing...too hard to generate nested dictionaries on the fly
-#        for par in range(len(scenariolist[scen].pars)):
-#            thesepars = scenariolist[scen].pars[par] # Shorten name
-#            original = getnested(scenariopars[scen].M, thesepars.keys)[thesepars.pop]
-#            initialindex = find(abs(D.opt.tvec - thesepars.startyear)<1e-6)
-#            finalindex = find(abs(D.opt.tvec - thesepars.startyear)<1e-6)
-#            initialvalue = original[initialindex] if thesepars.startval == -1 else thesepars.startval 
-#            finalvalue = original[finalindex] if thesepars.endval == -1 else thesepars.endval
-#            npts = finalindex-initialindex+1
-#            newvalues = linspace(initialvalue, finalvalue, npts)
-#            original[initialvalue:finalvalue+1] = newvalues
-#            setnested(scenariopars[scen].M, thesepars.keys, original)
-            
+        for par in range(len(scenariolist[scen].pars)):
+            thesepars = scenariolist[scen].pars[par] # Shorten name
+            data = getnested(scenariopars[scen].M, thesepars.names)
+            if ndim(data)>1: newdata = data[thesepars.pops] # If it's more than one dimension, use population data too
+            else: newdata = data # If it's not, just use the whole thing
+            initialindex = find(D.opt.tvec, thesepars.startyear)
+            finalindex = find(D.opt.tvec, thesepars.endyear)
+            initialvalue = newdata[initialindex] if thesepars.startval == -1 else thesepars.startval 
+            finalvalue = newdata[finalindex] if thesepars.endval == -1 else thesepars.endval
+            npts = finalindex-initialindex+1
+            newvalues = linspace(initialvalue, finalvalue, npts)
+            newdata[initialindex:finalindex+1] = newvalues
+            if ndim(data)>1: data[thesepars.pops] = newdata # If it's multidimensional, only reset this one population
+            else: data = newdata # Otherwise, reset the whole thing
+            setnested(scenariopars[scen].M, thesepars.names, data)
+
+                
     return scenariopars
 
 
@@ -117,12 +117,12 @@ def defaultscenarios(D, verbose=2):
     scenariolist[1].pars[2].startval = -1
     scenariolist[1].pars[2].endval = 1
     # Client commercial condom use
-    scenariolist[1].pars[2].names = ['condom','com']
-    scenariolist[1].pars[2].pops = 5
-    scenariolist[1].pars[2].startyear = 2010
-    scenariolist[1].pars[2].endyear = 2015
-    scenariolist[1].pars[2].startval = -1
-    scenariolist[1].pars[2].endval = 1
+    scenariolist[1].pars[3].names = ['condom','com']
+    scenariolist[1].pars[3].pops = 5
+    scenariolist[1].pars[3].startyear = 2010
+    scenariolist[1].pars[3].endyear = 2015
+    scenariolist[1].pars[3].startval = -1
+    scenariolist[1].pars[3].endval = 1
     
     return scenariolist
 
@@ -137,15 +137,37 @@ def getparvalues(D, scenariopars):
     
     Version: 2014nov27 by cliffk
     """
-    from numpy import ndim, nonzero # Normal people call nonzero "find"
-    eps = 1e-6 # For floats vs. ints
+    from numpy import ndim
     original = getnested(D.M, scenariopars.names)
     if ndim(original)>1: original = original[scenariopars.pops] # If it's more than one dimension, use population data too
-    initialindex = nonzero(abs(D.opt.tvec - scenariopars.startyear)<eps)
-    finalindex = nonzero(abs(D.opt.tvec - scenariopars.startyear)<eps)
+    initialindex = find(D.opt.tvec, scenariopars.startyear)
+    finalindex = find(D.opt.tvec, scenariopars.endyear)
     startval = original[initialindex]
     endval = original[finalindex]
     return [startval, endval]
+
+
+def find(val1, val2=None, eps=1e-6):
+    """
+    Little function to find matches even if two things aren't eactly equal (eg. 
+    due to floats vs. ints). If one argument, find nonzero values. With two arguments,
+    check for equality using eps. Returns a tuple of arrays if val1 is multidimensional,
+    else returns an array.
+    
+    Examples:
+        find(rand(10)<0.5) # e.g. array([2, 4, 5, 9])
+        find([2,3,6,3], 6) # e.g. array([2])
+    
+    Version: 2014nov27 by cliffk
+    """
+    from numpy import nonzero, array
+    if val2==None: # Check for equality
+        output = nonzero(val1) # If not, just check the truth condition
+    else:
+        output = nonzero(abs(array(val1)-val2)<eps) # If absolute difference between the two values is less than a certain amount
+    if ndim(val1)==1: # Uni-dimensional
+        output = output[0] # Return an array rather than a tuple of arrays if one-dimensional
+    return output
 
 
 ## Parse nested dictionaries -- from http://stackoverflow.com/questions/14692690/access-python-nested-dictionary-items-via-a-list-of-keys
