5192b960aa485b0844fec378da91d50330f2cecd
cliffckerr <cliff@thekerrlab.com>
2014-11-27 05:15:01 -0500

    finished equilibration, seems to give reasonable results

--- server/src/sim/test_makeccocs.py
+++ server/src/sim/test_makeccocs.py
@@ -1,6 +1,8 @@
-""" Simple tests...and perplexing """
+""" Test makeccocs code and bring up graphs using default values """
 
 verbose = 4
+
+## Initialize simulation
 
 print('\n\n\n1. Making project...')
 from makeproject import makeproject
@@ -15,6 +17,11 @@ from runsimulation import runsimulation
 D = runsimulation(D, startyear=2000, endyear=2015, verbose=verbose)
 
 
+# Actually run makeccocs
 from makeccocs import plotallcurves
-#out = plotallcurves(D, makeplot=False, verbose=verbose)
-out = plotallcurves(D=D, coparams=[], ccparams=[0.9, 0.2, 800000.0, 7000000.0], progname='SBCC', makeplot = False)
+try:
+    out = plotallcurves(D=D, coparams=[], ccparams=[0.9, 0.2, 800000.0, 7000000.0], progname='SBCC', makeplot = True)
+    from matplotlib.pylab import show
+    show()
+except:
+    out = plotallcurves(D=D, coparams=[], ccparams=[0.9, 0.2, 800000.0, 7000000.0], progname='SBCC', makeplot = False)
