4972fe40f2389b7c6533600dfe37c8c98dafb47b
Nik Graf <nikgraf.com@gmail.com>
2014-11-27 17:45:56 -0200

    refactor(scenarios): cleanup

--- client/source/js/modules/analysis/analysis-scenarios-ctrl.js
+++ client/source/js/modules/analysis/analysis-scenarios-ctrl.js
@@ -3,55 +3,54 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
 
     module.controller('AnalysisScenariosController', function ($scope, $http, $modal, meta) {
 
-        $scope.projectParams = {
-            name: ''
-        };
+        var linesGraphOptions, linesGraphData, responseData;
 
-        $scope.scenarios = [
+        // initialize all necessary data for this controller
+        var initialize = function() {
+          $scope.scenarios = [
             { name: 'Conditions remain according to model calibration', active: true }
-        ];
+          ];
 
-        $scope.types = [
-          { id: 'prev', name: 'Prevalence', active: true, byPopulation: true, total: false },
-          { id: 'daly', name: 'DALYs', active: false, byPopulation: false, total: false },
-          { id: 'death', name: 'Deaths', active: false, byPopulation: false, total: false },
-          { id: 'inci', name: 'New infections', active: false, byPopulation: false, total: false },
-          { id: 'dx', name: 'Diagnoses', active: false, byPopulation: false, total: false },
-          { id: 'tx1', name: 'First-line treatment', active: false, byPopulation: false, total: false },
-          { id: 'tx2', name: 'Second-line treatment', active: false, byPopulation: false, total: false }
-        ];
+          $scope.types = [
+            { id: 'prev', name: 'Prevalence', active: true, byPopulation: true, total: false },
+            { id: 'daly', name: 'DALYs', active: false, byPopulation: false, total: false },
+            { id: 'death', name: 'Deaths', active: false, byPopulation: false, total: false },
+            { id: 'inci', name: 'New infections', active: false, byPopulation: false, total: false },
+            { id: 'dx', name: 'Diagnoses', active: false, byPopulation: false, total: false },
+            { id: 'tx1', name: 'First-line treatment', active: false, byPopulation: false, total: false },
+            { id: 'tx2', name: 'Second-line treatment', active: false, byPopulation: false, total: false }
+          ];
 
-        $scope.runScenariosOptions = {};
+          $scope.runScenariosOptions = {};
 
-        var linesGraphOptions = {
-          height: 250,
-          width: 400,
-          margin: {
-            top: 20,
-            right: 20,
-            bottom: 60,
-            left: 100
-          },
-          xAxis: {
-            axisLabel: 'Year',
-            tickFormat: function (d) {
-              return d3.format('d')(d);
+          linesGraphOptions = {
+            height: 250,
+            width: 400,
+            margin: {
+              top: 20,
+              right: 20,
+              bottom: 60,
+              left: 100
+            },
+            xAxis: {
+              axisLabel: 'Year',
+              tickFormat: function (d) {
+                return d3.format('d')(d);
+              }
+            },
+            yAxis: {
+              axisLabel: '',
+              tickFormat: function (d) {
+                return d3.format(',.2f')(d);
+              }
             }
-          },
-          yAxis: {
-            axisLabel: 'lala',
-            tickFormat: function (d) {
-              return d3.format(',.2f')(d);
-            }
-          }
-        };
+          };
 
-        var linesGraphData = {
-          lines: [],
-          scatter: []
+          linesGraphData = {
+            lines: [],
+            scatter: []
+          };
         };
-
-        var cashedData;
 
         /*
         * Returns an array containing arrays with [x, y] for d3 line data.
@@ -120,8 +119,8 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
         $scope.runScenarios = function () {
           $http.post('/api/analysis/scenarios/run', $scope.runScenariosOptions)
             .success(function(data) {
-              cashedData = data;
-              updateGraphs(cashedData);
+              responseData = data;
+              updateGraphs(responseData);
             });
         };
 
@@ -168,21 +167,12 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
                 });
         };
 
-
-
-        var toCleanArray = function (collection) {
-            return _(collection).chain()
-                .where({ active: true })
-                .map(function (item) {
-                    return _(item).omit(['active', '$$hashKey']);
-                })
-                .value();
-        };
-
         $scope.onGraphTypeChange = function (type) {
           type.active = type.total || type.byPopulation;
-          updateGraphs(cashedData);
+          updateGraphs(responseData);
         };
+
+        initialize();
 
     });
 
