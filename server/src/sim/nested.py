86bb01ed933e9b6f0cecc4c96b65d8d5ce3665d1
(new name) <(new email)>
2014-11-30 07:21:01 +1100

    Cleaned up iternested and makenested
    Replaced with cleaner versions. Makenested now also has the additional
    functionality of being able to insert a particular item into the nested
    dictionary if required, rather than requiring an additional call to
    setnested() afterwards

--- server/src/sim/nested.py
+++ server/src/sim/nested.py
@@ -42,75 +42,17 @@ Example 2:
 Version: 2014nov29 by cliffk
 """
 
-
-
 def getnested(nesteddict, keylist): 
     """ Get a value from a nested dictionary"""
     output = reduce(lambda d, k: d[k], keylist, nesteddict)
     return output
-
-
 
 def setnested(nesteddict, keylist, value): 
     """ Set a value in a nested dictionary """
     getnested(nesteddict, keylist[:-1])[keylist[-1]] = value
     return None # Modify nesteddict in place
 
-
-
-def makenested(nesteddict, keylist):
-    """ Make the nested keys listed in keylist into a nested dictionary """
-    currentlevel = nesteddict
-    for i,key in enumerate(keylist):
-        try:
-            currentlevel[key]
-        except:
-            if type(currentlevel) != dict: currentlevel = dict()
-            if i==len(keylist)-1:
-                currentlevel[key] = None
-            else:
-                currentlevel[key] = dict()
-        currentlevel = currentlevel[key] # Recursion!! Ahahahaha!
-    return None # Modify nesteddict in place
-
-
-
-def iternested(nesteddict):
-    """ Loop over all twigs of a nested dict -- spent a long time thining how to make this more elegant and couldn't :("""
-    output = []
-    
-    def trykeys(mightbedict):
-        """ Return a list of keys if a dict, otherwise return an empty list """
-        if type(mightbedict)==dict: return mightbedict.keys()
-        else: return []
-    
-    for k1 in trykeys(nesteddict):
-        dict1 = nesteddict[k1]
-        l2=False
-        for k2 in trykeys(dict1):
-            dict2 = dict1[k2]
-            l2=True
-            l3=False
-            for k3 in trykeys(dict2):
-                dict3 = dict2[k3]
-                l3=True
-                l4=False
-                for k4 in trykeys(dict3):
-                    dict4 = dict3[k4]
-                    l4=True
-                    l5=False
-                    for k5 in trykeys(dict4):
-                        l5 = True
-                        output.append([k1, k2, k3, k4, k5])
-                        print('WARNING, reached maximum level of nested dictionary recursion (5), sorry this code sucks so much')
-                    if not(l5): output.append([k1, k2, k3, k4])
-                if not(l4): output.append([k1, k2, k3])
-            if not(l3): output.append([k1, k2])
-        if not(l2): output.append([k1])
-    
-    return output
-
-def makenested2(nesteddict, keylist,item=None):
+def makenested(nesteddict, keylist,item=None):
     """ Insert item into nested dictionary, creating keys if required """
     currentlevel = nesteddict
     for i,key in enumerate(keylist[:-1]):
@@ -119,9 +61,7 @@ def makenested2(nesteddict, keylist,item=None):
     	currentlevel = currentlevel[key]
     currentlevel[keylist[-1]] = item
 
-    
-
-def iternested2(nesteddict,previous = []):
+def iternested(nesteddict,previous = []):
 	output = []
 	for k in nesteddict.items():
 		if isinstance(k[1],dict):
@@ -130,27 +70,3 @@ def iternested2(nesteddict,previous = []):
 			output.append(previous+[k[0]])
 	return output
 
-
-
-
-foo = {}
-makenested(foo, ['a','x'])
-makenested(foo, ['a','y'])
-makenested(foo, ['a','z'])
-makenested(foo, ['b','a','x'])
-makenested(foo, ['b','a','y'])
-foo['c'] = 1
-
-foo2 = {}
-makenested2(foo2, ['a','x'])
-makenested2(foo2, ['a','y'])
-makenested2(foo2, ['a','z'])
-makenested2(foo2, ['b','a','x'])
-makenested2(foo2, ['b','a','y'])
-foo2['c'] = 1
-
-print foo == foo2
-print(foo2)
-
-print(iternested(foo))
-print iternested2(foo)
