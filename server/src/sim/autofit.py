7442335ffd5e587a187fc4a5cc78868495dc04f6
cliffckerr <cliff@thekerrlab.com>
2014-11-30 03:39:42 -0500

    removed tx1 and tx2 from fitted parameters

--- server/src/sim/autofit.py
+++ server/src/sim/autofit.py
@@ -56,7 +56,7 @@ def dict2list(Fdict):
     be sure the keys are in the right order.
     """
     Flist = []
-    for key in ['init','force','dx','tx1','tx2']:
+    for key in ['init','force','dx']:
         this = Fdict[key]
         for i in range(len(this)):
             Flist.append(Fdict[key][i])
@@ -68,7 +68,7 @@ def list2dict(Forig, Flist):
     """
     from copy import deepcopy
     Fdict = deepcopy(Forig)
-    for key in ['init','force','dx','tx1','tx2']:
+    for key in ['init','force','dx']:
         for i in range(len(Fdict[key])):
             Fdict[key][i] = Flist.pop(0)
     return Fdict
