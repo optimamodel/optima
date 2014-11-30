019707779003a57e19a6f7e8bdf4c03fb4a1dbc9
(new name) <(new email)>
2014-11-29 20:58:21 -0500

    ah the goddamn minus sign

--- server/src/sim/runsimulation.py
+++ server/src/sim/runsimulation.py
@@ -9,6 +9,10 @@ def runsimulation(D, startyear=2000, endyear=2030, verbose=2):
     printv('Running simulation...', 1, verbose)
     dosave = False # Flag for whether or not to save
     
+    # Set options to update year range
+    from setoptions import setoptions
+    D.opt = setoptions(D.opt, startyear=startyear, endyear=endyear)
+    
     print('WARNING should add conditionals here')
     from makeccocs import makeallccocs
     D = makeallccocs(D, verbose=verbose)
