433658f385678efe79105034e59f2e9ffec38885
Robyn Stuart <robynstuart@Robyns-MacBook-Pro-2.local>
2014-11-30 23:14:16 +1100

    removed D.opt.nsims from places it should not be

--- server/src/sim/financialanalysis.py
+++ server/src/sim/financialanalysis.py
@@ -4,6 +4,7 @@ Created on Sat Nov 29 17:40:34 2014
 @author: robynstuart
 """
 import numpy as np
+from matplotlib.pylab import figure, plot, hold, xlabel, ylabel, title
 
 def financialanalysis(D, sim = D, yscale = 'abs'):
     '''
@@ -134,7 +135,13 @@ def financialanalysis(D, sim = D, yscale = 'abs'):
     plotdata['cumulhivcostsfuture']['xlabel'] = 'Year'
     plotdata['cumulhivcostsfuture']['ylabel'] = 'USD'
 
+    figure()
+    hold(True)
+    plot(plotdata['cumulhivcosts']['xlinedata'], plotdata['cumulhivcosts']['ylinedata'], lw = 2)
+    title(plotdata['cumulhivcosts']['title'])
+    xlabel('Year')
+    ylabel('USD')
+
     # Get financial commitments
     return sim, plotdata
     
-sim, plotdata = financialanalysis(D, sim = D, yscale = 'abs')
