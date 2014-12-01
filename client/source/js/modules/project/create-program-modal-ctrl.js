d68b96a45971ceff24c457252a2349ee8bff55bf
Nik Graf <nikgraf.com@gmail.com>
2014-12-01 02:54:40 -0200

    feature(project): added WIP parameters

--- client/source/js/modules/project/create-program-modal-ctrl.js
+++ client/source/js/modules/project/create-program-modal-ctrl.js
@@ -1,13 +1,42 @@
 define(['./module'], function (module) {
   'use strict';
 
-  module.controller('ProjectCreateProgramModalController', function ($scope, $modalInstance, program) {
+  module.controller('ProjectCreateProgramModalController', function ($scope,
+    $modalInstance, program, availableParameters, populations) {
 
     // Initializes relevant attributes
     var initialize = function () {
       $scope.isNew = !program.name;
+
       $scope.program = program;
       $scope.program.active = true;
+
+      $scope.availableParameters = availableParameters;
+      $scope.populations = populations;
+    };
+
+    /*
+    * Returns true of the two provided arrays are identic
+    */
+    var areEqualArrays = function(arrayOne, arrayTwo) {
+      return _(arrayOne).every(function(element, index) {
+        return element === arrayTwo[index];
+      });
+    };
+
+    /*
+    * Finds a scenario paramter based on the names
+    */
+    var findParameters = function(parameters, keys) {
+      return _(parameters).find(function(parameterEntry) {
+        return areEqualArrays(parameterEntry.keys, keys);
+      });
+    };
+
+    $scope.addParameter = function() {
+      var entry = {};
+      $scope.program.parameters = $scope.program.parameters || [];
+      $scope.program.parameters.push(entry);
     };
 
     $scope.submit = function (form) {
@@ -16,14 +45,6 @@ define(['./module'], function (module) {
       } else {
         $modalInstance.close($scope.program);
       }
-    };
-
-    $scope.paramSpec = function (p) {
-      var name = p.name;
-      if (p.value.pops.length > 0) {
-        name += ': ' + p.value.pops.join(' ');
-      }
-      return name;
     };
 
     initialize();
