4a507c8ca4dfae4e0e2af4ea64c7f6c683111393
Sebastian Sastre <sebastian@flowingconcept.com>
2014-12-02 19:32:50 -0200

    fix(modal): a couple of inform

--- client/source/js/modules/common/save-graph-as-directive.js
+++ client/source/js/modules/common/save-graph-as-directive.js
@@ -32,7 +32,7 @@ define(['angular', 'saveAs'], function (angular, saveAs, modalService) {
               var message = 'This feature is currently under development. We are working hard in make it happen for you!';
               modalService.inform(
                 function (){ null }, 
-                'Okay'
+                'Okay',
                 message, 
                 'Thanks for your interest!'
               );
