ac33683dbaf607d8c4a036576ebdfbbfba79ce99
(new name) <(new email)>
2014-12-13 13:36:15 -0200

    wip(activeProject): introduces resetFor(user) and setProjectFor(projectName, user)

--- client/source/js/modules/common/active-project-service.js
+++ client/source/js/modules/common/active-project-service.js
@@ -5,29 +5,45 @@
 
 define([
   'angular',
-  '../common/local-storage-service'
+  '../common/local-storage-service' 
 ], function (angular) {
   'use strict';
 
   return angular.module('app.active-project', [
     'app.local-storage'
-  ])
-    .factory('activeProject', function ($http, localStorage, User) {
+    ])
+    .factory('activeProject', [ 
+      '$http', 'localStorage',
+      function ($http, localStorage) {
+        var project = {
+          setValue: function (name) {  // deprecated method 
+            console.warn('activeProject.setValue is deprecated. Use setProjectFor(projectName, user) instead.');
+            project.name = name;
+            $http.defaults.headers.common.project = name;
+            localStorage.project = name;
+          },
+          setProjectFor: function (projectName, user) { 
+            // Sets projectName as the active project for the given user 
+            debugger
+            project.name = projectName;
+            $http.defaults.headers.common.project = projectName;
+            localStorage.project = projectName;
+          },          
+           resetFor: function (user) { 
+            // Resets the projectName as the active project for the given user 
+            debugger
+            project.name = projectName;
+            $http.defaults.headers.common.project = projectName;
+            localStorage.project = projectName;
+          },  
+         isSet: function() {
+            debugger
+            return (project.name !== null && project.name !== undefined);
+          }
+        };
 
-      var project = {
-        setValue: function (name) {
-          project.name = name;
-          $http.defaults.headers.common.project = name;
-          localStorage.project = name;
-        },
-        isSet: function() {
-          debugger
-          return (project.name !== null && project.name !== undefined);
-        }
-      };
+        return project;
 
-      return project;
-
-    });
+    }]);
 
 });
