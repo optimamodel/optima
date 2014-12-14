af970a8541d0da5f0e9defc2710e2a1dd8864422
(new name) <(new email)>
2014-12-13 22:14:16 -0600

    refactor(nvd3): removed legacy graphing library

--- client/source/js/config-require.js
+++ client/source/js/config-require.js
@@ -8,7 +8,6 @@ define({
   paths: {
     'angular'       : './vendor/angular/angular',
     'angular-loading-bar'  : './vendor/angular-loading-bar/build/loading-bar.min',
-    'angular-nvd3'  : './vendor/angular-nvd3/dist/angular-nvd3',
     'async'         : './vendor/requirejs-plugins/src/async',
     'canvg'         : './vendor/canvg/dist/canvg.bundle',
     'canvas2blob'   : './vendor/canvas-toBlob.js/canvas-toBlob',
@@ -19,7 +18,6 @@ define({
     'jquery'        : './vendor/jquery/dist/jquery',
     'ng-file-upload': './vendor/ng-file-upload/angular-file-upload',
     'ng-file-upload-html5-shim': './vendor/ng-file-upload/angular-file-upload-html5-shim',
-    'nvd3'          : './vendor/nvd3/nv.d3',
     'angular-resource'    : './vendor/angular-resource/angular-resource',
     'ui.router'     : './vendor/angular-ui-router/release/angular-ui-router',
     'ui.bootstrap'  : './vendor/angular-bootstrap/ui-bootstrap-tpls',
@@ -32,8 +30,6 @@ define({
       'exports': 'angular'
     },
     'angular-loading-bar': ['angular'],
-    'angular-nvd3': ['angular', 'nvd3'],
-    'nvd3': ['d3'],
     'd3-box': ['d3'],
     'angular-resource': ['angular'],
     'ng-file-upload': ['angular'],
