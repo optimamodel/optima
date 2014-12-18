78d3a734f863a5552a70c3a2bbeb9c01f0f911a4
(new name) <(new email)>
2014-12-18 13:32:51 +0100

    added /api/user/modify by id

--- server/src/tests/user_test.py
+++ server/src/tests/user_test.py
@@ -76,5 +76,22 @@ class UserTestCase(OptimaTestCase):
         projects = self.list_projects(2)
         assert(len(projects)==0)
 
+    def test_modify_user(self):
+        import hashlib
+        response = self.create_user()
+        new_password = hashlib.sha224("test1").hexdigest()
+        new_email = 'test1@test.com'
+        response = self.client.put('/api/user/modify/1?secret=%s&email=%s&password=%s' \
+            % (self.test_password, new_email, new_password))
+        assert(response.status_code==200)
+        data = json.loads(response.data)
+        print('modified:%s' % data)
+        assert(data.get('status')=='OK')
+        assert(data.get('modified')=='1')
+        response = self.login(email=new_email, password = new_password)
+        response = self.client.post('/api/project/create/test', data = '{}')
+        assert(response.status_code==200)
+        response=self.logout()
+
 if __name__ == '__main__':
     unittest.main()
