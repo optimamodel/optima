586f19c8e07f793bb2eb832f1a5f759198c38b16
Nik Graf <nikgraf.com@gmail.com>
2014-12-17 22:50:00 -0600

    refactor(graph export): clean up svg export

--- client/source/js/modules/common/save-graph-as-directive.js
+++ client/source/js/modules/common/save-graph-as-directive.js
@@ -7,6 +7,8 @@ define(['angular', 'underscore', 'saveAs'], function (angular, _, saveAs) {
         restrict: 'A',
         link: function (scope, elem, attrs) {
 
+          var chartCssUrl = '/assets/css/chart.css';
+
           /**
            * Initializes the directive by appending the html and setting up the
            * event handlers.
@@ -27,8 +29,8 @@ define(['angular', 'underscore', 'saveAs'], function (angular, _, saveAs) {
                 event.preventDefault();
 
                 modalService.choice(
-                  exportGraphAsSVG, // first button callback
-                  exportGraphAsPNG, // second button callback
+                  exportGraphAsSvg, // first button callback
+                  exportGraphAsPng, // second button callback
                   'Download as SVG', // first button text
                   'Download as PNG', // second button text
                   'Please choose your preferred format', // modal message
@@ -68,45 +70,74 @@ define(['angular', 'underscore', 'saveAs'], function (angular, _, saveAs) {
             return result;
           };
 
-          var exportGraphAsSVG = function() {
-            var svgContent = elem.parent().find('svg').html();
+          var exportGraphAsSvg = function() {
+            var originalSvg = elem.parent().find('svg');
+            var orginalWidth = originalSvg.width();
+            var orginalHeight = originalSvg.height();
+            var scalingFactor = 1;
 
             // in order to have styled graphs the css content used to render
             // graphs is retrieved & inject it into the svg as style tag
-            var cssContentRequest = $http.get('/assets/css/chart.css');
+            var cssContentRequest = $http.get(chartCssUrl);
             cssContentRequest.success(function(cssContent) {
-              var styles = '<style>' + cssContent + '</style>';
-              var svgGraph = '<svg xmlns="http://www.w3.org/2000/svg" version="1.1">';
-              svgGraph = svgGraph + styles;
-              svgGraph = svgGraph + svgContent;
-              svgGraph = svgGraph + '</svg>';
 
-              saveAs(new Blob([svgGraph], { type: 'image/svg' }), 'graph.svg');
+              // create svg element
+              var svg = createSvg(orginalWidth, orginalHeight, scalingFactor);
+
+              // add styles and content to the svg
+              var styles = '<style>' + cssContent + '</style>';
+              svg.innerHTML = styles + originalSvg.html();
+
+              // create img element with the svg as data source
+              var svgXML = (new XMLSerializer()).serializeToString(svg);
+              saveAs(new Blob([svgXML], { type: 'image/svg' }), 'graph.svg');
             }).error(function() {
               alert("Please releod and try again, something went wrong while generating the graph.");
             });
           };
 
-          var exportGraphAsPNG = function() {
+          /**
+           * Create SVG element
+           *
+           * With the scalingFactor argument a SVG image can be blown up.
+           */
+          var createSvg = function(viewBoxWidth, viewBoxheight, scalingFactor) {
+            var xmlns = "http://www.w3.org/2000/svg";
+            var svg = document.createElementNS(xmlns, "svg");
+            var viewBox = "0 0 " + viewBoxWidth + " " + viewBoxheight;
+            svg.setAttributeNS(null, "viewBox", viewBox);
+            svg.setAttributeNS(null, "width", viewBoxWidth * scalingFactor);
+            svg.setAttributeNS(null, "height", viewBoxheight * scalingFactor);
+            svg.setAttributeNS(null, "version", "1.1");
+            return svg;
+          };
+
+          /**
+           * Initializes a download of the current graph as PNG
+           *
+           * In order to achieve this a new SVG is created including styles.
+           * This SVG element is used as data source inside an image which then
+           * is used to draw the content on a canvas to save it as PNG.
+           */
+          var exportGraphAsPng = function() {
             var originalSvg = elem.parent().find('svg');
-            var orginalWidth = originalSvgElement.width();
-            var orginalHeight = originalSvgElement.height();
+            var orginalWidth = originalSvg.width();
+            var orginalHeight = originalSvg.height();
             var scalingFactor = 4.2;
 
             // in order to have styled graphs the css content used to render
             // graphs is retrieved & inject it into the svg as style tag
-            var cssContentRequest = $http.get('/assets/css/chart.css');
+            var cssContentRequest = $http.get(chartCssUrl);
             cssContentRequest.success(function(cssContent) {
 
-              var xmlns = "http://www.w3.org/2000/svg";
-              var svg = document.createElementNS(xmlns, "svg");
-              svg.setAttributeNS(null, "viewBox", "0 0 " + orginalWidth + " " + orginalHeight);
-              svg.setAttributeNS(null, "width", orginalWidth * scalingFactor);
-              svg.setAttributeNS(null, "height", orginalHeight * scalingFactor);
+              // create svg element
+              var svg = createSvg(orginalWidth, orginalHeight, scalingFactor);
 
+              // add styles and content to the svg
               var styles = '<style>' + cssContent + '</style>';
               svg.innerHTML = styles + originalSvg.html();
 
+              // create img element with the svg as data source
               var svgXML = (new XMLSerializer()).serializeToString(svg);
               var tmpImage = document.createElement("img");
               tmpImage.width = orginalWidth * scalingFactor;
@@ -114,6 +145,7 @@ define(['angular', 'underscore', 'saveAs'], function (angular, _, saveAs) {
               tmpImage.src = "data:image/svg+xml;charset=utf-8,"+ svgXML;
 
               tmpImage.onload = function() {
+                // draw image into canvas in order to convert it to a blob
                 var canvas = document.createElement("canvas");
                 canvas.width = orginalWidth * scalingFactor;
                 canvas.height = orginalHeight * scalingFactor;
