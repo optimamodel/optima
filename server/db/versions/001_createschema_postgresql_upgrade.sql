a2ba278dd711a3f16f3fd00e4e5ac7cf9e575eea
Anna Nachesa <anachesa@gmail.com>
2014-12-19 01:37:25 +0100

    simplified the first migration script (verified)

--- server/db/versions/001_createschema_postgresql_upgrade.sql
+++ server/db/versions/001_createschema_postgresql_upgrade.sql
@@ -30,7 +30,7 @@ SET default_with_oids = false;
 --
 
 CREATE TABLE projects (
-    id integer NOT NULL,
+    id SERIAL PRIMARY KEY,
     name character varying(60),
     user_id integer,
     datastart integer,
@@ -47,14 +47,6 @@ ALTER TABLE public.projects OWNER TO optima;
 -- Name: projects_id_seq; Type: SEQUENCE; Schema: public; Owner: optima
 --
 
-CREATE SEQUENCE projects_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
 ALTER TABLE public.projects_id_seq OWNER TO optima;
 
 --
@@ -69,7 +61,7 @@ ALTER SEQUENCE projects_id_seq OWNED BY projects.id;
 --
 
 CREATE TABLE users (
-    id integer NOT NULL,
+    id SERIAL PRIMARY KEY,
     name character varying(60),
     email character varying(200),
     password character varying(200)
@@ -81,14 +73,6 @@ ALTER TABLE public.users OWNER TO optima;
 --
 -- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: optima
 --
-
-CREATE SEQUENCE users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
 
 ALTER TABLE public.users_id_seq OWNER TO optima;
 
@@ -104,51 +88,13 @@ ALTER SEQUENCE users_id_seq OWNED BY users.id;
 --
 
 CREATE TABLE working_projects (
-    id integer NOT NULL,
+    id integer PRIMARY KEY,
     is_calibrating boolean,
     model json
 );
 
 
 ALTER TABLE public.working_projects OWNER TO optima;
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: optima
---
-
-ALTER TABLE ONLY projects ALTER COLUMN id SET DEFAULT nextval('projects_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: optima
---
-
-ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
-
-
---
--- Name: projects_pkey; Type: CONSTRAINT; Schema: public; Owner: optima; Tablespace:
---
-
-ALTER TABLE ONLY projects
-    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
-
-
---
--- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: optima; Tablespace:
---
-
-ALTER TABLE ONLY users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: working_projects_pkey; Type: CONSTRAINT; Schema: public; Owner: optima; Tablespace:
---
-
-ALTER TABLE ONLY working_projects
-    ADD CONSTRAINT working_projects_pkey PRIMARY KEY (id);
-
 
 --
 -- Name: projects_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: optima
