bc83c6c7ba2f167432e12c48afa8e12ea8226e4b
Nik Graf <nikgraf.com@gmail.com>
2015-01-10 18:29:09 +0300

    feat(project & scenarios): check/uncheck checkbox even when clicking on the table cell

---
+++ client/source/js/modules/common/update-checkbox-on-click-directive.js
@@ -0,0 +1,26 @@
+define(['angular', 'jquery'], function (module, $) {
+  'use strict';
+
+  return angular.module('app.common.update-checkbox-on-click', [])
+
+  /**
+   * Toggle the checkbox inside the clicked element.
+   *
+   * In order to avoid checking/unchecking with one click the toggle will
+   * only be applied when the target is not the checkbox itselve.
+   */
+  .directive('updateCheckboxOnClick', [function () {
+    return {
+      link: function(scope, element, attrs, ctrl) {
+        element.click(event, function() {
+          // only toggle if the cell is clicked, but not the checkbox itselve
+          // in order to prevent instanly reverting the action
+          if (!$(event.target).is(':checkbox')) {
+            var checkbox = $(event.currentTarget).find('input[type=checkbox]');
+            checkbox.prop("checked", !checkbox.prop("checked"));
+          }
+        });
+      }
+    };
+  }]);
+});
