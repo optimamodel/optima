db6cc8db786c70b71c2c41a7d1a28529255be498
(new name) <(new email)>
2015-01-12 19:58:20 +0100

    changed the generated workbook according to the one provided

--- server/src/sim/loadworkbook.py
+++ server/src/sim/loadworkbook.py
@@ -43,7 +43,7 @@ def loadworkbook(filename='example.xlsx', verbose=2):
                  ['Testing & treatment', 'txrx',    ['hivtest', 'aidstest', 'numfirstline', 'numsecondline', 'txelig', 'prep', 'numpmtct', 'birth', 'breast']], \
                  ['Sexual behavior',     'sex',     ['numactsreg', 'numactscas', 'numactscom', 'condomreg', 'condomcas', 'condomcom', 'circum', 'numcircum']], \
                  ['Injecting behavior',  'inj',     ['numinject', 'sharing', 'numost']], \
-                 ['Macroeconomics',      'macro',   ['gdp', 'revenue', 'govtexpend', 'totalhealth', 'domestichealth', 'domestichiv', 'globalfund', 'pepfar', 'otherint', 'private']]
+                 ['Macroeconomics & costs',      'macro',   ['gdp', 'revenue', 'govtexpend', 'totalhealth', 'domestichealth', 'domestichiv', 'globalfund', 'pepfar', 'otherint', 'private']]
                 ]
                  
     # Matrix data -- array sizes are population x population
