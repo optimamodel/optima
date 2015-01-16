e6ced383776f0ada277a66bb8e8ecd64be1e8c42
Anna Nachesa <anachesa@gmail.com>
2015-01-17 00:34:02 +0100

    implemented work log and exception bubbling up

--- server/src/optima/dbmodels.py
+++ server/src/optima/dbmodels.py
@@ -90,6 +90,7 @@ class WorkingProjectDb(db.Model):
     is_working = db.Column(db.Boolean, unique=False, default=False)
     work_type = db.Column(db.String(32), default=None)
     model = deferred(db.Column(JSON))
+    work_log_id = db.Column(db.Integer, default = None)
 
     def __init__(self, project_id, is_working=False, model = None, work_type = None):
         self.id = project_id
@@ -99,16 +100,18 @@ class WorkingProjectDb(db.Model):
 
 class WorkLogDb(db.Model):
     __tablename__ = "work_log"
-    work_status = ('started', 'completed', 'cancelled','error')
+    work_status = ('started', 'completed', 'cancelled', 'error')
     id = db.Column(db.Integer, primary_key=True)
     work_type = db.Column(db.String(32), default = None)
     project_id = db.Column(db.Integer, db.ForeignKey('projects.id'), index = True)
     start_time = db.Column(db.DateTime(timezone=True), server_default=text('now()'))
     stop_time = db.Column(db.DateTime(timezone=True), default = None)
     status = db.Column(db.Enum(*work_status,name='work_status'), default='started')
+    error = db.Column(db.Text, default = None)
 
-    def __init__(self, project_id):
+    def __init__(self, project_id, work_type = None):
         self.project_id = project_id
+        self.work_type = work_type
 
 class ProjectDataDb(db.Model):
     __tablename__ = 'project_data'
