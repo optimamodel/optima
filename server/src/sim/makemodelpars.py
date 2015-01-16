a718aff7b02ffc02caa269296037fb56e9b1876f
(new name) <(new email)>
2015-01-17 00:09:15 +1100

    adding numcircum

--- server/src/sim/makemodelpars.py
+++ server/src/sim/makemodelpars.py
@@ -91,7 +91,8 @@ def makemodelpars(P, opt, withwhat='p', verbose=2):
     M.breast   = dpar2mpar(P.breast, withwhat)    
     
     ## Sexual behavior parameters -- all are parameters so can loop over all
-    M.circum  = dpar2mpar(P.circum, withwhat) # Circumcision
+    M.circum    = dpar2mpar(P.circum,    withwhat) # Circumcision percentage
+    M.numcircum = dpar2mpar(P.numcircum, withwhat) # Circumcision number
     M.numacts = struct()
     M.condom  = struct()
     M.numacts.reg = dpar2mpar(P.numactsreg, withwhat) # ...
