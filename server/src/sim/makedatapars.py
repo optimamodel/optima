b6ff7b92545bdf414545c6f40ca5eaef36649378
(new name) <(new email)>
2015-01-18 01:21:38 +1100

    working on number circumcised

--- server/src/sim/makedatapars.py
+++ server/src/sim/makedatapars.py
@@ -123,6 +123,7 @@ def makedatapars(D, verbose=2):
     D.P.birth     = popexpand(D.P.birth,     array(D.G.meta.pops.female)==1)
     D.P.circum    = popexpand(D.P.circum,    array(D.G.meta.pops.male)==1)
     D.P.numcircum = popexpand(D.P.numcircum, array(D.G.meta.pops.male)==1)
+    D.P.numcircum.p *= 0 # Reset since prevalence data is required and overwrites data on numbers of circumcisions
             
             
 
