fa5ab67f30c8a285e7e440b511885326dcefa7bc
(new name) <(new email)>
2015-01-19 11:28:05 +0100

    removed startyear and endyear from optimize in favor of internal structure

--- server/src/sim/test_optimization.py
+++ server/src/sim/test_optimization.py
@@ -28,7 +28,7 @@ D = runsimulation(D, startyear=2000, endyear=2030, verbose=verbose)
 
 print('\n\n\n4. Running optimization...')
 from optimize import optimize
-optimize(D, timelimit=timelimit, startyear=2000, endyear=2030, verbose=10)
+optimize(D, timelimit=timelimit, objectives={"year":{"start":2000,"end":2030}}, verbose=10)
 
 print('\n\n\n5. Viewing optimization...')
 from viewresults import viewmultiresults, viewallocpies
