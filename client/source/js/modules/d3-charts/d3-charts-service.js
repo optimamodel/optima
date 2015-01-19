30829e551ec2a0ea4e17c05174bfd497ac889284
(new name) <(new email)>
2015-01-19 13:04:58 +0200

    feat(charts): first version of a pie chart

--- client/source/js/modules/d3-charts/d3-charts-service.js
+++ client/source/js/modules/d3-charts/d3-charts-service.js
@@ -3,6 +3,48 @@ define(['./module', 'd3', 'underscore', './scale-helpers'], function (module, d3
 
   module.service('d3Charts', function () {
 
+
+
+    /**
+     * Returns a PieChart instance.
+     *
+     * Data has to be provided in the following format: {label: "xxzy", value: 44}
+     */
+    function PieChart(chart, chartSize, data) {
+      var width = 960,
+      height = 500,
+      radius = Math.min(width, height) / 2;
+
+      var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+      var arc = d3.svg.arc()
+       .outerRadius(radius - 10)
+       .innerRadius(0);
+
+      var pie = d3.layout.pie()
+       .sort(null)
+       .value(function(d) { return d.value; });
+
+      chart.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+      var g = chart.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+
+      g.append("path")
+        .attr("d", arc)
+        .style("fill", function(d) { return color(d.data.label); });
+
+      g.append("text")
+        .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+        .attr("dy", ".35em")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d.data.label; });
+    }
+
+
     /**
      * Returns a LineChart instance.
      *
@@ -419,7 +461,8 @@ define(['./module', 'd3', 'underscore', './scale-helpers'], function (module, d3
       setSvgHeightAndPadding: setSvgHeightAndPadding,
       AreaChart: AreaChart,
       LineChart: LineChart,
-      ScatterChart: ScatterChart
+      ScatterChart: ScatterChart,
+      PieChart: PieChart
     };
   });
 });
