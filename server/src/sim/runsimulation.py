a5a5326bb783ede678e4abef7f89189615d6a86c
Anna Nachesa <anachesa@gmail.com>
2015-01-21 02:10:05 +0100

    restored the default

--- server/src/sim/runsimulation.py
+++ server/src/sim/runsimulation.py
@@ -34,7 +34,7 @@ def runsimulation(D, startyear=2000, endyear=2030, verbose=2, makeplot = 1, dosa
 #    D = getcurrentbudget(D) # TODO Add verbose
     # Calculate results
     from makeresults import makeresults
-    D.R = makeresults(D, allsims, D.opt.quantiles, verbose=4)
+    D.R = makeresults(D, allsims, D.opt.quantiles, verbose=verbose)
 
     # Gather plot data
     from gatherplotdata import gatheruncerdata
