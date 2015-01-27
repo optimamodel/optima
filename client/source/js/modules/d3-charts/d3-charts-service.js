cd537b93bf02d921b553123f42a3b259e3584f66
(new name) <(new email)>
2015-01-27 12:55:46 +0200

    refactor(charts): rather fail then not drawing axes for charts

--- client/source/js/modules/d3-charts/d3-charts-service.js
+++ client/source/js/modules/d3-charts/d3-charts-service.js
@@ -383,8 +383,6 @@ define(['./module', 'd3', 'underscore', './scale-helpers'], function (module, d3
     }
 
     function drawAxes(scales, options, axesGroup, chartSize) {
-      if (scales === undefined) return;
-
       var xLabel = options.xAxis.axisLabel;
       var yLabel = options.yAxis.axisLabel;
 
