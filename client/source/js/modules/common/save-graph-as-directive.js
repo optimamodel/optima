e8ad6a25d6288ef926499f7204aaafd587d25094
Nik Graf <nikgraf.com@gmail.com>
2015-01-27 20:38:33 +0200

    refactor(export data): avoid the need for variant by using the data attribute to determine the accessor name

--- client/source/js/modules/common/save-graph-as-directive.js
+++ client/source/js/modules/common/save-graph-as-directive.js
@@ -39,22 +39,11 @@ define(['angular', 'jquery', 'underscore', 'saveAs', 'jsPDF', './svg-to-png', '.
               })
               .on('click', '.data', function (event) {
                 event.preventDefault();
-                //a big ugly hack to distinguish between cost and covariance graphs.
-                //they are both in the same ng-repeat scope :-(
-                var target = {};
-                if ( attrs.variant == "radarChart" ) {
-                  target = scope.radarChart;
-                } else if (attrs.variant == 'pieChart') {
-                  target = scope.pieChart;
-                } else if (attrs.variant == 'coGraph') {
-                  target = scope.coGraph;
-                } else if (attrs.variant == 'ccGraph') {
-                  target = scope.ccGraph;
-                } else {
-                  target = scope.chart;
-                }
 
-                scope.exportFrom(target);
+                // find the chart in the scope via the data attribute
+                var chartAccessor = attrs.data.replace(new RegExp('.data$'), '');
+
+                scope.exportFrom(scope[chartAccessor]);
               });
           };
 
