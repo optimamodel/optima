0aa7d78ba6bf92006eb0bf6d8d0f6db89e0b3e60
(new name) <(new email)>
2015-01-28 17:35:54 +0200

    feat(stacked area chart): choose color set

--- client/source/js/modules/d3-charts/stacked-area-directive.js
+++ client/source/js/modules/d3-charts/stacked-area-directive.js
@@ -4,8 +4,39 @@ define(['./module', './scale-helpers', 'angular'], function (module, scaleHelper
   module.directive('stackedAreaChart', function (d3Charts) {
     var svg;
 
-    var colors = [ '__light-blue', '__blue', '__violet', '__green', '__light-green',
-      '__gray', '__red' ];
+    var colors = [
+      'color-1',
+      'color-2',
+      'color-3',
+
+      'color-10',
+      'color-11',
+      'color-12',
+
+      'color-7',
+      'color-8',
+      'color-9',
+
+      'color-13',
+      'color-14',
+      'color-15',
+
+      'color-4',
+      'color-5',
+      'color-6',
+
+      'color-16',
+      'color-17',
+      'color-18',
+
+      'color-19',
+      'color-20',
+      'color-21',
+
+      'color-22',
+      'color-23',
+      'color-24',
+    ];
 
     /**
      * Return a line based on another line where y has the value 0
