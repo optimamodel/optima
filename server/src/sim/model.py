9ea227e458efb057dcb2b237967130300712538c
cliffckerr <questionably@gmail.com>
2015-01-28 23:57:19 -0500

    Revert "changed sharing to be per population...hope i didnt break anything else"
    This reverts commit 9818ad96438ce12f4dde03249aa50fe7b1ea38c2.

--- server/src/sim/model.py
+++ server/src/sim/model.py
@@ -244,8 +244,8 @@ def model(G, M, F, opt, initstate=None, verbose=2):
         for pop1 in range(npops):
             for pop2 in range(npops):
                 if pshipsinj[pop1,pop2]>0: # Ignore if this isn't a valid injecting partnership
-                    numacts1 = sharing[pop1,t] * totalacts['inj'][pop1,pop2,t] / 2 # Number of acts per person per year -- /2 since otherwise double-count
-                    numacts2 = sharing[pop2,t] * totalacts['inj'][pop2,pop1,t] / 2 # Number of acts per person per year
+                    numacts1 = sharing[t] * totalacts['inj'][pop1,pop2,t] / 2 # Number of acts per person per year -- /2 since otherwise double-count
+                    numacts2 = sharing[t] * totalacts['inj'][pop2,pop1,t] / 2 # Number of acts per person per year
                     forceinf1 = 1 - mpow((1-transinj), (dt*numacts1*osteff*effhivprev[pop2])) # The chance of "2" infecting "1"
                     forceinf2 = 1 - mpow((1-transinj), (dt*numacts2*osteff*effhivprev[pop1])) # The chance of "1" infecting "2"
                     forceinfvec[pop1] = 1 - (1-forceinfvec[pop1]) * (1-forceinf1) # Calculate the new "male" forceinf, ensuring that it never gets above 1
