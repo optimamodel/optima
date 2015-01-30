1f42623efe62d9cc070fb9d8729b6ff7e6b74ffa
Nik Graf <nikgraf.com@gmail.com>
2015-01-30 10:04:48 +0200

    refactor(admin): syntax clean up

--- client/source/js/modules/admin/manage-users-ctrl.js
+++ client/source/js/modules/admin/manage-users-ctrl.js
@@ -7,13 +7,12 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
 
     $scope.deleteUser = function (user) {
       modalService.confirm(function(){
-        user_id = user.id;
-        $http.delete('/api/user/delete/'+user_id).then(function(){
+        $http.delete('/api/user/delete/' + user.id).then(function(){
           modalService.inform(undefined,undefined, 'User deleted!');
           $scope.users = _($scope.users).filter(function(u){
-            return u.id != user_id
+            return u.id != user.id;
           });
-        })
+        });
         }, undefined, 'Delete it', 'Cancel',
         'Are you sure you want to delete this user? Note that all of his projects will be deleted too!', 'Warning!');
     };
