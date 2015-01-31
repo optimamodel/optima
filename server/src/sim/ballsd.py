23ad9712faba5f0c0fe4ee836c61ba1552adcc8f
cliffckerr <questionably@gmail.com>
2015-01-31 14:30:53 -0500

    more output

--- server/src/sim/ballsd.py
+++ server/src/sim/ballsd.py
@@ -168,31 +168,31 @@ def ballsd(function, x, options = None, stepsize = 0.1, sinc = 2, sdec = 2, pinc
         # Stopping criteria
         if (count+1) >= MaxFunEvals: # Stop if the function evaluation limit is exceeded
             exitflag = 0 
-            if verbose>=5: print('======== Maximum function evaluations reached, terminating ========')
+            if verbose>=5: print('======== Maximum function evaluations reached (%i >= %i), terminating ========' % ((count+1), MaxFunEvals))
             break
         if count >= MaxIter: # Stop if the iteration limit is exceeded
             exitflag = 0 
-            if verbose>=5: print('======== Maximum iterations reached, terminating ========')
+            if verbose>=5: print('======== Maximum iterations reached (%i >= %i), terminating ========' % (count, MaxIter))
             break 
         if mean(s1) < TolX: # Stop if the step sizes are too small
             exitflag = 1 
-            if verbose>=5: print('======== Step sizes too small, terminating ========')
+            if verbose>=5: print('======== Step sizes too small (%f < %f), terminating ========' % (mean(s1), TolX))
             break
         if (count > StallIterLimit) and (mean(abserrorhistory) < AbsTolFun): # Stop if improvement is too small
             exitflag = 2 
-            if verbose>=5: print('======== Absolute improvement too small, terminating ========')
+            if verbose>=5: print('======== Absolute improvement too small (%f < %f), terminating ========' % (mean(abserrorhistory), AbsTolFun))
             break
         if (count > StallIterLimit) and (mean(relerrorhistory) < RelTolFun): # Stop if improvement is too small
             exitflag = 2 
-            if verbose>=5: print('======== Relative improvement too small, terminating ========')
+            if verbose>=5: print('======== Relative improvement too small (%f < %f), terminating ========' % (mean(relerrorhistory), RelTolFun))
             break
         if count2 > MaxRangeIter: 
             exitflag = 3
-            if verbose>=5: print('======== Can\'t find parameters within range, terminating ========')
+            if verbose>=5: print('======== Can\'t find parameters within range (%i > %i), terminating ========' % (count2, MaxRangeIter))
             break
         if (time()-start)>timelimit:
             exitflag = 4
-            if verbose>=5: print('======== Time limit reached, terminating ========')
+            if verbose>=5: print('======== Time limit reached (%f > %f), terminating ========' % ((time()-start), timelimit))
             break
         if stoppingfunc is not None:
             if stoppingfunc():
