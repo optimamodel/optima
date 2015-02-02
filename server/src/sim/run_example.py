a916ad76558cbec73f71d3ff35b25b533629f7e5
cliffckerr <questionably@gmail.com>
2015-02-02 12:00:02 -0500

    pwid example works

--- server/src/sim/run_example.py
+++ server/src/sim/run_example.py
@@ -8,9 +8,11 @@ from dataio import loaddata
 from updatedata import updatedata
 from os import path
 
+kind = 'pwid'
+
 datadir = '../static/'
-datafile = 'sudanexample.json'
-spreadsheetfile = 'sudanexample.xlsx'
+datafile = kind+'example.json'
+spreadsheetfile = kind+'example.xlsx'
 if path.exists(datadir+datafile):
     D = loaddata(datadir+datafile)
     D.G.workbookname = datadir+spreadsheetfile
