35386cd62ddbd8338760ac0849461236e3f087a7
Anna Nachesa <anachesa@gmail.com>
2015-02-02 05:48:21 +0100

    do not run testpwid.py if file not there

--- server/src/sim/testpwid.py
+++ server/src/sim/testpwid.py
@@ -6,9 +6,10 @@ Load a project and test data upload.
 
 from dataio import loaddata
 from updatedata import updatedata
-from os import getcwd
+from os import getcwd, path
 
 datafile = getcwd()+'/pwidtest.json'
-D = loaddata(datafile)
-D.G.workbookname = getcwd()+'/PWID_example.xlsx'
-D = updatedata(D)
+if path.exists(datafile):
+    D = loaddata(datafile)
+    D.G.workbookname = getcwd()+'/PWID_example.xlsx'
+    D = updatedata(D)
