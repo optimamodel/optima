e036cdd356ec03a7ff833554b35ddfe723ef3631
cliffckerr <questionably@gmail.com>
2015-02-05 11:12:05 -0500

    restored makeresults

--- server/src/sim/makeresults.py
+++ server/src/sim/makeresults.py
@@ -1,4 +1,4 @@
-def makeresults(D, allsims=None, quantiles=None, verbose=2):
+def makeresults(D, allsims=None, quantiles=None, financial=True, verbose=2):
     """
     Generate all outputs required for the model:
         Prevalence
@@ -29,7 +29,10 @@ def makeresults(D, allsims=None, quantiles=None, verbose=2):
     if quantiles==None: quantiles = D.opt.quantiles # If no quantiles are specified, just use the default ones
     allpeople = deepcopy(array([allsims[s].people for s in range(nsims)])) # WARNING, might use stupid amounts of memory
     
-    for data in ['prev', 'plhiv', 'inci', 'force', 'daly', 'death', 'tx1', 'tx2', 'dx', 'costann', 'costcum']:
+    allcosts = [] # Initialize -- WARNING, need to do better
+    datatypes = ['prev', 'plhiv', 'inci', 'force', 'daly', 'death', 'tx1', 'tx2', 'dx']
+    if financial: datatypes.extend(['costann', 'costcum']) # Also run financial results, which are computationally expensive
+    for data in datatypes:
         R[data] = struct()
         if data[0:4] != 'cost':
             R[data].pops = []
@@ -115,10 +118,10 @@ def makeresults(D, allsims=None, quantiles=None, verbose=2):
         if data[0:4]=='cost':
             printv('Calculating costs...', 3, verbose)
             from financialanalysis import financialanalysis
-            allcosts = []
-            for s in range(nsims):
-                thesecosts = financialanalysis(D, postyear = D.data.epiyears[-1], S = allsims[s], makeplot = False)
-                allcosts.append(thesecosts)
+            if len(allcosts)==0 and financial:
+                for s in range(nsims):
+                    thesecosts = financialanalysis(D, postyear = D.data.epiyears[-1], S = allsims[s], makeplot = False)
+                    allcosts.append(thesecosts)
             
             if data=='costcum':
                 R.costcum.total = quantile(array([allcosts[s]['cumulative']['total']['ylinedata'] for s in range(nsims)]), quantiles=quantiles) 
