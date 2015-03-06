42648b0f015944baa435cadb79bdf712149ec1c4
Nik Graf <nikgraf.com@gmail.com>
2015-03-06 20:17:25 +0900

    chore(cleanup): clean up depedencies

--- client/source/js/modules/validations/ng-max-directive.js
+++ client/source/js/modules/validations/ng-max-directive.js
@@ -13,13 +13,13 @@ define(['angular', 'underscore'], function (angular, _) {
           });
 
           ctrl.$parsers.push( scope.maxValidator );
-          ctrl.$formatters.push( scope.maxValidator );          
+          ctrl.$formatters.push( scope.maxValidator );
         };
 
         /**
          * Returns the value and sets validity in true if value didn't surpass the upper limit.
          * Otherwise sets validity in false and returns undefined.
-         */        
+         */
         scope.maxValidator = function (value) {
           var max = scope.$eval(attr.ngMax) || Infinity;
           if (!scope.isValidable(value) && value > max) {
@@ -33,13 +33,13 @@ define(['angular', 'underscore'], function (angular, _) {
 
         /**
          * Returns true if value is not something we can validate against max.
-         */        
+         */
         scope.isValidable = function (value) {
             return angular.isUndefined(value) || value === '' || value === null || value !== value;
         };
 
         scope.initialize();
       }
-    }
+    };
   });
 });
