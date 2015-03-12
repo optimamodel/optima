89cdb5b3753c672b7602a3b99edd34e551f152e2
Sebastian Sastre <sebastian@flowingconcept.com>
2015-03-12 13:39:06 -0300

    translates identation

--- client/source/js/modules/common/after-render-directive.js
+++ client/source/js/modules/common/after-render-directive.js
@@ -1,17 +1,17 @@
 define(['angular'], function (angular) {
-	'use strict';
+  'use strict';
 
-	return angular.module('app.common.after-render', [])
-		.directive('afterRender', function($timeout) {
-			var def = {
-				restrict : 'A', 
-				terminal : true,
-				transclude : false,
-				link : function(scope, element, attrs) {
-					if (attrs) { scope.$eval(attrs.afterRender) }
-					scope.$emit('onAfterRender')
-				}
-		};
-		return def;
-	});
+  return angular.module('app.common.after-render', [])
+    .directive('afterRender', function($timeout) {
+      var def = {
+        restrict : 'A', 
+        terminal : true,
+        transclude : false,
+        link : function(scope, element, attrs) {
+          if (attrs) { scope.$eval(attrs.afterRender) }
+            scope.$emit('onAfterRender')
+        }
+    };
+    return def;
+  });
 });
