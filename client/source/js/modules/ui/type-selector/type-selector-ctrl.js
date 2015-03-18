89b6936fa62d5b7fd48af2156e7d20d57f020dd9
Nik Graf <nikgraf.com@gmail.com>
2015-03-18 15:21:46 +0700

    fix(type selector): fixed scoping issue in the type selector

--- client/source/js/modules/ui/type-selector/type-selector-ctrl.js
+++ client/source/js/modules/ui/type-selector/type-selector-ctrl.js
@@ -1,33 +1,34 @@
 define(['./module', 'underscore'], function (module, _) {
   'use strict';
 
-  module.controller('TypeSelectorController', ['$scope', '$state',
-    function ($scope, $state) {
+  module.controller('TypeSelectorController', function ($scope, $state) {
 
-        /**
-        * @description Adds a parameter on scope that will indicate if something should be visible depending on the current state
-        * @param part name of the parameter that should be set on $scope
-        * @param viewNames names of the views in which this parameter should return true
-        */
-        function showPartInViews(part, viewNames){
-            $scope[part+"IsVisible"] = _(viewNames).contains($state.current.name);
-        }
-        $scope.financialResults = {};
-        $scope.$on('$stateChangeSuccess',function(){
-          showPartInViews('typeSelector', [
-            'model.view',
-            'analysis.scenarios',
-            'analysis.optimization'
-          ]);
+      $scope.state = {};
 
-          showPartInViews('stackedCheckbox',[
-            'model.view'
-          ]);
+      /**
+       * @description Adds a parameter on scope that will indicate if something should be visible depending on the current state
+       * @param part name of the parameter that should be set on $scope
+       * @param viewNames names of the views in which this parameter should return true
+       */
+      function showPartInViews (part, viewNames) {
+        $scope.state[part+"IsVisible"] = _(viewNames).contains($state.current.name);
+      }
 
-          showPartInViews('plotUncertainties',[
-            'analysis.optimization'
-          ]);
-        });
+      $scope.$on('$stateChangeSuccess',function() {
+        showPartInViews('typeSelector', [
+          'model.view',
+          'analysis.scenarios',
+          'analysis.optimization'
+        ]);
+
+        showPartInViews('stackedCheckbox',[
+          'model.view'
+        ]);
+
+        showPartInViews('plotUncertainties',[
+          'analysis.optimization'
+        ]);
+      });
     }
-  ]);
+  );
 });
