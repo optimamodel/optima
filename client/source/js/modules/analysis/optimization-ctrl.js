b0896bdb5093ccc8465b14ccac33431a9023f326
Khurram Ali <khurram.ali@riksof.com>
2015-03-20 18:24:29 +0500

    Revert "Fix/move optimizationmessage to the template refactoring 2"

--- client/source/js/modules/analysis/optimization-ctrl.js
+++ client/source/js/modules/analysis/optimization-ctrl.js
@@ -671,10 +671,8 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
     }
 
     function constructOptimizationMessage() {
+      var optimizationMessageTemplate = _.template("Optimizing <%= checkedPrograms %> over years <%= startYear %> to <%= endYear %> with <%= budgetLevel %>.");
       var budgetLevel;
-      var checkedPrograms = joinArrayAsSentence(validateObjectivesToMinimize().checkedPrograms, 'name', true);
-      var startYear = $scope.params.objectives.year.start;
-      var endYear = $scope.params.objectives.year.end;
 
       if ($scope.params.objectives.funding === 'variable') {
         budgetLevel = " budget level " + joinArrayAsSentence(_.compact(_($scope.params.objectives.outcome.variable).toArray()), undefined, false, "$");
@@ -685,16 +683,12 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
         budgetLevel = budgetLevel + " to $" + $scope.params.objectives.outcome.budgetrange.maxval;
       }
 
-      if ( budgetLevel && checkedPrograms && startYear && endYear ) {
-        $scope.showOptimizationMessage = true;
-
-        $scope.optimizationMessage = {
-          checkedPrograms: checkedPrograms,
-          startYear: startYear,
-          endYear: endYear,
-          budgetLevel: budgetLevel
-        };
-      }
+      $scope.optimizationMessage = optimizationMessageTemplate({
+        checkedPrograms : joinArrayAsSentence(validateObjectivesToMinimize().checkedPrograms, 'name', true),
+        startYear: $scope.params.objectives.year.start,
+        endYear:$scope.params.objectives.year.end,
+        budgetLevel: budgetLevel
+      });
     }
 
     $scope.setActiveTab = function(tabNum){
