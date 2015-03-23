2c36cf6286b013614047bb17dcbe300667178f98
cliffckerr <questionably@gmail.com>
2015-03-23 11:32:22 +0100

    oops remade change

--- client/source/js/config.js
+++ client/source/js/config.js
@@ -25,7 +25,7 @@ define(['angular'], function (angular) {
           { id: 'tx2', name: 'People on subsequent lines of treatment', byPopulation: false, total: true, stacked: false }
         ],
         costs:[
-          {id:"costcum", name: "Cumulative costs", existing: false, future: false, total: true, stacked: true},
+          {id:"costcum", name: "Cumulative costs", existing: false, future: false, total: true, stacked: false},
           {id:"costann", name: "Annual costs", existing: false, future: false, total: true, stacked: true},
           { id: 'commit', name: 'Commitments', hasNoIntervals: true, checked: false, label: 'Annual new commitments' }
         ],
