bdeac7a30b968c73e701f43df5f226fde045f7c7
Dmitry Evseev <kradmiy@gmail.com>
2015-04-07 15:18:55 +0300

    improve(spreadsheet-upload-hint): fits it into a single file

--- client/source/js/modules/ui/spreadsheet-upload-hint/index.js
+++ client/source/js/modules/ui/spreadsheet-upload-hint/index.js
@@ -1,3 +1,21 @@
 define([
-  './spreadsheet-upload-hint-directive'
-], function () {});
+  'angular'
+], function (angular) {
+
+  'use strict';
+
+  return angular.module('app.ui.spreadsheet-upload-hint', [])
+
+  /**
+   * <spreadsheet-upload-hint></spreadsheet-upload-hint>
+   *
+   * If the model data is missing this html snipped should be shown.
+   */
+    .directive('spreadsheetUploadHint', function () {
+      return {
+        restrict: 'E',
+        templateUrl: 'js/modules/ui/spreadsheet-upload-hint/spreadsheet-upload-hint.html',
+      };
+    });
+
+});
