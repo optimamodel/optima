86e95db5d1c5fce75aef038d6ba279bcea7b6239
Dmitry Evseev <kradmiy@gmail.com>
2015-04-15 15:44:05 +0300

    refactor(mock data): FE:   - removes obsolete mock-data resource,   - moves mock-data from graphs controller into assets/mocks/graphs.json (it also makes build a bit smaller). BE:   - removes mock data api entries, static files, generators related.

--- client/source/js/modules/graphs/graphs-ctrl.js
+++ client/source/js/modules/graphs/graphs-ctrl.js
@@ -2,258 +2,40 @@ define([
   './module'
 ], function (module) {
   'use strict';
-  module.controller('GraphsController', function ($scope, dataMocks, CONFIG) {
-
+  module.controller('GraphsController', function ($scope, mocks) {
     // Lines
     // =======
-    $scope.options5 = {
-      height: 300,
-      width: 900,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 60
-      },
-      xAxis: {
-        axisLabel: 'Axis X'
-      },
-      yAxis: {
-        axisLabel: 'Axis Y'
-      }
-    };
-
-    dataMocks.lineScatterError().$promise.then(function (data) {
-      $scope.data5 = data;
-    });
-
-
+    $scope.options5 = mocks.data.lineScatter.options;
+    $scope.data5 = mocks.data.lineScatter.data;
 
     // Line + Area
     // =======
+    $scope.options6 = mocks.data.lineAreaScatter.options;
+    $scope.data6 = mocks.data.lineAreaScatter.data;
 
-    $scope.options6 = {
-      height: 300,
-      width: 900,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 60
-      },
-      areasOpacity: 0.1,
-      xAxis: {
-        axisLabel: 'Axis X'
-      },
-      yAxis: {
-        axisLabel: 'Axis Y'
-      },
-      title: "Willy Wonka Production"
-    };
-
-    dataMocks.lineAreaScatter().$promise.then(function (data) {
-      $scope.data6 = data;
-    });
-
-    // GRAPH 7
+    // Stacked Chart
     // =======
+    $scope.options7 = mocks.data.stackedChart.options;
+    $scope.data7 = mocks.data.stackedChart.data;
 
-    $scope.options7 = {
-      height: 800,
-      width: 900,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 60
-      },
-      xAxis: {
-        axisLabel: 'Axis X'
-      },
-      yAxis: {
-        axisLabel: 'Axis Y'
-      }
-    };
-
-    $scope.data7 = {
-      'areas': [
-        [[2001, 1], [2002, 3], [2004, 5], [2005, 6], [2006, 7]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]],
-        [[2001, 2], [2002, 1], [2004, 2], [2005, 2], [2006, 4]]
-      ]
-    };
-
-    // GRAPH 8
+    // Pie Chart
     // =======
-
-    $scope.options8 = {
-      height: 300,
-      width: 300,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 60
-      }
-    };
-
-    $scope.data8 = {
-      slices: [
-        {label: '<5', value: 2704659},
-        {label: '5-13', value: 4499890},
-        {label: '14-17', value: 4499890},
-        {label: 'Hello my long text A', value: 5},
-        {label: 'Ohhh, another long label', value: 10}
-      ]
-    };
+    $scope.options8 = mocks.data.pieChart.options;
+    $scope.data8 = mocks.data.pieChart.data;
 
     // Stacked bar chart
     // =======
-
-    $scope.options9 = {
-      legend: ['AA', 'BB', 'CC'],
-      height: 300,
-      width: 300,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 60
-      },
-      xAxis: {
-        axisLabel: 'Axis X'
-      },
-      yAxis: {
-        axisLabel: 'Axis Y'
-      }
-    };
-
-    $scope.data9 = {
-      bars: [
-       [2001, [552339, 259034, 450818]],
-       [2002, [85640, 42153, 74257]],
-       [2003, [592339, 559034, 250818]]
-      ]
-    };
+    $scope.options9 = mocks.data.stackedBarChart.options;
+    $scope.data9 = mocks.data.stackedBarChart.data;
 
     // Two sided horizontal bar chart
     // =======
+    $scope.options10 = mocks.data.twoSidedHorizontalChart.options;
+    $scope.data10 = mocks.data.twoSidedHorizontalChart.data;
 
-    $scope.options10 = {
-      legend: ['AA', 'BB', 'CC'],
-      height: 300,
-      width: 600,
-      margin: {
-        top: 20,
-        right: 20,
-        bottom: 60,
-        left: 60
-      },
-      leftTitle: "Left Title",
-      rightTitle: "Right Title"
-    };
-
-    $scope.data10 = {
-      bars: [
-        ['Level 1', 12030, [552339, 259034, 450818]],
-        ['Level 2', 8034, [85640, 42153, 74257]],
-        ['Level 3', 2023, [592339, 559034, 250818]]
-      ]
-    };
-
-    $scope.radarOptions = {};
-
-    $scope.radarData = [{
-      "axes": [{
-          "value": 54,
-          "axis": "SBCC"
-      }, {
-          "value": 45,
-          "axis": "NSP"
-      }, {
-          "value": 60,
-          "axis": "OST"
-      }, {
-          "value": 29,
-          "axis": "MSM programs"
-      }, {
-          "value": 40,
-          "axis": "FSW programs"
-      }, {
-          "value": 34,
-          "axis": "ART"
-      }, {
-          "value": 51,
-          "axis": "PMTCT"
-      }]
-    }, {
-      "axes": [{
-          "value": 12,
-          "axis": "SBCC"
-      }, {
-          "value": 10,
-          "axis": "NSP"
-      }, {
-          "value": 20,
-          "axis": "OST"
-      }, {
-          "value": 25,
-          "axis": "MSM programs"
-      }, {
-          "value": 15,
-          "axis": "FSW programs"
-      }, {
-          "value": 24,
-          "axis": "ART"
-      }, {
-          "value": 16,
-          "axis": "PMTCT"
-      }]
-    }, {
-      "axes": [{
-          "value": 10,
-          "axis": "SBCC"
-      }, {
-          "value": 5,
-          "axis": "NSP"
-      }, {
-          "value": 10,
-          "axis": "OST"
-      }, {
-          "value": 2,
-          "axis": "MSM programs"
-      }, {
-          "value": 3,
-          "axis": "FSW programs"
-      }, {
-          "value": 5,
-          "axis": "ART"
-      }, {
-          "value": 10,
-          "axis": "PMTCT"
-      }]
-    }];
-
+    // Radar Chart
+    // =======
+    $scope.radarOptions = mocks.data.radarChart.options;
+    $scope.radarData = mocks.data.radarChart.data;
   });
 });
