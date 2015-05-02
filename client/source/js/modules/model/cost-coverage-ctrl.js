fe8625529e061e865966a6b625d0ca45339fb9a8
Nik Graf <nikgraf.com@gmail.com>
2015-05-02 12:11:15 -0600

    fix(cost coverage): remove legacy debug statement

--- client/source/js/modules/model/cost-coverage-ctrl.js
+++ client/source/js/modules/model/cost-coverage-ctrl.js
@@ -162,7 +162,6 @@ define(['./module', 'underscore'], function (module, _) {
       $scope.state.nonHivDalys = program.ccparams.nonhivdalys;
       $scope.state.displayYear = program.ccparams.cpibaseyear;
       $scope.state.calculatePerPerson = program.ccparams.perperson;
-      console.log("info", info);
       $scope.state.info = info;
 
       var model = getPlotModel();
