7096a30fafcf5c8fbef762e33b4c92cd096ea1ca
Nik Graf <nikgraf.com@gmail.com>
2015-05-09 21:35:03 -0600

    fix(cost coverage): update api & clean up code

--- server/src/optima/plotting.py
+++ server/src/optima/plotting.py
@@ -15,27 +15,39 @@ class OptimaTickFormatter(plugins.PluginBase):
     def __init__(self):
         self.dict_ = {"type": "optimaTickFormatter"}
 
-def generate_cost_coverage_chart(plotdata):
-    """ Returns the cost-coverage chart as Mpld3 JSON format """
+def update_mpld3_plugins(chart):
+    """
+    Clears all existing plugings and set up the ones specific to Optima
 
-    blank_figure = figure(figsize=(3,2), dpi=100)
-    cost_coverage_figure = plot_cost_coverage(plotdata, figure=blank_figure)
-
-    # clear all plugins from the figure
-    plugins.clear(cost_coverage_figure)
+    This function modifies the figure passed in as parameter.
+    """
+    plugins.clear(chart)
     plugins.connect(
-        cost_coverage_figure,
+        chart,
         # Box zoom is needed to manually create a zoom button in the JS front-end
         plugins.BoxZoom(button=False, enabled=False),
         OptimaTickFormatter())
 
-    result = fig_to_dict(cost_coverage_figure)
+def close_figure(chart):
+    """
+    Clears & close the provided figure and it's axis.
 
-    # close figure
-    axis = cost_coverage_figure.gca()
+    This function modifies the figure passed in as parameter.
+    """
+    axis = chart.gca()
     axis.cla()
-    cost_coverage_figure.clf()
-    close(cost_coverage_figure)
+    chart.clf()
+    close(chart)
+
+def generate_cost_coverage_chart(plotdata):
+    """ Returns the cost-coverage chart as Mpld3 JSON format """
+
+    blank_figure = figure(figsize=(3,2), dpi=100)
+    cost_coverage_figure = plot_cost_coverage(plotdata, existingFigure=blank_figure)
+
+    update_mpld3_plugins(cost_coverage_figure)
+    result = fig_to_dict(cost_coverage_figure)
+    close_figure(cost_coverage_figure)
 
     return result
 
@@ -43,23 +55,11 @@ def generate_coverage_outcome_chart(plotdata):
     """ Returns the coverage-outcome chart as Mpld3 JSON format """
 
     blank_figure = figure(figsize=(3,2), dpi=100)
-    coverage_outcome_figure = plot_coverage_outcome(plotdata, figure=blank_figure)
+    coverage_outcome_figure = plot_coverage_outcome(plotdata, existingFigure=blank_figure)
 
-    # clear all plugins from the figure
-    plugins.clear(coverage_outcome_figure)
-    plugins.connect(
-        coverage_outcome_figure,
-        # Box zoom is needed to manually create a zoom button in the JS front-end
-        plugins.BoxZoom(button=False, enabled=False),
-        OptimaTickFormatter())
-
+    update_mpld3_plugins(coverage_outcome_figure)
     result = fig_to_dict(coverage_outcome_figure)
-
-    # close figure
-    axis = coverage_outcome_figure.gca()
-    axis.cla()
-    coverage_outcome_figure.clf()
-    close(coverage_outcome_figure)
+    close_figure(coverage_outcome_figure)
 
     return result
 
@@ -67,22 +67,10 @@ def generate_cost_outcome_chart(plotdata):
     """ Returns the cost-outcome chart as Mpld3 JSON format """
 
     blank_figure = figure(figsize=(3,2), dpi=100)
-    cost_outcome_figure = plot_cost_outcome(plotdata, figure=blank_figure)
+    cost_outcome_figure = plot_cost_outcome(plotdata, existingFigure=blank_figure)
 
-    # clear all plugins from the figure
-    plugins.clear(cost_outcome_figure)
-    plugins.connect(
-        cost_outcome_figure,
-        # Box zoom is needed to manually create a zoom button in the JS front-end
-        plugins.BoxZoom(button=False, enabled=False),
-        OptimaTickFormatter())
-
+    update_mpld3_plugins(cost_outcome_figure)
     result = fig_to_dict(cost_outcome_figure)
-
-    # close figure
-    axis = cost_outcome_figure.gca()
-    axis.cla()
-    cost_outcome_figure.clf()
-    close(cost_outcome_figure)
+    close_figure(cost_outcome_figure)
 
     return result
