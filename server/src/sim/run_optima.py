28ff20dc4ea2ddbdf170723a659b7547bada715e
David Kedziora <davidkedz@hotmail.com>
2015-05-27 20:44:32 +0100

    Change 1: Wrapper class 'Region' created
    Dictionary D in makeproject.py is now encapsulated by class 'Region'.
    Functions have been converted into methods.
    Thus far the program runs without major issue.
    However, the plots look different after each run.
    Global/local variable problem?

--- server/src/sim/run_optima.py
+++ server/src/sim/run_optima.py
@@ -19,16 +19,17 @@ show_wait = False
 nsims = 5
 
 print('\n\n\n1. Making project...')
-from makeproject import makeproject
-D = makeproject(projectname=projectname, pops=['']*6, progs = ['']*7, datastart=2000, dataend=2015, verbose=verbose)
-D['opt']['nsims'] = nsims # Reset options
+from makeproject import Region
+region1 = Region()
+region1.makeproject(projectname=projectname, pops=['']*6, progs = ['']*7, datastart=2000, dataend=2015, verbose=verbose)
+region1.getdata()['opt']['nsims'] = nsims # Reset options
 
 print('\n\n\n2. Updating data...')
 from updatedata import updatedata
-D = updatedata(D, verbose=verbose)
+region1.setdata(updatedata(region1.getdata(), verbose=verbose))
 
 print('\n\n\n3. Viewing results...')
 from viewresults import viewuncerresults
-viewuncerresults(D['plot']['E'])
+viewuncerresults(region1.getdata()['plot']['E'])
 
 print('\n\n\nDONE; elapsed: %f s' % (time()-starttime))
