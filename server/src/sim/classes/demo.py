f910f4f15a99848ce4a1a6f50726fcf0f332372c
David Kedziora <davidkedz@hotmail.com>
2015-06-04 20:39:51 +0100

    Change 11: Modified demo for portfolio class
    Have accepted Romesh's classes folder, but modified the demo script so
    that it works as a copy of runportfolio.py in the analyses/gpa folder.
    However, to avoid uploading json files in the optima directory, I will
    continue to call the portfolio class from the runportfolio.py script.

--- server/src/sim/classes/demo.py
+++ server/src/sim/classes/demo.py
@@ -1,16 +1,11 @@
-# This demo exists so that it can be included in the pull request - rather than
-# having two pull requests, one for each repository. This file should be deleted
-# after the pull request is merged - or else maybe stored here as a self-contained
-# demo in the same way that run_optima.py is in the main repository?
-
 """
-RUNPROJECT
+RUNPORTFOLIO
 
 Procedure to run this file:
 1. Go to ui.optimamodel.com and download the .json files to investigate.
 2. Place them in the regions folder. This should be a subfolder of the one that stores this script!
 3. Press F5 to run (or from the Run menu, select Run).
-4. Choose a name for your project and then follow the ensuing instructions.
+4. Choose a name for your portfolio and then follow the ensuing instructions.
 5. Load as many regions as you want.
 5. ...
 6. Profit?
@@ -18,11 +13,11 @@ Procedure to run this file:
 For developers:
 - Each loaded region has its own individually populated data structure D.
 - Any typical processes you wish to apply to individual Ds should be coded as a method of Region class (regionclass.py).
-- However, ALL processes should be initiated at the Project class level (projectclass.py) and propagate down.
-  'Project' should ideally be hooked up as the interface for the front-end.
-  For example, want to apply 'optimize' to the nth loaded region within the project?
+- However, ALL processes should be initiated at the Portfolio class level (portfolioclass.py) and propagate down.
+  'Portfolio' should ideally be hooked up as the interface for the front-end.
+  For example, want to apply 'optimize' to the nth loaded region within the portfolio?
   Code something like 'def optimize(self,n,parameters)', which should in turn call 'self.regionlist[n-1].optimise(parameters)'.
-- Likewise, any processes to apply across multiple Ds should be coded while iterating through a subset of Project's regionlist.
+- Likewise, any processes to apply across multiple Ds should be coded while iterating through a subset of Portfolio's regionlist.
   This includes the geo-prioritisation algorithm but can be extended.
 
 Version: 2015may29
@@ -50,12 +45,12 @@ import sys;
 sys.path.append(findoptima())
 sys.path.append(findoptima() + '/classes')
 
-from portfolio import Portfolio
+from portfolioclass import Portfolio
 
-projectname = raw_input('Please enter a name for your project: ')
+portfolioname = raw_input('Please enter a name for your portfolio: ')
 
-currentproject = Project(projectname)
-currentproject.run()
+currentportfolio = Portfolio(portfolioname)
+currentportfolio.run()
 
 
 #from optimize import optimize, defaultobjectives, defaultconstraints
