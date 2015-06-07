5388de3ea5b4777f059b57f6dd81604aaf54c774
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-06-07 13:00:07 +1000

    Tidied up layout

--- server/src/sim/classes/defaults.py
+++ server/src/sim/classes/defaults.py
@@ -4,14 +4,16 @@ Created on Tue Jun 02 00:08:51 2015
 
 @author: David Kedziora
 """
+current_version = 5
 
+### REGION DEFAULTS
+
+# Region information for default example project
 datastart = 2000
 dataend = 2015
 nsims = 5
 pops = [{u'name': u'Female sex workers', u'short_name': u'FSW', u'sexworker': True, u'injects': False, u'sexmen': True, u'client': False, u'female': True, u'male': False, u'sexwomen': False}, {u'name': u'Clients of sex workers', u'short_name': u'Clients', u'sexworker': False, u'injects': False, u'sexmen': False, u'client': True, u'female': False, u'male': True, u'sexwomen': True}]
 progs = [{u'category': u'Prevention', u'short_name': u'Condoms', u'name': u'Condom promotion and distribution', u'parameters': [{u'value': {u'pops': [u''], u'signature': [u'condom', u'reg']}}, {u'value': {u'pops': [u''], u'signature': [u'condom', u'cas']}}]}]
-
-current_version = 5
 
 # Region information for Haiti project - to go with Haiti.json
 haiti = {}
