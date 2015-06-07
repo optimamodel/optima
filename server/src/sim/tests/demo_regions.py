d3c40650ca76b0a9b63ea1a69061a15147499bfa
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-06-07 12:10:47 +1000

    Region demo now runs model

--- server/src/sim/tests/demo_regions.py
+++ server/src/sim/tests/demo_regions.py
@@ -1,7 +1,10 @@
 # This demonstration creates a region, saves a corresponding XLSX file
 # and then loads it back again
 import add_optima_paths
+import defaults
 import region
 
-import defaults
 r = region.Region('test',defaults.pops,defaults.progs,defaults.datastart,defaults.dataend)
+r.loadDfrom('./regions/Haiti.json')
+r.createsimbox('Simbox 1')
+r.runsimbox(r.simboxlist[0])
