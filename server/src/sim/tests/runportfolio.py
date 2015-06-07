017dee77a214cfe84dc7605b4ab1684f3ec3e2b7
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-06-07 11:55:29 +1000

    Moved path adding to separate file so that it can be reused

--- server/src/sim/tests/runportfolio.py
+++ server/src/sim/tests/runportfolio.py
@@ -22,28 +22,9 @@ For developers:
 
 Version: 2015may29
 """
+import add_optima_paths
 
 print('WELCOME TO OPTIMA')
-
-# Check that the Optima directory is even linked via system path.
-def findoptima():
-    """ Find the directory that Optima exists in. """
-    from os.path import exists
-    optimadirs = [ # List all common directories here.
-        '/u/cliffk/unsw/optima/server/src/sim',
-        '/Users/robynstuart/Documents/Optima2/server/src/sim',
-        'C:/Users/Ireporter/Documents/GitHub/Optima/server/src/sim',
-        'D:/Work Projects/Optima/Optima/server/src/sim',
-        '/Users/romesha/Desktop/Work/Optima/Optima/server/src/sim'
-    ]
-    for optimadir in optimadirs:
-        if exists(optimadir):
-            return optimadir
-    raise Exception('Unfortunately the Optima directory cannot be found! Please add its path to the optimadirs list.')
-
-import sys; 
-sys.path.append(findoptima())
-sys.path.append(findoptima() + '/classes')
 
 from portfolio import Portfolio
 
