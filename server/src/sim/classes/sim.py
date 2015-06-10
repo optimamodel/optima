6496f9947ae71ffd970e3e901d462806125ec420
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-06-10 22:28:24 +1000

    Initial implementation of scenarios

--- server/src/sim/classes/sim.py
+++ server/src/sim/classes/sim.py
@@ -96,9 +96,7 @@ class Sim:
         tempD = makedatapars(tempD)
         self.parsdata = tempD['P']
         
-        from makemodelpars import makemodelpars
-        
-        self.parsmodel = makemodelpars(self.parsdata, r.options)
+        self.makemodelpars()
         
         from updatedata import makefittedpars
         
@@ -118,6 +116,7 @@ class Sim:
         # but are otherwise almost identical. Thus this is the function that is
         # expected to be re-implemented in the derived classes
         from makemodelpars import makemodelpars
+        r = self.getregion()
 
         self.parsmodel = makemodelpars(self.parsdata, r.options)
 
@@ -206,3 +205,99 @@ class SimBudget(Sim):
 #        viewmultiresults(tempD['plot']['optim'][-1]['multi'], show_wait = True)
 #        viewoptimresults(tempD['plot']['optim'][-1])
 #        viewoptimresults(tempD['plot']['optim'][-1])
+
+class SimParameter(Sim):
+    def __init__(self, name, region):
+        Sim.__init__(self, name, region)
+        self.parameter_overrides = []
+
+    def create_override(self,parname,pop,startyear,endyear,startval,endval):
+        # Create override for a single parameter, preserving the current dictionary representation of scenarios
+        # pop can be a population index, or a population shortname. 'all' is a valid shortname
+        #
+        # Also need to validate the parameter name
+        if type(pop) is str:
+            r = self.getregion()
+            poplist = [x['short_name'] for x in r.metadata['populations']] + ['all']
+            try:
+                popidx = poplist.index(pop) + 1 # For some reason (frontend?) these indexes are 1-based rather than 0-based
+            except:
+                print 'Population not found! Valid populations are:'
+                print poplist
+                raise Exception('InvalidPopulation')
+        else:
+            popidx = pop
+
+        assert(type(parname) is list)
+
+        override = {}
+        override['names'] = parname # This can be a list e.g. [u'condom', u'com']??
+        override['pops'] = popidx
+        override['startval'] = startval
+        override['startyear'] = startyear
+        override['endval'] = endval
+        override['endyear'] = endyear
+        self.parameter_overrides.append(override)
+
+    def makemodelpars(self):
+        from numpy import linspace, ndim
+        from nested import getnested, setnested
+        from utils import findinds
+
+        # First, get the base model parameters
+        Sim.makemodelpars(self) 
+
+        # Now compute the overrides as per scenarios.py -> makescenarios()
+        r = self.getregion()
+        for thesepars in self.parameter_overrides:
+            data = getnested(self.parsmodel, thesepars['names'])
+            if ndim(data)>1:
+                if thesepars['pops'] < len(data):
+                    newdata = data[thesepars['pops']] # If it's more than one dimension, use population data too
+                else:
+                    newdata = data[:] # Get all populations
+            else:
+                newdata = data # If it's not, just use the whole thing
+            
+            # Get current values
+            initialindex = findinds(r.options['partvec'], thesepars['startyear'])
+            finalindex = findinds(r.options['partvec'], thesepars['endyear'])
+            
+            if thesepars['startval'] == -1:
+                if ndim(newdata)==1: 
+                    initialvalue = newdata[initialindex]
+                else: 
+                    initialvalue = newdata[:,initialindex].mean(axis=0) # Get the mean if multiple populations
+            else:
+                initialvalue = thesepars['startval']
+            
+            if thesepars['endval'] == -1:
+                if ndim(newdata)==1: 
+                    finalvalue = newdata[finalindex]
+                else: 
+                    finalvalue = newdata[:,finalindex].mean() # Get the mean if multiple populations
+            else:
+                finalvalue = thesepars['endval'] 
+            
+            # Set new values
+            npts = finalindex-initialindex
+            newvalues = linspace(initialvalue, finalvalue, npts)
+            if ndim(newdata)==1:
+                newdata[initialindex:finalindex] = newvalues
+                newdata[finalindex:] = newvalues[-1] # Fill in the rest of the array with the last value
+                if ndim(data)==1:
+                    data = newdata
+                else:
+                    data[thesepars['pops']] = newdata
+            else:
+                for p in xrange(len(newdata)):
+                    newdata[p,initialindex:finalindex] = newvalues
+                    newdata[p,finalindex:] = newvalues[-1] # Fill in the rest of the array with the last value
+            
+            # Update data
+            if ndim(data)>1 and ndim(newdata)==1:
+                data[thesepars['pops']] = newdata # Data is multiple populations, but we're only resetting one
+            else:
+                data = newdata # In all other cases, reset the whole thing (if both are 1D, or if both are 2D
+
+            setnested(self.parsmodel, thesepars['names'], data)
---
+++ server/src/sim/tests/demo_scenario.py
@@ -0,0 +1,21 @@
+# This demonstration creates a region, saves a corresponding XLSX file
+# and then loads it back again
+import add_optima_paths
+import defaults
+import region
+import sim
+
+# Make the region
+r = region.Region.load('./regions/georgia_working.json')
+
+# Make the simulations
+sim1 = sim.Sim('Base',r)
+sim2 = sim.SimParameter('Test & Treat only',r)
+sim2.create_override(['propaware'],'all',2015,2030,0.6,0.9)
+sim2.create_override(['txtotal'],'all',2015,2030,0.4,0.81)
+
+# Put them in a simbox, run them, and plot results
+sbox = r.createsimbox('Test scenarios')
+sbox.simlist = [sim1,sim2]
+sbox.runallsims()
+sbox.viewmultiresults()
