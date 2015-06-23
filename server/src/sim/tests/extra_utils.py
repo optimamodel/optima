436c330326ea99c17e24b9c0b0eb9a8708a9f7de
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-06-23 15:42:10 +1000

    Fixed unhandled types and numpy array comparison

--- server/src/sim/tests/extra_utils.py
+++ server/src/sim/tests/extra_utils.py
@@ -7,21 +7,30 @@ def dict_equal(d1,d2):
 	if type(d1) != type(d2):
 		return False
 
+	if d1 is None and d2 is None:
+		return True
+
 	# If they are dictionaries, check all of their fields
 	if isinstance(d1,dict):
 		if d1.keys() != d2.keys():
 			return False
 		else:
-			print [dict_equal(d1[k],d2[k]) for k in d1.keys()]
 			return all([dict_equal(d1[k],d2[k]) for k in d1.keys()])
 	
 	# Vector reduction for ndarrays
 	elif isinstance(d1,numpy.ndarray):
-		return (d1 == d2).all()
+		is_eq = d1 == d2
+		if isinstance(is_eq,numpy.ndarray):
+			return (d1 == d2).all()
+		elif isinstance(is_eq,bool):
+			return is_eq
+		else:
+			raise Exception('Unknown returned data type!')
 
 	# Direct equality
-	elif isinstance(d1,(list,tuple,float,int,str)):
+	elif isinstance(d1,(list,tuple,float,int,str,unicode)):
 		return d1 == d2
+
 	else:
 		raise Exception("Do not know how to compare objects of type %s" % type(d1))
 
