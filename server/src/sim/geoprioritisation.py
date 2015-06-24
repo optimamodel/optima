4488ea22a6d2241d5e559fe57a9300e686cf2ec1
David Kedziora <davidkedz@hotmail.com>
2015-06-24 21:45:36 +0100

    Quick Sync
    BALLSD not working well with Lagrange multipliers yet. The quest
    continues...

--- server/src/sim/geoprioritisation.py
+++ server/src/sim/geoprioritisation.py
@@ -5,15 +5,17 @@ Created on Thu Jun 18 21:46:25 2015
 @author: David Kedziora
 """
 
-from numpy import arange, zeros, ones
-from scipy.optimize import minimize
+from numpy import arange, linspace
+#from scipy.optimize import minimize
 
 # The object returned by getBOCspline() is a pchip. See SimBoxOpt.
 #from scipy.interpolate import PchipInterpolator as pchip
 
-def totalobjectivefunc(x, (gpasimboxes, grandtotal)):
+def totalobjectivefunc(xin, (gpasimboxes, scalingfactors, grandtotal)):
     """Objective function. A secondary argument of all simboxes under gpa must be passed in during minimisation."""
     """Input x contains len(x)-1 budget totals, while x[len(x)] is a lagrangian multiplier."""
+
+    x = [xin[i]*scalingfactors[i] for i in arange(0,len(xin))]    
     
     l = x[-1]    # Lagrangian multiplier
     
@@ -24,6 +26,21 @@ def totalobjectivefunc(x, (gpasimboxes, grandtotal)):
     print totalobj
     return totalobj
     
+def testobjectivefunc(xin, (gpapchips, scalingfactors, grandtotal)):
+    """Objective function. A secondary argument of all pchips under gpa must be passed in during minimisation."""
+    """Input x contains len(x)-1 budget totals, while x[len(x)] is a lagrangian multiplier."""
+    
+    x = [xin[i]*scalingfactors[i] for i in arange(0,len(xin))]    
+    
+    l = x[-1]    # Lagrangian multiplier
+    
+    totalobj = 0
+    for i in arange(0,len(x)-1):
+        totalobj += (gpapchips[i].derivative()(x[i]) + l)**2
+    totalobj += (sum(x[:-1])-grandtotal)**2
+    print totalobj**0.5
+    return totalobj**0.5
+    
 #def totalobjectivefunc(x, gpasimboxes):
 #    """Objective function. A secondary argument of all simboxes under gpa must be passed in during minimisation."""
 #    
@@ -32,12 +49,12 @@ def totalobjectivefunc(x, (gpasimboxes, grandtotal)):
 #        totalobj += gpasimboxes[i].getBOCspline()(x[i])
 #    return totalobj
     
-def totalobjectivejac(x, gpasimboxes):
-    
-    totalobjder = zeros([len(x)])
-    for i in arange(0,len(x)):
-        totalobjder[i] = gpasimboxes[i].getBOCspline().derivative()(x[i])
-    return totalobjder
+#def totalobjectivejac(x, gpasimboxes):
+#    
+#    totalobjder = zeros([len(x)])
+#    for i in arange(0,len(x)):
+#        totalobjder[i] = gpasimboxes[i].getBOCspline().derivative()(x[i])
+#    return totalobjder
 
 #totalbudget = 300000
 #def totalbudgetconstraint(t):
@@ -77,6 +94,36 @@ def totalobjectivejac(x, gpasimboxes):
 #res = minimize(totalobjectivefunc, [100000,200000], args=([f1,f2]), jac=totalobjectivejac,               
 #               constraints=cons, tol=1e-15, options={'disp': True})
 
+GIB = 14746612.0
+HIB = 220650283.0
+GBOCx = [1474661.2, 2949322.4, 7373305.999999999, 14746611.999999998, 29493223.999999996, 73733059.99999999]
+GBOCy = [7420.566529193031, 6895.548719803941, 3774.207669823583, 3708.429066239846, 3618.188538137224, 3503.591871259382]
+HBOCx = [22065028.3, 44130056.6, 110325141.5, 220650283.0, 441300566.0, 1103251415.0]
+HBOCy = [25005.62384682375, 23444.48190211528, 18266.5441001453, 17310.937016319283, 17312.31503529964, 17312.550566584538]
+
+from scipy.interpolate import PchipInterpolator as pchip
+
+GS = pchip(GBOCx, GBOCy, extrapolate=True)
+HS = pchip(HBOCx, HBOCy, extrapolate=True)
+
+import matplotlib.pyplot as plt
+
+#xnew = linspace(max(min(GBOCx),min(HBOCx)), min(max(GBOCx),max(HBOCx)), 500)
+xnew = linspace(0, min(max(GBOCx),max(HBOCx)), 500)
+plt.plot(xnew,GS(xnew),'-',xnew,HS(xnew),'--')
+plt.legend(['Georgia','Haiti'], loc='best')
+plt.show()
+
+plt.plot(xnew,GS.derivative()(xnew),'-',xnew,HS.derivative()(xnew),'--')
+plt.legend(['Georgia','Haiti'], loc='best')
+plt.show()
+
+from ballsd import ballsd
+
+grandtotal = GIB + HIB
+lin = min(abs(GS.derivative()(GIB)),abs(HS.derivative()(HIB)))
+X, FVAL, EXITFLAG, OUTPUT = ballsd(testobjectivefunc, [GIB, HIB, lin], options=([GS,HS], [1, 1, -1], grandtotal), TolX = 1e-30, AbsTolFun = 1e-30, RelTolFun = 1e-15, xmin = [0,0,None])
+
 def gpaoptimisefixedtotal(simboxref):
 #    budgettotals = zeros([len(simboxref)+1])
     budgettotals = []
@@ -88,21 +135,21 @@ def gpaoptimisefixedtotal(simboxref):
     grandtotal = sum(budgettotals)
     print budgettotals
     
-    # The constraint is that all region budgets must add up to the same amount.
-    def totalbudgetconstraint(t):
-        return sum(t) - grandtotal
-        
-    def totalbudgetconstraintjac(t):
-        return ones([len(t)])
-    
-    cons = ({'type': 'eq',
-         'fun' : totalbudgetconstraint})#,
-#         'jac' : totalbudgetconstraintjac})
-
-    print 'Yo'
-    print totalobjectivefunc(budgettotals,(simboxref,grandtotal))
-#    print totalobjectivejac(budgettotals,simboxref)
-    print 'No'
+#    # The constraint is that all region budgets must add up to the same amount.
+#    def totalbudgetconstraint(t):
+#        return sum(t) - grandtotal
+#        
+#    def totalbudgetconstraintjac(t):
+#        return ones([len(t)])
+#    
+#    cons = ({'type': 'eq',
+#         'fun' : totalbudgetconstraint})#,
+##         'jac' : totalbudgetconstraintjac})
+#
+#    print 'Yo'
+#    print totalobjectivefunc(budgettotals,(simboxref,grandtotal))
+##    print totalobjectivejac(budgettotals,simboxref)
+#    print 'No'
          
 #    res = minimize(totalobjectivefunc, budgettotals, args=(simboxref), jac=totalobjectivejac,               
 #               bounds=[(0,None) for x in simboxref], constraints=cons, options={'disp': True})
