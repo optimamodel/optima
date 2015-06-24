6ccded8cade2fdc0da986d145e2c2616a7174207
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-06-25 06:26:49 +1000

    Tests for dataio

---
+++ server/src/sim/unit_tests/io_tests.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('../tests')
+import add_optima_paths
+from dataio import tojson,fromjson,savedata,loaddata
+from extra_utils import dict_equal
+import unittest
+import numpy
+
+class TestDataIO(unittest.TestCase):
+
+	def test_basic(self):
+		x = 1
+		savedata('./cache.json',x)
+		y = loaddata('./cache.json',x)
+		self.assertTrue(dict_equal(x,y))
+
+	def test_list(self):
+		x = [2]
+		savedata('./cache.json',x)
+		y = loaddata('./cache.json',x)
+		self.assertTrue(dict_equal(x,y))
+
+	def test_tuple(self):
+		x = (3)
+		savedata('./cache.json',x)
+		y = loaddata('./cache.json',x)
+		self.assertTrue(dict_equal(x,y))
+
+	def test_mixed(self):
+		x = [(3),[4]]
+		savedata('./cache.json',x)
+		y = loaddata('./cache.json',x)
+		self.assertTrue(dict_equal(x,y))
+
+if __name__ == '__main__':
+    unittest.main()
