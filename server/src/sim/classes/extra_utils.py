c3922f5bd61e2f0a91013c44a0fa8b504e02b76d
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-07-02 17:07:48 +1000

    Preparing to shift to array_equal

--- server/src/sim/classes/extra_utils.py
+++ server/src/sim/classes/extra_utils.py
@@ -6,7 +6,7 @@ import region
 import sim
 import simbox
 
-def dict_equal(d1,d2,verbose=2):
+def dict_equal(d1,d2,verbose=2,keyname=''):
 	# Check if two dictionaries contain the same stuff
 
 	# Check that they're the same type
@@ -14,7 +14,6 @@ def dict_equal(d1,d2,verbose=2):
 		d1 = unicode(d1)
 	if isinstance(d2,str):
 		d2 = unicode(d2)
-
 
 	if type(d1) != type(d2):
 		if verbose:
@@ -26,7 +25,7 @@ def dict_equal(d1,d2,verbose=2):
 
 	if d1 is None and d2 is None:
 		if verbose:
-			print '%s and %s are None' % (d1,d2)
+			print '%s and %s are None (keyname=%s)' % (d1,d2,keyname)
 		return True
 
 	# If they are dictionaries, check all of their fields
@@ -36,12 +35,12 @@ def dict_equal(d1,d2,verbose=2):
 				print 'Keys do not match: d1=%s,d2=%s' % (d1.keys(),d2.keys())
 			return False
 		else:
-			return all([dict_equal(d1[k],d2[k]) for k in d1.keys() if k is not 'UUID']) # Need to skip the UUID
+			return all([dict_equal(d1[k],d2[k],keyname=k) for k in d1.keys() if k is not 'UUID']) # Need to skip the UUID
 	
 	# Vector reduction for ndarrays
 	elif isinstance(d1,numpy.ndarray):
-		is_eq = d1 == d2
-		if isinstance(is_eq,numpy.ndarray):
+		is_eq = (d1 == d2)
+		if isinstance(is_eq,numpy.ndarray): # Why is this necessary sometimes...
 			is_eq = (d1 == d2).all()
 		elif not isinstance(is_eq,bool):
 			raise Exception('Unknown returned data type!')
@@ -49,14 +48,24 @@ def dict_equal(d1,d2,verbose=2):
 			print 'Numpy array mismatch'
 		return is_eq
 
+	elif isinstance(d1,float):
+		if numpy.isnan(d1) and numpy.isnan(d2):
+			return true
+		else:
+			return
 	# We might have a list of ndarrays 
 	elif isinstance(d1,list):
 		if len(d1) != len(d2):
+			print "List length doesn't match - (keyname=%s)" % (keyname)
 			return False
-		return all([dict_equal(d1[x],d2[x]) for x in xrange(0,len(d1))]) 
-
+		rval = all([dict_equal(d1[x],d2[x]) for x in xrange(0,len(d1))]) 
+		if not rval:
+			print d1[1]
+			print d2[1]
+			print "List items don't match - (keyname=%s)" % (keyname)
+		return rval
 	# Direct equality
-	elif isinstance(d1,(tuple,float,int,str,unicode)):
+	elif isinstance(d1,(tuple,int,str,unicode)):
 		return d1 == d2
 	else:
 		raise Exception("Do not know how to compare objects of type %s" % type(d1))
