8e6d2e58204faea3dc9b1c8f83004f1b8331e378
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-07-08 13:00:01 +1000

    Updated program tests to print more output again

--- server/src/sim/tests/demo_programs.py
+++ server/src/sim/tests/demo_programs.py
@@ -5,7 +5,7 @@ import sim
 import numpy
 
 # This function tests the program calculation
-r = region.Region.load('./regions/haiti.json')
+r = region.Region.load('./regions/georgia_working.json')
 
 # Make an example budget
 a = r.D['data']['origalloc']
@@ -14,12 +14,12 @@ alloc = a
 for i in xrange(0,len(r.D['opt']['partvec'])-1):
     alloc = numpy.append(alloc,a,axis=1)
 
-# Use the new programs to get the model parameters
-s = sim.SimBudget2('Test',r,alloc)
+
+s = sim.SimBudget('Test',r,alloc)
 s.initialise()
+
 m_new = s.parsmodel # New D.M
 
-# Use the old programs to get the model parametesr
 from copy import deepcopy
 D = deepcopy(r.D)
 from getcurrentbudget import getcurrentbudget
@@ -27,28 +27,28 @@ D, newcov, newnonhivdalysaverted = getcurrentbudget(D,a)
 from makemodelpars import makemodelpars
 m_old = makemodelpars(D['P'],D['opt'], withwhat='c')
 
-# Do some comparisons
+# print s.
+# print D['P']['hivtest']['c']
 print [x.name for x in s.program_set['programs']]
 
-print m_old['numost'][0]
-print m_new['numost'][0]
+for par in m_new.keys():
+	if type(m_old[par]) == dict:
+		for key in m_old[par].keys():
+			try:
+				print "%s[%s]: old = %.6f, new = %.6f" % (par,key,m_old[par][key][0][0],m_new[par][key][0][0])
+			except:
+				try:
+					print "%s[%s] (single): old = %.6f, new = %.6f" % (par,key,m_old[par][key][0],m_new[par][key][0])
+				except:
+					print "%s[%s] (??)" % (par,key)
+	else:
+		try:
+			print "%s: old = %.6f, new = %.6f" % (par,key,m_old[par][0][0],m_new[par][0][0])
+		except:
+			try:
+				print "%s (single): old = %.6f, new = %.6f" % (par,m_old[par][0],m_new[par][0])
+			except:
+				print "%s (??)" % (par)
 
-# for par in m_new.keys():
-# 	if type(m_old[par]) == dict:
-# 		for key in m_old[par].keys():
-# 			try:
-# 				print "%s[%s]: old = %.6f, new = %.6f" % (par,key,m_old[par][key][0][0],m_new[par][key][0][0])
-# 			except:
-# 				try:
-# 					print "%s[%s] (single): old = %.6f, new = %.6f" % (par,key,m_old[par][key][0],m_new[par][key][0])
-# 				except:
-# 					print "%s[%s] (??)" % (par,key)
-# 	else:
-# 		try:
-# 			print "%s: old = %.6f, new = %.6f" % (par,key,m_old[par][0][0],m_new[par][0][0])
-# 		except:
-# 			try:
-# 				print "%s (single): old = %.6f, new = %.6f" % (par,m_old[par][0],m_new[par][0])
-# 			except:
-# 				print "%s (??)" % (par)
-
+import extra_utils
+print extra_utils.dict_equal(m_old,m_new)
