79fe5f9c85f84ab6e0edcfa3a61cadd647595d7a
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-07-19 15:16:20 +1000

    Added None test showing flaw in dataio.savedata

--- server/src/sim/unit_tests/io_tests.py
+++ server/src/sim/unit_tests/io_tests.py
@@ -10,32 +10,56 @@ class TestDataIO(unittest.TestCase):
 
 	def test_basic(self):
 		x = 1
-		savedata('./cache.json',x)
-		y = loaddata('./cache.json',x)
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
 		self.assertTrue(dict_equal(x,y,verbose=True))
 
 	def test_list(self):
 		x = [2]
-		savedata('./cache.json',x)
-		y = loaddata('./cache.json',x)
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
 		self.assertTrue(dict_equal(x,y,verbose=True))
 
 	def test_tuple(self):
 		x = (3)
-		savedata('./cache.json',x)
-		y = loaddata('./cache.json',x)
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
 		self.assertTrue(dict_equal(x,y,verbose=True))
 
 	def test_mixed(self):
 		x = [(3),[4]]
-		savedata('./cache.json',x)
-		y = loaddata('./cache.json',x)
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
 		self.assertTrue(dict_equal(x,y,verbose=True))
 
 	def test_numpy_float(self):
 		x = numpy.float64(1)
-		savedata('./cache.json',x)
-		y = loaddata('./cache.json',x)
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
+		self.assertTrue(dict_equal(x,y,verbose=True))
+
+	def test_numpy_array(self):
+		x = numpy.array([1,2,3])
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
+		self.assertTrue(dict_equal(x,y,verbose=True))
+
+	def test_numpy_nan(self):
+		x = numpy.nan
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
+		self.assertTrue(dict_equal(x,y,verbose=True))
+
+	def test_numpy_array_nan(self):
+		x = numpy.array([1,numpy.nan,2])
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
+		self.assertTrue(dict_equal(x,y,verbose=True))
+
+	def test_none(self):
+		x = None
+		savedata('./cache.json',x,verbose=0)
+		y = loaddata('./cache.json',verbose=0)
 		self.assertTrue(dict_equal(x,y,verbose=True))
 
 if __name__ == '__main__':
