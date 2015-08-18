76a7794a0fe86496ef21d7a32aef4ad1ef56ee56
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-08-18 16:51:23 +1000

    Added automatic file compression to binary IO

--- server/src/sim/classes/dataio_binary.py
+++ server/src/sim/classes/dataio_binary.py
@@ -1,11 +1,12 @@
 import cPickle
+import gzip
 
 def save(obj,fname):
-	with open(fname,'wb') as file_data:
+	with gzip.GzipFile(fname, 'wb') as file_data:
 		cPickle.dump(obj,file_data)
 
 def load(fname):
-	with open(fname,'rb') as file_data:
+	with gzip.GzipFile(fname, 'rb') as file_data:
 		obj = cPickle.load(file_data)
 	return obj
 
