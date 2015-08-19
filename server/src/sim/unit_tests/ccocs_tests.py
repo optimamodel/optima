bd92d46be720419a64b3ab6a120750a7cb61a89a
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-08-19 23:20:40 +1000

    Add initial sketch for new programs framework

---
+++ server/src/sim/unit_tests/ccocs_tests.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('../tests')
+import add_optima_paths
+from extra_utils import dict_equal
+from copy import deepcopy
+import unittest
+import numpy
+import region
+import ccocs
+from pylab import *
+from numpy import arange,linspace
+
+class TestCCOCs(unittest.TestCase):
+
+	def test_scaleup(self):
+		# Make a normal type program and test expected output
+		ccdata = {u'coveragelower': 0.1, u'nonhivdalys': 1.0, u'funding': 300000.0, u'saturation': 0.98, u'coverageupper': 0.9, u'scaleup': 0.73}
+		cost_coverage = ccocs.cc_scaleup(ccdata)
+
+		codata = [0, 0, 0.5, 0.5] # Linear coverage-outcome from 0 to 1 exactly
+		coverage_outcome = ccocs.co_cofun(codata)
+
+		# Various tests here
+		c = cost_coverage.evaluate(300000.0)
+		c2 = cost_coverage.evaluate(100000000.0)
+		outcomes = coverage_outcome.evaluate(c)
+
+		self.assertAlmostEqual(c2,numpy.array([0.98]),places=7) 
+		self.assertAlmostEqual(c,numpy.array([0.5]),places=7) 
+		self.assertAlmostEqual(c,2*outcomes,places=7) 
+
+	def test_linear(self):
+		# Test linear CC and CO functions (for testing modalities)
+		codata = [2.0, 0.0]
+		coverage_outcome = ccocs.co_linear(codata)
+
+		self.assertAlmostEqual(coverage_outcome.evaluate(0.0),0,places=7) 
+		self.assertAlmostEqual(coverage_outcome.evaluate(0.5),1,places=7) 
+		self.assertAlmostEqual(coverage_outcome.evaluate(1.0),2,places=7) 
+
+	def test_co_cofun(self):
+
+		codata = [0, 0, 2, 2] # Linear coverage-outcome from 0 to 1 exactly
+		coverage_outcome = ccocs.co_cofun(codata)
+
+		self.assertAlmostEqual(coverage_outcome.evaluate(0.0),0,places=7) 
+		self.assertAlmostEqual(coverage_outcome.evaluate(0.5),1,places=7) 
+		self.assertAlmostEqual(coverage_outcome.evaluate(1.0),2,places=7) 
+
+if __name__ == '__main__':
+    unittest.main()
+
+
