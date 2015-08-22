d30b4676a8ba7c5086d44eb773b3bbdb69a9c58e
cliffckerr <questionably@gmail.com>
2015-08-23 02:13:35 +1000

    removed axis

--- server/src/sim/makemap.py
+++ server/src/sim/makemap.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Aug 22 16:38:48 2015
 
@@ -6,26 +5,12 @@ Created on Sat Aug 22 16:38:48 2015
 """
 
 import sys; sys.path.append('/u/cliffk/unsw/optima/server/src/sim')
-from utils import printdata as pd
-
-from pylab import *
+from utils import printdata as pd # analysis:ignore
+from pylab import axis, gca, Polygon, show, figure, axes, hold
 import shapefile as sh
 
-filename = '/u/cliffk/unsw/countries/malawi/gis/MWI_adm0'
+filename = '/u/cliffk/unsw/countries/malawi/gis/MWI_adm1'
 
-sf = sh.Reader(filename)
-
-testshape = sf.shape(0)
-
-x = []
-y = []
-
-for p in xrange(len(testshape.points)):
-    x.append(testshape.points[p][0])
-    y.append(testshape.points[p][1])
-
-figure()
-axes()
 
 def plotshape(points, color):
     polygon = Polygon(points, color=color)
@@ -34,7 +19,20 @@ def plotshape(points, color):
     show()
 
 
-points = [[2, 1], [8, 1], [8, 4]]
-plotshape(testshape.points, (1,0,0))
+sf = sh.Reader(filename)
+
+numregions = sf.numRecords
+
+figure()
+ax = axes(frameon=False)
+ax.get_xaxis().set_visible(False)
+ax.get_yaxis().set_visible(False)
+hold(True)
+
+from gridcolormap import gridcolormap
+colors = gridcolormap(numregions)
+
+for r in xrange(numregions):
+    plotshape(sf.shape(r).points, colors[r])
 
 print('Done.')
