bcc910057bff08f93258951993e4f8ea6714a6f4
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-08-25 21:20:24 +1000

    Vastly improve file saving time and size

--- server/src/sim/classes/dataio_binary.py
+++ server/src/sim/classes/dataio_binary.py
@@ -1,20 +1,11 @@
 import cPickle
 import gzip
-import numpy
 
 def save(obj,fname):
-	# Using numpy. WARNING - this will add a .npz extension
-	#numpy.savez_compressed(fname,obj)
-
-	# Using cPickle
 	with gzip.GzipFile(fname, 'wb') as file_data:
-		cPickle.dump(obj,file_data)
+		cPickle.dump(obj,file_data,protocol=2)
 
 def load(fname):
-	# Using numpy
-	#obj = numpy.load(fname)['arr_0'].tolist() 
-
-	# Using cPickle
 	with gzip.GzipFile(fname, 'rb') as file_data:
 		obj = cPickle.load(file_data)
 	
