8d74c0c456faef4b8d3312af5a8997e7df47b3fe
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-09-03 17:46:28 +1000

    Tests run again

--- server/src/sim/unit_tests/ccocs_tests.py
+++ server/src/sim/unit_tests/ccocs_tests.py
@@ -1,12 +1,10 @@
 import sys
-sys.path.append('../tests')
-import add_optima_paths
-from extra_utils import dict_equal
+sys.path.append('..')
+import optima
+from liboptima.utils import dict_equal
 from copy import deepcopy
 import unittest
 import numpy
-import project
-import ccocs
 from pylab import *
 from numpy import arange,linspace
 
@@ -15,10 +13,10 @@ class TestCCOCs(unittest.TestCase):
 	def test_scaleup(self):
 		# Make a normal type program and test expected output
 		ccdata = {u'coveragelower': 0.1, u'nonhivdalys': 1.0, u'funding': 300000.0, u'saturation': 0.98, u'coverageupper': 0.9, u'scaleup': 0.73}
-		cost_coverage = ccocs.cc_scaleup(ccdata)
+		cost_coverage = optima.ccocs.cc_scaleup(ccdata)
 
 		codata = [0, 0, 0.5, 0.5] # Linear coverage-outcome from 0 to 1 exactly
-		coverage_outcome = ccocs.co_cofun(codata)
+		coverage_outcome = optima.ccocs.co_cofun(codata)
 
 		# Various tests here
 		c = cost_coverage.evaluate(300000.0)
@@ -32,7 +30,7 @@ class TestCCOCs(unittest.TestCase):
 	def test_linear(self):
 		# Test linear CC and CO functions (for testing modalities)
 		codata = [2.0, 0.0]
-		coverage_outcome = ccocs.co_linear(codata)
+		coverage_outcome = optima.ccocs.co_linear(codata)
 
 		self.assertAlmostEqual(coverage_outcome.evaluate(0.0),0,places=7) 
 		self.assertAlmostEqual(coverage_outcome.evaluate(0.5),1,places=7) 
@@ -41,7 +39,7 @@ class TestCCOCs(unittest.TestCase):
 	def test_co_cofun(self):
 
 		codata = [0, 0, 2, 2] # Linear coverage-outcome from 0 to 1 exactly
-		coverage_outcome = ccocs.co_cofun(codata)
+		coverage_outcome = optima.ccocs.co_cofun(codata)
 
 		self.assertAlmostEqual(coverage_outcome.evaluate(0.0),0,places=7) 
 		self.assertAlmostEqual(coverage_outcome.evaluate(0.5),1,places=7) 
--- server/src/sim/unit_tests/syntax_tests.py
+++ server/src/sim/unit_tests/syntax_tests.py
@@ -3,54 +3,52 @@
 # Tests in here perform minimal, if any, validation of variable values
 
 import sys
-sys.path.append('../tests')
-import add_optima_paths
-from extra_utils import dict_equal
+sys.path.append('..')
+import optima
+from liboptima.utils import dict_equal
 import unittest
 import numpy
-import project
-import portfolio
-import defaults
+
 
 class TestSyntax(unittest.TestCase):
 
 	def test_portfolio_save_load(self):
-		r1 = project.Project.load_json('../tests/projects/Dedza.json')
-		r2 = project.Project.load_json('../tests/projects/Dowa.json')
+		r1 = optima.Project.load_json('../projects/Dedza.json')
+		r2 = optima.Project.load_json('../projects/Dowa.json')
 		tempsimbox1 = r1.createsimbox('GPA1', isopt = True, createdefault = True)
 		tempsimbox2 = r2.createsimbox('GPA2', isopt = True, createdefault = True)
 		
-		p1 = portfolio.Portfolio('test')
+		p1 = optima.Portfolio('test')
 		p1.appendproject(r1)
 		p1.appendproject(r2)
 		p1.gpalist = [tempsimbox1,tempsimbox2]
 		print p1.gpalist
 		p1.save('cache.bin')
-		p2 = portfolio.Portfolio.load('cache.bin')
+		p2 = optima.Portfolio.load('cache.bin')
 		print p2.gpalist
 
 	def test_saving_and_loading(self):
-		r = project.Project.load_json('../tests/projects/Dedza.json') # Load old style JSON
-		r.save_json('../tests/projects/Dedza_newstyle.json')
-		r2 = project.Project.load_json('../tests/projects/Dedza_newstyle.json') # Load new style JSON
+		r = optima.Project.load_json('../projects/Dedza.json') # Load old style JSON
+		r.save_json('../projects/Dedza_newstyle.json')
+		r2 = optima.Project.load_json('../projects/Dedza_newstyle.json') # Load new style JSON
 		r2.createsimbox('Simbox 1')
 		r2.simboxlist[0].createsim('sim1')
 		r2.runsimbox(r2.simboxlist[0])
-		r2.save('../tests/projects/Dedza_newstyle_withsim.bin')
-		r3 = project.Project.load('../tests/projects/Dedza_newstyle_withsim.bin') # Load new style JSON
+		r2.save('../projects/Dedza_newstyle_withsim.bin')
+		r3 = optima.Project.load('../projects/Dedza_newstyle_withsim.bin') # Load new style JSON
 		print r
 		print r3
 		print r3.simboxlist[0].simlist[0].processed
 
 	def test_saving_and_loading_binary(self):
-		r = project.Project.load_json('../tests/projects/Dedza.json') # Load old style JSON
-		r.save('../tests/projects/Dedza_newstyle.bin')
-		r2 = project.Project.load('../tests/projects/Dedza_newstyle.bin') # Load new style JSON
+		r = optima.Project.load_json('../projects/Dedza.json') # Load old style JSON
+		r.save('../projects/Dedza_newstyle.bin')
+		r2 = optima.Project.load('../projects/Dedza_newstyle.bin') # Load new style JSON
 		r2.createsimbox('Simbox 1')
 		r2.simboxlist[0].createsim('sim1')
 		r2.runsimbox(r2.simboxlist[0])
-		r2.save('../tests/projects/Dedza_newstyle_withsim.bin')
-		r3 = project.Project.load('../tests/projects/Dedza_newstyle_withsim.bin') # Load new style JSON
+		r2.save('../projects/Dedza_newstyle_withsim.bin')
+		r3 = optima.Project.load('../projects/Dedza_newstyle_withsim.bin') # Load new style JSON
 		print r
 		print r3
 		print r3.simboxlist[0].simlist[0].processed
@@ -66,8 +64,8 @@ class TestSyntax(unittest.TestCase):
 	# def test_from_xlsx(self):
 	# 	# Test running a simulation from XLSX
 	# 	r = project.Project('Haiti (from XLSX)',defaults.haiti['populations'],defaults.haiti['programs'],defaults.haiti['datastart'],defaults.haiti['dataend'])
-	# 	r.makeworkbook('../tests/projects/Haiti_Test.xlsx') # Write to a dummy file for test purposes
-	# 	r.loadworkbook('../tests/projects/Haiti.xlsx')
+	# 	r.makeworkbook('../projects/Haiti_Test.xlsx') # Write to a dummy file for test purposes
+	# 	r.loadworkbook('../projects/Haiti.xlsx')
 	# 	r.createsimbox('Simbox 1')
 	# 	r.simboxlist[0].createsim('sim1') # This is really confusing....
 	# 	r.runsimbox(r.simboxlist[0])
