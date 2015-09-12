ee010c03a676e7cc4346f5e862ef7d772962f895
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-09-12 17:40:04 +1000

    Testing standalone functionality

--- server/src/sim/optima/sim.py
+++ server/src/sim/optima/sim.py
@@ -55,6 +55,10 @@ class Sim(object):
             self.calibration = None
 
     def todict(self):
+        # Guard against temporary Sims being passed around via regions or being saved to files
+        if not isinstance(self.project,weakref.ref):
+            raise Exception('You cannot save a Sim that has been turned into a standalone simulation')
+
         simdict = {}
         simdict['type'] = 'Sim'
         simdict['name'] = self.name
@@ -78,11 +82,24 @@ class Sim(object):
         # the project you need to do self.project() rather than
         # self.project. This function abstracts away this 
         # implementation detail in case it changes in future
-        r = self.project()
-        if r is None:
-            raise Exception('The parent project has been garbage-collected and the reference is no longer valid.')
+        if isinstance(self.project,weakref.ref)
+            r = self.project()
+            if r is None:
+                raise Exception('The parent project has been garbage-collected and the reference is no longer valid.')
+            else:
+                return r
         else:
-            return r
+            return self.project
+
+    def make_standalone(self):
+        # I'm not entirely happy about this function
+        # The problem is when you want to do a parallel optimization, you need to 
+        # run a multiprocessing pool inside a *simbox*, and it makes no sense to 
+        # pass the entire parent region around. But a weakref cannot be pickled
+        # So this function should STRICTLY only be used for TEMPORARY Sims that
+        # need to be passed into a multiprocessing loop/pool
+        self.project = deepcopy(self.getproject())
+        self.project.simboxlist = list() # delete the simboxlist
 
     def getcalibration(self):
         # Return a deepcopy of the selected calibration
@@ -395,122 +412,12 @@ class Sim(object):
     def __setstate__(self, state):
         raise Exception('Simobjects must be re-created via a project')
 
+    def duplicate(self):
+        # Return a copy of the sim with a new UUID
+        self.uuid = str(uuid.uuid4()) # Store UUID as a string - we just want a (practically) unique tag, no advanced functionality
+
 
 #------------------------------------------------------------------------------
-
-
-
-# Derived Sim class that should store parameter overwrites.
-class SimParameter(Sim):
-    def __init__(self, name, project, calibration = None):
-        Sim.__init__(self, name, project, calibration)
-        self.parameter_overrides = []
-
-    def todict(self):
-        simdict = Sim.todict(self)
-        simdict['type'] = 'SimParameter'
-        simdict['parameter_overrides'] = self.parameter_overrides
-        return simdict
-
-    def load_dict(self,simdict):
-        Sim.load_dict(self,simdict)
-        self.parameter_overrides = simdict['parameter_overrides'] 
-
-    def create_override(self,parname,pop,startyear,endyear,startval,endval):
-        # Create override for a single parameter, preserving the current dictionary representation of scenarios
-        # pop can be a population index, or a population shortname. 'all' is a valid shortname
-        #
-        # Also need to validate the parameter name
-        if type(pop) is str:
-            r = self.getproject()
-            poplist = [x['short_name'] for x in r.metadata['populations']] + ['all']
-            try:
-                popidx = poplist.index(pop) + 1 # For some reason (frontend?) these indexes are 1-based rather than 0-based
-            except:
-                print 'Population not found! Valid populations are:'
-                print poplist
-                raise Exception('InvalidPopulation')
-        else:
-            popidx = pop
-
-        assert(type(parname) is list)
-
-        override = {}
-        override['names'] = parname # This can be a list e.g. [u'condom', u'com']??
-        override['pops'] = popidx
-        override['startval'] = startval
-        override['startyear'] = startyear
-        override['endval'] = endval
-        override['endyear'] = endyear
-        self.parameter_overrides.append(override)
-
-    def makemodelpars(self):
-        from numpy import linspace, ndim
-        from nested import getnested, setnested
-        from utils import findinds
-
-        # First, get the base model parameters
-        Sim.makemodelpars(self) 
-
-        # Now compute the overrides as per scenarios.py -> makescenarios()
-        r = self.getproject()
-        for thesepars in self.parameter_overrides:
-            data = getnested(self.parsmodel, thesepars['names'])
-            if ndim(data)>1:
-                if thesepars['pops'] < len(data):
-                    newdata = data[thesepars['pops']] # If it's more than one dimension, use population data too
-                else:
-                    newdata = data[:] # Get all populations
-            else:
-                newdata = data # If it's not, just use the whole thing
-            
-            # Get current values
-            initialindex = findinds(r.options['partvec'], thesepars['startyear'])
-            finalindex = findinds(r.options['partvec'], thesepars['endyear'])
-            
-            if thesepars['startval'] == -1:
-                if ndim(newdata)==1: 
-                    initialvalue = newdata[initialindex]
-                else: 
-                    initialvalue = newdata[:,initialindex].mean(axis=0) # Get the mean if multiple populations
-            else:
-                initialvalue = thesepars['startval']
-            
-            if thesepars['endval'] == -1:
-                if ndim(newdata)==1: 
-                    finalvalue = newdata[finalindex]
-                else: 
-                    finalvalue = newdata[:,finalindex].mean() # Get the mean if multiple populations
-            else:
-                finalvalue = thesepars['endval'] 
-            
-            # Set new values
-            npts = finalindex-initialindex
-            newvalues = linspace(initialvalue, finalvalue, npts)
-            if ndim(newdata)==1:
-                newdata[initialindex:finalindex] = newvalues
-                newdata[finalindex:] = newvalues[-1] # Fill in the rest of the array with the last value
-                if ndim(data)==1:
-                    data = newdata
-                else:
-                    data[thesepars['pops']] = newdata
-            else:
-                for p in xrange(len(newdata)):
-                    newdata[p,initialindex:finalindex] = newvalues
-                    newdata[p,finalindex:] = newvalues[-1] # Fill in the rest of the array with the last value
-            
-            # Update data
-            if ndim(data)>1 and ndim(newdata)==1:
-                data[thesepars['pops']] = newdata # Data is multiple populations, but we're only resetting one
-            else:
-                data = newdata # In all other cases, reset the whole thing (if both are 1D, or if both are 2D
-
-            setnested(self.parsmodel, thesepars['names'], data)
-
-    def __repr__(self):
-        return "SimParameter %s ('%s')" % (self.uuid,self.name)   
-
-
         
 def calculate_totalacts(M):
     eps = 1e-3 # TODO WARNING KLUDGY avoid divide-by-zero
@@ -569,6 +476,7 @@ def reconcileacts(symmetricmatrix,popsize,popacts):
 
     return pshipacts
 
+# Tail imports
 from simbudget import SimBudget
 from simbudget2 import SimBudget2
 from simparameter import SimParameter
