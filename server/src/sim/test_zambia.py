79dd45bfc999840240e037be123269407c650695
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-09-29 23:05:16 +1000

    Updated to run base sim

--- server/src/sim/test_zambia.py
+++ server/src/sim/test_zambia.py
@@ -2,9 +2,5 @@ import optima,liboptima
 from timevarying import timevarying
 
 r = optima.Project.load_json('./projects/zambia-with-new-testing-removed-null-prog.json')
-s = optima.SimBudget2('test',r)
-
-opt = optima.Optimization('Example',r,calibration=r.calibrations[0],programset=r.programsets[0],initial_alloc=r.data['origalloc']) # Make a sim, implicitly selecting a calibration and programset/ccocs
-#opt = optima.Optimization('Example',r,sim=s)
-opt.optimize(timelimit = 5)
-optima.plot([opt.initial_sim,opt.optimized_sim])
+s = optima.Sim('test',r)
+optima.plot(s)
