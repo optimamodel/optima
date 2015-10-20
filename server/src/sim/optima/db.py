359fb1a64b3987db1a69173780cdfeedbe9f1800
Romesh Abeysuriya <romesh.abey@gmail.com>
2015-10-21 09:24:52 +1100

    Added REDIS-like storage function wrappers

---
+++ server/src/sim/optima/db.py
@@ -0,0 +1,56 @@
+import sqlite3
+import os
+import cPickle
+
+def makedb():
+	try:
+		os.remove('optima.db')
+	except:
+		pass
+
+	conn = sqlite3.connect('optima.db')
+	c = conn.cursor()
+	c.execute('CREATE TABLE projects(uuid text, data blob)')
+	c.execute('CREATE TABLE simboxes(uuid text, data blob)')
+	c.execute('CREATE TABLE sims(uuid text, data blob)')
+	c.execute('CREATE TABLE calibrations(uuid text, data blob)')
+	c.execute('CREATE TABLE programsets(uuid text, data blob)')
+
+	conn.commit()
+	conn.close()
+
+def getconn():
+	return sqlite3.connect('optima.db')
+
+def sanitize(table_name):
+	# Prevent SQL injection for table name substitution
+    return ''.join( chr for chr in table_name if chr.isalnum() )
+
+def retrieve(table,uuid):
+	conn = getconn()
+	c = conn.cursor()
+	c.execute('SELECT data FROM %s WHERE %s.uuid = "%s"' % (sanitize(table),sanitize(table),sanitize(uuid)))
+	d = c.fetchall()
+	assert(len(d)==1)
+	datastr = str(d[0][0])
+	conn.commit()
+	conn.close()
+	return cPickle.loads(datastr)
+
+
+def store(table,uuid,data):
+	# data needs to be storable as a cPickle string
+	datastr = cPickle.dumps(data)
+	conn = getconn()
+	c = conn.cursor()
+	c.execute('INSERT OR REPLACE INTO %s (uuid, data) VALUES (?,?)' % (sanitize(table)),(uuid,datastr))
+	conn.commit()
+	conn.close()
+
+	
+# x = ['asdf',1]
+# print x
+# makedb()
+# store('projects','1d83',x)
+# x2 = retrieve('projects','1d83')
+# print x2
