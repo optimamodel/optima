e869536adce71e983dd49a042aa93f2520062b43
cliffckerr <questionably@gmail.com>
2015-10-22 17:45:10 -0500

    hmm, want to see what data structure is

--- optima/parameters.py
+++ optima/parameters.py
@@ -53,38 +53,30 @@ class Parameterset(object):
         ''' Plot all parameters, I guess against data '''
 
 
-    def makeparams(data, verbose=2):
+    def makeparams(self, data, verbose=2):
         """
         Translates the raw data (which were read from the spreadsheet). into
         parameters that can be used in the model. These data are then used to update 
         the corresponding model (project). This method should be called before a 
         simulation is run.
         
-        Version: 2015sep04 by cliffk
+        Version: 2015oct22 by cliffk
         """
         
-        ###############################################################################
-        ## Preliminaries
-        ###############################################################################
-    
-        
         printv('Converting data to parameters...', 1, verbose)
-        
-        
         
         
         ###############################################################################
         ## Loop over quantities
         ###############################################################################
         
-        params = dict() # Initialize parameters structure
-        
         ## Key parameters
-        for parname in data['key'].keys():
-            printv('Converting data parameter %s...' % parname, 2, verbose)
-            params[parname] = dataindex(data['key'][parname][0], 0) # Population size and prevalence -- # TODO: use uncertainties!
+        self.popsize = dataindex(data['popsize'][0], 0) # WARNING, will want to change
+        self.hivprev = dataindex(data['hivprev'][0], 0) # WARNING, will want to change
         
         ## Loop over parameters that can be converted automatically
+        self.numfirstline = data2par(data[parclass][parname], usetime=True)
+        self.
         for parclass in ['epi', 'txrx', 'sex', 'inj']:
             printv('Converting data parameter %s...' % parclass, 3, verbose)
             for parname in data[parclass].keys():
@@ -160,18 +152,19 @@ class Parameterset(object):
 def data2par(dataarray, usetime=True):
     """ Take an array of data and turn it into default parameters -- here, just take the means """
     nrows = shape(dataarray)[0] # See how many rows need to be filled (either npops, nprogs, or 1)
-    output = dict() # Create structure
-    output['p'] = [0]*nrows # Initialize array for holding population parameters
+    par = Parameter() # Create structure
+    par.m = 1 # Set metaparameter to 1
+    par.y = [0]*nrows # Initialize array for holding population parameters
     if usetime:
-        output['t'] = [0]*nrows # Initialize array for holding time parameters
+        par.t = [0]*nrows # Initialize array for holding time parameters
         for r in range(nrows): 
             validdata = ~isnan(dataarray[r])
             if sum(validdata): # There's at least one data point
-                output['p'][r] = sanitize(dataarray[r]) # Store each extant value
-                output['t'][r] = ar(data['epiyears'])[~isnan(dataarray[r])] # Store each year
+                par.y[r] = sanitize(dataarray[r]) # Store each extant value
+                par.t[r] = ar(data['epiyears'])[~isnan(dataarray[r])] # Store each year
             else: # Blank, assume zero
-                output['p'][r] = ar([0])
-                output['t'][r] = ar([0])
+                par.y[r] = ar([0])
+                par.t[r] = ar([0])
 
     else:
         print('TMP6666')
@@ -179,7 +172,9 @@ def data2par(dataarray, usetime=True):
             output['p'][r] = mean(sanitize(dataarray[r])) # Calculate mean for each population
             print('TMP223')
     
-    return output
+    return par
+
+
 
 def dataindex(dataarray, index):
     """ Take an array of data return either the first or last (...or some other) non-NaN entry """
