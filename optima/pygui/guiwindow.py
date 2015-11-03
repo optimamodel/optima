7fb98f3e62311861b6f864c248fbfab87b0854a5
cliffckerr <questionably@gmail.com>
2015-11-02 21:22:50 -0800

    created template with actual components

---
+++ optima/pygui/guiwindow.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'window.ui'
+#
+# Created: Mon Nov  2 21:22:29 2015
+#      by: PyQt4 UI code generator 4.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName(_fromUtf8("MainWindow"))
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtGui.QWidget(MainWindow)
+        self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.checkBox_2 = QtGui.QCheckBox(self.centralwidget)
+        self.checkBox_2.setObjectName(_fromUtf8("checkBox_2"))
+        self.gridLayout.addWidget(self.checkBox_2, 1, 2, 1, 1)
+        self.mplfigs = QtGui.QListWidget(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.mplfigs.sizePolicy().hasHeightForWidth())
+        self.mplfigs.setSizePolicy(sizePolicy)
+        self.mplfigs.setMaximumSize(QtCore.QSize(200, 16777215))
+        self.mplfigs.setObjectName(_fromUtf8("mplfigs"))
+        self.gridLayout.addWidget(self.mplfigs, 2, 2, 1, 1)
+        self.pushButton = QtGui.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName(_fromUtf8("pushButton"))
+        self.gridLayout.addWidget(self.pushButton, 3, 2, 1, 1)
+        self.checkBox = QtGui.QCheckBox(self.centralwidget)
+        self.checkBox.setObjectName(_fromUtf8("checkBox"))
+        self.gridLayout.addWidget(self.checkBox, 0, 2, 1, 1)
+        self.mplwindow = QtGui.QWidget(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.mplwindow.sizePolicy().hasHeightForWidth())
+        self.mplwindow.setSizePolicy(sizePolicy)
+        self.mplwindow.setObjectName(_fromUtf8("mplwindow"))
+        self.mplvl = QtGui.QVBoxLayout(self.mplwindow)
+        self.mplvl.setMargin(0)
+        self.mplvl.setObjectName(_fromUtf8("mplvl"))
+        self.gridLayout.addWidget(self.mplwindow, 1, 3, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow", None))
+        self.checkBox_2.setText(_translate("MainWindow", "CheckBox", None))
+        self.pushButton.setText(_translate("MainWindow", "Close", None))
+        self.checkBox.setText(_translate("MainWindow", "CheckBox", None))
+
