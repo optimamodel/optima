a89591ccdb297be8c259d304627976a0ec37de20
cliffckerr <questionably@gmail.com>
2015-11-19 13:54:01 -0500

    just realized that this is a terrible, circular idea

--- optima/optima.py
+++ optima/optima.py
@@ -8,25 +8,30 @@ import optima as op
 or
 from optima import *
 
+
+
 Version: 2015nov19
 """
 
-## Top-level imports
+## Load general modules
 from uuid import uuid4 as uuid
 from datetime import datetime; today = datetime.today
 from copy import deepcopy as dcp
 
-## Load general modules
-from numpy import array # TEMP?
-
-## Load classes
-from project import *
-from settings import *
-from parameters import *
-
-## Load other Optima functions
+## Load non-Optima-specific functions
 from ballsd import *
 from colortools import *
+from utils import *
+
+## Load Optima classes
+from project import *
+from parameters import *
+from settings import *
+
+## Load other Optima functions
 from loadspreadsheet import *
 from model import *
-from utils import *
+
+## Optional imports
+try: from gui import *
+except:pass
