a3a68a08924e37c6ae872ce03ad1ba943d43b56d
cliffckerr <questionably@gmail.com>
2015-11-21 16:33:42 -0500

    converted to using odicts

--- optima/project.py
+++ optima/project.py
@@ -11,7 +11,7 @@ Version: 2015nov02 by cliffk
 ## Header -- imports and version
 #######################################################################################################
 
-from optima import Settings, Parameterset, loadspreadsheet, model, run, getdate, today, uuid, dcp
+from optima import odict, Settings, Parameterset, loadspreadsheet, model, run, getdate, today, uuid, dcp
 from numpy import array
 
 ## Specify the version, for the purposes of figuring out which version was used to create a project
@@ -59,10 +59,10 @@ class Project(object):
         ''' Initialize the project ''' 
 
         ## Define the structure sets
-        self.parsets = {}
-        self.respsets = {}
-        self.scens = {}
-        self.optims = {}
+        self.parsets = odict()
+        self.respsets = odict()
+        self.scens = odict()
+        self.optims = odict()
         
         ## Define other quantities
         self.name = name
---
+++ optima/testutils.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+TESTUTILS
+
+A little script for testing various utilities.
+
+Version: 2015nov21 by cliffk
+"""
+
+
+## Define tests to run here!!!
+tests = [
+'odict',
+]
+
+
+
+##############################################################################
+## Initialization
+##############################################################################
+
+from utils import tic, toc, blank, pd # analysis:ignore
+
+def done(t=0):
+    print('Done.')
+    toc(t)
+    blank()
+    
+
+
+
+
+
+blank()
+print('Running tests:')
+for i,test in enumerate(tests): print(('%i.  '+test) % (i+1))
+blank()
+
+
+
+
+##############################################################################
+## The tests
+##############################################################################
+
+T = tic()
+
+
+
+## Spreadsheet creation test
+if 'odict' in tests:
+    t = tic()
+    print('Running odict tests...')
+    from optima import odict
+    foo = odict({'ah':3,'boo':4, 'cough':6, 'dill': 8})
+    bar = foo.sort() # Sort the list
+    assert(bar['boo'] == 4) # Show get item by value
+    assert(bar[1] == 4) # Show get item by index
+    assert(bar[0:1] == [3,4]) # Show get item by slice
+    assert(bar['cough':'dill'] == [6,8]) # Show alternate slice notation
+    assert(bar[[2,1]] == [6,4]) # Show get item by list
+    bar[3] = [3,4,5] # Show assignment by item
+    bar[0:1] = ['the', 'power'] # Show assignment by slice -- NOTE, inclusive slice!!
+    bar.rename('cough','chill') # Show rename
+    print(bar) # Print results
+    done(t)
