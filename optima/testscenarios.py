bebd79879731a9c0bd70f99c3335ed9e9279817d
cliffckerr <questionably@gmail.com>
2015-11-23 16:43:01 -0500

    added more informative error message to utils.py

--- optima/testscenarios.py
+++ optima/testscenarios.py
@@ -5,4 +5,41 @@ Created on Mon Nov 23 16:24:45 2015
 @author: cliffk
 """
 
-k
+scenlist = [
+{u'name': u'Current conditions', u'pars': []},
+ {u'name': u'less fsw condom',
+  u'pars': [{u'endval': 0.1,
+    u'endyear': 2015,
+    u'names': [u'condom', u'com'],
+    u'pops': 0,
+    u'startval': 0.1,
+    u'startyear': 2005}]},
+ {u'name': u'less reg cond',
+  u'pars': [{u'endval': 0.01,
+    u'endyear': 2015,
+    u'names': [u'condom', u'reg'],
+    u'pops': 11,
+    u'startval': 0.01,
+    u'startyear': 2005}]},
+ {u'name': u'more comm acts',
+  u'pars': [{u'endval': 1000.0000000000001,
+    u'endyear': 2015,
+    u'names': [u'numacts', u'com'],
+    u'pops': 0,
+    u'startval': 1000.0000000000001,
+    u'startyear': 2005}]},
+ {u'name': u'reg acts',
+  u'pars': [{u'endval': 0,
+    u'endyear': 2015,
+    u'names': [u'numacts', u'reg'],
+    u'pops': 11,
+    u'startval': 200,
+    u'startyear': 2005}]},
+ {u'name': u'discharge-all',
+  u'pars': [{u'endval': 0.5,
+    u'endyear': 2015,
+    u'names': [u'stiprevdis'],
+    u'pops': 11,
+    u'startval': 0.5,
+    u'startyear': 2005}]}
+]
