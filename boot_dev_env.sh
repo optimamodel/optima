73875a4717fd7aed46b443d027ee2a25785c1f3a
Nik Graf <nik@nikgraf.com>
2015-11-25 17:20:32 +0100

    chore(dev env): add boot script

---
+++ boot_dev_env.sh
@@ -0,0 +1,28 @@
+osascript 2>/dev/null <<EOF
+    tell application "System Events"
+        tell process "Terminal" to keystroke "t" using command down
+    end
+    tell application "Terminal"
+        activate
+        do script with command "postgres -D /usr/local/var/postgres" in window 0
+    end
+EOF
+sleep 2
+osascript 2>/dev/null <<EOF
+    tell application "System Events"
+        tell process "Terminal" to keystroke "t" using command down
+    end
+    tell application "Terminal"
+        activate
+        do script with command "cd \"$PWD/gui\"; $*" in window 0
+        do script with command "./clean_dev_build.sh" in window 0
+    end
+    tell application "System Events"
+        tell process "Terminal" to keystroke "t" using command down
+    end
+    tell application "Terminal"
+        activate
+        do script with command "cd \"$PWD/server\"; $*" in window 0
+        do script with command "./run.sh" in window 0
+    end
+EOF
