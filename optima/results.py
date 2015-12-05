76f5160a72ff71a35abd6eacc5b97b836f2a7d21
cliffckerr <questionably@gmail.com>
2015-12-04 23:42:29 -0500

    working on results dict

--- optima/results.py
+++ optima/results.py
@@ -72,25 +72,29 @@ class Results(object):
         quantiles = [0.5]
         allpeople = array([self.people]) 
         
-        self.prev.pops = quantile(allpeople[:,1:,:,:].sum(axis=1) / allpeople[:,:,:,:].sum(axis=1), quantiles=quantiles) # Axis 1 is health state
-        self.prev.tot = quantile(allpeople[:,1:,:,:].sum(axis=(1,2)) / allpeople[:,:,:,:].sum(axis=(1,2)), quantiles=quantiles) # Axis 2 is populations
+        self.main['prev'].pops = quantile(allpeople[:,1:,:,:].sum(axis=1) / allpeople[:,:,:,:].sum(axis=1), quantiles=quantiles) # Axis 1 is health state
+        self.main['prev'].tot = quantile(allpeople[:,1:,:,:].sum(axis=(1,2)) / allpeople[:,:,:,:].sum(axis=(1,2)), quantiles=quantiles) # Axis 2 is populations
         
-        self.numplhiv.pops = quantile(allpeople[:,1:,:,:].sum(axis=1), quantiles=quantiles) # Axis 1 is health state
-        self.numplhiv.tot = quantile(allpeople[:,1:,:,:].sum(axis=(1,2)), quantiles=quantiles) # Axis 2 is populations
+        self.main['numplhiv'].pops = quantile(allpeople[:,1:,:,:].sum(axis=1), quantiles=quantiles) # Axis 1 is health state
+        self.main['numplhiv'].tot = quantile(allpeople[:,1:,:,:].sum(axis=(1,2)), quantiles=quantiles) # Axis 2 is populations
         
         allinci = array([self.inci])
-        self.numinci.pops = quantile(allinci, quantiles=quantiles)
-        self.numinci.tot = quantile(allinci.sum(axis=1), quantiles=quantiles) # Axis 1 is populations
+        self.main['numinci'].pops = quantile(allinci, quantiles=quantiles)
+        self.main['numinci'].tot = quantile(allinci.sum(axis=1), quantiles=quantiles) # Axis 1 is populations
 
 
         allinci = array([self.inci])
-        self.force.pops = quantile(allinci / allpeople[:,:,:,:].sum(axis=1), quantiles=quantiles) # Axis 1 is health state
-        self.force.tot = quantile(allinci.sum(axis=1) / allpeople[:,:,:,:].sum(axis=(1,2)), quantiles=quantiles) # Axis 2 is populations
+        self.main['force'].pops = quantile(allinci / allpeople[:,:,:,:].sum(axis=1), quantiles=quantiles) # Axis 1 is health state
+        self.main['force'].tot = quantile(allinci.sum(axis=1) / allpeople[:,:,:,:].sum(axis=(1,2)), quantiles=quantiles) # Axis 2 is populations
     
     
         alldeaths = array([self.death])
-        self.numdeath.pops = quantile(alldeaths, quantiles=quantiles)
-        self.numdeath.tot = quantile(alldeaths.sum(axis=1), quantiles=quantiles) # Axis 1 is populations
+        self.main['numdeath'].pops = quantile(alldeaths, quantiles=quantiles)
+        self.main['numdeath'].tot = quantile(alldeaths.sum(axis=1), quantiles=quantiles) # Axis 1 is populations
+
+        alldx = array([self.dx])
+        self.main['numdiag'].pops = quantile(alldx, quantiles=quantiles)
+        self.main['numdiag'].tot = quantile(alldx.sum(axis=1), quantiles=quantiles) # Axis 1 is populations
 
 # WARNING, need to implement
 #        disutils = [D[self.pars['const']['disutil'][key] for key in D['G['healthstates']]
@@ -104,9 +108,7 @@ class Results(object):
 #        self.daly.tot = quantile(tmpdalytot, quantiles=quantiles)
         
     
-        alldx = array([self.dx])
-        self.numdiag.pops = quantile(alldx, quantiles=quantiles)
-        self.numdiag.tot = quantile(alldx.sum(axis=1), quantiles=quantiles) # Axis 1 is populations
+
     
     
     def makeplots(self, whichplots=None, verbose=2):
