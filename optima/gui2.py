3a4eefa9ee03da6cd322705f00760e697baa4633
cliffckerr <questionably@gmail.com>
2015-12-05 17:56:45 -0500

    layout doesnt work but otherwise it does!

--- optima/gui2.py
+++ optima/gui2.py
@@ -7,8 +7,9 @@ results = P.runsim('default')
 
 
 #def gui(results):
-from pylab import subplots, subplots_adjust, axes, ceil, sqrt, close, isinteractive, ion, ioff, array
+from pylab import subplots, subplots_adjust, axes, ceil, sqrt, array
 from matplotlib.widgets import CheckButtons
+global currentaxes
 
 # Define options for selection
 epikeys = results.main.keys()
@@ -19,9 +20,9 @@ for key in epikeys:
         checkboxes.append(key+'-'+subkey)
 nboxes = len(checkboxes)
 
-fig, tmp = subplots()
-fig._axstack.remove(tmp)
-subplots_adjust(right=0.8)
+fig, currentaxes = subplots()
+if type(currentaxes)!=list: currentaxes = [currentaxes]
+subplots_adjust(right=0.5)
 rax = axes([0.82, 0.05, 0.17, 0.9])
 
 check = CheckButtons(rax, checkboxes, [False]*nboxes)
@@ -35,6 +36,7 @@ def getchecked(check):
     return ischecked
 
 def update(label):
+    global currentaxes
     ischecked = getchecked(check)
     toplot = array(checkboxes)[array(ischecked)].tolist() # Use logical indexing to get names to plot
     
@@ -43,11 +45,16 @@ def update(label):
     nrows = int(ceil(sqrt(nplots)))
     ncols = nrows-1 if nrows*(nrows-1)>=nplots else nrows
     
+    print('hi!')
+    print nplots
+    
+    for ax in currentaxes:
+        fig._axstack.remove(ax)
+    currentaxes = []
+    
     # Do plotting
     if nplots>0: # Don't do anything if no plots
-#        wasinteractive = isinteractive()
-#        if wasinteractive: ioff()
-        height,width = fig.get_size_inches()
+        width,height = fig.get_size_inches()
 #        fig, fakeaxes = subplots(ncols, nrows, sharex='all', figsize=(height, width)) # Create figure with correct number of plots
 #        close(fig) # Close unneeded figure
 #        if wasinteractive: ion()
@@ -60,35 +67,7 @@ def update(label):
             thisplot = plots[p].axes[0]
             fig._axstack.add(fig._make_key(thisplot), thisplot)
             thisplot.change_geometry(nrows,ncols,p+1)
-#    else:
-#        fig = figure() # Blank figure
+            currentaxes.append(thisplot)
 
 
 check.on_clicked(update)
-
-#import numpy as np
-#import matplotlib.pyplot as plt
-#from matplotlib.widgets import CheckButtons
-#
-#t = np.arange(0.0, 2.0, 0.01)
-#s0 = np.sin(2*np.pi*t)
-#s1 = np.sin(4*np.pi*t)
-#s2 = np.sin(6*np.pi*t)
-#
-#
-#
-#
-#def func(label):
-#    isticked = []
-#    for box in range(len(check.lines)):
-#        isticked.append(check.lines[box][0].get_visible())
-#    
-#    ax.cla()
-#    if isticked[0]: ax.plot(t, s0, lw=2)
-#    if isticked[1]: ax.plot(t, s1, lw=2)
-#    if isticked[2]: ax.plot(t, s2, lw=2)
-#
-#    plt.draw()
-#check.on_clicked(func)
-#
-#plt.show()
