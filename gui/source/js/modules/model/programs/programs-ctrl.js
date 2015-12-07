617d37ed2e8a1256e5c5238a3e31fce6c4bf889b
jpuri <jyotipuri@gmail.com>
2015-12-07 10:09:20 +0530

    feat(ManagePrograms): add new route and page for manage programs

---
+++ gui/source/js/modules/model/programs/programs-ctrl.js
@@ -0,0 +1,7 @@
+define(['./module', 'angular', 'underscore'], function (module, angular, _) {
+  'use strict';
+
+  module.controller('ProgramsController', function ($scope, $http) {
+    console.log('into manage responses');
+  });
+});
