253a47d831722b14633099bbb466ccbcf61ff0b1
jpuri <jyotipuri@gmail.com>
2015-12-07 13:56:17 +0530

    feat(ManageResponse): adding popup to edit response name

--- gui/source/js/modules/model/response-ctrl.js
+++ gui/source/js/modules/model/response-ctrl.js
@@ -1,21 +1,38 @@
 define(['./module', 'angular', 'underscore'], function (module, angular, _) {
   'use strict';
 
-  module.controller('ResponseController', function ($scope, $http, modalService) {
+  module.controller('ResponseController', function ($scope, $http, modalService, $timeout) {
 
+    const defaultResponse = {name:'Default'};
     $scope.state = {
-      responses: [{name:'Default'}],
-      activeResponseName: 'Default'
+      responses: [defaultResponse],
+      activeResponse: defaultResponse
     };
 
     $scope.addResponse = function () {
       var add = function (name) {
-        $scope.state.responses[$scope.state.responses.length] = {name:name};
-        this.state.activeResponseName = name;
+        const addedResponse = {name:name};
+        $scope.state.responses[$scope.state.responses.length] = addedResponse;
+        $scope.state.activeResponse = addedResponse;
       };
 
       modalService.addResponse( add.bind(this) , $scope.state.responses);
     };
 
+    $scope.editResponse = function () {
+      // This removing existing enter from array of responses and re-adding it with updated name was needed,
+      // coz it turns out that angular does not refreshes the select unless there is change in its size.
+      // https://github.com/angular/angular.js/issues/10939
+      var edit = function (name) {
+        $scope.state.responses = _.filter($scope.state.responses, function(response) {
+          return response.name !== $scope.state.activeResponse.name;
+        });
+        $timeout(function(){
+          $scope.state.activeResponse.name = name;
+          $scope.state.responses[$scope.state.responses.length] = $scope.state.activeResponse;
+        });
+      };
+      modalService.editResponse($scope.state.activeResponse.name, edit.bind(this) , $scope.state.responses);
+    };
   });
 });
