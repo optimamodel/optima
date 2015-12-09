34609c9fc03879d78b52c2606c409fa607498638
cliffckerr <questionably@gmail.com>
2015-12-08 20:34:47 -0500

    sort of working, but no results yet

--- optima/gui3.py
+++ optima/gui3.py
@@ -7,18 +7,21 @@ and when "Update" is clicked, will clear the contents of the plotting window and
 Version: 2015dec08 by cliffk
 '''
 
+
+## TEMP
 print('Running GUI test...')
 from optima import Project
 
 P = Project(spreadsheet='test.xlsx')
 results = P.runsim('default')
+## /TEMP
+
 
 
 
 #def gui(results):
-from pylab import subplots, subplots_adjust, axes, ceil, sqrt, array
-from matplotlib.widgets import CheckButtons
-global currentaxes
+from pylab import subplots, subplots_adjust, axes, ceil, sqrt, array, figure
+from matplotlib.widgets import CheckButtons, Button
 
 # Define options for selection
 epikeys = results.main.keys()
@@ -29,12 +32,15 @@ for key in epikeys:
         checkboxes.append(key+'-'+subkey)
 nboxes = len(checkboxes)
 
-fig, currentaxes = subplots()
-if type(currentaxes)!=list: currentaxes = [currentaxes]
-subplots_adjust(right=0.5)
-rax = axes([0.82, 0.05, 0.17, 0.9])
+# Set up control panel
+controlfig = figure(figsize=(4,8))
+checkboxaxes = axes([0.1, 0.15, 0.8, 0.8])
+buttonaxes = axes([0.1, 0.05, 0.8, 0.08])
+check = CheckButtons(checkboxaxes, checkboxes, [False]*nboxes)
+button = Button(buttonaxes, 'Update') 
 
-check = CheckButtons(rax, checkboxes, [False]*nboxes)
+# Set up results panel
+plotfig = figure()
 
 
 def getchecked(check):
@@ -44,8 +50,10 @@ def getchecked(check):
         ischecked.append(check.lines[box][0].get_visible()) # Stupid way of figuring out if a box is ticked or not
     return ischecked
 
-def update(label):
-    global currentaxes
+
+def update(event):
+    plotfig.clear() # Clear figure
+    
     ischecked = getchecked(check)
     toplot = array(checkboxes)[array(ischecked)].tolist() # Use logical indexing to get names to plot
     
@@ -54,19 +62,12 @@ def update(label):
     nrows = int(ceil(sqrt(nplots)))
     ncols = nrows-1 if nrows*(nrows-1)>=nplots else nrows
     
-    print('hi!')
-    print nplots
-    
-    for ax in currentaxes:
-        fig._axstack.remove(ax)
-    currentaxes = []
-    
     # Do plotting
     if nplots>0: # Don't do anything if no plots
-        width,height = fig.get_size_inches()
-#        fig, fakeaxes = subplots(ncols, nrows, sharex='all', figsize=(height, width)) # Create figure with correct number of plots
-#        close(fig) # Close unneeded figure
-#        if wasinteractive: ion()
+        width,height = plotfig.get_size_inches()
+        tmpfig, fakeaxes = subplots(ncols, nrows, sharex='all', figsize=(height, width)) # Create figure with correct number of plots
+        close(tmpfig) # Close unneeded figure
+        if wasinteractive: ion()
 #        for fa in array(fakeaxes).flatten(): fig._axstack.remove(fa) # Remove placeholder axes
         
         # Actually create plots
@@ -74,9 +75,9 @@ def update(label):
 
         for p in range(len(plots)):
             thisplot = plots[p].axes[0]
-            fig._axstack.add(fig._make_key(thisplot), thisplot)
+            plotfig._axstack.add(plotfig._make_key(thisplot), thisplot)
             thisplot.change_geometry(nrows,ncols,p+1)
-            currentaxes.append(thisplot)
+#            currentaxes.append(thisplot)
 
-
-check.on_clicked(update)
+# Update figure if button is clicked
+button.on_clicked(update)
--- optima/untitled4.py
+++ optima/untitled4.py
@@ -1,50 +1,35 @@
-# local modules
-from pylab import *
-import matplotlib.pyplot as plt
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib.widgets import Button 
 
-figsize = (4,3)
-fig1 = figure()
-plot([3,4,7])
-ax1 = fig1.axes[0]
+freqs = np.arange(2, 20, 3) 
+fig, ax = plt.subplots() 
+plt.subplots_adjust(bottom=0.2) 
+t = np.arange(0.0, 1.0, 0.001) 
+s = np.sin(2*np.pi*freqs[0]*t) 
+l, = plt.plot(t, s, lw=2) 
 
-fig2 = figure()
-plot([15,23,7])
-ax2 = fig2.axes[0]
+class Index(object): 
+    ind = 0 
+    def next(self, event): 
+        self.ind += 1 
+        i = self.ind % len(freqs) 
+        ydata = np.sin(2*np.pi*freqs[i]*t) 
+        l.set_ydata(ydata) 
+        plt.draw() 
+        
+    def prev(self, event): 
+        self.ind -= 1 
+        i = self.ind % len(freqs) 
+        ydata = np.sin(2*np.pi*freqs[i]*t) 
+        l.set_ydata(ydata) 
+        plt.draw() 
 
-fig3, (ax3, ax4) = plt.subplots(1,2)
-
-fig3._axstack.remove( ax3)
-fig3._axstack.remove( ax4)
-ax1.set_subplotspec( ax3.get_subplotspec() )
-ax2.set_subplotspec( ax4.get_subplotspec() )
-
-
-#ax1.set_figure(fig3)
-#ax2.set_figure(fig3)
-
-ax1.set_axes_locator(ax3.get_axes_locator())
-
-
-fig3._axstack.add(fig1._make_key(ax1), ax1)
-fig3._axstack.add(fig2._make_key(ax2), ax2)
-ax1.set_xscale('log')
-ax2.set_xscale('log')
-ax2.yaxis.set_ticks(())
-
-# If possible (easy access to plotting data) use
-# ax3.plot(x, y1)
-# ax4.lpot(x, y2)
-
-
-# Add legend
-#for line3 in ax3.lines:
-#    fig3.legend((line3,),
-#                ('label 3',),
-#                loc = 'upper center',
-#                bbox_to_anchor = [0.5, -0.05])
-# Make space for the legend beneath the subplots
-plt.subplots_adjust(bottom = 0.2)
-# Show only fig3
-fig3.set_size_inches( fig1.get_size_inches() )
-fig3.show()
-#plot_defaults.savefig( fig_z07, small=False, figsize=figsize )
+callback = Index() 
+axprev = plt.axes([0.7, 0.05, 0.1, 0.075]) 
+axnext = plt.axes([0.81, 0.05, 0.1, 0.075]) 
+bnext = Button(axnext, 'Next') 
+bnext.on_clicked(callback.next) 
+bprev = Button(axprev, 'Previous') 
+bprev.on_clicked(callback.prev) 
+plt.show()
