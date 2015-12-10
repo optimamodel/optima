c4c78707ee314483f9cdbdca942d9be05d9f4682
jpuri <jyotipuri@gmail.com>
2015-12-10 15:54:09 +0530

    feat(ProgramSet): add warning message for not allowing edit, delete and copying if no program is currently active

--- client/source/js/modules/model/program-set/program-set-ctrl.js
+++ client/source/js/modules/model/program-set/program-set-ctrl.js
@@ -21,43 +21,57 @@ define(['./../module', 'angular', 'underscore'], function (module, angular, _) {
     };
 
     $scope.editProgramSet = function () {
-      // This removing existing enter from array of programSetList and re-adding it with updated name was needed,
-      // coz it turns out that angular does not refreshes the select unless there is change in its size.
-      // https://github.com/angular/angular.js/issues/10939
-      var edit = function (name) {
-        $scope.state.programSetList = _.filter($scope.state.programSetList, function(programSet) {
-          return programSet.name !== $scope.state.activeProgramSet.name;
-        });
-        $timeout(function(){
-          $scope.state.activeProgramSet.name = name;
-          $scope.state.programSetList[$scope.state.programSetList.length] = $scope.state.activeProgramSet;
-        });
-      };
-      programSetModalService.editProgramSet($scope.state.activeProgramSet.name, edit , $scope.state.programSetList, 'Edit program set', true);
+      if (!$scope.state.activeProgramSet) {
+        modalService.informError([{message: 'No program set selected.'}]);
+      } else {
+        // This removing existing enter from array of programSetList and re-adding it with updated name was needed,
+        // coz it turns out that angular does not refreshes the select unless there is change in its size.
+        // https://github.com/angular/angular.js/issues/10939
+        var edit = function (name) {
+          $scope.state.programSetList = _.filter($scope.state.programSetList, function (programSet) {
+            return programSet.name !== $scope.state.activeProgramSet.name;
+          });
+          $timeout(function () {
+            $scope.state.activeProgramSet.name = name;
+            $scope.state.programSetList[$scope.state.programSetList.length] = $scope.state.activeProgramSet;
+          });
+        };
+        programSetModalService.editProgramSet($scope.state.activeProgramSet.name, edit, $scope.state.programSetList, 'Edit program set', true);
+      }
     };
 
     $scope.deleteProgramSet = function () {
-      var remove = function() {
-        $scope.state.programSetList = _.filter($scope.state.programSetList, function(programSet) {
-          return programSet.name !== $scope.state.activeProgramSet.name;
-        });
-        $scope.state.activeProgramSet = $scope.state.programSetList ? $scope.state.programSetList[0] : void 0;
-      };
-      modalService.confirm(
-        function(){remove()}, function(){}, 'Yes, remove this program set', 'No',
-        'Are you sure you want to permanently remove program set "' + $scope.state.activeProgramSet.name + '"?',
-        'Delete program set'
-      );
+      if (!$scope.state.activeProgramSet) {
+        modalService.informError([{message: 'No program set selected.'}]);
+      } else {
+        var remove = function () {
+          $scope.state.programSetList = _.filter($scope.state.programSetList, function (programSet) {
+            return programSet.name !== $scope.state.activeProgramSet.name;
+          });
+          $scope.state.activeProgramSet = $scope.state.programSetList ? $scope.state.programSetList[0] : void 0;
+        };
+        modalService.confirm(
+          function () {
+            remove()
+          }, function () {
+          }, 'Yes, remove this program set', 'No',
+          'Are you sure you want to permanently remove program set "' + $scope.state.activeProgramSet.name + '"?',
+          'Delete program set'
+        );
+      }
     };
 
     $scope.copyProgramSet = function () {
-      // TODO: program details to also be copied
-      var copy = function (name) {
-        const copiedProgramSet = {name:name};
-        $scope.state.programSetList[$scope.state.programSetList.length] = copiedProgramSet;
-        $scope.state.activeProgramSet = copiedProgramSet;
-      };
-      programSetModalService.copyProgramSet($scope.state.activeProgramSet.name, copy.bind(this) , $scope.state.programSetList);
+      if (!$scope.state.activeProgramSet) {
+        modalService.informError([{message: 'No program set selected.'}]);
+      } else {
+        var copy = function (name) {
+          const copiedProgramSet = {name: name};
+          $scope.state.programSetList[$scope.state.programSetList.length] = copiedProgramSet;
+          $scope.state.activeProgramSet = copiedProgramSet;
+        };
+        programSetModalService.copyProgramSet($scope.state.activeProgramSet.name, copy.bind(this), $scope.state.programSetList);
+      }
     };
 
     $scope.categories = predefined.categories;
@@ -71,7 +85,7 @@ define(['./../module', 'angular', 'underscore'], function (module, angular, _) {
         $event.preventDefault();
       }
 
-      return programSetModalService.openProgramModal(program, predefined, availableParameters).result.then(
+      return programSetModalService.openProgramModal(program, predefined, availableParameters.data.parameters).result.then(
         function (newProgram) {
           _(program).extend(newProgram);
         }
@@ -90,7 +104,7 @@ define(['./../module', 'angular', 'underscore'], function (module, angular, _) {
       }
       var program = {};
 
-      return programSetModalService.openProgramModal(program, predefined, availableParameters).result.then(
+      return programSetModalService.openProgramModal(program, predefined, availableParameters.data.parameters).result.then(
         function (newProgram) {
           $scope.programs.push(newProgram);
         }
