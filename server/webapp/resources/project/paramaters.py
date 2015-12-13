2fb87587b9ba74455a8f90c1a5d3db9c82ca8178
Fauzan Emmerling <erich@emfeld.com>
2015-12-13 23:31:11 +0700

    Adjusted parameters output fields

--- server/webapp/resources/project/paramaters.py
+++ server/webapp/resources/project/paramaters.py
@@ -1,14 +1,20 @@
-from flask import jsonify
 from flask.ext.login import login_required
 from flask_restful import Resource
+from flask_restful import marshal_with
+from flask_restful_swagger import swagger
+from server.webapp.fields import Json
+
+result_fields = {
+    'parameters': Json
+}
 
 
 class Parameters(Resource):
-
-    # @swagger.operation(
-    #     responseClass=ProjectDb.__name__,
-    #     summary='Create a new Project'
-    # )
+    @swagger.operation(
+        responseClass=None,
+        summary='Gives back project parameters (modifiable)'
+    )
+    @marshal_with(result_fields)
     @login_required
     def get(self):
         """
@@ -17,4 +23,7 @@ class Parameters(Resource):
         from parameters import parameters
         project_parameters = [
             p for p in parameters() if 'modifiable' in p and p['modifiable']]
-        return jsonify({"parameters": project_parameters}), 200
+        payload = {
+            'parameters': project_parameters
+        }
+        return payload
