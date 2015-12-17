d9ebb6d3c6fc7b8676a2cb996f91611fa8e45a4c
cliffckerr <questionably@gmail.com>
2015-12-17 06:22:29 -0800

    reverted change to make everything a 2d array

--- optima/parameters.py
+++ optima/parameters.py
@@ -319,6 +319,7 @@ class Timepar(object):
         output = zeros((npops,len(tvec)))
         for pop,key in enumerate(keys): # Loop over each population, always returning an [npops x npts] array
             output[pop,:] = self.m * smoothinterp(tvec, self.t[pop], self.y[pop], smoothness=smoothness) # Use interpolation
+        if npops==1: return output[0,:]
         else: return output
 
 
