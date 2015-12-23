775c240ef6e4f957b8d9250c2982e24997a6c2d3
cliffckerr <questionably@gmail.com>
2015-12-23 04:15:48 -0800

    trying adding to gui now...

--- optima/gui.py
+++ optima/gui.py
@@ -102,14 +102,46 @@ def gui(results):
 
 
 
+
+
+
+
+
+
+
 def browser(results):
     ''' Create an mpld3 GUI '''
-    wasinteractive = isinteractive() # Get current state of interactivity
-    if not(wasinteractive): ion()
     import mpld3
+    import json
+
+    wasinteractive = isinteractive() # Get current state of interactivity
+    if wasinteractive: ioff()
+    
+    html = '''
+    <html><body>
+    !MAKE DIVS!
+    <script>function mpld3_load_lib(url, callback){var s = document.createElement('script'); s.src = url; s.async = true; s.onreadystatechange = s.onload = callback; s.onerror = function(){console.warn("failed to load library " + url);}; document.getElementsByTagName("head")[0].appendChild(s)} mpld3_load_lib("https://mpld3.github.io/js/d3.v3.min.js", function(){mpld3_load_lib("https://mpld3.github.io/js/mpld3.v0.3git.js", function(){
+    !DRAW FIGURES!
+    })});
+    </script></body></html>
+    '''
+
     figs = []
+    jsons = []
     plots = epiplot(results)
-    for p in range(len(plots)): 
+    nplots = len(plots)
+    for p in range(nplots): 
         figs.append(figure())
         addplot(figs[-1], plots[p].axes[0])
-        mpld3.display(fig=figs[-1])
+        jsons.append(str(json.dumps(mpld3.fig_to_dict(figs[-1]))))
+    
+    divstr = ''
+    jsonstr = ''
+    for p in range(nplots):
+        divstr += '<div id="fig%i"></div>\n' % p
+        jsonstr += 'mpld3.draw_figure("fig%i", %s);\n' % (p, jsons[p])
+    
+    html = html.replace('!MAKE DIVS!',divstr)
+    html = html.replace('!DRAW FIGURES!',jsonstr)
+    
+    mpld3._server.serve(html, ip='127.0.0.1', port=8888, n_retries=50, files=None, open_browser=True, http_server=None)
