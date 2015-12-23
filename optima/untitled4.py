3048dbf14423ef466ec2ba7976d07f2995d373dc
cliffckerr <questionably@gmail.com>
2015-12-23 04:05:09 -0800

    simplified

--- optima/untitled4.py
+++ optima/untitled4.py
@@ -29,19 +29,9 @@ fig1 = create_plot()
 
 
 html = '''
-<html><head><title>Optima plot</title></head><body>
+<html><body>
 <div id="fig01"></div>
-<script>
-function mpld3_load_lib(url, callback){
-  var s = document.createElement('script');
-  s.src = url;
-  s.async = true;
-  s.onreadystatechange = s.onload = callback;
-  s.onerror = function(){console.warn("failed to load library " + url);};
-  document.getElementsByTagName("head")[0].appendChild(s);
-}
-mpld3_load_lib("https://mpld3.github.io/js/d3.v3.min.js", function(){
-mpld3_load_lib("https://mpld3.github.io/js/mpld3.v0.3git.js", function(){
+<script>function mpld3_load_lib(url, callback){var s = document.createElement('script'); s.src = url; s.async = true; s.onreadystatechange = s.onload = callback; s.onerror = function(){console.warn("failed to load library " + url);}; document.getElementsByTagName("head")[0].appendChild(s)} mpld3_load_lib("https://mpld3.github.io/js/d3.v3.min.js", function(){mpld3_load_lib("https://mpld3.github.io/js/mpld3.v0.3git.js", function(){
 mpld3.draw_figure("fig01", {<snip1>});
 })});
 </script></body></html>
