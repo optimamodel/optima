a412c7343b8016318d7f2b72216569af544aff6f
cliffckerr <questionably@gmail.com>
2015-12-23 04:12:24 -0800

    ok, no, now working for an arbitrary number of plots

--- optima/untitled4.py
+++ optima/untitled4.py
@@ -4,6 +4,14 @@ import numpy as np, matplotlib.pyplot as plt
 import mpld3
 import json
 
+html = '''
+<html><body>
+!MAKE DIVS!
+<script>function mpld3_load_lib(url, callback){var s = document.createElement('script'); s.src = url; s.async = true; s.onreadystatechange = s.onload = callback; s.onerror = function(){console.warn("failed to load library " + url);}; document.getElementsByTagName("head")[0].appendChild(s)} mpld3_load_lib("https://mpld3.github.io/js/d3.v3.min.js", function(){mpld3_load_lib("https://mpld3.github.io/js/mpld3.v0.3git.js", function(){
+!DRAW FIGURES!
+})});
+</script></body></html>
+'''
 
 
 def create_plot():
@@ -24,34 +32,24 @@ def create_plot():
     plt.subplots_adjust(right=.5)
     return fig
 
-fig1 = create_plot()
-fig2 = create_plot()
 
 
-html = '''
-<html><body>
-<div id="fig01"></div>
-<div id="fig02"></div>
-<script>function mpld3_load_lib(url, callback){var s = document.createElement('script'); s.src = url; s.async = true; s.onreadystatechange = s.onload = callback; s.onerror = function(){console.warn("failed to load library " + url);}; document.getElementsByTagName("head")[0].appendChild(s)} mpld3_load_lib("https://mpld3.github.io/js/d3.v3.min.js", function(){mpld3_load_lib("https://mpld3.github.io/js/mpld3.v0.3git.js", function(){
-mpld3.draw_figure("fig01", {<snip1>});
-mpld3.draw_figure("fig02", {<snip2>});
-})});
-</script></body></html>
-'''
+nplots = 4
 
+figs = []
+jsons = []
+for p in range(nplots): 
+    figs.append(create_plot())
+    jsons.append(str(json.dumps(mpld3.fig_to_dict(figs[-1]))))
 
+divstr = ''
+jsonstr = ''
+for p in range(nplots):
+    divstr += '<div id="fig%i"></div>\n' % p
+    jsonstr += 'mpld3.draw_figure("fig%i", %s);\n' % (p, jsons[p])
 
-json01 = str(json.dumps(mpld3.fig_to_dict(fig1)))
-json02 = str(json.dumps(mpld3.fig_to_dict(fig2)))
-html = html.replace('{<snip1>}',json01)
-html = html.replace('{<snip2>}',json02)
-
-
-#plot([3,5])
-#fig1 = gcf()
-#
-#html = mpld3.fig_to_html(fig1)
-
+html = html.replace('!MAKE DIVS!',divstr)
+html = html.replace('!DRAW FIGURES!',jsonstr)
 
 mpld3._server.serve(html, ip='127.0.0.1', port=8888, n_retries=50, files=None, open_browser=True, http_server=None)
 
