42c27d7268b3e7a9974ca82eba5ea7296c03b011
cliffckerr <questionably@gmail.com>
2015-12-23 03:39:14 -0800

    works, now starting to deconstruct

--- optima/untitled4.py
+++ optima/untitled4.py
@@ -10,26 +10,26 @@ ion()
 
 #mpld3.enable_notebook()
 
-def create_plot():
-    fig, ax = plt.subplots(figsize=(12,4))
-
-    n = 100
-    x = np.linspace(0, 10, n)
-    l1 = ax.plot(x, np.sin(x)+randn(n), label='sin', lw=3, alpha=1)
-    l2 = ax.plot(x, np.cos(x)+randn(n), label='cos', lw=3, alpha=1)
-    l3 = ax.plot(x[::5], 0.5 * np.sin(x[::5] + 2), 'ob', label='dots',
-                 alpha=1)
-
-    labels = ['sin', 'cos', 'dots']
-    interactive_legend = mpld3.plugins.InteractiveLegendPlugin([l1, l2, l3], labels, start_visible=[1, 0, True])
-    mpld3.plugins.connect(fig, interactive_legend)
-
-    ax.set_title("Interactive legend test", size=20)
-    plt.subplots_adjust(right=.5)
-    return fig
-
-fig1 = create_plot()
-fig2 = create_plot()
+#def create_plot():
+#    fig, ax = plt.subplots(figsize=(12,4))
+#
+#    n = 100
+#    x = np.linspace(0, 10, n)
+#    l1 = ax.plot(x, np.sin(x)+randn(n), label='sin', lw=3, alpha=1)
+#    l2 = ax.plot(x, np.cos(x)+randn(n), label='cos', lw=3, alpha=1)
+#    l3 = ax.plot(x[::5], 0.5 * np.sin(x[::5] + 2), 'ob', label='dots',
+#                 alpha=1)
+#
+#    labels = ['sin', 'cos', 'dots']
+#    interactive_legend = mpld3.plugins.InteractiveLegendPlugin([l1, l2, l3], labels, start_visible=[1, 0, True])
+#    mpld3.plugins.connect(fig, interactive_legend)
+#
+#    ax.set_title("Interactive legend test", size=20)
+#    plt.subplots_adjust(right=.5)
+#    return fig
+#
+#fig1 = create_plot()
+#fig2 = create_plot()
 #mpld3.show()
 
 
@@ -52,6 +52,10 @@ fig2 = create_plot()
 #json02 = json.dumps(mpld3.fig_to_dict(fig2))
 #html.replace('{<snip1}',json01)
 #html.replace('{<snip2}',json02)
+
+
+plot([3,5])
+fig1 = gcf()
 
 html = mpld3.fig_to_html(fig1)
 
