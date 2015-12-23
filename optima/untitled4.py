a9624b2e442d5997dcb33fbb617f5a5498378a65
cliffckerr <questionably@gmail.com>
2015-12-23 04:06:03 -0800

    works for arbitrary number of figures

--- optima/untitled4.py
+++ optima/untitled4.py
@@ -25,25 +25,26 @@ def create_plot():
     return fig
 
 fig1 = create_plot()
-#fig2 = create_plot()
+fig2 = create_plot()
 
 
 html = '''
 <html><body>
 <div id="fig01"></div>
+<div id="fig02"></div>
 <script>function mpld3_load_lib(url, callback){var s = document.createElement('script'); s.src = url; s.async = true; s.onreadystatechange = s.onload = callback; s.onerror = function(){console.warn("failed to load library " + url);}; document.getElementsByTagName("head")[0].appendChild(s)} mpld3_load_lib("https://mpld3.github.io/js/d3.v3.min.js", function(){mpld3_load_lib("https://mpld3.github.io/js/mpld3.v0.3git.js", function(){
 mpld3.draw_figure("fig01", {<snip1>});
+mpld3.draw_figure("fig02", {<snip2>});
 })});
 </script></body></html>
 '''
 
 
 
-#
 json01 = str(json.dumps(mpld3.fig_to_dict(fig1)))
-#json02 = json.dumps(mpld3.fig_to_dict(fig2))
+json02 = str(json.dumps(mpld3.fig_to_dict(fig2)))
 html = html.replace('{<snip1>}',json01)
-#html.replace('{<snip2>}',json02)
+html = html.replace('{<snip2>}',json02)
 
 
 #plot([3,5])
