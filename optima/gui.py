35dc33c617093f45014524e5f18b7849f4983069
jpuri <jyotipuri@gmail.com>
2015-12-30 17:46:10 +0530

    feat(Mpld3 plot): adding lines to legends

--- optima/gui.py
+++ optima/gui.py
@@ -11,10 +11,10 @@ def addplot(thisfig, thisplot, nrows=1, ncols=1, n=1):
     ''' Add a plot to an existing figure '''
     thisfig._axstack.add(thisfig._make_key(thisplot), thisplot) # Add a plot to the axis stack
     thisplot.change_geometry(nrows, ncols, n) # Change geometry to be correct
-    orig = thisplot.get_position() # get the original position 
+    orig = thisplot.get_position() # get the original position
     widthfactor = 0.9/ncols**(1/4.)
     heightfactor = 0.9/nrows**(1/4.)
-    pos2 = [orig.x0, orig.y0,  orig.width*widthfactor, orig.height*heightfactor] 
+    pos2 = [orig.x0, orig.y0,  orig.width*widthfactor, orig.height*heightfactor]
     thisplot.set_position(pos2) # set a new position
 
     return None
@@ -24,31 +24,31 @@ def addplot(thisfig, thisplot, nrows=1, ncols=1, n=1):
 def gui(results, which=None):
     '''
     GUI
-    
+
     Make a Python GUI for plotting results. Opens up a control window and a plotting window,
     and when "Update" is clicked, will clear the contents of the plotting window and replot.
-    
+
     Usage:
         gui(results, [which])
-    
+
     where results is the output of e.g. runsim() and which is an optional list of form e.g.
         which = ['prev-tot', 'inci-pops']
-    
+
     Warning: the plots won't resize automatically if the figure is resized, but if you click
-    "Update", then they will.    
-    
+    "Update", then they will.
+
     Version: 1.1 (2015dec29) by cliffk
     '''
     global check, updatebutton, closebutton, panelfig
-    
-    
+
+
     def getchecked(check):
         ''' Return a list of whether or not each check box is checked or not '''
         ischecked = []
         for box in range(len(check.lines)): ischecked.append(check.lines[box][0].get_visible()) # Stupid way of figuring out if a box is ticked or not
         return ischecked
-    
-    
+
+
     def update(event):
         ''' Close current window if it exists and open a new one based on user selections '''
         global plotfig
@@ -56,33 +56,33 @@ def gui(results, which=None):
         # If figure exists, get size, then close it
         try: width,height = plotfig.get_size_inches(); close(plotfig) # Get current figure dimensions
         except: width,height = 14,12 # No figure: use defaults
-        
+
         # Get user selections
         ischecked = getchecked(check)
         toplot = array(checkboxes)[array(ischecked)].tolist() # Use logical indexing to get names to plot
         nplots = sum(ischecked) # Calculate rows and columns of subplots
         nrows = int(ceil(sqrt(nplots)))
         ncols = nrows-1 if nrows*(nrows-1)>=nplots else nrows
-        
+
         # Do plotting
         if nplots>0: # Don't do anything if no plots
             wasinteractive = isinteractive()
             if wasinteractive: ioff()
             plotfig = figure(figsize=(width, height), facecolor=(1,1,1)) # Create figure with correct number of plots
-            
+
             # Actually create plots
             plots = epiplot(results, which=toplot, figsize=(width, height))
             for p in range(len(plots)): addplot(plotfig, plots[p].axes[0], nrows, ncols, p+1)
             if wasinteractive: ion()
             show()
-    
+
     def closegui(event):
         ''' Close all GUI windows '''
         global plotfig, panelfig
         close(plotfig)
         close(panelfig)
-    
-    
+
+
     ## Define options for selection
     epikeys = results.main.keys()
     epinames = [thing.name for thing in results.main.values()]
@@ -97,7 +97,7 @@ def gui(results, which=None):
         for subname in episubnames: # e.g. 'total'
             checkboxnames.append(name+' -- '+subname)
     nboxes = len(checkboxes) # Number of choices
-    
+
     ## Set up what to plot when screen first opens
     truebydefault = 2 # Number of boxes to check true by default
     if which is None: # No inputs: set the first couple true by default
@@ -107,7 +107,7 @@ def gui(results, which=None):
         for name in checkboxes: # Check to see if they match
             if name in which: defaultchecks.append(True)
             else: defaultchecks.append(False)
-            
+
     ## Set up control panel
     try: fc = results.project.settings.optimablue # Try loading global optimablue
     except: fc = (0.16, 0.67, 0.94) # Otherwise, just specify it :)
@@ -135,18 +135,18 @@ def gui(results, which=None):
 
 
 def browser(results, which=None, doplot=True):
-    ''' 
-    Create an MPLD3 GUI and display in the browser. This is basically a testbed for 
+    '''
+    Create an MPLD3 GUI and display in the browser. This is basically a testbed for
     the Optima frontend.
-    
+
     Usage:
         browser(results, [which])
-    
+
     where results is the output of e.g. runsim() and which is an optional list of form e.g.
         which = ['prev-tot', 'inci-pops']
-    
+
     With doplot=True, launch a web server. Otherwise, return the HTML representation of the figures.
-    
+
     Version: 1.1 (2015dec29) by cliffk
     '''
     import mpld3 # Only import this if needed, since might not always be available
@@ -155,8 +155,8 @@ def browser(results, which=None, doplot=True):
 
     wasinteractive = isinteractive() # Get current state of interactivity so the screen isn't flooded with plots
     if wasinteractive: ioff()
-    
-    
+
+
     ## Specify the div style, and create the HTML template we'll add the data to
     divstyle = "float: left"
     html = '''
@@ -174,7 +174,6 @@ def browser(results, which=None, doplot=True):
         var al = $('.mpld3-baseaxes').length;
         var dl = $('div.fig').length
         if (al === dl) {
-            console.log('doing it');
             $('.mpld3-baseaxes > text').each(function() {
                 var value = $(this).text();
                 if (value === 'Year') {
@@ -199,9 +198,28 @@ def browser(results, which=None, doplot=True):
             setTimeout(format_xaxis, 150);
         }
     }
+    function add_lines_to_legends() {
+        console.log('adding lines to legends');
+        var al = $('.mpld3-baseaxes').length;
+        var dl = $('div.fig').length
+        if (al === dl) {
+            $('div.fig').each(function() {
+                var paths = $(this).find('.mpld3-baseaxes > text');
+                if (paths) {
+                    var legend_length = paths.length - 2;
+                    var lines = $(this).find('.mpld3-axes > path');
+                    var lines_to_copy = lines.slice(lines.length - legend_length, lines.length);
+                    $(this).find('.mpld3-baseaxes').append(lines_to_copy);
+                }
+            });
+        } else {
+            setTimeout(add_lines_to_legends, 150);
+        }
+    }
     $(document).ready(function() {
         format_xaxis();
         move_year();
+        add_lines_to_legends();
     });
     </script>
     </body></html>
@@ -217,7 +235,7 @@ def browser(results, which=None, doplot=True):
         mpld3.plugins.connect(fig, mpld3.plugins.MousePosition(fontsize=14,fmt='.4r')) # Add plugins
         jsons.append(str(json.dumps(mpld3.fig_to_dict(fig)))) # Save to JSON
         close(fig) # Close
-    
+
     ## Create div and JSON strings to replace the placeholers above
     divstr = ''
     jsonstr = ''
@@ -226,7 +244,7 @@ def browser(results, which=None, doplot=True):
         jsonstr += 'mpld3.draw_figure("fig%i", %s);\n' % (p, jsons[p]) # Add the JSON representation of each figure -- THIS IS KEY!
     html = html.replace('!MAKE DIVS!',divstr) # Populate div information
     html = html.replace('!DRAW FIGURES!',jsonstr) # Populate figure information
-    
+
     ## Launch a server or return the HTML representation
     if doplot: serve(html)
     else: return html
