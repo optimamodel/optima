c20f6103da6d31a1d24a9478e9c8e08c23d823ba
robynstuart <robyna.s@gmail.com>
2016-01-09 20:14:37 +0100

    added program scenarios to standard tests

--- optima/scenarios.py
+++ optima/scenarios.py
@@ -1,8 +1,8 @@
 ## Imports
 from numpy import append # array, isnan, zeros, shape, argmax, log, polyfit, exp, arange
-from optima import dcp, today, odict, printv, findinds #, sanitize, uuid, getdate, smoothinterp
+from optima import dcp, today, odict, printv, findinds, runmodel #, sanitize, uuid, getdate, smoothinterp
 
-def runscenarios(P, parset, scenlist=None, verbose=2, debug=False):
+def runscenarios(scenlist=None, default_parset=None, verbose=2, debug=False):
     """
     Run all the scenarios.
     """
@@ -10,67 +10,88 @@ def runscenarios(P, parset, scenlist=None, verbose=2, debug=False):
     printv('Running scenarios...', 1, verbose)
     
     # Make sure scenarios exist
-    if scenlist==None: scenlist = defaultscenarios(parset, verbose=verbose)
+    if scenlist==None:
+        if default_parset==None: 
+            errormsg = 'Please specify a parameter set to use as a baseline.'
+            raise Exception(errormsg)
+        else: scenlist = defaultscenarios(default_parset, verbose=verbose)
     nscenarios = len(scenlist)
     
     # Convert the list of scenarios to the actual parameters to use in the model
-    scenparsets = makescenarios(parset,scenlist,verbose=verbose)
-    
+    scenparsets = makescenarios(scenlist,verbose=verbose)
+#    import traceback; traceback.print_exc(); import pdb; pdb.set_trace()    
     # Run scenarios
     allresults = []
     for scenno, scen in enumerate(scenparsets):
-        P.addparset(name=scen,parset=scenparsets[scen])
-        allresults.append(P.runsim(scen))
+        allresults.append(runmodel(pars=scenparsets[scen].pars[0]))
         printv('Scenario: %i/%i' % (scenno+1, nscenarios), 2, verbose)
     
     printv('...done running scenarios.', 2, verbose)
     return allresults
 
 
-def makescenarios(parset,scenlist,verbose=2):
+def makescenarios(scenlist, verbose=2):
     """ Convert dictionary of scenario parameters into parset to model parameters """
 
     scenparsets = odict()
     for scenno, scen in enumerate(scenlist):
         
-        thisparset = dcp(parset)
-        thisparset.modified = today()
-        thisparset.name = scen['name']
-        npops = len(parset.pars[0]['popkeys'])
+        if scen['type']=='parameter':
         
-        for sc in range(len(parset.pars)): # Loop over all parameter sets
-            for par in scenlist[scenno]['pars']: # Loop over all parameters being changed
-                thispar = thisparset.pars[sc][par['name']]
-                if type(par['for'])==tuple: # If it's a partnership...
-                    par2 = (par['for'][1],par['for'][0])
-                    pops = [par['for'], par2] # This is confusing - for partnership parameters, pops is a list of the two different partnership orderings.
-                elif type(par['for'])==int: #... if its a population.
-                    pops = range(npops) if par['for'] > npops else [par['for']]
-                else: 
-                    errormsg = 'Unrecognized population or partnership type.'
-                    raise Exception(errormsg)
-                for pop in pops:
-                    if par['startyear'] < max(thispar.t[pop]):
-                        thispar.t[pop] = thispar.t[pop][thispar.t[pop] < par['startyear']]
-                        thispar.y[pop] = thispar.y[pop][thispar.t[pop] < par['startyear']]
-                    thispar.t[pop] = append(thispar.t[pop], par['startyear'])
-                    thispar.y[pop] = append(thispar.y[pop], par['startval']) 
-                    if par['endyear']: # Add end year values if supplied
-                        thispar.t[pop] = append(thispar.t[pop], par['endyear'])
-                        thispar.y[pop] = append(thispar.y[pop], par['endval'])
-
+            thisparset = dcp(scen['parset'])
+            thisparset.modified = today()
+            thisparset.name = scen['name']
+            npops = len(thisparset.pars[0]['popkeys'])
+            
+            for sc in range(len(thisparset.pars)): # Loop over all parameter sets
+                for par in scenlist[scenno]['pars']: # Loop over all parameters being changed
+                    thispar = thisparset.pars[sc][par['name']]
+                    if type(par['for'])==tuple: # If it's a partnership...
+                        par2 = (par['for'][1],par['for'][0])
+                        pops = [par['for'], par2] # This is confusing - for partnership parameters, pops is a list of the two different partnership orderings.
+                    elif type(par['for'])==int: #... if its a population.
+                        pops = range(npops) if par['for'] > npops else [par['for']]
+                    else: 
+                        errormsg = 'Unrecognized population or partnership type.'
+                        raise Exception(errormsg)
+                    for pop in pops:
+                        if par['startyear'] < max(thispar.t[pop]):
+                            thispar.t[pop] = thispar.t[pop][thispar.t[pop] < par['startyear']]
+                            thispar.y[pop] = thispar.y[pop][thispar.t[pop] < par['startyear']]
+                        thispar.t[pop] = append(thispar.t[pop], par['startyear'])
+                        thispar.y[pop] = append(thispar.y[pop], par['startval']) 
+                        if par['endyear']: # Add end year values if supplied
+                            thispar.t[pop] = append(thispar.t[pop], par['endyear'])
+                            thispar.y[pop] = append(thispar.y[pop], par['endval'])
+    
+        elif scen['type']=='program':
+            
+            if scen['budgets']:
+                for budget in scen['budgets']:
+                    thisprogset = scen['progset']
+                    thiscoverage = thisprogset.getprogcoverage(budget=budget, t=[2016], parset=scen['parset'])
+                    thisparset = thisprogset.getparset(coverage=thiscoverage, t=[2016], parset=scen['parset'], newparsetname=scen['name'])
+            if scen['coveragelevels']:
+                for coveragelevel in scen['coveragelevels']:
+                    thisprogset = scen['progset']
+                    thisparset = thisprogset.getparset(coverage=thiscoverage, t=[2016], parset=scen['parset'], newparsetname=scen['name'])
+        else: 
+            errormsg = 'Unrecognized scenario type.'
+            raise Exception(errormsg)
+            
         scenparsets[scen['name']] = thisparset
 
     return scenparsets
 
 
-def defaultscenarios(parset, verbose=2):
+def defaultscenarios(default_parset, verbose=2):
     """ Define a list of default scenarios -- only "Current conditions" by default """
     
     scenlist = [odict()]
     
     ## Current conditions
     scenlist[0]['name'] = 'Current conditions'
+    scenlist[0]['parset'] = default_parset
     scenlist[0]['pars'] = [] # No changes
     
     return scenlist
--- optima/testscenarios.py
+++ optima/testscenarios.py
@@ -11,7 +11,7 @@ tests = [
 'standardscen',
 ]
 
-
+doplot=False
 ##############################################################################
 ## Initialization -- same for every test script
 ##############################################################################
@@ -45,37 +45,78 @@ if 'standardscen' in tests:
 
     print('Running standard scenarios test...')
     from optima import Project
+    from defaultprograms import defaultprogset
+    from numpy import array
     
-    P = Project(spreadsheet='test.xlsx')
+    P = Project(spreadsheet='test7pops.xlsx')
+    R = defaultprogset(P, addpars=True, filterprograms=['Condoms', 'FSW_programs'])
+    R.covout['condcas'][('Clients', 'FSW')].addccopar({'intercept': (0.3,0.35), 't': 2016.0, 'Condoms':(0.45,0.55), 'FSW_programs':(0.55,0.65)})
+    R.covout['condcas'][('Clients', 'F 15+')].addccopar({'intercept': (0.2,0.3), 't': 2016.0, 'Condoms':(0.35,0.45)})
+    R.covout['condcas'][('MSM', 'MSM')].addccopar({'intercept': (0.5,0.55), 't': 2016.0, 'Condoms':(0.55,0.65)})
+    R.covout['condcas'][('M 15+', 'FSW')].addccopar({'intercept': (0.3,0.35), 't': 2016.0, 'Condoms':(0.45,0.55), 'FSW_programs':(0.55,0.65)})
+    R.covout['condcas'][('M 15+', 'F 15+')].addccopar({'intercept': (0.2,0.3), 't': 2016.0, 'Condoms':(0.35,0.45)})
+    R.covout['condcom'][('Clients', 'FSW')].addccopar({'intercept': (0.6,0.65), 't': 2016.0, 'FSW_programs':(0.9,0.95)})
+    R.covout['hivtest']['FSW'].addccopar({'intercept': (0.35,0.45), 't': 2016.0, 'FSW_programs':(0.6,0.65)})
     
     ## Define scenarios
     scenlist = [
-        {'name': 'Current conditions', 'pars': []},
+        {'name': 'Current conditions',
+                 'parset': P.parsets['default'],
+                 'type': 'parameter',
+                 'pars': []},
         {'name': 'Less casual condom use',
-          'pars': [{'endval': 0.1,
+         'parset': P.parsets['default'],
+         'type': 'parameter',
+         'pars': [{'endval': 0.1,
             'endyear': 2015,
             'name': 'condcas',
-            'for': ('M 15-49', 'F 15-49'),
+            'for': ('M 15+', 'F 15+'),
             'startval': 0.1,
             'startyear': 2005}]},
          {'name': 'More casual acts',
+          'parset': P.parsets['default'],
+          'type': 'parameter',
           'pars': [{'endval': 100.,
             'endyear': 2015,
             'name': 'actscas',
-            'for': ('F 15-49', 'M 15-49'),
+            'for': ('M 15+', 'F 15+'),
             'startval': 100.,
             'startyear': 2005}]},
          {'name': 'Increased STI prevalence in women',
+          'parset': P.parsets['default'],
+          'type': 'parameter',
           'pars': [{'endval': 0.5,
             'endyear': 2015,
             'name': 'stiprev',
             'for': 1,
             'startval': 0.5,
-            'startyear': 2005}]}
+            'startyear': 2005}]},
+         {'name': 'Double investment in condom program',
+          'parset': P.parsets['default'],
+          'type': 'program',
+          'progset': R,
+          'budgets': [
+           {'Condoms':array([1e7]),
+            'FSW_programs':array([1e6])},
+           {'Condoms':array([2e7]),
+            'FSW_programs':array([1e6])},
+            ],
+          'coveragelevels': None},
+         {'name': 'A million people covered by the condom program',
+          'parset': P.parsets['default'],
+          'type': 'program',
+          'progset': R,
+          'budgets': None,
+          'coveragelevels': [
+           {'Condoms':array([285706.84495908]),
+            'FSW_programs':array([15352.67106128])},
+           {'Condoms':array([1e6]),
+            'FSW_programs':array([15352.67106128])},
+            ]}
         ]
     
     from scenarios import runscenarios
-    allresults = runscenarios(P, P.parsets['default'], scenlist=scenlist)
+    allresults = runscenarios(scenlist=scenlist)
      
 #    from gui import gui
 #    gui([results1, results2])
