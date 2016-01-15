445f3c45cc9b1d215ba6d67cf28e021f79e1aa9f
Arun Gupta <Arun_Gupta_XFT@mckinsey.com>
2016-01-15 23:40:52 +0530

    Bug - Population name & Short name in create/edit project page should have unique constraint

--- client/source/js/modules/project/create-population-modal-ctrl.js
+++ client/source/js/modules/project/create-population-modal-ctrl.js
@@ -1,13 +1,33 @@
 define(['./module', 'angular'], function (module, angular) {
   'use strict';
 
-  module.controller('ProjectCreatePopulationModalController', function ($scope, $modalInstance, population,modalService) {
+  module.controller('ProjectCreatePopulationModalController', function ($scope, $modalInstance, population, populations, modalService) {
 
     // Initializes relevant attributes
     $scope.isNew = !population.name;
     // in order to not perform changes directly on the final value here is created a copy
     $scope.population = angular.copy(population);
     $scope.population.active = true;
+
+    populations = _.filter(populations, function(p) { return p.short_name !== $scope.population.short_name; });
+
+    $scope.populationExists = function(key){
+      return _.find(populations, function (population) {
+        return $scope.population[key] === population[key];
+      });
+    };
+
+    $scope.isFormValid = function(){
+      if(!$scope.populationExists('name') && !$scope.populationExists('short_name')){
+        if($scope.PopulationForm.$invalid){
+          return false;  
+        }else{
+          return true;
+        }
+      }else{
+        return false;
+      }
+    };
 
     $scope.submit = function (form) {
       if (form.$invalid) {
