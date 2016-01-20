90938e84e44df4984ab2c5a250f41ee066a41263
David Kedziora <davidkedz@hotmail.com>
2016-01-20 09:06:56 +0000

    Quick commit
    Just cause.

---
+++ tests/testgeospatial.py
@@ -0,0 +1,138 @@
+"""
+Test script to see if geospatial analysis works.
+
+To use: comment out lines in the definition of 'tests' to not run those tests.
+
+NOTE: for best results, run in interactive mode, e.g.
+
+python -i tests.py
+
+Version: 2016jan20 by davidkedz
+"""
+
+
+
+## Define tests to run here!!!
+tests = [
+'makeportfolio',
+'saveload',
+'loadspreadsheet',
+'loadeconomics',
+'runsim'
+]
+
+##############################################################################
+## Initialization -- same for every test script
+##############################################################################
+
+from optima import tic, toc, blank, pd # analysis:ignore
+
+if 'doplot' not in locals(): doplot = True
+
+def done(t=0):
+    print('Done.')
+    toc(t)
+    blank()
+
+blank()
+print('Running tests:')
+for i,test in enumerate(tests): print(('%i.  '+test) % (i+1))
+blank()
+
+
+
+##############################################################################
+## The tests
+##############################################################################
+
+T = tic()
+
+
+## Project creation test
+if 'makeproject' in tests:
+    t = tic()
+    print('Running make project test...')
+    from optima import Project
+    P = Project()
+    print(P)
+    done(t)
+
+
+
+
+## Project save/load test
+if 'saveload' in tests:
+    t = tic()
+    print('Running save/load test...')
+    
+    from optima import Project, saveobj, loadobj
+    from os import remove
+    filename = 'testproject.prj'
+    
+    print('  Checking saving...')
+    P = Project()
+    saveobj(filename, P)
+    
+    print('  Checking loading...')
+    Q = loadobj(filename)
+    
+    print('Cleaning up...')
+    remove(filename)
+    
+    done(t)
+
+
+
+
+## Load spreadsheet test
+if 'loadspreadsheet' in tests:
+    t = tic()
+    print('Running loadspreadsheet test...')
+    from optima import Project
+    
+    print('  Create a project from a spreadsheet')
+    P = Project(spreadsheet='test.xlsx')
+    
+    print('  Load a project, then load a spreadsheet')
+    Q = Project()
+    Q.loadspreadsheet('test.xlsx')
+    
+    assert Q.data['const']['effcondom'][0]==0.95, 'Condom efficacy not 95% or not being read in properly'
+    
+    done(t)
+
+
+
+## Load economics spreadsheet test
+if 'loadeconomcs' in tests:
+    t = tic()
+    print('Running loadeconomics test...')
+    from optima import Project
+    
+    print('  Create an empty project and add economic data')
+    P = Project()
+    P.loadeconomics(filename='testeconomics.xlsx')
+
+    print('  Create a project from a spreadsheet and add economic data')
+    P = Project(spreadsheet='test.xlsx')
+    P.loadeconomics(filename='testeconomics.xlsx')
+
+
+
+## Run simulation test
+if 'runsim' in tests:
+    t = tic()
+    print('Running runsim test...')
+    
+    from optima import Project
+    P = Project(spreadsheet='test.xlsx')
+    results = P.runsim('default')
+    
+    done(t)
+
+
+
+
+
+print('\n\n\nDONE: ran %i tests' % len(tests))
+toc(T)
