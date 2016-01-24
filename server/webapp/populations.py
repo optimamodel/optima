78c6fd2d655a1309b88de9685a14f90d4c50d975
robynstuart <robyna.s@gmail.com>
2016-01-24 20:13:21 +0100

    change populations.py

--- server/webapp/populations.py
+++ server/webapp/populations.py
@@ -1,25 +1,22 @@
 ALL_POPULATIONS_SOURCE = \
 """
-Short name;Full name;Male;Female;AgeFrom;AgeTo
-FSW;Female sex workers;0;1;15;49;
-Clients;Clients of sex workers;1;0;15;49;
-MSM;Men who have sex with men;1;0;15;49;
-Transgender;Transgender individuals;0;0;15;49;
-PWID;People who inject drugs;0;0;15;49;
-Male PWID;Males who inject drugs;1;0;15;49;
-Female PWID;Females who inject drugs;0;1;15;49;
-Children;Children;0;0;2;15;
-Infants;Infants;0;0;0;2;
-Males;Other males;1;0;15;49;
-Females;Other females;0;1;0;15;49;
-Other males;Other males [enter age];1;0;0;0;
-Other females;Other females [enter age];0;1;0;0;
+Short name;Full name;Male;Female;AgeFrom;AgeTo;Injects;SexWorker
+FSW;Female sex workers;0;1;15;49;0;1;
+Clients;Clients of sex workers;1;0;15;49;0;1;
+MSM;Men who have sex with men;1;0;15;49;0;1;
+Transgender;Transgender individuals;0;0;15;49;0;1;
+PWID;People who inject drugs;0;0;15;49;1;0;
+Male PWID;Males who inject drugs;1;0;15;49;1;0;
+Female PWID;Females who inject drugs;0;1;15;49;1;0;
+Children;Children;0;0;2;15;0;0;
+Infants;Infants;0;0;0;2;0;0;
+Males;Other males;1;0;15;49;0;0;
+Females;Other females;0;1;0;15;49;0;0;
+Other males;Other males [enter age];1;0;0;0;0;0;
+Other females;Other females [enter age];0;1;0;0;0;0;
 """
 
-## TODO Cliff, Robyn: are these populations still going to be used or not?
-## we need this to show populations list in Create Project screen
-
-fields = {0:"short_name",1:"name", 2:"male", 3:"female",4:"age_from",5:"age_to"}
+fields = {0:"short_name",1:"name", 2:"male", 3:"female",4:"age_from",5:"age_to",6:"injects",7:"sexworker"}
 
 def populations():
     from parameters import maybe_bool
@@ -34,6 +31,8 @@ def project_population_data():
         'short': [pop['short_name'] for pop in pops],
         'male': [pop['male'] for pop in pops],
         'female': [pop['female'] for pop in pops],
+        'injects': [pop['injects'] for pop in pops],
+        'sexworker': [pop['sexworker'] for pop in pops],
     }
 
 
