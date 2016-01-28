4e3e8ca1f9d640e07330f1ce8b9f329b97226beb
Emmanuelle Delescolle <emma@levit.be>
2016-01-28 08:11:30 +0100

    (task)resource file cleanup

--- server/webapp/resources/project_progsets.py
+++ server/webapp/resources/project_progsets.py
@@ -1,14 +1,12 @@
 from flask import current_app
 
 from flask.ext.login import login_required
-from flask_restful import Resource, marshal_with, fields
+from flask_restful import Resource, marshal_with
 from flask_restful_swagger import swagger
 from flask import helpers
 
-from server.webapp.inputs import SubParser, Json as JsonInput
-from server.webapp.fields import Json, Uuid
 from server.webapp.dataio import TEMPLATEDIR, upload_dir_user
-from server.webapp.utils import load_project, load_progset, RequestParser, report_exception
+from server.webapp.utils import load_project, load_progset, report_exception
 from server.webapp.exceptions import ProjectDoesNotExist, ProgsetDoesNotExist
 from server.webapp.resources.common import file_resource, file_upload_form_parser
 
@@ -16,32 +14,8 @@ from server.webapp.dbconn import db
 
 from server.webapp.dbmodels import ProgsetsDb, ProgramsDb
 
-
-costcov_parser = RequestParser()
-costcov_parser.add_arguments({
-    'year': {'required': True, 'location': 'json'},
-    'spending': {'required': True, 'type': float, 'location': 'json', 'dest': 'cost'},
-    'coverage': {'required': True, 'type': float, 'location': 'json', 'dest': 'cov'},
-})
-
-program_parser = RequestParser()
-program_parser.add_arguments({
-    'name': {'required': True, 'location': 'json'},
-    'short': {'location': 'json'},
-    'short_name': {'location': 'json'},
-    'category': {'required': True, 'location': 'json'},
-    'active': {'type': bool, 'default': False, 'location': 'json'},
-    'parameters': {'type': list, 'dest': 'pars', 'location': 'json'},
-    'populations': {'type': list, 'location': 'json', 'dest': 'targetpops'},
-    'addData': {'type': SubParser(costcov_parser), 'dest': 'costcov', 'action': 'append', 'default': []},
-})
-
-
-progset_parser = RequestParser()
-progset_parser.add_arguments({
-    'name': {'required': True},
-    'programs': {'required': True, 'type': SubParser(program_parser), 'action': 'append'}
-})
+from server.webapp.serializers.project_progsets import (progset_parser, param_fields,
+    effect_parser, progset_effects_fields)
 
 
 class Progsets(Resource):
@@ -234,12 +208,6 @@ class ProgsetData(Resource):
         }
         return reply
 
-param_fields = {
-        'name': fields.String,
-        'populations': Json,
-        'coverage': fields.Boolean,
-    }
-
 
 class ProgsetParams(Resource):
 
@@ -270,99 +238,12 @@ class ProgsetParams(Resource):
         return params
 
 
-program_effect_parser = RequestParser()
-program_effect_parser.add_arguments({
-    'name': {'required': False, 'location': 'json'},
-    'intercept_lower': {'required': False, 'type': float, 'location': 'json'},
-    'intercept_upper': {'required': False, 'type': float, 'location': 'json'},
-})
-
-
-param_year_effect_parser = RequestParser()
-param_year_effect_parser.add_arguments({
-    'year': {'required': False, 'location': 'json'},
-    'intercept_lower': {'required': False, 'type': float, 'location': 'json'},
-    'intercept_upper': {'required': False, 'type': float, 'location': 'json'},
-    'interact': {'location': 'json', 'required': False},
-    'programs': {
-        'type': SubParser(program_effect_parser),
-        'action': 'append',
-        'default': [],
-        'location': 'json',
-        'required': False,
-    },
-})
-
-
-param_effect_parser = RequestParser()
-param_effect_parser.add_arguments({
-    'name': {'required': False, 'location': 'json'},
-    'pop': {'required': False, 'location': 'json', 'type': JsonInput},
-    'years': {
-        'type': SubParser(param_year_effect_parser),
-        'action': 'append',
-        'default': [],
-        'location': 'json',
-        'required': False,
-    },
-})
-
-
-parset_effect_parser = RequestParser()
-parset_effect_parser.add_arguments({
-    'parset': {'required': True, 'location': 'json'},
-    'parameters': {
-        'type': SubParser(param_effect_parser),
-        'action': 'append',
-        'type': JsonInput,
-        'default': [],
-        'location': 'json',
-        'required': False
-    }
-})
-
-
-effect_parser = RequestParser()
-effect_parser.add_argument('effects', type=SubParser(parset_effect_parser), action='append')
-
-
-program_effect_fields = {
-    'name': fields.String,
-    'intercept_lower': fields.Float,
-    'intercept_upper': fields.Float
-}
-
-
-param_year_effect_fields = {
-    'year': fields.Integer,
-    'intercept_lower': fields.Float,
-    'intercept_upper': fields.Float,
-    'interact': fields.List(fields.String),
-    'programs': fields.List(fields.Nested(program_effect_fields), default=[])
-}
-
-param_effect_fields = {
-    'name': fields.String,
-    'pop': fields.Raw,
-    'years': fields.List(fields.Nested(param_year_effect_fields), default=[])
-}
-
-parset_effect_fields = {
-    'parset': Uuid,
-    'parameters': fields.List(fields.Nested(param_effect_fields), default=[])
-}
-
-progset_effects_fields = {
-    'effects': fields.List(fields.Nested(parset_effect_fields), default=[])
-}
-
-
 class ProgsetEffects(Resource):
 
     method_decorators = [report_exception, login_required]
 
     @swagger.operation(
-        description='Get List of Progset effects  for the selected progset'
+        summary='Get List of existing Progset effects for the selected progset'
     )
     @marshal_with(progset_effects_fields)
     def get(self, project_id, progset_id):
@@ -372,7 +253,7 @@ class ProgsetEffects(Resource):
         return progset_entry
 
     @swagger.operation(
-        description='Get List of Progset effects  for the selected progset',
+        summary='Saves a list of Progset effects for the selected progset',
         parameters=effect_parser.swagger_parameters()
     )
     @marshal_with(progset_effects_fields)
---
+++ server/webapp/serializers/project_progsets.py
@@ -0,0 +1,130 @@
+from flask_restful import fields
+
+from server.webapp.inputs import SubParser, Json as JsonInput
+from server.webapp.fields import Json, Uuid
+from server.webapp.utils import RequestParser
+
+
+# Progset
+
+costcov_parser = RequestParser()
+costcov_parser.add_arguments({
+    'year': {'required': True, 'location': 'json'},
+    'spending': {'required': True, 'type': float, 'location': 'json', 'dest': 'cost'},
+    'coverage': {'required': True, 'type': float, 'location': 'json', 'dest': 'cov'},
+})
+
+program_parser = RequestParser()
+program_parser.add_arguments({
+    'name': {'required': True, 'location': 'json'},
+    'short': {'location': 'json'},
+    'short_name': {'location': 'json'},
+    'category': {'required': True, 'location': 'json'},
+    'active': {'type': bool, 'default': False, 'location': 'json'},
+    'parameters': {'type': list, 'dest': 'pars', 'location': 'json'},
+    'populations': {'type': list, 'location': 'json', 'dest': 'targetpops'},
+    'addData': {'type': SubParser(costcov_parser), 'dest': 'costcov', 'action': 'append', 'default': []},
+})
+
+
+progset_parser = RequestParser()
+progset_parser.add_arguments({
+    'name': {'required': True},
+    'programs': {'required': True, 'type': SubParser(program_parser), 'action': 'append'}
+})
+
+
+param_fields = {
+        'name': fields.String,
+        'populations': Json,
+        'coverage': fields.Boolean,
+    }
+
+
+program_effect_parser = RequestParser()
+program_effect_parser.add_arguments({
+    'name': {'required': False, 'location': 'json'},
+    'intercept_lower': {'required': False, 'type': float, 'location': 'json'},
+    'intercept_upper': {'required': False, 'type': float, 'location': 'json'},
+})
+
+
+param_year_effect_parser = RequestParser()
+param_year_effect_parser.add_arguments({
+    'year': {'required': False, 'location': 'json'},
+    'intercept_lower': {'required': False, 'type': float, 'location': 'json'},
+    'intercept_upper': {'required': False, 'type': float, 'location': 'json'},
+    'interact': {'location': 'json', 'required': False},
+    'programs': {
+        'type': SubParser(program_effect_parser),
+        'action': 'append',
+        'default': [],
+        'location': 'json',
+        'required': False,
+    },
+})
+
+
+param_effect_parser = RequestParser()
+param_effect_parser.add_arguments({
+    'name': {'required': False, 'location': 'json'},
+    'pop': {'required': False, 'location': 'json', 'type': JsonInput},
+    'years': {
+        'type': SubParser(param_year_effect_parser),
+        'action': 'append',
+        'default': [],
+        'location': 'json',
+        'required': False,
+    },
+})
+
+
+parset_effect_parser = RequestParser()
+parset_effect_parser.add_arguments({
+    'parset': {'required': True, 'location': 'json'},
+    'parameters': {
+        'type': SubParser(param_effect_parser),
+        'action': 'append',
+        'type': JsonInput,
+        'default': [],
+        'location': 'json',
+        'required': False
+    }
+})
+
+
+effect_parser = RequestParser()
+effect_parser.add_argument('effects', type=SubParser(parset_effect_parser), action='append')
+
+
+program_effect_fields = {
+    'name': fields.String,
+    'intercept_lower': fields.Float,
+    'intercept_upper': fields.Float
+}
+
+
+param_year_effect_fields = {
+    'year': fields.Integer,
+    'intercept_lower': fields.Float,
+    'intercept_upper': fields.Float,
+    'interact': fields.List(fields.String),
+    'programs': fields.List(fields.Nested(program_effect_fields), default=[])
+}
+
+param_effect_fields = {
+    'name': fields.String,
+    'pop': fields.Raw,  # ToDo implement a field type that matches String or List of Strings
+    'years': fields.List(fields.Nested(param_year_effect_fields), default=[])
+}
+
+
+parset_effect_fields = {
+    'parset': Uuid,
+    'parameters': fields.List(fields.Nested(param_effect_fields), default=[])
+}
+
+
+progset_effects_fields = {
+    'effects': fields.List(fields.Nested(parset_effect_fields), default=[])
+}
