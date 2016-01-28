1c9bf36ab986260bdaaaa5446bcaaf2e7c84997a
DrMud <madhura.killedar.astro@gmail.com>
2016-01-28 17:19:58 +1100

    add code to just compare simple 2 pop spreadsheet cascade

--- tests/testcascade.py
+++ tests/testcascade.py
@@ -8,7 +8,8 @@ Version: 2016jan23 by cliffk
 
 ## Define tests to run here!!!
 tests = [
-'compare',
+#'compare',
+'comparesimple'
 ]
 
 
@@ -68,6 +69,43 @@ if 'compare' in tests:
         plotresults(P.results[-1], toplot=toplot, figsize=(16,10), num='No cascade')
         plotresults(Q.results[-1], toplot=toplot, figsize=(16,10), num='With cascade')
     
+    done(t)
+
+
+
+if 'comparesimple' in tests:
+    t = tic()
+    print('Running comparison test...')
+    
+    toplot = ['prev-tot', 'numinci-sta', 'numplhiv-sta', 'numtreat-sta'] # Specify what plots to display here
+    
+    from optima import Project
+    Q = Project(spreadsheet='cascade2p.xlsx')
+    Q.settings.usecascade = True
+    qq = tic()
+    Q.runsim()
+    toc(qq, label='model run with cascade')
+    
+    
+    if doplot:
+        """
+        from optima import plotresults
+        plotresults(P.results[-1], toplot=toplot, figsize=(16,10), num='No cascade')
+        plotresults(Q.results[-1], toplot=toplot, figsize=(16,10), num='With cascade')
+        """
+        settings = Q.settings
+        from numpy import linspace
+        tvec = linspace(settings.start, settings.end, round((settings.end-settings.start)/settings.dt)+1)
+
+        plhiv = Q.results.raw[0]['people'][settings.allplhiv,:,:].sum(axis=(0,1))
+        alldx = Q.results.raw[0]['people'][settings.alldx,:,:].sum(axis=(0,1))
+        care = Q.results.raw[0]['people'][settings.care,:,:].sum(axis=(0,1))
+
+        from pylab import figure, plot
+        figure()
+        plot(tvec,care)
+        
+        
     done(t)
 
 
