3985b8c31f4432216caafdbb3ea0bec53c9d1f62
DrMud <madhura.killedar.astro@gmail.com>
2016-01-29 17:57:18 +1100

    no simple test; add allcare results; adjust spacing

--- tests/testcascade.py
+++ tests/testcascade.py
@@ -9,7 +9,7 @@ Version: 2016jan23 by cliffk
 ## Define tests to run here!!!
 tests = [
 #'compare',
-'simple',
+#'simple',
 'cascade'
 ]
 
@@ -129,24 +129,26 @@ if 'cascade' in tests:
         from matplotlib import pyplot as plt
 
 
-        alive = results.raw[0]['people'][settings.allstates,:,:].sum(axis=(0,1))
-        sus   = results.raw[0]['people'][settings.sus,:,:].sum(axis=(0,1))
-        alldx = results.raw[0]['people'][settings.alldx,:,:].sum(axis=(0,1))
-        diag  = results.raw[0]['people'][settings.dx,:,:].sum(axis=(0,1))
-        care  = results.raw[0]['people'][settings.care,:,:].sum(axis=(0,1))
-        plhiv = results.raw[0]['people'][settings.allplhiv,:,:].sum(axis=(0,1))
-        treat = results.raw[0]['people'][settings.alltreat,:,:].sum(axis=(0,1))
-        supp  = results.raw[0]['people'][settings.svl,:,:].sum(axis=(0,1))
-        lost  = results.raw[0]['people'][settings.lost,:,:].sum(axis=(0,1))
-        off   = results.raw[0]['people'][settings.off,:,:].sum(axis=(0,1))
+        alive   = results.raw[0]['people'][settings.allstates,:,:].sum(axis=(0,1))
+        sus     = results.raw[0]['people'][settings.sus,:,:].sum(axis=(0,1))
+        alldx   = results.raw[0]['people'][settings.alldx,:,:].sum(axis=(0,1))
+        diag    = results.raw[0]['people'][settings.dx,:,:].sum(axis=(0,1))
+        care    = results.raw[0]['people'][settings.care,:,:].sum(axis=(0,1))
+        allcare = results.raw[0]['people'][settings.allcare,:,:].sum(axis=(0,1))
+        plhiv   = results.raw[0]['people'][settings.allplhiv,:,:].sum(axis=(0,1))
+        treat   = results.raw[0]['people'][settings.alltreat,:,:].sum(axis=(0,1))
+        supp    = results.raw[0]['people'][settings.svl,:,:].sum(axis=(0,1))
+        lost    = results.raw[0]['people'][settings.lost,:,:].sum(axis=(0,1))
+        off     = results.raw[0]['people'][settings.off,:,:].sum(axis=(0,1))
         deadhiv = (results.raw[0]['death'][:,:].sum(axis=0)).cumsum()
         deadoth = (results.raw[0]['otherdeath'][:,:].sum(axis=0)).cumsum()
 
         fig = plt.figure()
         plt.plot(tvec,plhiv,label='PLHIV')
-        plt.plot(tvec,alldx,label='All diagnosed')
+        plt.plot(tvec,alldx,label='all diagnosed')
         #plt.plot(tvec,diag,label='diagnosed but not in care/pre-treatment')
-        plt.plot(tvec,care,label='care')
+        #plt.plot(tvec,care,label='care')
+        plt.plot(tvec,allcare,label='all in care')
         plt.plot(tvec,treat,label='on treatment')
         plt.plot(tvec,supp,label='suppressed viral load')
         #plt.plot(tvec,lost,label='off ART, not in care')
