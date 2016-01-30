e165d3e9b4dfe6261a1fed9d6224fa4c05b35407
jpuri <jyotipuri@gmail.com>
2016-01-30 10:30:55 +0530

    feat(CostCoverage): changes in add cost covergae year section

--- client/source/js/modules/project-set/cost-coverage/cost/cost-ctrl.js
+++ client/source/js/modules/project-set/cost-coverage/cost/cost-ctrl.js
@@ -12,6 +12,7 @@ define(['./../../module', 'underscore'], function (module, _) {
     $scope.changeSelectedProgram = function() {
       $scope.state.ccData = angular.copy($scope.selectedProgram.addData);
       fetchGraph();
+      fetchDefaultData();
     };
 
     $scope.addCCData = function() {
@@ -19,12 +20,25 @@ define(['./../../module', 'underscore'], function (module, _) {
       $scope.state.newAddData = {};
     };
 
+    $scope.deleteCCData = function(data) {
+      var index = $scope.state.ccData.indexOf(data);
+      $scope.state.ccData.splice(index, 1);
+    };
+
     var fetchGraph = function() {
       $http.get('/api/project/' + $scope.vm.openProject.id + '/progset/' + $scope.vm.selectedProgramSet.id + '/program/' + $scope.selectedProgram.id + '/costcoverage/graph')
         .success(function (response) {
           console.log('response', response);
         });
+    };
+
+    var fetchDefaultData = function() {
+      $http.get('/api/project/' + $scope.vm.openProject.id + '/progset/' + $scope.vm.selectedProgramSet.id + '/program/' + $scope.selectedProgram.id + '/costcoverage')
+        .success(function (response) {
+          console.log('response', response);
+        });
     }
+
   });
 
 });
