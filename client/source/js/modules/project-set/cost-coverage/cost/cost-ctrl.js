1118f980f60d367c1a9b9b51a5311263a08caf38
jpuri <jyotipuri@gmail.com>
2016-01-30 10:20:40 +0530

    feat(CostCoverage): fixes in year section

--- client/source/js/modules/project-set/cost-coverage/cost/cost-ctrl.js
+++ client/source/js/modules/project-set/cost-coverage/cost/cost-ctrl.js
@@ -4,21 +4,20 @@ define(['./../../module', 'underscore'], function (module, _) {
   module.controller('CostController', function ($scope, $http) {
 
     var vc = $scope;
-    $scope.newAddData = {};
+    $scope.state = {
+      newAddData: {},
+      ccData: []
+    };
 
     $scope.changeSelectedProgram = function() {
-      $scope.ccData = angular.copy($scope.selectedProgram.addData);
+      $scope.state.ccData = angular.copy($scope.selectedProgram.addData);
       fetchGraph();
     };
 
     $scope.addCCData = function() {
-      if (!$scope.ccData) {
-        $scope.ccData = [];
-      }
-      $scope.ccData.push($scope.newAddData);
-      $scope.newAddData = {};
+      $scope.state.ccData.push($scope.state.newAddData);
+      $scope.state.newAddData = {};
     };
-
 
     var fetchGraph = function() {
       $http.get('/api/project/' + $scope.vm.openProject.id + '/progset/' + $scope.vm.selectedProgramSet.id + '/program/' + $scope.selectedProgram.id + '/costcoverage/graph')
