92e790ae71de726f95bd050417ee310621e3c1b4
Anna Nachesa <anachesa@gmail.com>
2016-01-30 22:40:44 +0100

    added ccopar update

--- server/webapp/resources/project_progsets.py
+++ server/webapp/resources/project_progsets.py
@@ -53,6 +53,15 @@ costcov_data_locator_parser.add_arguments({
     'year': {'required': True, 'location': 'args'}
 })
 
+costcov_param_parser = RequestParser()
+costcov_param_parser.add_arguments({
+    'year': {'required': True, 'type': int},
+    'saturationpercent_lower': {'required': True, 'type': int},
+    'saturationpercent_upper': {'required': True, 'type': int},
+    'unitcost_lower': {'required': True, 'type': int},
+    'unitcost_upper': {'required': True, 'type': int},
+})
+
 program_parser = RequestParser()
 program_parser.add_arguments({
     'name': {'required': True, 'location': 'json'},
@@ -440,5 +449,67 @@ class CostCoverageData(Resource):
         """
         args = costcov_data_locator_parser.parse_args()
         result = modify_program(project_id, progset_id, program_id, args, self.delete_data_for_instance)
+
+        return result
+
+
+class CostCoverageParam(Resource):
+    """
+    Modification of parameters for the given program.
+    """
+    method_decorators = [report_exception, login_required]
+
+    def add_param_for_instance(self, program_instance, args, overwrite=False):
+        program_instance.costcovfn.addccopar(
+            {'saturation': (args['saturationpercent_lower'], args['saturationpercent_upper']),
+                't': args['year'],
+                'unitcost': (args['unitcost_lower'], args['unitcost_upper'])},
+            overwrite=overwrite)
+
+    def update_param_for_instance(self, program_instance, args):
+        self.add_param_for_instance(program_instance, args, overwrite=True)
+
+    def delete_param_for_instance(self, program_instance, args):
+        program_instance.costcovfn.rmccopar(t=args['year'])
+
+    @swagger.operation(description="Add new cco parameter.",
+                       parameters=costcov_param_parser.swagger_parameters())
+    def post(self, project_id, progset_id, program_id):
+        """
+        adds a _new_ cco param to program parameters.
+        It should then be given to BE in this way:
+        program.addccopar({
+            'saturation': (saturationpercent_lower,saturationpercent_upper),
+            't': year,
+            'unitcost': (unitcost_lower,unitcost_upper)})
+        """
+        args = costcov_param_parser.parse_args()
+        result = modify_program(project_id, progset_id, program_id, args, self.add_param_for_instance)
+
+        return result, 201
+
+    @swagger.operation(description="Edit existing cco parameter.",
+                       parameters=costcov_param_parser.swagger_parameters())
+    def put(self, project_id, progset_id, program_id):
+        """
+        edits existing data point to program parameters.
+        It should then be given to BE in this way:
+        program.addccopar({
+            'saturation': (saturationpercent_lower,saturationpercent_upper),
+            't': year,
+            'unitcost': (unitcost_lower,unitcost_upper)})
+        """
+        args = costcov_param_parser.parse_args()
+        result = modify_program(project_id, progset_id, program_id, args, self.update_param_for_instance)
+
+        return result
+
+    @swagger.operation(description="Remove cco parameter.")
+    def delete(self, project_id, progset_id, program_id):
+        """
+        removes cco parameter for the given year from program parameters.
+        """
+        args = costcov_data_locator_parser.parse_args()
+        result = modify_program(project_id, progset_id, program_id, args, self.delete_param_for_instance)
 
         return result
