c19a4717a2cd79cdd9592b7bb549faf4f3df0b92
HawkOwl <hawkowl@atleastfornow.net>
2016-02-01 16:07:30 +0800

    fix syntax error

--- server/db/versions/023_add_scenario_table_postgresql_upgrade.sql
+++ server/db/versions/023_add_scenario_table_postgresql_upgrade.sql
@@ -10,5 +10,5 @@ CREATE TABLE scenario (
     PRIMARY KEY (id),
     FOREIGN KEY(project_id) REFERENCES projects (id),
     FOREIGN KEY(progset_id) REFERENCES progsets (id),
-    FOREIGN KEY(parset_id) REFERENCES parsets (id),
+    FOREIGN KEY(parset_id) REFERENCES parsets (id)
 );
