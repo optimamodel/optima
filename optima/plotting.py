e42a763dace1dbc5d69d92a5f85e51a10c4a4f8e
cliffckerr <questionably@gmail.com>
2016-02-01 16:31:35 -0500

    changes to plotting didnt fix problem of multiplots not showing up

--- optima/plotting.py
+++ optima/plotting.py
@@ -13,7 +13,7 @@ Version: 2016jan24
 
 from optima import OptimaException, Resultset, Multiresultset, odict, printv, gridcolormap, sigfig
 from numpy import array, ndim, maximum, arange, zeros, mean, shape
-from pylab import isinteractive, ioff, ion, figure, plot, close, ylim, fill_between, scatter, gca, subplot
+from pylab import isinteractive, ioff, ion, figure, plot, close, ylim, fill_between, scatter, gca, subplot, hold, bar
 
 # Define allowable plot formats -- 3 kinds, but allow some flexibility for how they're specified
 epiformatslist = array([['t', 'tot', 'total'], ['p', 'per', 'per population'], ['s', 'sta', 'stacked']])
@@ -475,13 +475,12 @@ def plotallocs(multires=None, which=None, die=True, figsize=(14,10), verbose=2, 
     fig.subplots_adjust(bottom=0.30) # Less space on bottom
     fig.subplots_adjust(hspace=0.50) # More space between
     colors = gridcolormap(nprogs)
-    ax = []
     ymax = 0
     
     for plt in range(nallocs):
         nbudgetyears = len(budgetyearstoplot[plt])
-        ax.append(subplot(nallocs,1,plt+1))
-        ax[-1].hold(True)
+        subplot(nallocs,1,plt+1)
+        hold(True)
         barwidth = .5/nbudgetyears
         for y in range(nbudgetyears):
             progdata = array([x[y] for x in toplot[plt][:]]) # Otherwise, multiplication simply duplicates the array
@@ -492,17 +491,19 @@ def plotallocs(multires=None, which=None, die=True, figsize=(14,10), verbose=2, 
                 else: barcolor = colors[p] # Only one year? Color by program
                 if p==nprogs-1: yearlabel = budgetyearstoplot[plt][y]
                 else: yearlabel=None
-                ax[-1].bar([xbardata[p]], [progdata[p]], label=yearlabel, width=barwidth, color=barcolor)
-        if nbudgetyears>1: ax[-1].legend()
-        ax[-1].set_xticks(arange(nprogs)+1)
-        if plt<nprogs: ax[-1].set_xticklabels('')
-        if plt==nallocs-1: ax[-1].set_xticklabels(proglabels,rotation=90)
-        ax[-1].set_xlim(0,nprogs+1)
+                bar([xbardata[p]], [progdata[p]], label=yearlabel, width=barwidth, color=barcolor)
+        
+        ax = gca()        
+        if nbudgetyears>1: ax.legend()
+        ax.set_xticks(arange(nprogs)+1)
+        if plt<nprogs: ax.set_xticklabels('')
+        if plt==nallocs-1: ax.set_xticklabels(proglabels,rotation=90)
+        ax.set_xlim(0,nprogs+1)
         
         ylabel = 'Spending (US$)' if which=='budget' else 'Coverage (% of targeted)'
-        ax[-1].set_ylabel(ylabel)
-        ax[-1].set_title(alloclabels[plt])
-        ymax = maximum(ymax, ax[-1].get_ylim()[1])
+        ax.set_ylabel(ylabel)
+        ax.set_title(alloclabels[plt])
+        ymax = maximum(ymax, ax.get_ylim()[1])
         
     close(fig)
     
