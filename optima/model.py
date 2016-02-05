85b5df3be20173c615da0dc8d28e4008d75a026c
cliffckerr <questionably@gmail.com>
2016-02-05 00:39:45 -0500

    putting things back

--- optima/model.py
+++ optima/model.py
@@ -3,7 +3,7 @@ from math import pow as mpow
 from numpy import zeros, exp, maximum, minimum, hstack, inf
 from optima import OptimaException, printv, tic, toc, dcp, odict, makesimpars, Resultset
 
-def model(simpars=None, settings=None, verbose=None, benchmark=False, die=False):
+def model(simpars=None, settings=None, verbose=None, benchmark=False, die=True):
     """
     Runs Optima's epidemiological model.
     
