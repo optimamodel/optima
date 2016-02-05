2cb7650041bf4e4d20b098e8a6c9570f3cbd5ef5
cliffckerr <questionably@gmail.com>
2016-02-04 22:06:05 -0500

    added error checking back in

--- optima/model.py
+++ optima/model.py
@@ -783,6 +783,11 @@ def model(simpars=None, settings=None, verbose=None, benchmark=False, die=True):
             newpeople = popsize[:,t+1]-people[:,:,t+1].sum(axis=0) # Number of people to add according to simpars['popsize'] (can be negative)
             people[susreg,:,t+1] += newpeople - numcirc[:,t] # Add new people, then subtract if from circumcision
             people[circ,:,t+1]   += numcirc[:,t] # And add these people into the circumcised compartment
+            actualpeople = people[:,:,t+1].sum()
+            wantedpeople = popsize[:,t+1].sum()
+            if abs(actualpeople-wantedpeople)>1.0: # Nearest person is fiiiiine
+                errormsg = 'model(): Population size inconsistent at time t=%f: %f vs. %f' % (tvec[t+1], actualpeople, wantedpeople)
+                raise OptimaException(errormsg)
             if not((people[:,:,t+1]>=0).all()): # If not every element is a real number >0, throw an error
                 for errstate in range(nstates): # Loop over all heath states
                     for errpop in range(npops): # Loop over all populations
