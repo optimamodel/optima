15d44cfa6b4dfcb5d209db0e948a740dd6950d9e
jpuri <jyotipuri@gmail.com>
2016-02-08 02:03:02 +0530

    feat(Scenario): fix saving program scenarios

--- client/source/js/modules/program-scenarios-modal/program-scenarios-modal.js
+++ client/source/js/modules/program-scenarios-modal/program-scenarios-modal.js
@@ -8,33 +8,36 @@ define(['angular'], function (module) {
         $scope.progsets = progsets;
         $scope.ykeys = ykeys.data.keys;
     	$scope.openProject = openProject.populations;
-    	
+
     	$scope.progsetsOptimized = _.filter(progsets[0].programs, {optimizable: true});
 
-    	if(!angular.isDefined($scope.row[$scope.row.scenario_type])){
-    		$scope.row[$scope.row.scenario_type] = {};
-    	}
+      $scope.row.t = [];
+      $scope.row.budget = [];
+      _.forEach($scope.progsetsOptimized, function(progset) {
+        $scope.row.budget.push(
+          {'program':progset.short_name, 'values':[]}
+        );
+      });
 
-    	$scope.manageScenario = function(){
+      $scope.manageScenario = function(){
     		var row = {
     			"scenario_type": $scope.row.scenario_type,
     			"name": $scope.row.name, 
     			"parset_id": $scope.row.parset_id || null,
     			"active": true, 
-    			//"t": [], 
+    			"t": $scope.row.t,
+          "budget": $scope.row.budget,
     			"id": $scope.row.id || null, 
     			"progset_id": $scope.row.progset_id || null
     		};
-
-			row[$scope.row.scenario_type] = $scope.row[$scope.row.scenario_type];
     		$modalInstance.close(row);
     	};
 
         $scope.addParam = function(row) {
-            if (!row.pars) {
-                row.pars = [];
+          if (!$scope.temp) {
+            $scope.temp = [];
             }
-            row.pars.push({});
+          $scope.temp.push({});
         };
 
         $scope.removeParam = function(row, paramIndex) {
@@ -42,10 +45,5 @@ define(['angular'], function (module) {
                 return index === paramIndex
             });
         };
-
-        $scope.closeModal = function() {
-            $modalInstance.close($scope.row);
-        };
-
     });
 });
