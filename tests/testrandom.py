78b7c63c83a37901786c1426fec8d7728864b89b
cliffckerr <questionably@gmail.com>
2016-02-08 10:26:51 -0500

    calibration works

--- tests/testrandom.py
+++ tests/testrandom.py
@@ -2,7 +2,32 @@
 Version:
 """
 
-import optima as op
-P = op.defaults.defaultproject('concentrated')
-op.pygui(P.results[0])
+from optima import defaults, pygui
+P = defaults.defaultproject('concentrated')
 
+## Calibration
+precalibrate = True
+if precalibrate: 
+    P.parsets[0].pars[0]['force'].y[:] = [ 1.4   ,  0.9125,  0.9   ,  0.8   ,  1.35  ,  0.625 ]
+    P.runsim()
+else: P.autofit(name='default', maxiters=60)
+
+
+pygui(P.parsets[-1].getresults())
+
+#defaultbudget = P.progsets['default'].getdefaultbudget()
+#maxbudget = dcp(defaultbudget)
+#for key in maxbudget: maxbudget[key] += 1e14
+#scenlist = [
+#    Budgetscen(name='Current conditions', parsetname='default', progsetname='default', t=[2016], budget=defaultbudget),
+#    Budgetscen(name='Unlimited spending', parsetname='default', progsetname='default', t=[2016], budget=maxbudget),
+#    ]
+#
+## Run the scenarios
+#P.addscenlist(scenlist)
+#P.runscenarios() 
+# 
+#if doplot:
+#    from optima import pygui, plotpars
+#    apd = plotpars([scen.scenparset.pars[0] for scen in P.scens.values()])
+#    pygui(P.results[-1], toplot='default')
