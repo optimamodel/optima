19c8daab761ac694acd8b434aa06859600638aa7
cliffckerr <questionably@gmail.com>
2016-02-08 10:03:03 -0500

    azfar project is very complex, going to try default

--- tests/testrandom.py
+++ tests/testrandom.py
@@ -3,11 +3,5 @@ Version:
 """
 
 import optima as op
-P = op.Project(
-P = op.defaults.defaultproject('generalized')
+P = op.Project(spreadsheet='example.xlsx')
 op.pygui(P.results[0])
-
-#plotpars(P)
-#for p in range(12):
-#    plotpeople(P, start=0, pops=p, skipempty=True, animate=False)
-#pygui(P.results[-1])
