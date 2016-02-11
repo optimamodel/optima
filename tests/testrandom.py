25759cb5912b02a293f857103ffc62da2102c912
cliffckerr <questionably@gmail.com>
2016-02-11 10:48:06 -0500

    trying out azfar project

--- tests/testrandom.py
+++ tests/testrandom.py
@@ -1,9 +1,10 @@
 """
 Create a good test project
 
-Version: 2016feb08
+Version: 2016feb11
 """
 
 import optima as op
-filename = '/u/cliffk/unsw/optima/tests/Colombia 20160209 (2.0) (1).xlsx'
-P = op.Project(spreadsheet=filename)
+filename = '/u/cliffk/unsw/optima/tests/exercise_optimization.prj'
+P = op.loadobj(filename)
+
