887f48bab1451252254053ad3e17a668f60ca415
cliffckerr <questionably@gmail.com>
2016-02-11 10:54:04 -0500

    diagnosing

--- tests/testrandom.py
+++ tests/testrandom.py
@@ -7,4 +7,7 @@ Version: 2016feb11
 import optima as op
 filename = '/u/cliffk/unsw/optima/tests/exercise_optimization.prj'
 P = op.loadobj(filename)
+P.progsets[0].reconcile(parset=P.parsets[0], year=2016)
 
+P.optimize(maxtime=20)
+op.pygui(P.results[-1])
