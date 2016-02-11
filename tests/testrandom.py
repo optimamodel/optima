138c6b2cd8b180c23bd7624451a2f9114988267c
cliffckerr <questionably@gmail.com>
2016-02-11 12:41:34 -0500

    quite reconciled by now

--- tests/testrandom.py
+++ tests/testrandom.py
@@ -8,8 +8,8 @@ import optima as op
 infile = 'exercise_optimization.prj'
 outfile = 'exercise_optimization.prj'
 P = op.loadobj(infile)
-P.progsets[0].reconcile(parset=P.parsets[0], year=2016)
-P = op.saveobj(outfile,P)
+#P.progsets[0].reconcile(parset=P.parsets[0], year=2016, maxiters=500)
+#P = op.saveobj(outfile,P)
 
 P.optimize(maxtime=20)
 op.pygui(P.results[-1])
