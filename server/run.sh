beef44c47d8b26710141367b09207c7947dc305d
Bosco Ho <apposite@gmail.com>
2016-02-14 21:35:13 +1100

    improved grep sort in run.sh

--- server/run.sh
+++ server/run.sh
@@ -21,12 +21,9 @@ TMP_DEPS=/tmp/temp_deps_${RANDOM}
 # create a sorted list of existing dependencies 
 # against requirements.txt and compare that
 # to a sorted version of requirements.txt
-pip freeze -l > ${TMP_DEPS}
-grep -f requirements.txt ${TMP_DEPS} > ${TMP_DEPS}.grep
-sort ${TMP_DEPS}.grep > ${TMP_DEPS}.sort
+pip freeze -l | grep -f requirements.txt | sort > ${TMP_DEPS}
 sort ./requirements.txt > ${TMP_DEPS}.requirements.txt
-
-if ! cmp ${TMP_DEPS}.requirements.txt ${TMP_DEPS}.sort > /dev/null 2>&1
+if ! cmp ${TMP_DEPS}.requirements.txt ${TMP_DEPS} > /dev/null 2>&1
 then
   echo "Installing Python dependencies ..."
   cat ${TMP_DEPS}
