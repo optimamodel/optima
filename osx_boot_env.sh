e81446a2e38515328641a2eefceecd5b38f775c1
Bosco Ho <apposite@gmail.com>
2016-03-09 15:29:06 +1100

    sh script tweaks

--- osx_boot_env.sh
+++ osx_boot_env.sh
@@ -25,4 +25,12 @@ osascript 2>/dev/null <<EOF
         do script with command "cd \"$PWD/server\"; $*" in window 0
         do script with command "./run.sh" in window 0
     end
+    tell application "System Events"
+        tell process "Terminal" to keystroke "t" using command down
+    end
+    tell application "Terminal"
+        activate
+        do script with command "cd \"$PWD/server\"; $*" in window 0
+        do script with command "./celery.sh" in window 0
+    end
 EOF
