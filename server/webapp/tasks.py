14a22c89644e92127a4bce58cb0199fa88eefc39
Bosco Ho <apposite@gmail.com>
2016-07-05 10:04:44 +1000

    double checking of setting workingproject

--- server/webapp/tasks.py
+++ server/webapp/tasks.py
@@ -67,6 +67,15 @@ def start_or_report_calculation(project_id, parset_id, work_type):
     calculation to proceed. If a job is requested for a given project with
     the same parset and work_type, then a can-join = True is issued.
 
+    Only one job per project_id is allowed to run. There should only be
+    one existing WorkingProjectDb in existence for a project id. Obviously.
+    This WorkingProjectDb should be deleted when the job is finished. The
+    results are saved in a ResultsDb and a ParsetsDb (for autofit).
+
+    There should only be one WorkLogDb active with a self.status == 'started'.
+    Other WorkLogDb must have self.status == 'completed', 'cancelled', 'error'.
+
+
     Args:
         project_id: uuid of project that will be extracted and pickled for async calc
         parset_id: uuid of chosen parset for the calculation
@@ -97,11 +106,9 @@ def start_or_report_calculation(project_id, parset_id, work_type):
         'work_type': ''
     }
 
-    work_log_records = db_session.query(WorkLogDb).filter_by(
-        project_id=project_id)
-
-    # a work_log exists for this project and it has started,
+    # if any work_log exists for this project and it has started,
     # then this calculation is blocked from starting
+    work_log_records = db_session.query(WorkLogDb).filter_by(project_id=project_id)
     for work_log_record in work_log_records:
         if work_log_record.status == 'started':
             close_db_session(db_session)
@@ -113,6 +120,8 @@ def start_or_report_calculation(project_id, parset_id, work_type):
     work_log_records = db_session.query(WorkLogDb).filter_by(
         project_id=project_id, parset_id=parset_id, work_type=work_type)
     work_log_records.delete()
+
+    # shouldn't do this but just in case
     work_project_records = db_session.query(WorkingProjectDb).filter_by(
         id=project_id)
     work_project_records.delete()
@@ -210,10 +219,11 @@ def check_calculation_status(project_id, parset_id, work_type):
     return result
 
 
-def get_parset_from_project(project, parset_name):
-    parsets = project.parsets;
-    if parset_name in parsets:
-        return parsets[parset_name]
+def get_parset_from_project_by_id(project, parset_id):
+    for key, parset in project.parsets.items():
+        print ">> Matching", key, str(parset_id), str(parset.uid)
+        if str(parset.uid) == str(parset_id):
+            return parset
     else:
         return None
 
@@ -222,16 +232,22 @@ def get_parset_from_project(project, parset_name):
 def run_autofit(project_id, parset_name, maxtime=60):
     import traceback
     app.logger.debug("started autofit: {} {}".format(project_id, parset_name))
-    error_text = ""
-    status = 'completed'
 
     db_session = init_db_session()
     working_project_record = db_session.query(WorkingProjectDb).filter_by(id=project_id).first()
     project = op.loads(working_project_record.project)
+    work_log_id = working_project_record.work_log_id
+    work_log = db_session.query(WorkLogDb).get(work_log_id)
+    parset = get_parset_from_project_by_id(project, work_log.parset_id)
     close_db_session(db_session)
 
     result = None
+    error_text = ""
+    status = 'completed'
     try:
+        # sanity check
+        assert work_log.status == "started"
+        assert parset_name == parset.name
         project.autofit(
             name=str(parset_name),
             orig=str(parset_name),
@@ -243,24 +259,20 @@ def run_autofit(project_id, parset_name, maxtime=60):
         var = traceback.format_exc()
         print("ERROR for project_id: %s, args: %s calculation: %s\n %s" % (project_id, parset_name, 'autofit', var))
         error_text = var
-        status='error'
+        status = 'error'
 
     db_session = init_db_session()
     working_project_record = db_session.query(WorkingProjectDb).filter_by(id=project_id).first()
-    working_project_record.project = op.saves(project)
-    working_project_record.is_working = False
-    working_project_record.work_type = None
-    db_session.add(working_project_record)
-
-    work_lob_id = working_project_record.work_log_id
-    work_log = db_session.query(WorkLogDb).get(work_lob_id)
+    if working_project_record is not None:
+        db_session.delete(working_project_record)
+    work_log = db_session.query(WorkLogDb).get(work_log_id)
     work_log.status = status
     work_log.error = error_text
     work_log.stop_time = datetime.datetime.now(dateutil.tz.tzutc())
+    db_session.add(work_log)
 
     if result:
         print(">> Save autofitted parset '%s'" % parset_name)
-        parset = get_parset_from_project(project, parset_name)
         update_or_create_parset_record(
             project_id, parset_name, parset, db_session)
         delete_result(project_id, parset.uid, 'calibration', db_session=db_session)
@@ -271,11 +283,8 @@ def run_autofit(project_id, parset_name, maxtime=60):
         db_session.add(result_record)
         work_log.result_id = result_record.id
 
-    db_session.add(work_log)
-
     db_session.commit()
     close_db_session(db_session)
-
     app.logger.debug("stopped autofit")
 
 
@@ -289,19 +298,24 @@ def run_optimization(project_id, optimization_name, parset_name, progset_name, o
     app.logger.debug(pprint.pformat(constraints, indent=2))
     app.logger.debug(pprint.pformat("maxtime %f" % maxtime))
 
-    error_text = ""
-    status = 'completed'
-
     db_session = init_db_session()
     working_project_record = db_session.query(WorkingProjectDb).filter_by(id=project_id).first()
     project = op.loads(working_project_record.project)
+    work_log_id = working_project_record.work_log_id
+    work_log = db_session.query(WorkLogDb).get(work_log_id)
+    parset = get_parset_from_project_by_id(project, work_log.parset_id)
     close_db_session(db_session)
 
-    result = None
     if not objectives['budget']:
         objectives['budget'] = 1000000
 
+    error_text = ""
+    status = 'completed'
+    result = None
     try:
+        # sanity check
+        assert work_log.status == "started"
+        assert parset_name == parset.name
         result = project.optimize(
             name=optimization_name,
             parsetname=parset_name,
@@ -313,7 +327,6 @@ def run_optimization(project_id, optimization_name, parset_name, progset_name, o
         # by default result.name = "optim-" + optimization_name
         result.parsetname = parset_name
         print "Creating result '%s'" % result.name
-        status = 'completed'
     except Exception:
         var = traceback.format_exc()
         print("ERROR for project_id: %s, args: %s calculation: %s\n %s" % (project_id, optimization_name, 'optimization', var))
@@ -322,22 +335,21 @@ def run_optimization(project_id, optimization_name, parset_name, progset_name, o
 
     db_session = init_db_session()
     working_project_record = db_session.query(WorkingProjectDb).filter_by(id=project_id).first()
-    working_project_record.project = op.saves(project)
-    work_log_record = db_session.query(WorkLogDb).get(working_project_record.work_log_id)
+    if working_project_record is not None:
+        db_session.delete(working_project_record)
+    work_log_record = db_session.query(WorkLogDb).get(work_log_id)
     work_log_record.status = status
     work_log_record.error = error_text
     work_log_record.stop_time = datetime.datetime.now(dateutil.tz.tzutc())
+
     if result:
         delete_optimization_result(project_id, result.name, db_session)
         result_record = update_or_create_result_record(project_id, result, parset_name, 'optimization', db_session=db_session)
         db_session.add(result_record)
         db_session.flush()
         work_log_record.result_id = result_record.id
-    db_session.delete(working_project_record)
 
-    working_project_record.is_working = False
-    working_project_record.work_type = None
-    db_session.add(working_project_record)
+    db_session.delete(working_project_record)
     db_session.commit()
     close_db_session(db_session)
 
