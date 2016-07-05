9479c07c32f9a64da8302b845224ba0d978f2874
Bosco Ho <apposite@gmail.com>
2016-07-05 10:54:52 +1000

    remove unloaded

--- server/webapp/plot.py
+++ server/webapp/plot.py
@@ -109,10 +109,6 @@ def make_mpld3_graph_dict(result, which=None):
         for selector in selectors:
             selector['checked'] = selector['key'] in which
 
-    for selector in selectors:
-        if not selector['checked']:
-            selector['name'] = '(unloaded) ' + selector['name']
-
     graphs = op.plotting.makeplots(result, toplot=which, figsize=(4, 3))
 
     graph_selectors = []
