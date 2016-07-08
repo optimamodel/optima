e20055d38c65f8abafd9cff18f7d9dbc78b10253
Bosco Ho <apposite@gmail.com>
2016-07-08 11:29:49 +1000

    changed osx_boot_env.sh for bin/run_server

--- osx_boot_env.sh
+++ osx_boot_env.sh
@@ -23,15 +23,15 @@ osascript 2>/dev/null <<EOF
     end
     tell application "Terminal"
         activate
-        do script with command "cd \"$PWD/server\"; $*" in window 0
-        do script with command "./run.sh" in window 0
+        do script with command "cd \"$PWD/\"; $*" in window 0
+        do script with command "python bin/run_server.py" in window 0
     end
     tell application "System Events"
         tell process "Terminal" to keystroke "t" using command down
     end
     tell application "Terminal"
         activate
-        do script with command "cd \"$PWD/server\"; $*" in window 0
-        do script with command "./celery.sh" in window 0
+        do script with command "cd \"$PWD/\"; $*" in window 0
+        do script with command "celery -A server.webapp.tasks.celery_instance worker -l info" in window 0
     end
 EOF
