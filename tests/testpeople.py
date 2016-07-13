b2500814e3ee951f85d6b44fcdc3b676d8c09989
cliffckerr <questionably@gmail.com>
2016-07-13 15:25:24 +0200

    removed temporary code in testpeople.py

--- tests/testpeople.py
+++ tests/testpeople.py
@@ -11,15 +11,10 @@ import os
 
 refresh = 0 # Creates defaultpeople.ppl rather than copares
 eps = 1e-3 # Don't expect a totally exact match
-filename = 'proptxtestraw.npy'
+filename = '2016jul13.npy'
 
 P = op.defaults.defaultproject('generalized')
 P.results = op.odict() # Clear
-
-# TEMP
-pars = P.parsets[0].pars[0]
-pars['proptx'].t[0] = [0, 2017.7, 2017.8, 2018.0, 2020.0, 2025.0]
-pars['proptx'].y[0] = [nan, nan, 0.6, 0.7, 0.8, 0.9]
 P.runsim()
 
 newraw = P.results[0].raw[0]
@@ -40,7 +35,7 @@ else:
                     for t in range(shape(diffraw)[2]): # Loop over all heath states
                         for errstate in range(shape(diffraw)[0]): # Loop over all heath states
                             for errpop in range(shape(diffraw)[1]): # Loop over all populations
-                                if diffraw[errstate,errpop,t]!=0:
+                                if abs(diffraw[errstate,errpop,t])>eps:
                                     errormsg = 'WARNING, people do not match!\npeople[%i, %i, %i] = %f vs. %f' % (errstate, errpop, t, oldraw['people'][errstate,errpop,t], newraw['people'][errstate,errpop,t])
                                     raise Exception(errormsg)
     else:
