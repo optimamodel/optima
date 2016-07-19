1fe9e66f2a102750371eb258531432073298a8a3
cliffckerr <questionably@gmail.com>
2016-07-19 16:53:59 +0200

    slightly modified suggestion

--- optima/migrations/migrate.py
+++ optima/migrations/migrate.py
@@ -1,34 +1,51 @@
 import optima as op
 
-
-def _TwoToTwoOneMigration(project):
+def addscenuid(project, **kwargs):
     """
     Migration between Optima 2.0 and 2.1.
     """
     for scen in project.scens.values():
         if not hasattr(scen, 'uid'):
             scen.uid = op.uuid()
+    project.version = "2.0.1"
+    return None
 
-    project.version = "2.1"
+
+def addforcepopsize(project, **kwargs):
+    """
+    Migration between Optima 2.0 and 2.1.
+    """
+    if not hasattr(project.settings, 'forcepopsize'):
+        project.settings.forcepopsize = True
+    project.version = "2.0.2"
+    return None
 
 
-_MIGRATIONS = {
-    '2.0': _TwoToTwoOneMigration
+
+
+migrations = {
+    '2.0.0': addscenuid,
+    '2.0.1': addforcepopsize,
 }
 
 
-def migrateproject(project):
+
+def runmigrations(project, verbose=2):
     """
     Migrate an Optima Project by inspecting the version and working its way up.
     """
     while str(project.version) != str(op.__version__):
-        if not str(project.version) in _MIGRATIONS:
+        if not str(project.version) in migrations:
             raise ValueError("We can't upgrade version %s" % (project.version,))
 
-        upgrader = _MIGRATIONS[str(project.version)]
+        upgrader = migrations[str(project.version)]
 
-        print("Migrating from %s" % (project.version,))
-        upgrader(project)
-        print("Migrated to %s" % (project.version,))
+        op.printv("Migrating from %s" % project.version, 2, verbose)
+        try: 
+            upgrader(project, verbose=verbose)
+            op.printv("Migrated to %s" % project.version, 2, verbose)
+        except Exception as E:
+            print('Migration failed!!!!')
+            raise E
 
     return project
