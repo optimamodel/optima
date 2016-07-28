0f92817e410ed0e1aacc2fd17787e6660de504d9
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-07-29 02:30:13 +0800

    nicer import/export

--- server/export.py
+++ server/export.py
@@ -7,13 +7,11 @@ from hashlib import sha224
 
 @click.command()
 @click.option('--old', default="http://athena.optimamodel.com")
-@click.option('--new', default="http://localhost:8080")
 @click.option('--username', default='test')
 @click.option('--password', default='test')
 def main(old, new, username, password):
 
     old_session = requests.Session()
-    new_session = requests.Session()
 
     click.echo('Logging in as %s...' % (username,))
     hashed_password = sha224()
@@ -29,22 +27,10 @@ def main(old, new, username, password):
         sys.exit(1)
     click.echo("Logged in as %s on old server" % (old_login.json()["displayName"],))
 
-    # New server login
-    new_login = new_session.post(new + "/api/user/login",
-                                 json={'username': username,
-                                       'password': password})
-    if not new_login.status_code == 200:
-        click.echo("Failed login:\n%s" % (new_login.content,))
-        sys.exit(1)
-    click.echo("Logged in as %s on new server" % (new_login.json()["displayName"],))
-
-    # Get the projects
-    click.echo("Getting projects...")
-    old_projects = old_session.get(old + "/api/project").json()["projects"]
-    click.echo("Got list of %d projects." % (len(old_projects),))
+    project_path = '%sprojects' % (user,)
 
     try:
-        os.makedirs('projects')
+        os.makedirs(project_path)
     except:
         pass
 
@@ -52,37 +38,17 @@ def main(old, new, username, password):
         click.echo("Downloading project '%s'" % (project["name"],))
         url = old + "/api/project/" + project["id"] + "/data"
 
-        if os.path.isfile("projects/" + project["name"] + ".prj"):
+        if os.path.isfile(project_path +  "/" + project["name"] + ".prj"):
             click.echo("Downloaded already, skipping...")
             continue
 
         download = old_session.get(url)
 
-        with open("projects/" + project["name"] + ".prj", 'wb') as f:
+        with open(project_path + "/" + project["name"] + ".prj", 'wb') as f:
             f.write(download.content)
 
 
-    click.echo("All downloaded! Reuploading...")
-    click.echo("First, getting the projects off the new server. Same named projects will not be reuploaded.")
-
-    new_projects = new_session.get(new + "/api/project").json()["projects"]
-    new_project_names = [x["name"] for x in new_projects]
-
-    for project in old_projects:
-
-        if project["name"] in new_project_names:
-            click.echo("%s is already on new server, skipping." % (project["name"],))
-            continue
-
-        f = open("projects/" + project["name"] + ".prj", 'rb')
-
-        new_project_upload = new_session.post(new + "/api/project/data",
-                                                  data={"name": project["name"]},
-                                                  files={"file": (project["name"] + ".prj", f)})
-        f.close()
-
-        click.echo("Done with response:\n%s" % (new_project_upload.content,))
-
+    click.echo("All downloaded! In folder %s" % (project_path,))
 
 if __name__ == '__main__':
     main()
---
+++ server/import.py
@@ -0,0 +1,69 @@
+import click
+import requests
+import sys
+import os.path
+import glob
+
+from hashlib import sha224
+
+@click.command()
+@click.option('--new', default="http://localhost:8080")
+@click.option('--username', default='test')
+@click.option('--password', default='test')
+@click.option('--overwrite', default=False)
+@click.argument('projectpath', nargs=-1, type=click.Path(exists=True))
+def main(projectpath, new, username, password, overwrite):
+
+    new_session = requests.Session()
+
+    click.echo('Logging in as %s...' % (username,))
+    hashed_password = sha224()
+    hashed_password.update(password)
+    password = hashed_password.hexdigest()
+
+    # New server login
+    new_login = new_session.post(new + "/api/user/login",
+                                 json={'username': username,
+                                       'password': password})
+    if not new_login.status_code == 200:
+        click.echo("Failed login:\n%s" % (new_login.content,))
+        sys.exit(1)
+    click.echo("Logged in as %s on new server" % (new_login.json()["displayName"],))
+
+    click.echo("Uploading...")
+    click.echo("First, getting the projects off the new server.")
+
+    new_projects = new_session.get(new + "/api/project").json()["projects"]
+    new_project_names = [x["name"] for x in new_projects]
+
+    projects = {x["name"]:x for x in new_projects}
+
+    for project_path in list(set([click.format_filename(x) for x in projectpath])):
+        project_name = os.path.basename(project_path)[:-4]
+
+        f = open(project_path, 'rb')
+
+        if project_name in projects.keys():
+
+            if overwrite:
+                click.echo("will overwrite %s" % (project_name,))
+                # todo!!!!!
+
+            else:
+                click.echo("NOT UPLOADING %s because --overwrite=False" % (project_name,))
+                f.close()
+                continue
+
+        else:
+            # New upload
+            new_project_upload = new_session.post(
+                new + "/api/project/data",
+                data={"name": project_name},
+                files={"file": (project_name + ".prj", f)})
+            click.echo("Uploaded %s" % (project_name,))
+
+        f.close()
+
+
+if __name__ == '__main__':
+    main()
