11fdd03fcf9d07cd92816d2f0b282c3be6fb8ba5
Bosco Ho <apposite@gmail.com>
2016-08-01 12:06:54 +1000

    saved work

--- client/source/js/modules/programs/cost-coverage/cost-coverage-ctrl.js
+++ client/source/js/modules/programs/cost-coverage/cost-coverage-ctrl.js
@@ -51,6 +51,8 @@ define(['./../module', 'underscore'], function (module, _) {
         }
       ];
 
+      vm.state = {};
+
       vm.selectTab = selectTab;
       vm.submitOutcomes = submitOutcomes;
       vm.getProgramName = getProgramName;
@@ -103,6 +105,8 @@ define(['./../module', 'underscore'], function (module, _) {
         return program.targetpars && program.targetpars.length > 0 && program.active;
       }
       vm.programs = _.sortBy(_.filter(vm.selectedProgset.programs, isActive), 'name');
+      vm.state.selectedProgram = vm.programs[0];
+      vm.state.popsizes = {};
 
       // Fetch outcomes for this progset
       $http.get(
@@ -115,6 +119,194 @@ define(['./../module', 'underscore'], function (module, _) {
         changeParset();
       })
     }
+
+    vm.changeSelectedProgram = function() {
+      console.log('ask server to change program');
+      $http.get(
+        '/api/project/' + vm.openProject.id
+          + '/progsets/' + vm.selectedProgset.id
+          + '/program/' + vm.state.selectedProgram.id
+          + '/parset/' + vm.selectedParset.id
+          + '/popsizes')
+      .success(function (response) {
+        vm.state.popsizes = response;
+
+        vm.state.yearSelector = [];
+        var years = _.keys($scope.popsizes);
+        years.forEach(function(year) {
+          vm.state.yearSelector.push({'value':year, 'label':year.toString()});
+        });
+
+        buildTables();
+        vm.updateGraph();
+      });
+    };
+
+   vm.updateGraph = function() {
+      vm.chartData = {};
+      var years = vm.state.selectedProgram.ccopars.t;
+      if (years.length == 0) {
+        vm.chartData = null;
+        return;
+      }
+      var url = '/api/project/' + vm.openProject.id
+          + '/progsets/' + vm.selectedProgset.id
+          + '/programs/' + vm.state.selectedProgram.id
+          + '/costcoverage/graph?t=' + years.join(',')
+          + '&parset_id=' + vm.selectedParset.id;
+      if (vm.state.remarks) {
+        vm.state.displayCaption = angular.copy(vm.state.remarks);
+        url += '&caption=' + encodeURIComponent(vm.state.remarks);
+      }
+      if (vm.state.maxFunc) {
+        url += '&xupperlim=' + vm.state.maxFunc;
+      }
+      if (vm.state.dispCost) {
+        url += '&perperson=1';
+      }
+      $http
+        .get(url)
+        .success(
+          function (data) {
+            console.log('draw graph', vm.state.selectedProgram.short, data);
+            vm.state.chartData = data;
+          })
+        .error(
+          function() {
+            console.log('failed graph', vm.state.selectedProgram.short);
+          }
+        );
+    };
+
+    var saveSelectedProgram = function() {
+      var payload = { 'program': $scope.selectedProgram };
+      // consoleLogJson("payload", payload);
+      $http
+        .post(
+          '/api/project/' + $scope.vm.openProject.id
+            + '/progsets/' + $scope.selectedProgram.progset_id
+            + '/program',
+          payload)
+        .success(function() {
+          toastr.success('Cost data were saved');
+          $scope.updateGraph();
+        });
+    };
+
+    function toNullIfEmpty(val) {
+      if (_.isUndefined(val)) {
+        return null;
+      }
+      if (val == "") {
+        return null;
+      }
+      return val;
+    }
+
+    var validateCostcovTable = function(table) {
+      var costcov = [];
+      table.rows.forEach(function(row, iRow, rows) {
+        if (iRow != table.iEditRow) {
+          costcov.push({
+            year: parseInt(row[0]),
+            cost: toNullIfEmpty(row[1]),
+            coverage: toNullIfEmpty(row[2])
+          });
+        }
+      });
+      vm.state.selectedProgram.costcov = costcov;
+      consoleLogJson('save costcov', costcov);
+      saveSelectedProgram();
+    };
+
+    var validateCcoparsTable = function(table) {
+      var ccopars = {t: [], saturation: [], unitcost: []};
+      table.rows.forEach(function(row, iRow) {
+        if (iRow != table.iEditRow) {
+          ccopars.t.push(parseInt(row[0]));
+          ccopars.saturation.push([row[2]/100., row[3]/100.]);
+          ccopars.unitcost.push([row[4], row[5]]);
+        }
+      });
+      vm.state.selectedProgram.ccopars = ccopars;
+      saveSelectedProgram();
+    };
+
+    var showEstPopFn = function(row) {
+      var year = row[0];
+      if (_.isUndefined(year) || _.isUndefined(vm.state.popsizes)) {
+        return "";
+      }
+      var popsize = vm.state.popsizes[year.toString()];
+      if (!_.isNumber(popsize))
+          return "";
+      return parseInt(popsize);
+    };
+
+    var getYearSelectors = function(row) {
+      if (_.isUndefined(vm.state.popsizes)) {
+        console.log("no popsizes for selectors");
+        return [];
+      }
+      var years = _.keys(vm.state.popsizes);
+      var result = [];
+      years.forEach(function(year) {
+        var yearStr = year.toString();
+        result.push({'value':yearStr, 'label':yearStr});
+      });
+      return result;
+    };
+
+    var buildTables = function() {
+
+      vm.state.ccoparsTable = {
+        titles: [
+          "Year", "Estimated Population", "Saturation % (low)", "Saturation % (high)",
+          "Unit cost (low)", "Unit cost (high)"],
+        rows: [],
+        types: ["selector", "display", "number", "number", "number", "number"],
+        widths: ["5em", "5em", "5em", "5em", "5em", "5em"],
+        displayRowFns: [null, showEstPopFn, null, null, null, null],
+        options: [vm.state.yearSelector],
+        validateFn: validateCcoparsTable
+      };
+      var ccopars = angular.copy(vm.state.selectedProgram.ccopars);
+      var table = vm.state.ccoparsTable;
+      if (ccopars && ccopars.t && ccopars.t.length > 0) {
+        for (var iYear = 0; iYear < ccopars.t.length; iYear++) {
+          table.rows.push([
+            ccopars.t[iYear].toString(),
+            "",
+            ccopars.saturation[iYear][0]*100.,
+            ccopars.saturation[iYear][1]*100.,
+            ccopars.unitcost[iYear][0],
+            ccopars.unitcost[iYear][1]
+          ])
+        }
+      }
+      console.log('ccoparsTable', vm.state.ccoparsTable);
+
+      vm.state.costcovTable = {
+        titles: ["Year", "Cost", "Coverage"],
+        rows: [],
+        types: ["selector", "number", "number"],
+        widths: ["5em", "5em", "5em"],
+        displayRowFns: [],
+        selectors: [getYearSelectors],
+        options: [$scope.yearSelector],
+        validateFn: validateCostcovTable,
+      };
+      var table = vm.state.costcovTable;
+      vm.state.selectedProgram.costcov.forEach(function(val, i, list) {
+        table.rows.push([val.year.toString(), val.cost, val.coverage]);
+      });
+      console.log("costcovTable", vm.state.costcovTable);
+
+    };
+
+
+
+
 
     function submitOutcomes() {
       var outcomes = angular.copy(vm.outcomes);
@@ -291,7 +483,7 @@ define(['./../module', 'underscore'], function (module, _) {
       vm.selectedOutcomes = _.filter(vm.outcomes, function(outcome) {
         return outcome.name == vm.selectedParameter.short;
       });
-      consoleLogJson('selected outcomes', vm.selectedOutcomes);
+      console.log('selected outcomes', vm.selectedOutcomes);
       buildParameterSelectors();
     }
 
