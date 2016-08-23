246b1941bc174f883ce9176d2c0ad7852f6c6759
cliffckerr <questionably@gmail.com>
2016-08-23 18:31:53 +1000

    added very brief documentation

--- server/config.example.py
+++ server/config.example.py
@@ -1,3 +1,4 @@
+# Copy this file to config.py
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://optima:optima@localhost:5432/optima'
 SECRET_KEY = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 UPLOAD_FOLDER = '/tmp/uploads'
