8ee60b4691d401cb9549947bea14dd8c8a0b1078
cliffckerr <questionably@gmail.com>
2016-08-30 12:35:47 +1000

    combined tests into one script

--- tests/testgeospatial.py
+++ tests/testgeospatial.py
@@ -21,7 +21,8 @@ tests = [
 #'makeprojects',
 #'makeportfolio',
 #'generateBOCs',
-'rungui',
+#'rungui',
+'runbackend'
 ]
 
 ##############################################################################
@@ -163,6 +164,31 @@ if 'rungui' in tests and doplot:
         geogui()
     
     done(t)
+
+
+
+
+if 'runbackend' in tests:
+    from optima import defaultobjectives
+    from optima.geospatial import makesheet, makeproj, create, addproj, saveport, loadport, rungeo, export, plotgeo
+
+    # Creating portfolio from subdivision spreadsheet.
+    makesheet(projectpath='./geo/blantyre.prj', spreadsheetpath='./geo/blantyre-split-blank.xlsx', copies=2, refyear=2017)
+    makeproj(projectpath='./geo/blantyre.prj', spreadsheetpath='./geo/blantyre-split-filled.xlsx', destination='./geo')
+    create(filepaths=['./geo/blantyre-district-1.prj','./geo/blantyre-district-2.prj'])
+    
+    # Creating portfolio from projects (that already have BOCs).
+    create(filepaths=['./geo/blantyre.prj'])
+    addproj(filepaths=['./geo/balaka.prj'])   # Use addportfolio argument if not wanting to work with globals.
+    saveport(filepath='./geo/blantyre-balaka.prt')    # Use portfolio argument if not wanting to work with globals.
+
+    gaobjectives = defaultobjectives(verbose=0)
+    gaobjectives['budget'] = 15000000.0 # Reset
+    
+    loadport(filepath='./geo/blantyre-balaka.prt')
+    rungeo(objectives=gaobjectives, BOCtime=2)     # Use portfolio argument if not wanting to work with globals.
+    export(filepath='./geo/blantyre-balaka-results.xlsx')
+    plotgeo()
 
 
 
