37f273c9321892fb2608b5c9c6435a5431693443
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-09-01 02:19:25 +0800

    fix up the scripts

--- deployment/fabfile.py
+++ deployment/fabfile.py
@@ -5,13 +5,17 @@ env.hosts = ['athena.optimamodel.com']
 
 def service_restart(service):
 
-    sudo('service %scelery stop' % (service,))
+    sudo('systemctl daemon-reload')
+    try:
+        sudo('service %scelery stop' % (service,))
+    except:
+        pass
     sudo('service %s restart' % (service,))
 
 
 def service_logs(service, follow=True):
 
-    if follow:
+    if follow is True:
         sudo('journalctl -u %s -f' % (service,))
     else:
         sudo('journalctl -u %s -n 100' % (service,))
