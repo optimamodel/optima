e961df36c5f74224ef65723f475143b432a79c5a
Amber Brown (HawkOwl) <hawkowl@atleastfornow.net>
2016-10-25 16:08:03 +1100

    /4

--- deployment/config/staging.config.py
+++ deployment/config/staging.config.py
@@ -1,7 +1,7 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://optima:optima@localhost:5432/dev_optima'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://optima:optima@localhost:5432/hiv_optima'
 SECRET_KEY = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
 UPLOAD_FOLDER = '/tmp/uploads'
-CELERY_BROKER_URL = 'redis://localhost:6379/2'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/2'
+CELERY_BROKER_URL = 'redis://localhost:6379/4'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/4'
 REDIS_URL = CELERY_BROKER_URL
 MATPLOTLIB_BACKEND = "agg"
