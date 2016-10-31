e2bb542133940309bf4c7063b8722acb38a18d2b
Bosco Ho <apposite@gmail.com>
2016-11-01 10:49:54 +1100

    moved rename modal to common/service

--- client/source/js/modules/home/home-ctrl.js
+++ client/source/js/modules/home/home-ctrl.js
@@ -6,6 +6,7 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
     'ProjectOpenController',
     function ($scope, $http, activeProject, projects, modalService,
         fileUpload, UserManager, projectApiService, $state, $upload,
+              renameModalService,
         $modal, toastr) {
 
       function initialize() {
@@ -211,53 +212,10 @@ define(['./module', 'angular', 'underscore'], function (module, angular, _) {
           .click();
       };
 
-      function openEditNameModal(
-         acceptName, title, message, name, errorMessage, invalidNames) {
-
-        var modalInstance = $modal.open({
-          templateUrl: 'js/modules/portfolio/portfolio-modal.html',
-          controller: [
-            '$scope', '$document',
-            function($scope, $document) {
-
-              $scope.name = name;
-              $scope.title = title;
-              $scope.message = message;
-              $scope.errorMessage = errorMessage;
-
-              $scope.checkBadForm = function(form) {
-                var isGoodName = !_.contains(invalidNames, $scope.name);
-                form.$setValidity("name", isGoodName);
-                return !isGoodName;
-              };
-
-              $scope.submit = function() {
-                acceptName($scope.name);
-                modalInstance.close();
-              };
-
-              function onKey(event) {
-                if (event.keyCode == 27) {
-                  return modalInstance.dismiss('ESC');
-                }
-              }
-
-              $document.on('keydown', onKey);
-              $scope.$on(
-                '$destroy',
-                function() { $document.off('keydown', onKey); });
-
-            }
-          ]
-        });
-
-        return modalInstance;
-      }
-
       $scope.editProjectName = function(project) {
         var otherNames = _.pluck($scope.projects, 'name');
         otherNames = _.without(otherNames, project.name)
-        openEditNameModal(
+        renameModalService.openEditNameModal(
           function(name) {
             project.name = name;
             projectApiService
