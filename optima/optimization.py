9ff8230382a2ee910ea419d14d28aeac1def295d
cliffckerr <questionably@gmail.com>
2016-12-21 04:07:28 -0500

    working on it, but separating out

--- optima/optimization.py
+++ optima/optimization.py
@@ -473,10 +473,10 @@ def minoutcomes(project=None, optim=None, name=None, inds=None, tvec=None, verbo
     tmpresults = [orig]
 
     ## Loop over budget scale factors
-    for sf in optim.objectives['budgetscale']:
+    for scalefactor in optim.objectives['budgetscale']:
 
         ## Get the total budget & constrain it 
-        totalbudget = dcp(optim.objectives['budget'])*sf
+        totalbudget = dcp(optim.objectives['budget'])*scalefactor
         constrainedbudget, constrainedbudgetvec, lowerlim, upperlim = constrainbudget(origbudget=origbudget, budgetvec=budgetvec, totalbudget=totalbudget, budgetlims=optim.constraints, optiminds=optiminds, outputtype='full')
 
         ## Actually run the optimization
@@ -486,11 +486,12 @@ def minoutcomes(project=None, optim=None, name=None, inds=None, tvec=None, verbo
         constrainedbudgetnew, constrainedbudgetvecnew, lowerlim, upperlim = constrainbudget(origbudget=origbudget, budgetvec=budgetvecnew, totalbudget=totalbudget, budgetlims=optim.constraints, optiminds=optiminds, outputtype='full')
         args['totalbudget'] = totalbudget
         new = objectivecalc(constrainedbudgetvecnew, outputresults=True, debug=False, **args)
-        new.name = 'Optimal (%.0f%% budget)' % (sf*100.)
+        if len(optim.objectives['budgetscale'])==1: new.name = 'Optimal' # If there's just one optimization, just call it optimal
+        else: new.name = 'Optimal (%.0f%% budget)' % (scalefactor*100.) # Else, say what the budget is
         tmpresults.append(new)
 
     ## Output
-    multires = Multiresultset(resultsetlist=tmpresults, name='optim-%s' % name)
+    multires = Multiresultset(resultsetlist=tmpresults, name='optim-%s' % new.name)
     for k,key in enumerate(multires.keys): multires.budgetyears[key] = tmpresults[k].budgetyears # WARNING, this is ugly
     multires.improvement = [output.fval] # Store full function evaluation information -- wrap in list for future multi-runs
     optim.resultsref = multires.name # Store the reference for this result
