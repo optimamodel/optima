b2fd1fee51cfc96c5d5245e9bce6ed2676d02160
cliffckerr <questionably@gmail.com>
2016-12-27 19:43:43 -0500

    all tests should run now

--- tests/testall.py
+++ tests/testall.py
@@ -17,11 +17,10 @@ Version: 2016jan29 by cliffk
 
 ## Initialization
 from time import time as TIME # Use caps to distinguish 'global' variables
-from sys import exc_info, argv
-from glob import glob
-import optima as op
-import os
-EVERYTHING = False # Whether to run all Python scripts in the folder
+from sys import exc_info as EXC_INFO, argv as ARGV
+from glob import glob as GLOB
+import os as OS
+EVERYTHING = True # Whether to run all Python scripts in the folder
 doplot = False # When running all tests, don't try to run the GUIs -- this must match the name in the script files
 
 
@@ -40,11 +39,22 @@ MASTER = [
 ]
 
 ## Optionally run everything
-if len(argv)>1 and argv[1]=='everything': EVERYTHING = True
-if EVERYTHING: # Figure out the path -- adapted from defaults.py
-    MASTER = glob(os.sep.join(op.__file__.split(os.sep)[:-2] + ['tests'+os.sep])+'*.py')
-else:
-    for INDEX in range(len(MASTER)): MASTER[INDEX] += '.py' # Append .py ending
+if len(ARGV)>1 and ARGV[1]=='everything': EVERYTHING = True
+if not EVERYTHING: # If using the master list supplied, just need to append .py ending
+    for INDEX in range(len(MASTER)): MASTER[INDEX] += '.py' 
+else: # We're using everything
+    MASTER = GLOB(OS.path.dirname(OS.path.realpath(__file__))+OS.sep+'*.py') # Figure out the path -- adapted from defaults.py
+    try: MASTER.remove(OS.path.realpath(__file__)) # Simple solution if file is in list
+    except:
+        print('Automatic infinite loop removal failed; trying manual...')
+        REMOVED = False # Assume not removed
+        for INDEX in range(len(MASTER)): # Loop over all tests
+            THISFILE = OS.path.basename(__file__) # Find the name of this file
+            if MASTER[INDEX][-len(THISFILE):] == THISFILE: # Check if this index matches
+                MASTER.pop(INDEX) # If so, remove it
+                REMOVED = True # And report success
+        if not REMOVED: raise Exception('Could not find this file, aborting to avoid infinite loop!')
+
     
 
 ## Run the tests in a loop
@@ -59,14 +69,14 @@ for TEST in MASTER:
         print('\n'*10+'#'*200)
         print('NOW RUNNING: %s' % TEST)
         print('#'*200+'\n'*3)
-        exec(open(TEST).read()) # Run the test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        exec(open(TEST).read()) # Run the test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         SUCCEEDED.append({'test':TEST, 'time':TIME()-THISSTART})
         for KEY in locals().keys(): # Clean up -- delete any new variables added
             if KEY not in VARIABLES:
                 print('       "%s" complete; deleting "%s"' % (TEST, KEY))
                 exec('del '+KEY)
     except:
-        FAILED.append({'test':TEST, 'msg':exc_info()[1]})
+        FAILED.append({'test':TEST, 'msg':EXC_INFO()[1]})
 
 
 print('\n'*5)
