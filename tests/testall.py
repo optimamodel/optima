9a8fdce659b30cc1c7ad17d92e6503a9d89896a1
cliffckerr <questionably@gmail.com>
2016-12-27 19:00:20 -0500

    standardized testbest.py

--- tests/testall.py
+++ tests/testall.py
@@ -8,13 +8,22 @@ Run all tests, skipping GUI ones by default (doplot = False).
 It runs everything in the same namespace, but deletes variables that get
 added along the way. Extremely un-Pythonic, I know.
 
+To run everything, either set everything = True below, or at the command line type
+
+./testall everything
+
 Version: 2016jan29 by cliffk
 """
 
 ## Initialization
 from time import time as TIME # Use caps to distinguish 'global' variables
-from sys import exc_info
-doplot = False # When running all tests, don't try to run the GUIs
+from sys import exc_info, argv
+from glob import glob
+import optima as op
+import os
+EVERYTHING = False # Whether to run all Python scripts in the folder
+doplot = False # When running all tests, don't try to run the GUIs -- this must match the name in the script files
+
 
 ## Define master list of tests to run
 MASTER = [
@@ -30,14 +39,13 @@ MASTER = [
 'testoptimization',
 ]
 
-## Other tests, for completeness -- not run by default since a subset of the other tests and/or does not make sense to run in batch
-OTHER = [
-'testgui',
-'testworkflow',
-]
-
 ## Optionally run everything
-#MASTER += OTHER
+if len(argv)>1 and argv[1]=='everything': EVERYTHING = True
+if EVERYTHING: # Figure out the path -- adapted from defaults.py
+    MASTER = glob(os.sep.join(op.__file__.split(os.sep)[:-2] + ['tests'+os.sep])+'*.py')
+else:
+    for INDEX in range(len(MASTER)): MASTER[INDEX] += '.py' # Append .py ending
+    
 
 ## Run the tests in a loop
 VARIABLES = []
@@ -48,8 +56,10 @@ for TEST in MASTER:
     try:
         VARIABLES = locals().keys() # Get the state before the test is run
         THISSTART = TIME()
-        print('#'*200)
-        exec(open(TEST+'.py').read()) # Run the test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        print('\n'*10+'#'*200)
+        print('NOW RUNNING: %s' % TEST)
+        print('#'*200+'\n'*3)
+        exec(open(TEST).read()) # Run the test!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         SUCCEEDED.append({'test':TEST, 'time':TIME()-THISSTART})
         for KEY in locals().keys(): # Clean up -- delete any new variables added
             if KEY not in VARIABLES:
