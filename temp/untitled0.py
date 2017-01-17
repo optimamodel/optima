4462abc507c20c6a2c9821004c0370b0663ebd45
cliffckerr <questionably@gmail.com>
2017-01-17 01:14:38 -0300

    no idea why tool tips dont work

--- temp/untitled0.py
+++ temp/untitled0.py
@@ -126,15 +126,40 @@ class CKTest(plugins.PluginBase):
              alpha_fg = this.props.alpha_fg;
              alpha_bg = this.props.alpha_bg;
          obj2.elements()
-             .on("mouseover", function(d, i){
-                            d3.select(this).transition().duration(50)
-                              .style("fill-opacity", alpha_fg);})
-             .on("mouseout", function(d, i){
-                            d3.select(this).transition().duration(200)
-                              .style("fill-opacity", alpha_bg); });
+            .on("mouseover", function(d, i){
+                d3.select(this).transition().duration(50).style("fill-opacity", alpha_fg);
+                this.ckhighlight
+                    .style("visibility", "visible")
+                    .text((labels === null) ? "(" + d + ")" : getMod(labels, i));
+            })
+            
+            .on("mousemove", function(d, i){
+                if (loc === "mouse") {
+                var pos = d3.mouse(this.fig.canvas.node())
+                this.x = pos[0] + this.props.hoffset;
+                this.y = pos[1] - this.props.voffset;
+                }
+                this.ckhighlight
+                    .attr('x', this.x)
+                    .attr('y', this.y);
+            })
+            
+            .on("mouseout", function(d, i){
+                d3.select(this).transition().duration(200).style("fill-opacity", alpha_bg);
+                this.ckhighlight.style("visibility", "hidden");
+            });
             
     }
 '''
+#             .on("mouseover", function(d, i){
+#                            d3.select(this).transition().duration(50)
+#                              .style("fill-opacity", alpha_fg);})
+#             .on("mouseout", function(d, i){
+#                            d3.select(this).transition().duration(200)
+#                              .style("fill-opacity", alpha_bg); });
+
+
+
 #         obj.elements()
 #             .on("mouseover", function(d, i){
 #                            d3.select(this).transition().duration(50).style("fill-opacity", alpha_fg);})
