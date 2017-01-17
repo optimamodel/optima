0b0b8a48afd8d38b90b671c6beed6aea3166f4eb
cliffckerr <questionably@gmail.com>
2017-01-17 05:28:27 -0300

    removed unused folder

--- temp/untitled1.py
+++
@@ -1,8 +0,0 @@
-"""
-Version:
-"""
-
-from pylab import *; from optima import *
-#filename = '/u/cliffk/unsw/analyses/malawi/prj/districts/Balaka.prj'
-filename = '/u/cliffk/unsw/applications/malawi/cdc/calibrated/Balaka.prj'
-P = loadobj(filename)
