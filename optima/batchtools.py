e06907dfdc75ce1067f36a76d7d97d55290c406a
cliffckerr <questionably@gmail.com>
2017-01-30 11:27:08 +0200

    this should work?

--- optima/batchtools.py
+++ optima/batchtools.py
@@ -76,6 +76,7 @@ def batchautofit(
     for i in range(nfiles):
         loadbalancer(0.5)
         project = loadproj(filelist[i])
+        project.filename = filelist[i]
         prc = Process(
             target=autofit_task, 
             args=(project, i, outputqueue, name, fitwhat, fitto, maxtime, 
@@ -84,7 +85,7 @@ def batchautofit(
         processes.append(prc)
     for i in range(nfiles):
         outputlist[i] = outputqueue.get()
-        outputlist[i].save(filename=filelist[i]) # NO, this randomizes it, need to draw from outputlist
+        outputlist[i].save()
     
     return outputlist
 
