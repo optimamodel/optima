cf2666686b3ae243390932d54bc5ff130c7b21c9
cliffckerr <questionably@gmail.com>
2017-01-31 19:56:26 +0200

    how the hell can this still work

--- tests/testmigrations.py
+++ tests/testmigrations.py
@@ -9,7 +9,8 @@ from optima import tic, toc, blank, pd # analysis:ignore
 
 ## Options
 tests = [
-'migrations',
+'basicmigration',
+'scenario',
 ]
 
 ## Housekeeping
@@ -34,7 +35,7 @@ blank()
 
 T = tic()
 
-if 'migrations' in tests:
+if 'basicmigration' or 'scenario' in tests:
     
     # Figure out the path 
     import os
@@ -48,3 +49,27 @@ if 'migrations' in tests:
 
 
     
+## Set up project etc.
+if 'scenario' in tests:
+    t = tic()
+
+    print('Running migration-scenario test...')
+    from optima import Budgetscen, dcp
+    
+    ## Define scenarios
+    defaultbudget = P.progset().getdefaultbudget()
+    maxbudget = dcp(defaultbudget)
+    for key in maxbudget: maxbudget[key] += 1e14
+    scenlist = [
+        Budgetscen(name='Current conditions', parsetname='default', progsetname='default', t=[2016], budget=defaultbudget),
+        Budgetscen(name='Unlimited spending', parsetname='default', progsetname='default', t=[2016], budget=maxbudget),
+        ]
+    
+    # Run the scenarios
+    P.addscenlist(scenlist)
+    P.runscenarios() 
+     
+    if doplot:
+        from optima import pygui, plotpars
+        pygui(P.results[-1], toplot='default')
+#        apd = plotpars([scen.scenparset.pars[0] for scen in P.scens.values()])
