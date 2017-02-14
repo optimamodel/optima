3b6a8ec4f30beead1a9562561fca86a2a13ca05b
cliffckerr <questionably@gmail.com>
2017-02-14 21:50:54 +0200

    made path more robust

--- bin/run_server.py
+++ bin/run_server.py
@@ -1,7 +1,8 @@
 # Ensure that the current folder is used, not the global defaults
 import sys
 import os
-sys.path.insert(0, os.path.abspath("..")) 
+optimafolder = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+sys.path.insert(0, optimafolder) 
 
 # Load Optima
 from server import _autoreload
