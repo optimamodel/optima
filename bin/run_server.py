b4f47626ac574c4fd0f608e66cd47bedc2657ae2
cliffckerr <questionably@gmail.com>
2017-02-14 18:25:10 +0200

    changed how server runs

--- bin/run_server.py
+++ bin/run_server.py
@@ -1,12 +1,11 @@
+# Ensure that the current folder is used, not the global defaults
 import sys
 import os
+sys.path.insert(0, os.path.abspath("..")) 
 
-try:
-    import server
-except:
-    sys.path.insert(0, os.path.abspath(".."))
-
+# Load Optima
 from server import _autoreload
 from server import _twisted_wsgi
 
+# Run the server
 _autoreload.main(_twisted_wsgi.run)
