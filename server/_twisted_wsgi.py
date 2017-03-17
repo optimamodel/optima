8e58b9e16ec0abe47f7af548126f6245a69ceac8
cliffckerr <questionably@gmail.com>
2017-03-17 19:07:31 +0200

    hmm, not sure if compression worked, but worth a try

--- server/_twisted_wsgi.py
+++ server/_twisted_wsgi.py
@@ -3,8 +3,8 @@ import sys
 from twisted.internet import reactor
 from twisted.internet.endpoints import serverFromString
 from twisted.logger import globalLogBeginner, FileLogObserver, formatEvent
-from twisted.web.resource import Resource
-from twisted.web.server import Site
+from twisted.web.resource import Resource, EncodingResourceWrapper
+from twisted.web.server import Site, GzipEncoderFactory
 from twisted.web.static import File
 from twisted.web.wsgi import WSGIResource
 from twisted.python.threadpool import ThreadPool
@@ -38,13 +38,21 @@ def run():
                 b'Cache-Control', [b'no-cache', b'no-store', b'must-revalidate'])
             request.responseHeaders.setRawHeaders(b'expires', [b'0'])
             return r
+    
+    class WrappedFile(File):        
+        def getChild(self, path, request):
+            child = File.getChild(self, path, request)            
+            return EncodingResourceWrapper(child, [GzipEncoderFactory()])
 
+    # From http://stackoverflow.com/questions/5428473/twisted-http-gzip-support
 
     base_resource = File('client/build/')
-    base_resource.putChild('dev', File('client/source/'))
-    base_resource.putChild('api', OptimaResource(wsgi_app))
+    optima_resource = OptimaResource(wsgi_app)
+    base_resource.putChild('api', optima_resource)
+    
+    wrapped = EncodingResourceWrapper(base_resource, [GzipEncoderFactory()])
 
-    site = Site(base_resource)
+    site = Site(wrapped)
 
     try:
         port = str(sys.argv[1])
