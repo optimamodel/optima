05ad7e28f602d765be319a95d08f79b983e2eb0f
cliffckerr <questionably@gmail.com>
2017-03-25 21:52:41 +0200

    build client more informative

--- bin/build_client.sh
+++ bin/build_client.sh
@@ -11,27 +11,31 @@ cd ../client # Change to client folder
 
 # Sometimes, old junk gets in the build
 if [ -d "build" ]; then
+  echo -e '\nRemoving existing build directory...'
   rm -r build
 fi
 
 if [ -d "node_modules" ]; then
+  echo -e '\nPruning node modules...'
   npm prune
 fi
 
 if [ -d "source/vendor" ]; then
   if [ -d "node_modules/bower/bin" ]; then
+  	echo -e '\nPruning bower modules...'
     node_modules/bower/bin/bower prune
   fi
 fi
 
 # install npm and bower deps
+echo -e '\nInstalling npm dependencies...'
 npm install --skip-installed
 
 # compile sass scripts
 if [ $# -eq 0 ]; then
-	echo 'Compiling client (including minifying JavaScript)'
+	echo -e '\nCompiling client (including minifying JavaScript)'
 	node_modules/gulp/bin/gulp.js compile-build-js-client-uglify copy-assets-and-vendor-js write-version-js
 else
-	echo 'Compiling client (quick version)...'
+	echo -e '\nCompiling client (quick version)...'
 	node_modules/gulp/bin/gulp.js compile-build-js-client-quick copy-assets-and-vendor-js write-version-js
 fi
