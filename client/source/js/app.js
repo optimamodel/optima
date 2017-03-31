55923e74d2f9c3cb450186d771b275c18a672915
cliffckerr <questionably@gmail.com>
2017-04-01 01:19:49 +0300

    removed junk, made help icon

--- client/source/js/app.js
+++ client/source/js/app.js
@@ -71,10 +71,10 @@ define([
             } else {
               var message, errorText;
               if (rejection.data && (rejection.data.message || rejection.data.exception || rejection.data.reason)) {
-                message = 'We are very sorry, but it seems an error has occurred. Please contact us (info@optimamodel.com). In your email, copy and paste the error message below, and please also provide the date and time, your user name, the project you were working on (if applicable), as much detail as possible about the steps leading up to the error. We apologize for the inconvenience.';
+                message = 'We are very sorry, but it seems an error has occurred. Please contact us (info@optimamodel.com). In your email, copy and paste the error message below, and please also provide the date and time, your user name, the project you were working on (if applicable), and as much detail as possible about the steps leading up to the error. We apologize for the inconvenience.';
                 errorText = rejection.data.message || rejection.data.exception || rejection.data.reason;
               } else {
-                message = 'We are very sorry, but it seems an unknown server error has occurred. Please contact us (info@optimamodel.com), including the date and time, your user name, the project you were working on (if applicable), as much detail as possible about the steps leading up to the error. We apologize for the inconvenience.';
+                message = 'We are very sorry, but it seems an unknown server error has occurred. Please contact us (info@optimamodel.com), including the date and time, your user name, the project you were working on (if applicable), and as much detail as possible about the steps leading up to the error. We apologize for the inconvenience.';
               }
               var modalService = $injector.get('modalService');
               modalService.inform(angular.noop, 'Okay', message, 'Server Error', errorText);
