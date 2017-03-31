f7227f0010560cc5824b175cee517f711055a08a
cliffckerr <questionably@gmail.com>
2017-04-01 02:32:53 +0300

    reverted, dont think this will work though

--- client/source/js/modules/common/help-service.js
+++ client/source/js/modules/common/help-service.js
@@ -2,12 +2,12 @@ define(['angular'], function (angular) {
   'use strict';
 
   return angular.module('app.open-help', [])
-    .factory('helpService', [
-      '$modal', 'url',
-      function ($modal, url) {
+    .service('helpService', function () {
+      this.openHelp = function ($modal, url) {
         return $modal.open({
           templateUrl: 'js/modules/programs/program-set/program-modal.html',
           size: 'lg'
         });
-      }]);
+      }
+    });
 });
