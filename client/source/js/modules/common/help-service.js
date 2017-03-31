16640882110e5d2e794cc4f34526aaff36bf4f2c
cliffckerr <questionably@gmail.com>
2017-04-01 02:50:04 +0300

    ok, trying something else...

--- client/source/js/modules/common/help-service.js
+++ client/source/js/modules/common/help-service.js
@@ -1,13 +1,32 @@
-define(['angular'], function (angular) {
+//define(['angular'], function (angular) {
+//  'use strict';
+//
+//  return angular.module('app.open-help', [])
+//    .service('helpService', function () {
+//      this.openHelp = function ($modal, url) {
+//        return $modal.open({
+//          templateUrl: 'js/modules/programs/program-set/program-modal.html',
+//          size: 'lg'
+//        });
+//      }
+//    });
+//});
+
+
+
+define(['./module', 'angular', 'underscore'], function (module, angular, _) {
   'use strict';
 
-  return angular.module('app.open-help', [])
-    .service('helpService', function () {
-      this.openHelp = function ($modal, url) {
-        return $modal.open({
-          templateUrl: 'js/modules/programs/program-set/program-modal.html',
-          size: 'lg'
-        });
-      }
-    });
+  module.controller('HelpModalController', function (
+    $scope, $modal, helpURL) {
+
+    function openHelpModal(helpURL) {
+      return $modal.open({
+        templateUrl: 'js/modules/programs/program-set/program-modal.html',
+        size: 'lg'
+      });
+    }
+
+  });
+
 });
