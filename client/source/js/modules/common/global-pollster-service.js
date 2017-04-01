ffe24e1424ddccf4588f1b58593b30615353655a
cliffckerr <questionably@gmail.com>
2017-04-01 03:33:40 +0300

    still works

--- client/source/js/modules/common/global-pollster-service.js
+++ client/source/js/modules/common/global-pollster-service.js
@@ -5,8 +5,6 @@ define(['angular' ], function (angular) {
 
     .factory('tmpHelpService', ['$modal', function($modal) {
 
-      var polls = {};
-
       function openHelp(helpURL) {
         return $modal.open({
           templateUrl: 'js/modules/programs/program-set/program-modal.html',
@@ -14,74 +12,8 @@ define(['angular' ], function (angular) {
         });
       }
 
-      function getPoll(id) {
-        if (!(id in polls)) {
-          console.log('Creating polling slot for', id);
-          polls[id] = {isRunning: false, id: id};
-        }
-        return polls[id];
-      }
-
-      function startPoll(id, url, callback) {
-        var poll = getPoll(id);
-        poll.url = url;
-        poll.callback = callback;
-
-        if (!poll.isRunning) {
-          console.log('Launch polling for', poll.id);
-          poll.isRunning = true;
-
-          function pollWithTimeout() {
-            var poll = getPoll(id);
-            $http
-              .get(poll.url)
-              .success(function(response) {
-                if (response.status === 'started') {
-                  poll.timer = $timeout(pollWithTimeout, 1000);
-                } else {
-                  stopPoll(id);
-                }
-                poll.callback(response);
-              })
-              .error(function(response) {
-                stopPoll(id);
-                poll.callback(response);
-              });
-          }
-
-          pollWithTimeout();
-        }
-      }
-
-      function stopPoll(id) {
-        var poll = getPoll(id);
-        if (poll.isRunning) {
-          console.log('Stop polling for', poll.id);
-          poll.isRunning = false;
-          $timeout.cancel(poll.timer);
-        }
-      }
-
-      function stopPolls() {
-        _.each(polls, function(poll) {
-          if (poll.isRunning) {
-            console.log('Stop polling for', poll.id);
-            poll.isRunning = false;
-            $timeout.cancel(poll.timer);
-          }
-        });
-      }
-
-      function killJob(projectId, workType) {
-        $http
-          .delete('/api/task/' + projectId + '/type/' + workType);
-      }
-
       return {
-        openHelp: openHelp,
-        startPoll: startPoll,
-        stopPolls: stopPolls,
-        killJob: killJob
+        openHelp: openHelp
       };
 
     }]);
