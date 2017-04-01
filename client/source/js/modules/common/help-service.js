6f1f1c713116f3f43d7e7e93b4c23e36aed9ec52
cliffckerr <questionably@gmail.com>
2017-04-01 04:52:33 +0300

    it works omg so happy

--- client/source/js/modules/common/help-service.js
+++ client/source/js/modules/common/help-service.js
@@ -6,13 +6,13 @@ define(['angular' ], function (angular) {
     .factory('helpService', ['$modal', function($modal) {
 
       function openHelp(helpURL) {
-        var fullURL = 'http://optimamodel.com/man/'+helpURL;
+        var fullURL = 'http://optimamodel.com/man/'+helpURL; // Actual mapping is defined in mapping.json in the optimawebsite repository
         var scrh = screen.height;
         var scrw = screen.width;
         var h = scrh*0.8; // Height of window
-        var w = scrw*0.5; // Width of window
-        var t = scrh*0.1; // Position from top of screen
-        var l = scrw*0.4; // Position from left of screen
+        var w = scrw*0.6; // Width of window
+        var t = scrh*0.1; // Position from top of screen -- centered
+        var l = scrw*0.37; // Position from left of screen -- almost all the way right
         newwindow = window.open(fullURL,'Reference manual','width='+w + ',height='+h + ',top='+t + ',left='+l);
         if (window.focus) {
           newwindow.focus()
