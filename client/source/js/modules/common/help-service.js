f2bded959fe41e519600cac6183168b0b480fb01
cliffckerr <questionably@gmail.com>
2017-04-01 04:43:56 +0300

    first functional version omg yay

--- client/source/js/modules/common/help-service.js
+++ client/source/js/modules/common/help-service.js
@@ -7,9 +7,13 @@ define(['angular' ], function (angular) {
 
       function openHelp(helpURL) {
         var fullURL = 'http://optimamodel.com/man/'+helpURL;
-        var h = screen.height*0.8;
-        var w = screen.width*0.5;
-        newwindow = window.open(fullURL,'Reference manual','width='+w +',height=' +h);
+        var scrh = screen.height;
+        var scrw = screen.width;
+        var h = scrh*0.8; // Height of window
+        var w = scrw*0.5; // Width of window
+        var t = scrh*0.1; // Position from top of screen
+        var l = scrw*0.4; // Position from left of screen
+        newwindow = window.open(fullURL,'Reference manual','width='+w + ',height='+h + ',top='+t + ',left='+l);
         if (window.focus) {
           newwindow.focus()
         }
