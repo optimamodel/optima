88e9fc8d27b95e6bfdf25ec6390b8230c6e38785
cliffckerr <questionably@gmail.com>
2017-04-01 03:38:13 +0300

    removed temp files

--- client/source/js/modules/common/help-service.js
+++ client/source/js/modules/common/help-service.js
@@ -1,32 +1,17 @@
-//define(['angular'], function (angular) {
-//  'use strict';
-//
-//  return angular.module('app.open-help', [])
-//    .controller('helpModalController', function ($modal, helpURL) {
-//      function openHelp(helpURL) {
-//        return $modal.open({
-//          templateUrl: 'js/modules/programs/program-set/program-modal.html',
-//          size: 'lg'
-//        });
-//      }
-//    });
-//});
-
-
-define(['angular'], function (angular) {
+define(['angular' ], function (angular) {
   'use strict';
 
   return angular.module('app.common.help-service', [])
-    .factory('helpService', ['$modal', 'helpURL',
-      function ($modal, helpURL) {
 
-        function openHelp(helpURL) {
+    .factory('helpService', ['$modal', function($modal) {
 
-          return $modal.open({
-            templateUrl: 'js/modules/programs/program-set/program-modal.html',
-            size: 'lg'
-          });
-        }
+      function openHelp(helpURL) {
+        return $modal.open({
+          templateUrl: 'js/modules/programs/program-set/program-modal.html',
+          size: 'lg'
+        });
       }
-    ])
+
+      return {openHelp: openHelp};
+    }]);
 });
--- client/source/js/modules/ui/index.js
+++ client/source/js/modules/ui/index.js
@@ -16,12 +16,12 @@ define([
         ])
       .controller(
         'MainCtrl',
-        function ($scope, $state, activeProject, tmpHelpService, userManager) {
+        function ($scope, $state, activeProject, helpService, userManager) {
           $scope.user = userManager.user;
           $scope.state = $state;
           $scope.userLogged = function () { return userManager.isLoggedIn; };
           $scope.activeProject = activeProject;
-          $scope.openHelp = tmpHelpService.openHelp;
+          $scope.openHelp = helpService.openHelp;
          });
     return module;
   }
