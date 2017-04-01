677d22d8ee8a26ec35c36a3229d0395622b5e8ae
cliffckerr <questionably@gmail.com>
2017-04-01 04:24:25 +0300

    trying with popup...

--- client/source/js/modules/common/help-service.js
+++ client/source/js/modules/common/help-service.js
@@ -5,11 +5,17 @@ define(['angular' ], function (angular) {
 
     .factory('helpService', ['$modal', function($modal) {
 
+      //function openHelp(helpURL) {
+      //  return $modal.open({
+      //    templateUrl: 'js/modules/common/help-modal.html',
+      //    size: 'lg'
+      //  });
+      //}
+
       function openHelp(helpURL) {
-        return $modal.open({
-          templateUrl: 'js/modules/common/help-modal.html',
-          size: 'lg'
-        });
+        newwindow = window.open('https://docs.google.com/document/d/18hGjBb1GO8cR_sZRTjMqBvBb0Fkmsz-DwzslstaqG-Y/edit?usp=sharing','name','height=200,width=150');
+        if (window.focus) {newwindow.focus()}
+        return false;
       }
 
       return {openHelp: openHelp};
