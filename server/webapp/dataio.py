ef081d3e8ea3bd159f14be6b84b9a490f6b802a6
cliffckerr <questionably@gmail.com>
2017-04-07 15:11:16 +0300

    BE changes

--- server/webapp/dataio.py
+++ server/webapp/dataio.py
@@ -1464,7 +1464,12 @@ def upload_optimization_summary(project_id, optimization_id, optimization_summar
 def load_optimization_graphs(project_id=None, optimization_id=None, which=None, zoom=None, startYear=None, endYear=None):
     project = load_project(project_id)
     optimization = parse.get_optimization_from_project(project, optimization_id)
-    result_name = "optim-" + optimization.name
+    result_name = optimization.resultsref
+    print('LOOKING FOR %s' % result_name)
+    print('ACTUAL NAMES:')
+    for optim in project.optims.values():
+        print(optim.name)
+    print('TEMP DONE')
     result = load_result(project.uid, None, "optimization", which, result_name)
     if not result:
         return {}
