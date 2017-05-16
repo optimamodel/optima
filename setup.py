2045bf55ce6ec76cf9b3f0dfaed7d742598877ee
cliffckerr <questionably@gmail.com>
2017-05-16 16:43:21 +0200

    and again

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 with open("optima/version.py", "r") as f:
     version_file = {}
     exec(f.read(), version_file)
-    version = version_file["__version__"]
+    version = version_file["version"]
 
 try:
     from pypandoc import convert
