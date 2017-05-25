b5f98588465c0f76d69f449fedd7d20c945cd096
cliffckerr <questionably@gmail.com>
2017-05-25 05:07:05 +0300

    working on backup script

--- bin/backup.py
+++ bin/backup.py
@@ -1,101 +1,179 @@
-import click
 import requests
 import sys
-import os.path
+import os
 import json
 from hashlib import sha224
+from pylab import argsort
+try:    import optima as op
+except: pass
+import datetime
 
 
-@click.command()
-@click.argument('savelocation')
-@click.option('--server', default="http://hiv.optimamodel.com",
-              help="Optima 2.0 server. Default: http://hiv.optimamodel.com")
-@click.option('--username', default='admin',
-              help="Username for logging on to the server. Default: admin")
-@click.option('--password',
-              help="Password for logging on to the server.")
-@click.option('--overwrite', default=True, type=bool,
-              help="Whether or not to overwrite local projects with server ones. Default: True.")
-def main(server, username, password, overwrite, savelocation):
-    """
+def safemkdir(*args):
+    ''' Skip exceptions and handle join() '''
+    try:    os.makedirs(os.path.join(*args))
+    except: pass
+    return None
+    
+
+def downloadprojects(password=None, savelocation=None, server=None, username=None, overwrite=False):
+    '''
     A utility for downloading all projects from an Optima 2.0+ server for an admin account.
-
-    An example:
-         python backup.py --username=batman --password=batcar! --server http://athena.optimamodel.com batprojects
-
-    The command above will log into http://athena.optimamodel.com as the user
-    'batman' with the password 'batcar!', and download all of that user's
-    projects into the folder 'batprojects' in the current directory.
 
     Here's how to use it with the defaults for 'admin' at hiv.optimamodel.com, saving to folder optimaprojects:
 
-         python backup.py --password=<secret> optimaprojects
+         python downloadprojects.py <password> <savelocation>
 
-    """
-
-    # Make sure that we don't send duplicate /s
-    if server[-1:] == "/":
-        server = server[:-1]
-
+    Version: 2017may23
+    '''
+    
+    # Define defaults
+    if username is None:     username     = 'admin'
+    if password is None:     password     = 'zzz'
+    if savelocation is None: savelocation = 'optimaprojects'
+    if server is None:       server       = 'http://localhost:8080'
+    
+    # Try timing, but don't try too hard
+    try:    T = op.tic()
+    except: pass
+    
+    print('Starting HTTP sesion...')
     session = requests.Session()
 
-    click.echo('Logging in as %s...' % (username,))
+    print('Logging in as %s...' % (username,))
     hashed_password = sha224()
     hashed_password.update(password)
     password = hashed_password.hexdigest()
 
-    # login to servers
-    response = session.post(
-        server + "/api/user/login", json={'username': username, 'password': password})
+    print('Logging into server...')
+    response = session.post(server + '/api/user/login', json={'username': username, 'password': password})
     if not response.status_code == 200:
-        click.echo("Failed login:\n%s" % (response.content,))
+        print('Failed login:\n%s' % response.content)
         sys.exit(1)
-    click.echo("Logged in as %s on %s" % (response.json()["username"], server))
+    print('Logged in as %s on %s' % (response.json()['username'], server))
 
-    try:
-        os.makedirs(savelocation)
-    except:
-        pass
-
-    users = session.get(server + "/api/user").json()["users"]['users']
-    f = open(os.path.join(savelocation, 'users.json'), "w")
+    print('Downloading user list...')
+    safemkdir(savelocation)
+    users = session.get(server + '/api/user').json()['users']['users']
+    f = open(os.path.join(savelocation, 'users.json'), 'w')
     json.dump(users, f, indent=2)
     username_by_id = {}
     for user in users:
-        username_by_id[user["id"]] = user["username"]
+        username_by_id[user['id']] = user['username']
 
-    click.echo("Downloading projects...")
+    print('Downloading project list, please be patient...')
     url = server + '/api/procedure'
     payload = {'name': 'load_all_project_summaries'}
-    projects = session.post(url, data=json.dumps(payload)).json()["projects"]
+    projects = session.post(url, data=json.dumps(payload)).json()['projects']
 
+    print('Processing projects...')
+    success = []
+    failed = []
+    projectnames = []
+    usernames = []
+    combined = []
     for project in projects:
-        username = username_by_id[project["userId"]]
-
-        click.echo("Downloading user '%s' project '%s'" % (username, project["name"],))
-
-        dirname = os.path.join(savelocation, username)
         try:
-            os.makedirs(dirname)
+            username    = username_by_id[project['userId']] # Pull out the user names
+            projectname = project['name'] # Pull out the project names
+            usernames.append(username)
+            projectnames.append(projectname)
+            combined.append(username+projectname) # Make a key by combining them
         except:
-            pass
-
-        fname = os.path.join(dirname, project["name"] + ".prj")
-        if os.path.isfile(fname):
-            if overwrite:
-                click.echo("Downloaded already, overwriting...")
-            else:
-                click.echo("Downloaded already, skipping (set --overwrite=True if you want to overwrite)")
+            try: failedname = str(project)
+            except: failedname = 'Could not even append string representation, what the hell'
+            print('WARNING, the following project failed: %s' % failedname)
+            failed.append(failedname)
+    
+    print('Sorting projects by user...')
+    order = argsort(combined) # Figure out the alphabetical order
+    sortedusernames = []
+    sortedprojectnames = []
+    for o in order:
+        sortedusernames.append(usernames[o])
+        sortedprojectnames.append(projectnames[o])
+    
+    print('Downloading all projects...')
+    nprojects = len(sortedprojectnames)
+    for i in range(nprojects):
+        username = sortedusernames[i]
+        projectname = sortedprojectnames[i]
+        try:
+            print('  Downloading user "%s" project "%s" (%i/%i)' % (username, projectname, i+1, nprojects))
+    
+            dirname = os.path.join(savelocation, username)
+            safemkdir(dirname)
+    
+            fname = os.path.join(dirname, projectname+'.prj')
+            while os.path.isfile(fname):
+                if overwrite:  print('    Downloaded already, overwriting...')
+                else:          fname += '.new' # Just keep appending till the cows come home
+            try:
+                url = server + '/api/download'
+                payload = {'name': 'download_project', 'args': [project['id']]}
+                response = session.post(url, data=json.dumps(payload))
+                try:
+                    with open(fname, 'wb') as f:
+                        f.write(response.content)
+                except Exception as E:
+                    print('WARNING, download succeed but write failed for project %s:\n%s' % (projectname, E.__repr__()))
+                    failed.append(project['name'])
+                    continue
+            except Exception as E:
+                print('WARNING, read succeeded but download failed for project %s:\n%s' % (projectname, E.__repr__()))
+                failed.append(project['name'])
                 continue
+        except Exception as E:
+            print('WARNING, read failed for project %s:\n%s' % (projectname, E.__repr__()))
+            failed.append(projectname)
+            continue
+        success.append(fname)
+        
+    if len(success):
+        print('The following projects were successfully downloaded:')
+        for succ in success: print(succ)
+    
+    if len(failed):
+        print('The following projects FAILED MISERABLY:')
+        for fail in failed: print(fail)
+    else:
+        print('No projects failed downloading.')
+    
+    print('Done, at last.')
+    
+    try:    op.toc(T)
+    except: print('Unable to calculate elapsed time, but it was probably a while.')
 
-        url = server + '/api/download'
-        payload = {'name': 'download_project_with_result', 'args': [project["id"]]}
-        response = session.post(url, data=json.dumps(payload))
-        with open(fname, 'wb') as f:
-            f.write(response.content)
 
-    click.echo("All downloaded in folder %s" % (savelocation,))
+
+
+def backup():
+    backupsfolder = 'backups'
+    
+    # Figure out last folder and current folder
+    try:    
+        mostrecent = sorted(os.listdir(backupsfolder))[-1]
+    except:
+        mostrecent = '0000-00-00'
+        safemkdir(backupsfolder,mostrecent)
+        try:    os.makedirs(os.path.join())
+        except: pass
+
+    now = datetime.datetime.now()
+    current = '%4i-%02i-%02i' % (now.year, now.month, now.day)
+    safemkdir(backupsfolder,current)
+    
+    
+    # Download projects
+
+    try:    password = sys.argv[1]
+    except: password = None
+    try:    savelocation = sys.argv[2]
+    except: savelocation = None
+    downloadprojects(password=password, savelocation=savelocation)
+    
+    return None
 
 
 if __name__ == '__main__':
-    main()
+    backup()
--- client/source/js/modules/admin/users.js
+++ client/source/js/modules/admin/users.js
@@ -15,12 +15,16 @@ define(['angular', 'ui.router',], function(angular) {
 
   module.controller('AdminManageUsersController', function($scope, userManager, modalService, rpcService, toastr) {
 
-    rpcService
-      .rpcRun(
-        'get_user_summaries')
-      .then(function(response) {
-        $scope.users = response.data.users;
-      });
+    $scope.refresh = function () {
+      rpcService
+        .rpcRun(
+          'get_user_summaries')
+        .then(function (response) {
+          $scope.users = response.data.users;
+        });
+    };
+
+    $scope.refresh();
 
     $scope.grantAdmin = function(user) {
       modalService.confirm(
@@ -28,10 +32,8 @@ define(['angular', 'ui.router',], function(angular) {
           rpcService
             .rpcRun('grant_admin', [user.id])
             .then(function(response) {
-              toastr.success('Admin rights granted!');
-              $scope.users = _($scope.users).filter(function(u) {
-                return u.id != user.id;
-              });
+              toastr.success('Admin rights granted! Please refresh page.');
+              $scope.refresh();
             });
         },
         undefined,
@@ -47,10 +49,8 @@ define(['angular', 'ui.router',], function(angular) {
           rpcService
             .rpcRun('revoke_admin', [user.id])
             .then(function(response) {
-              toastr.success('Admin rights revoked!');
-              $scope.users = _($scope.users).filter(function(u) {
-                return u.id != user.id;
-              });
+              toastr.success('Admin rights revoked! Please refresh page.');
+              $scope.refresh();
             });
         },
         undefined,
@@ -67,9 +67,6 @@ define(['angular', 'ui.router',], function(angular) {
             .rpcRun('reset_password', [user.id])
             .then(function(response) {
               toastr.success('Password reset!');
-              $scope.users = _($scope.users).filter(function(u) {
-                return u.id != user.id;
-              });
             });
         },
         undefined,
@@ -86,9 +83,7 @@ define(['angular', 'ui.router',], function(angular) {
             .rpcRun('delete_user', [user.id])
             .then(function(response) {
               toastr.success('User deleted!');
-              $scope.users = _($scope.users).filter(function(u) {
-                return u.id != user.id;
-              });
+              $scope.refresh();
             });
         },
         undefined,
