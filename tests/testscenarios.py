208f19df6099ca8f483a2dffdf106a9ee5e7237c
Robyn Stuart <robyna.s@gmail.com>
2017-08-18 14:15:30 +0200

    diffs should be part of result sets

--- tests/testscenarios.py
+++ tests/testscenarios.py
@@ -393,7 +393,7 @@ if 'budget' in tests:
     
     # Run the scenarios
     P.addscens(scenlist)
-    P.runscenarios(nruns=5,tosample='force') 
+    P.runscenarios(nruns=5,tosample='force',verbose=3) 
      
     if doplot:
         from optima import pygui
