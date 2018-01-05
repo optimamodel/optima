a02cda41094b7a0aef39ec24da35de1cc3868271
cliffckerr <questionably@gmail.com>
2018-01-05 03:02:57 -0800

    works with that one line commented out

--- tests/testgeospatial.py
+++ tests/testgeospatial.py
@@ -85,8 +85,8 @@ if 'runGA' in tests:
     # Make portfolio and run
     F = Portfolio(projects=projlist, objectives=defaultobjectives())
     F.objectives['end'] = 2022 # This speeds it up by only simulating 5 years
-    F.genBOCs(maxtime=2, mc=0) # Generate BOCs
-    F.runGA(maxtime=2, mc=0) # Run GA
+    F.genBOCs(maxtime=1, mc=0, budgetratios=[1.0, 0.5, 0.1, 5.0]) # Generate BOCs
+    F.runGA(maxtime=1, mc=0) # Run GA
     F.export(filename=outputpath) # Export
     
     # Tidy
