48820cc26b1a555d5caa159c1b83d29084c878a4
mariadelmarq <mariadelmarq@users.noreply.github.com>
2018-10-24 14:38:13 +1100

    Update mpld3

--- client/source/js/config.js
+++ client/source/js/config.js
@@ -21,7 +21,7 @@ define({
     'tooltip': './vendor/tooltip/dist/tooltip',
     'rzModule': './vendor/angularjs-slider/dist/rzslider.min',
     'd3': './vendor/d3/d3',
-    'mpld3': './js/modules/charts/mpld3.v0.3.1.dev1',
+    'mpld3': './js/modules/charts/mpld3.v0.4.1.min',
     'canvas2blob': './vendor/canvas-toBlob.js/canvas-toBlob',
     'saveAs': './vendor/FileSaver/FileSaver',
     'jsPDF': './vendor/jspdf/dist/jspdf.min',
