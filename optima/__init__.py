da0bf878d71117f3e9bb3aab1b4f1a09b94003a5
cliffckerr <questionably@gmail.com>
2019-01-09 19:10:14 -0500

    fix up relative imports

--- optima/__init__.py
+++ optima/__init__.py
@@ -31,7 +31,7 @@ Now, the legal part:
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-Version: 2017may27
+Version: 2019jan09
 """
 
 
@@ -61,7 +61,7 @@ from .utils import Link, LinkException, loadbalancer, loadtext, makefilepath, ob
 from .utils import odict, percentcomplete, perturb, printarr, printdata as pd, printdr, printv, printvars, printtologfile
 from .utils import promotetoarray, promotetolist, promotetoodict, quantile, runcommand, sanitize, sanitizefilename, savetext, scaleratio, setylim
 from .utils import sigfig, SItickformatter, SIticks, slacknotification, smoothinterp, tic, toc, today, vec2obj
-import utils as _utils; del utils
+from . import utils as _utils; del utils
 
 # Optimization algorithm
 from .asd import asd
@@ -71,7 +71,7 @@ from .pchip import pchip, plotpchip
 
 # Color definitions
 from .colortools import alpinecolormap, bicolormap, gridcolors, vectocolor, shifthue
-import colortools as _colortools; del colortools
+from . import colortools as _colortools; del colortools
 
 
 #####################################################################################################################
@@ -116,20 +116,20 @@ class OptimaException(Exception):
 
 # File I/O
 from .fileio import loadobj, saveobj, loadstr, dumpstr, optimafolder, loadpartable, loadtranstable, loaddatapars # CK: may want to tidy up
-import fileio as _fileio; del fileio
+from . import fileio as _fileio; del fileio
 
 # Project settings
 from .settings import Settings, convertlimits, gettvecdt
-import settings as _settings; del settings
+from . import settings as _settings; del settings
 
 # Generate results -- import first because parameters use results
 from .results import Result, Resultset, Multiresultset, BOC, ICER, getresults
-import results as _results; del results
+from . import results as _results; del results
 
 # Define the model parameters -- import before makespreadsheet because makespreadsheet uses partable to make a pre-filled spreadsheet
 from .parameters import Par, Dist, Constant, Metapar, Timepar, Popsizepar, Yearpar, Parameterset # Parameter and Parameterset classes
 from .parameters import makepars, makesimpars, applylimits, comparepars, comparesimpars, sanitycheck
-import parameters as _parameters; del parameters
+from . import parameters as _parameters; del parameters
 
 # Create a blank spreadsheet
 try: from .makespreadsheet import makespreadsheet, makeprogramspreadsheet, default_datastart, default_dataend
@@ -143,19 +143,19 @@ from .model import model
 
 # Define the programs and cost functions
 from .programs import Program, Programset
-import programs as _programs; del programs
+from . import programs as _programs; del programs
 
 # Automatic calibration and sensitivity
 from .calibration import autofit
-import calibration as _calibration; del calibration
+from . import calibration as _calibration; del calibration
 
 # Scenario analyses
 from .scenarios import Parscen, Budgetscen, Coveragescen, Progscen, runscenarios, makescenarios, baselinescenario, setparscenvalues, defaultscenarios
-import scenarios as _scenarios; del scenarios
+from . import scenarios as _scenarios; del scenarios
 
 # Optimization and ICER analyses
 from .optimization import Optim, defaultobjectives, defaultconstraints, defaulttvsettings, optimize, multioptimize, tvoptimize, outcomecalc, icers, tvfunction
-import optimization as _optimization; del optimization
+from . import optimization as _optimization; del optimization
 
 # Plotting functions
 try:
@@ -169,24 +169,24 @@ except Exception as E: _failed.append('plotting: %s' % repr(E))
 
 # Load the code to load projects and portfolios (before defining them, oddly!)
 from .loadtools import migrate, loadproj, loadportfolio, optimaversion
-import loadtools as _loadtools; del loadtools
+from . import loadtools as _loadtools; del loadtools
 changelog = _loadtools.setmigrations('changelog')
 
 # Load batch functions (has to load projects, so has to come after migration)
 from .batchtools import batchautofit, batchBOC, reoptimizeprojects, getprojects
-import batchtools as _batchtools; del batchtools
+from . import batchtools as _batchtools; del batchtools
 
 # Import the Project class that ties everything together
 from .project import Project
-import project as _project; del project
+from . import project as _project; del project
 
 # Load the portfolio class (container of Projects), relies on batch functions, hence is here
 from .portfolio import Portfolio, makegeospreadsheet, makegeoprojects
-import portfolio as _portfolio; del portfolio
+from . import portfolio as _portfolio; del portfolio
 
 # Finally, load defaults
 from .defaults import defaultproject, defaultprogset, defaultprograms, demo
-import defaults as _defaults; del defaults
+from . import defaults as _defaults; del defaults
 
 
 #####################################################################################################################
@@ -196,12 +196,12 @@ import defaults as _defaults; del defaults
 # Load high level GUI module
 try: 
     from .gui import plotresults, pygui, plotpeople, plotpars, manualfit, showplots, loadplot, geogui
-    import gui as _gui; del gui
+    from . import gui as _gui; del gui
 except Exception as E: _failed.append('gui: %s' % repr(E))
 
 try: 
     from .webserver import browser
-    import webserver as _webserver; del webserver
+    from . import webserver as _webserver; del webserver
 except Exception as E: _failed.append('webserver: %s' % repr(E))
 
 
