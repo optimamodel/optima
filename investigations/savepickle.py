a0ab15f092c0a298c24c9d28a9535c24a802874c
cliffckerr <questionably@gmail.com>
2019-07-05 12:13:45 +0900

    loads, doesnt run yet

--- investigations/savepickle.py
+++ investigations/savepickle.py
@@ -9,8 +9,11 @@ P.created = str(P.created)
 P.modified = str(P.modified)
 P.spreadsheetdate = str(P.spreadsheetdate)
 P.data['meta']['date'] = str(P.data['meta']['date'])
+for parset in P.parsets.values():
+    parset.created = str(parset.created)
+    parset.modified = str(parset.modified)
 
-P.parsets = op.odict()
+#P.parsets = op.odict()
 P.progsets = op.odict()
 P.scens = op.odict()
 P.optims = op.odict()
