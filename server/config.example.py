b1b4c094611998d3078e8f12aff7183f8a4f242f
Vlad-Ștefan Harbuz <vlad@vladh.net>
2019-07-16 12:21:00 +0200

    Attempt to fix Redis

--- server/config.example.py
+++ server/config.example.py
@@ -7,7 +7,7 @@ import math
 SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI','postgresql+psycopg2://postgres:A3nl3D0jLFbk8cI4@localhost:5432/optima')
 SECRET_KEY = os.getenv('SECRET_KEY','F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT')
 UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER','/tmp/uploads')
-REDIS_URL = os.getenv('REDIS_URL', 'redis://127.0.0.1:6379/4') # Shortcut for setting both the celery broker and result backend cache
+REDIS_URL = os.getenv('REDIS_URL', 'redis://10.0.0.3:6379/4') # Shortcut for setting both the celery broker and result backend cache
 CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL',REDIS_URL)
 CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND',REDIS_URL)
 CELERY_ACCEPT_CONTENT = os.getenv('CELERY_ACCEPT_CONTENT','pickle,json,msgpack,yaml').split(',') # Comma separated list
