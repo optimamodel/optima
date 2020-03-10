1f8e43144358059bcbe28f11ace93c9dd1e93e8e
Robyn Stuart <robyna.s@gmail.com>
2020-03-10 15:39:52 +1100

    missed a couple\

--- optima/scenarios.py
+++ optima/scenarios.py
@@ -181,7 +181,7 @@ def makescenarios(project=None, scenlist=None, verbose=2, ccsample=False, randse
                         this_y = promotetoarray(scenpar['startval']) # Use supplied starting value if there is one
                     else:
                         if int(thispar.fromdata): # If it's a regular parameter made from data, we get the default start value from the data
-                            this_y = thispar.interp(tvec=scenpar['startyear'], sample='no')[popind] # Find what the model would get for this value
+                            this_y = thispar.interp(tvec=scenpar['startyear'], usemeta=False)[popind] # Find what the model would get for this value
                         else:
                             this_y = inf # Another special value, indicating this should be filled in to the maximum                 
                             fixproppar = thisparset.pars['fix'+scenpar['name']] # Pull out e.g. fixpropdx
@@ -303,7 +303,7 @@ def setparscenvalues(parset=None, parname=None, forwhom=None, startyear=None, ve
     ## Generate dictionary
     if parset.pars[parname].fromdata: # If it's a regular parameter made from data, we get the default start value from the data
         if startyear is None: startyear = parset.pars[parname].t[forwhom][-1]
-        startval = parset.pars[parname].interp(startyear,asarray=False, sample='no')[forwhom][0]
+        startval = parset.pars[parname].interp(startyear,asarray=False, usemeta=False)[forwhom][0]
     else: # Otherwise, give up -- we can't predict a proportion until the model is run
         if startyear is None: startyear = parset.projectref().settings.now
         startval = None
