f4c876426f863fa3fab51fa68d11f92db0919f96
Kelvin Burke <kelv.burke@gmail.com>
2022-09-16 08:58:30 +1000

    Error message when results.parsetuid does not exist

--- server/webapp/dataio.py
+++ server/webapp/dataio.py
@@ -1116,6 +1116,9 @@ def load_result_mpld3_graphs(result_id=None, which=None, zoom=None, startYear=No
     if needtorerun:
         print(">> load_result_mpld3_graphs needtorerun with advancedtracking")
 
+        if not hasattr(result,parsetuid) or result.parsetuid is None:
+            raise op.OptimaException(f"The current results need to be refreshed for these graphs (results does not have parsetuid). Please click Save & run.")
+
         out = load_parset_graphs(result.projectinfo['uid'], result.parsetuid, 'calibration', which=which, parameters=None,
                                  advanced_pars=None, zoom=zoom,startYear=startYear, endYear=endYear,
                                  includeadvancedtracking=includeadvancedtracking, forcerunadvancedtracking=True)
