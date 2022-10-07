186b9489e59977370e9f40e6906c33eb09cb3cbe
Kelvin Burke <kelv.burke@gmail.com>
2022-10-07 12:03:28 +1100

    Update benchmarkmodel to use timeit.timeit which is more consistent

--- tests/benchmarkmodel.py
+++ tests/benchmarkmodel.py
@@ -17,7 +17,10 @@ Version: 2018apr24
 """
 
 dobenchmark = True
-doprofile = True
+doprofile = False
+
+n_benchmark = 10  # Number of times to run the cpu benchmark
+n_runsim = 1     # Number of times to run the model
 
 # If running profiling, choose which function to line profile. 
 functiontoprofile = 'model' # Choices are: model, runsim, makesimpars, interp
@@ -29,34 +32,36 @@ tobenchmark = 'runsim' # Choices are 'runsim' or 'runbudget'
 ############################################################################################################################
 if dobenchmark:
     print('Benchmarking...')
-    
+
     from optima import demo, gitinfo, today, getdate, loadtext, savetext
-    from time import time
-    
+    import timeit
+
     # Settings
     hashlen = 7
     filename = 'benchmark.txt'
     dosave = True
-    
+
     # Run a benchmarking test
     def cpubenchmark():
-        starttime = time()
-        tmp = [0+tmp for tmp in range(int(1e6))]
-        endtime = time()
-        performance = 1.0/(endtime-starttime)
+        elapsed = timeit.timeit(lambda: [0+tmp for tmp in range(int(1e6))], number=n_benchmark)
+        elapsed = elapsed / n_benchmark
+        performance = 1.0/(elapsed)
         return performance
-    
+
     # Prelminaries
     P = demo(doplot=False, dorun=False)
     performance1 = cpubenchmark()
-    t = time()
 
-    # Run the model 
-    if   tobenchmark == 'runsim':    P.runsim()
-    elif tobenchmark == 'runbudget': P.runbudget()
+    elapsed = 0
+    # Run the model
+    if   tobenchmark == 'runsim':
+        elapsed = timeit.timeit(lambda: P.runsim(), number=n_runsim)
+    elif tobenchmark == 'runbudget':
+        elapsed = timeit.timeit(lambda: P.runbudget(), number=n_runsim)
     else: raise Exception('tobenchmark "%s" not recognized' % tobenchmark)
 
-    elapsed = time()-t
+    elapsed = elapsed / n_runsim  # Average of N runs
+
     performance2 = cpubenchmark()
     performance = sum([performance1, performance2])/2. # Find average of before and after
     benchmarktxt = "for %s (benchmark:%0.2fm iterations/second)" % (tobenchmark, performance)
@@ -74,17 +79,17 @@ if dobenchmark:
         output = loadtext(filename, splitlines=True)
         output.append(thisout)
         savetext(filename, output)
-    
-    print('Done benchmarking: model runtime was %s s.' % elapsedstr)
+
+    print(f'Done benchmarking: average model runtime was {elapsedstr} s. (over {n_runsim} run(s))')
 
 
 
 ############################################################################################################################
 ## Profiling
 ############################################################################################################################
-try: 
+try:
     import line_profiler # analysis:ignore
-except: 
+except:
     print('WARNING: Line profiler "line_profiler" not available, not running line profiling')
     doprofile = False # Don't profile if it can't be loaded
 
@@ -125,4 +130,4 @@ if doprofile:
     profile()
 
 if 'elapsedstr' in locals():
-    print('And to summarize, model runtime was %s s.' % elapsedstr)
+    print(f'And to summarize,  average model runtime was {elapsedstr} s. (over {n_runsim} run(s))')
