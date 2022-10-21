cc1d38eebcc748a57b951ed74776711311498cb9
Kelvin Burke <kelv.burke@gmail.com>
2022-10-21 15:56:45 +1100

    Plot stacked and population+stacked plots for Multiresultset 4

--- optima/plotting.py
+++ optima/plotting.py
@@ -200,12 +200,17 @@ def getadvancedtrackingplotselections(results=None,advanced=False):
 
 
 def checkifneedtorerunwithadvancedtracking(results=None, which=None):
-    print(f' >> checkifneedtorerunwithadvancedtracking {type(results)=} {results.advancedtracking=}, {which=}')
+    print(f' >> checkifneedtorerunwithadvancedtracking {type(results)=} {type(results.advancedtracking)=} {results.advancedtracking=}, {which=}')
     if results is not None:
         if not hasattr(results, 'parsetuid'):  # need it later, so results need refreshing
             return True
         if not hasattr(results, 'advancedtracking'): #might not really need it but the results need refreshing
             return True
+        elif isinstance(results.advancedtracking, odict):
+            alladvancedtracking = True
+            for advancedtracking in results.advancedtracking.values():
+                if not advancedtracking: alladvancedtracking = False
+            return alladvancedtracking
         elif results.advancedtracking:
             return False
 
