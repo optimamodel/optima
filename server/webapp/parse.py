0345a943884698a06044c0c132080a24a4762d2c
Kelvin Burke <kelv.burke@gmail.com>
2023-01-20 18:08:16 +0800

    try to fix scenarios page crashing after long load 7

--- server/webapp/parse.py
+++ server/webapp/parse.py
@@ -590,7 +590,7 @@ def get_coverages_for_scenarios(project, year=None):
                 prog_list = coverage.keys()
                 coverage = np.column_stack(coverage.values())
                 for yrno,year in enumerate(years):
-                    result[parset_id][progset_id][year] = op.odict(zip(prog_list,coverage[yrno,:]))
+                    result[parset_id][progset_id][year] = normalize_obj(op.odict(zip(prog_list,coverage[yrno,:])))
                 print('~~~',result[parset_id][progset_id])
 
             except:
