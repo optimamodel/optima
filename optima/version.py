1ca36086546141b4f91be25a3b657dfc1097f33b
Kelvin Burke <kelv.burke@gmail.com>
2023-10-24 15:02:32 +1100

    Test un-bump revision

--- optima/version.py
+++ optima/version.py
@@ -1,4 +1,4 @@
 supported_versions = ['2.11.4','2.12.0']
-revision = '1'
+revision = '0'
 revisiondate = "2023-02-07"
 versions_different_databook = ['2.10.13']  # Versions where we start using a different databook format
