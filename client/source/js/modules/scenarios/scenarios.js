6c78ae73074100bbce0ff80e47d5a2c764e8e500
Kelvin Burke <kelv.burke@gmail.com>
2023-10-25 13:30:45 +1100

    Test quicker scenarios 6

--- client/source/js/modules/scenarios/scenarios.js
+++ client/source/js/modules/scenarios/scenarios.js
@@ -51,7 +51,7 @@ define([
           $scope.years = _.range($scope.project.startYear, $scope.project.endYear+21);
           $scope.isMissingData = !$scope.project.calibrationOK;
 
-          return new Promise((resolve, reject) => {resolve('bob');})
+//          return new Promise((resolve, reject) => {resolve('bob');})
         });
 
 //        promise.then(function(emptyresponse) {
