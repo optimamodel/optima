21ec68144dfff7eb9ed6288913baccdc9446f50c
Kelvin Burke <kelv.burke@gmail.com>
2023-10-25 16:26:26 +1100

    Proper debug info to FE exception

--- server/webapp/dataio.py
+++ server/webapp/dataio.py
@@ -316,7 +316,7 @@ def report_exception_decorator(api_call):
             return api_call(*args, **kwargs)
         except Exception as e:
             exception = traceback.format_exc()
-            exception += f'\nOptima HIV debug info:\n{op.debuginfo()}'
+            if not isinstance(e, op.OptimaException): exception = exception+op.debuginfo(dooutput=True)
             # limiting the exception information to 10000 characters maximum
             # (to prevent monstrous sqlalchemy outputs)
             current_app.logger.error("Exception during request %s: %.10000s" % (request, exception))
