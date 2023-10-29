8958b0d0a3f9f6a4dd6010741bf14f023ce12f6c
Kelvin Burke <kelv.burke@gmail.com>
2023-10-29 13:34:58 +1100

    Parse error when downloading 15

--- client/source/js/app.js
+++ client/source/js/app.js
@@ -120,7 +120,7 @@ define([
               }
 
               getRejectionMessagePromise(rejection)
-              .then(function(response) {
+              .then(function(errorText) {
                 message = 'We are very sorry, but it seems an error has occurred. Please contact us (info@optimamodel.com). In your email, copy and paste the error message below, and please also provide the date and time, your user name, the project you were working on (if applicable), and as much detail as possible about the steps leading up to the error. We apologize for the inconvenience.';
                 var modalService = $injector.get('modalService');
                 modalService.inform(angular.noop, 'Okay', message, 'Server Error', errorText);
