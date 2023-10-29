597ca592dbd9d28bd226b5334011e6974ae5b388
Kelvin Burke <kelv.burke@gmail.com>
2023-10-29 13:00:56 +1100

    Parse error when downloading 7

--- client/source/js/app.js
+++ client/source/js/app.js
@@ -99,13 +99,23 @@ define([
               var message, errorText;
               console.log('catching error', rejection);
 
-              isJsonBlob = function(data) {
+              isJsonBlob = function(data) { // Used for the rpcDownload which has responseType: 'blob'
                 return data instanceof Blob && data.type === "application/json";
               }
-              console.log('catching error2', isJsonBlob(rejection.data));
 
               if (rejection.data && (rejection.data.message || rejection.data.exception || rejection.data.reason)) {
                 errorText = rejection.data.message || rejection.data.exception || rejection.data.reason;
+              } else if (isJsonBlob(rejection.data)) {
+                reader = new FileReader();
+                reader.addEventListener('loadend', (e) => {
+                  console.log(JSON.parse(e.srcElement['result']));
+                });
+                rejection.data = reader.readAsText(rejection.data);
+//                const responseData = isJsonBlob(response.data) ? (response.data).text() : response.data || {};
+//                const responseJson = (typeof responseData === "string") ? JSON.parse(responseData) : responseData;
+//                response.data = responseJson
+                errorText = rejection.data.message || rejection.data.exception || rejection.data.reason;
+//                errorText = 'Unknown error, when downloading.\n' + JSON.stringify(rejection, null, 2);
               } else {
                 errorText = 'Unknown error, check Internet connection and try again.\n' + JSON.stringify(rejection, null, 2);
               }
