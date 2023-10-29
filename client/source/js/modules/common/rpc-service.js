689a11d9dbd32105b54d1c0d1f8d0845e087f097
Kelvin Burke <kelv.burke@gmail.com>
2023-10-29 12:20:18 +1100

    Parse error when downloading

--- client/source/js/modules/common/rpc-service.js
+++ client/source/js/modules/common/rpc-service.js
@@ -97,6 +97,11 @@ define(['angular' ], function (angular) {
               deferred.resolve(response);
             },
             function(response) {
+              // Convert error blob to json using https://github.com/axios/axios/issues/3779
+              const isJsonBlob = (data) => data instanceof Blob && data.type === "application/json";
+              const responseData = isJsonBlob(response?.data) ? await (response?.data)?.text() : response?.data || {};
+              const responseJson = (typeof responseData === "string") ? JSON.parse(responseData) : responseData;
+              response.data = responseJson
               deferred.reject(response);
             });
         return deferred.promise;
