3b3c3c02359815f634d8967c9228967801d2fe63
Kelvin Burke <kelv.burke@gmail.com>
2024-01-16 13:25:22 +1100

    Test adding sc.sc_fileio._unpickleMethod 2

--- optima/__init__.py
+++ optima/__init__.py
@@ -76,8 +76,9 @@ del sc_version; del sc_versiondate
 
 # Some pickled projects expect there to be a function sc.sc_fileio._unpickleMethod
 def blank_function(a,b,c): pass # raise Exception('sc.sc_fileio._unpickleMethod is gone, don\'t try to use it')
-sc.sc_fileio._unpickleMethod = blank_function
-del blank_function
+from sciris import sc_fileio
+sc_fileio._unpickleMethod = blank_function
+del blank_function, sc_fileio
 
 # Color definitions
 from .colortools import *
