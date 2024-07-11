4b481fbb5d4559926153eeea41acad7b125fdaca
Kelvin Burke <kelv.burke@gmail.com>
2024-07-11 16:15:51 +1000

    Add 2.12.2 version and black migration

--- optima/version.py
+++ optima/version.py
@@ -1,4 +1,4 @@
-supported_versions = ['2.11.4','2.12.0','2.12.1']
+supported_versions = ['2.11.4','2.12.0','2.12.1','2.12.2']
 revision = '5'
 revisiondate = '2024-05-27'
 versions_different_databook = ['2.10.13']  # Versions where we start using a different databook format
