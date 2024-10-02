cfb07a387afd14f47cc9457f8a0e4c8e565d3eb6
Kelvin Burke <kelv.burke@gmail.com>
2024-10-02 17:46:43 +1000

    Update setup.py to correctly extract the latest version from version.py

--- optima/version.py
+++ optima/version.py
@@ -1,4 +1,4 @@
-supported_versions = ['2.11.4','2.12.0','2.12.1','2.12.2']
+supported_versions = ['2.11.4','2.12.0','2.12.1','2.12.2']  # (note: ' not " because setup.py)
 revision = '8'
 revisiondate = '2024-09-27'
 versions_different_databook = ['2.10.13']  # Versions where we start using a different databook format
